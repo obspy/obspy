dc57d0573cd09502bbddb106594b149528112db1
Moritz Beyreuther <random.seed@web.de>
2008-05-18 13:40:41 +0000

    modified leak script

--- obspy/trunk/leak.py
+++ obspy/trunk/leak.py
@@ -6,15 +6,19 @@ from numpy import *
 
 rand = array(1000*random.random(1e7),dtype='l')
 
-obspy.writegse({}, rand, "leak.gse")
+file = "leak.gse"
+
+obspy.writegse({}, rand, file)
 print rand[0:10]
 print rand[-11:-1]
 
 del rand
 
 while True:
-	(header,data) = obspy.readgse("leak.gse")
+	(header,data) = obspy.readgse(file)
 	print len(data)
 	print data[0:10]
 	print data[-11:-1]
 	del header, data
+
+os.remove(file)
