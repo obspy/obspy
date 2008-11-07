1c84a0cf069e08d8da2865bc5741e9c17ad7c828
Tobias Megies <megies@geophysik.uni-muenchen.de>
2008-11-07 11:29:44 +0000

    Für die anderen Filter auch Zero-Phase-Shift-Versionen hinzugefügt.

--- obspy/trunk/obspy.py
+++ obspy/trunk/obspy.py
@@ -26,6 +26,16 @@ def bandpass(data,freqmin,freqmax,df=200,corners=4):
   [b,a]=iirfilter(corners, [freqmin/fe, freqmax/fe], btype='band',ftype='butter',output='ba')
   return lfilter(b, a, data)
 
+def bandpassZPHSH(data,freqmin,freqmax,df=200,corners=2):
+  """Zero-Phase-Shift Butterworth-Bandpass: filter data from freqmin to freqmax
+  using corners corners and doing 2 runs over the data, one from left to right
+  and one from right to left.
+  Note that the default corners is 2, because of the two runs involved.
+  """
+  x=bandpass(data, freqmin, freqmax, df, corners)
+  x=bandpass(x[::-1], freqmin, freqmax, df, corners)
+  return x[::-1]
+
 def bandstop(data,freqmin,freqmax,df=200,corners=4):
   """Butterworth-Bandstop: filter data removing data between frequencies
   freqmin and freqmax using corners corners
@@ -33,6 +43,16 @@ def bandstop(data,freqmin,freqmax,df=200,corners=4):
   fe=.5*df
   [b,a]=iirfilter(corners, [freqmin/fe, freqmax/fe], btype='bandstop',ftype='butter',output='ba')
   return lfilter(b, a, data)
+
+def bandstopZPHSH(data,freqmin,freqmax,df=200,corners=2):
+  """Zero-Phase-Shift Butterworth-Bandstop: filter data removing data between
+  frequencies freqmin and freqmax using corners corners and doing 2 runs over
+  the data, one from left to right and one from right to left.
+  Note that the default corners is 2, because of the two runs involved.
+  """
+  x=bandstop(data, freqmin, freqmax, df, corners)
+  x=bandstop(x[::-1], freqmin, freqmax, df, corners)
+  return x[::-1]
 
 def lowpass(data,freq,df=200,corners=4):
   """Butterworth-Lowpass: filter data removing data over certain frequency freq
@@ -42,10 +62,11 @@ def lowpass(data,freq,df=200,corners=4):
   [b,a]=iirfilter(corners, freq/fe, btype='lowpass',ftype='butter',output='ba')
   return lfilter(b, a, data)
 
-def lowpassZPHSH(data,freq,df=200,corners=4):
-  """Zero-Phase_shift Butterworth-Lowpass: filter data removing data over
+def lowpassZPHSH(data,freq,df=200,corners=2):
+  """Zero-Phase-Shift Butterworth-Lowpass: filter data removing data over
   certain frequency freq using corners corners and doing 2 runs over the data,
   one from left to right and one from right to left.
+  Note that the default corners is 2, because of the two runs involved.
   """
   x=lowpass(data, freq, df, corners)
   x=lowpass(x[::-1], freq, df, corners)
@@ -58,6 +79,16 @@ def highpass(data,freq,df=200,corners=4):
   fe=.5*df
   [b,a]=iirfilter(corners, freq/fe, btype='highpass',ftype='butter',output='ba')
   return lfilter(b, a, data)
+
+def highpassZPHSH(data,freq,df=200,corners=2):
+  """Zero-Phase-Shift Butterworth-Highpass: filter data removing data below
+  certain frequency freq using corners corners and doing 2 runs over the data,
+  one from left to right and one from right to left.
+  Note that the default corners is 2, because of the two runs involved.
+  """
+  x=highpass(data, freq, df, corners)
+  x=highpass(x[::-1], freq, df, corners)
+  return x[::-1]
 
 def readgse(gsefile):
   """NOTE: documentation is assigned AFTER definition by:
