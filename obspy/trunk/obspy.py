1bfbee6e5599271262ef39e1dc2a6afb8a0207a1
Moritz Beyreuther <random.seed@web.de>
2008-11-20 15:55:26 +0000

    Cleaned up a bit and added new version of GSE driver of Stefan Stange

--- obspy/trunk/obspy.py
+++ obspy/trunk/obspy.py
@@ -1,12 +1,26 @@
-# obspy module
 #---------------------------------------------------
 # Filename: obspy.py
 #  Purpose: Python module for gse conversion...
 #  Version: n.a.
-#   Author: Moritz Beyreuther
+#   Author: Moritz Beyreuther, Tobias Megies
 #    Email: moritz.beyreuther@geophysik.uni-muenchen.de
 # Revision: 2008/03/23 mob
-#---------------------------------------------------
+#           2008/11/07 tobi
+#
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#---------------------------------------------------------------------
 
 import os,sys
 from numpy import zeros,mean,concatenate,std,pi
