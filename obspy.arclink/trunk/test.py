2129be06686555f8d7e72fabf31dc34a1b61dbbb
Robert Barsch <barsch@egu.eu>
2009-06-11 15:01:11 +0000

    - obspy.arclink: moved command line options into factory

--- obspy.arclink/trunk/test.py
+++ obspy.arclink/trunk/test.py
@@ -18,54 +18,46 @@ PURGE = 8
 BYE = 9
 
 
-class ArcLinkClient(basic.LineReceiver):
+class ArcLinkClientProtocol(basic.LineReceiver):
     """
     """
-    def __init__(self, user="Anonymous", institute="Anonymous",
-                 request_type="WAVEFORM format=MSEED",
-                 request_data=["2008,1,1,0,0,0 2008,1,1,1,0,0 BW ROTZ EHE .", ],
-                 outfile='temp.mseed', debug=True):
-        self.user = user or 'Anonymous'
-        self.institute = institute or 'Anonymous'
-        self.request_type = request_type
-        self.request_data = request_data
-        self.outfile = outfile
-        self.debug = debug
-
     def sendLine(self, line):
-        # Log sendLine only if you are in debug mode for performance
-        if self.debug:
+        """
+        """
+        if self.factory.debug:
             print '>>> ' + line
         basic.LineReceiver.sendLine(self, line)
 
     def connectionMade(self):
-        self.content = open(self.outfile, 'wb')
+        """
+        """
+        self.content = open(self.factory.outfile, 'wb')
         self.state = HELLO
         self.sendLine('HELLO')
 
     def lineReceived(self, line):
-        if self.debug:
+        if self.factory.debug:
             print '... ' + line
         if self.state == HELLO:
             self.state = HELLO2
             return
         elif self.state == HELLO2:
             self.state = USER
-            self.sendLine('USER ' + self.user)
+            self.sendLine('USER ' + self.factory.user)
             return
         elif self.state == USER:
             if line != 'OK':
                 raise Exception('USER: expected OK, got %s' % line)
             self.state = INSTITUTION
-            self.sendLine('INSTITUTION ' + self.user)
+            self.sendLine('INSTITUTION ' + self.factory.institution)
             return
         elif self.state == INSTITUTION:
             if line != 'OK':
                 raise Exception('INSTITUTION: expected OK, got %s' % line)
             self.state = REQUEST
-            self.sendLine("REQUEST " + self.request_type)
-            for r in self.request_data:
-                self.sendLine(r)
+            self.sendLine("REQUEST " + self.factory.request_type)
+            for data in self.factory.request_data:
+                self.sendLine(data)
             self.sendLine("END")
             return
         elif self.state == REQUEST:
@@ -96,7 +88,7 @@ class ArcLinkClient(basic.LineReceiver):
             return
         elif self.state == PURGE:
             if line != 'OK':
-                raise Exception('USER: expected OK got %s' % line)
+                raise Exception('PURGE: expected OK got %s' % line)
         self.sendLine("BYE")
         #self.content.close()
         self.transport.loseConnection()
@@ -105,12 +97,12 @@ class ArcLinkClient(basic.LineReceiver):
         pos = self.content.tell()
         if pos + len(data) < self.size:
             self.content.write(data)
-            if self.debug:
+            if self.factory.debug:
                 print '... (data chunk)', pos, 'of', self.size
             return
         self.content.write(data[0:-5])
         self.setLineMode()
-        if self.debug:
+        if self.factory.debug:
             print '... (data chunk)', self.content.tell(), 'of', self.size
             print '>>> END', '(%.3lf seconds)' % (time.clock() - self.clock)
         self.state = PURGE
@@ -120,7 +112,18 @@ class ArcLinkClient(basic.LineReceiver):
 
 
 class ArcLinkClientFactory(protocol.ClientFactory):
-    protocol = ArcLinkClient
+    protocol = ArcLinkClientProtocol
+
+    def __init__(self, user="Anonymous", institution="Anonymous",
+                 request_type="WAVEFORM format=MSEED",
+                 request_data=["2008,1,1,0,0,0 2008,1,1,1,0,0 BW ROTZ EHE .", ],
+                 outfile='temp.mseed', debug=False):
+        self.user = user
+        self.institution = institution
+        self.request_type = request_type
+        self.request_data = request_data
+        self.outfile = outfile
+        self.debug = debug
 
     def startedConnecting(self, connector):
         print "CONNECTED"
@@ -135,6 +138,8 @@ class ArcLinkClientFactory(protocol.ClientFactory):
 
 
 if __name__ == '__main__':
-    factory = ArcLinkClientFactory()
+    factory = ArcLinkClientFactory(request_type="WAVEFORM format=MSEED",
+        request_data=["2008,1,1,0,0,0 2008,1,1,1,0,0 BW ROTZ EHE .", ],
+        outfile='output.mseed', debug=True)
     reactor.connectTCP('erde.geophysik.uni-muenchen.de', 18001, factory)
     reactor.run()
