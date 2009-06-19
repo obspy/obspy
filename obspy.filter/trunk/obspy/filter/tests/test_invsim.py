e82aa0360d7cc24f0894ef4be0d2b5d773db110b
Moritz Beyreuther <random.seed@web.de>
2009-06-19 10:01:37 +0000

    - Add test for seismometer simulation of STS-2 seismometer

--- obspy.filter/trunk/obspy/filter/tests/test_invsim.py
+++ obspy.filter/trunk/obspy/filter/tests/test_invsim.py
@@ -46,7 +46,8 @@ class InvSimTestCase(unittest.TestCase):
     
     def test_seisSimVsPitsa1(self):
         """
-        Test seisSim seismometer simulation against seismometer simulation of pitsa
+        Test seisSim seismometer simulation against seismometer simulation
+        of pitsa --- LE3D seismometer.
         """
         # load test file
         file = os.path.join(self.path, 'rjob_20051006.gz')
@@ -86,36 +87,57 @@ class InvSimTestCase(unittest.TestCase):
         ##pl.title('original data')
         ##pl.savefig("instrument.ps")
 
-    ####def test_seisSimVsPitsa2(self):
-    ####    from obspy.mseed import test as tests_
-    ####    path = os.path.dirname(inspect.getsourcefile(tests_))
-    ####    file = os.path.join(path, 'data', 'BW.BGLD..EHE.D.2008.001')
-    ####    g = Trace()
-    ####    g.read(file,format='MSEED')
-    ####    # paz of test file
-    ####    samp_rate = 200.0
-    ####    le3dE = {
+    def test_seisSimVsPitsa2(self):
+        """
+        Test seisSim seismometer simulation against seismometer simulation of pitsa
+        --- STS-2 seismometer.
+        """
+        # load test file
+        file = os.path.join(self.path, 'rotz_20081028.gz')
+        f = gzip.open(file)
+        data = load(f)
+        f.close()
 
-    ####        'poles' : [-4.44221 +4.44355j, -4.44221 -4.44355j],
-    ####        'zeroes' : [0.0 +0.0j, 0.0 +0.0j, 0.0 +0.0j],
-    ####        'gain' : 0.4
-    ####    }
-    ####    
-    ####    ##import pylab as pl
-    ####    ##pl.figure()
-    ####    ##ii = 1
-    ####    for instrument in ['None','wood_anderson', 'wwssn_sp', 'wwssn_lp', 'kirnos']:
-    ####        # simulate instrument
-    ####        exec "datcorr = seisSim(data,samp_rate,le3dE,inst_sim=%s,water_level=600.0)" % instrument
-    ####        # load pitsa file
-    ####        #file = os.path.join(self.path, 'rjob_20051006_%s.gz'%instrument)
-    ####        #f = gzip.open(file)
-    ####        #data_pitsa = load(f)
-    ####        #f.close()
-    ####        # calculate normalized rms
-    ####        #rms = N.sqrt(N.sum((datcorr-data_pitsa)**2)/N.sum(data_pitsa**2))
-    ####        #print "RMS misfit %15s:"%instrument,rms
-    ####        #self.assertEqual(rms < 1.e-5, True)
+        # paz of test file
+        samp_rate = 200.0
+        sts2 = {
+            'poles' :  [-0.03736 -0.03617j, -0.03736 +0.03617j],
+            'zeroes' : [0.0 +0.0j, 0.0 +0.0j, 0.0 +0.0j],
+            'gain' : 1.5
+        }
+        
+        ##import pylab as pl
+        ##pl.figure()
+        ##ii = 1
+        for instrument in ['None','wood_anderson', 'wwssn_sp', 'wwssn_lp', 'kirnos']:
+            # simulate instrument
+            exec "datcorr = seisSim(data,samp_rate,sts2,inst_sim=%s,water_level=600.0)" % instrument
+            # load pitsa file
+            file = os.path.join(self.path, 'rotz_20081028_%s.gz'%instrument)
+            f = gzip.open(file)
+            data_pitsa = load(f)
+            f.close()
+            # calculate normalized rms
+            rms = N.sqrt(N.sum((datcorr-data_pitsa)**2)/N.sum(data_pitsa**2))
+            print "RMS misfit %15s:"%instrument,rms
+            self.assertEqual(rms < 1.e-4, True)
+            ##pl.subplot(3,2,ii)
+            ##pl.plot(datcorr,'r',data_pitsa,'b--')
+            ##pl.title(instrument)
+            ##ii += 1
+        ##pl.subplot(3,2,6)
+        ##pl.plot(data,'k')
+        ##pl.title('original data')
+        ##pl.savefig("instrument.ps")
+
+    #def test_seisSimVsPitsa2(self):
+    #    from obspy.mseed import test as tests_
+    #    path = os.path.dirname(inspect.getsourcefile(tests_))
+    #    file = os.path.join(path, 'data', 'BW.BGLD..EHE.D.2008.001')
+    #    g = Trace()
+    #    g.read(file,format='MSEED')
+    #    # paz of test file
+    #    samp_rate = 200.0
 
 def suite():
     return unittest.makeSuite(InvSimTestCase, 'test')
