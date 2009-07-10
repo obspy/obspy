e897573e1ca8b48ef1609c285de0a800ee197b1b
Tobias Megies <megies@geophysik.uni-muenchen.de>
2009-07-10 14:08:15 +0000

    - added gps2DistAzimuth() to rotate.py, this calculates distance and forward/backward azimuths for two latitude/longitude points accurately. It's useful to get interstation distances or get angle for seismogramm rotation.

--- obspy.filter/trunk/obspy/filter/rotate.py
+++ obspy.filter/trunk/obspy/filter/rotate.py
@@ -26,6 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
 
 from numpy import array, sin, cos, pi
+from math import sqrt,pi,asin,tan,atan,atan2
 
 def rotate_NE_RT(n,e,ba):
   """Rotates horizontal components of a seismogram:
@@ -47,3 +48,115 @@ def rotate_NE_RT(n,e,ba):
   r=e*sin((ba+180)*2*pi/360)+n*cos((ba+180)*2*pi/360)
   t=e*cos((ba+180)*2*pi/360)-n*sin((ba+180)*2*pi/360)
   return r,t
+
+
+def gps2DistAzimuth(lat1,lon1,lat2,lon2) :
+    """
+    Computes the distance between two geographic points on the WGS84
+    ellipsoid and the forward and backward azimuths between these points.
+    
+    This code is based on an implementation incorporated in
+    Matplotlib Basemap Toolkit 0.9.5
+    http://sourceforge.net/projects/matplotlib/files/
+    (basemap-0.9.5/lib/matplotlib/toolkits/basemap/greatcircle.py)
+
+    Algorithm from Geocentric Datum of Australia Technical Manual.
+    http://www.icsm.gov.au/gda/gdatm/index.html
+    http://www.icsm.gov.au/gda/gdatm/gdav2.3.pdf
+
+    It states:
+                Computations on the Ellipsoid
+
+            There are a number of formulae that are available
+            to calculate accurate geodetic positions,
+            azimuths and distances on the ellipsoid.
+
+            Vincenty's formulae (Vincenty, 1975) may be used
+            for lines ranging from a few cm to nearly 20,000 km,
+            with millimetre accuracy.
+            The formulae have been extensively tested
+            for the Australian region, by comparison with results
+            from other formulae (Rainsford, 1955 & Sodano, 1965).
+
+            * Inverse problem: azimuth and distance from known
+                                latitudes and longitudes
+            * Direct problem: Latitude and longitude from known
+                                position, azimuth and distance.
+
+            Vincenty's Inverse formulae
+            Given: latitude and longitude of two points
+                                (lat1, lon1 and lat2, lon2)
+            Calculate: the ellipsoidal distance (dist) and
+            forward and reverse azimuths between the points (alpha12, alpha21).
+
+    @param lat1: Latitude of point A in degrees
+    @param lon1: Longitude of point A in degrees
+    @param lat2: Latitude of point B in degrees
+    @param lon2: Longitude of point B in degrees
+    @return: (Great circle distance in m, azimuth A->B in degrees, azimuth B->A in degrees)
+    """
+    #Data on the WGS84 reference ellipsoid:
+    a = 6378137.0       #semimajor axis in m
+    f = 1/298.257223563 #flattening
+    b = a*(1-f)         #semiminor axis
+
+    if (abs(lat1-lat2)<1e-8) and (abs(lon1-lon2)<1e-8):
+        return 0.0,0.0,0.0
+
+    #convert latitudes and longitudes to radians:
+    lat1=lat1*2.0*pi/360.
+    lon1=lon1*2.0*pi/360.
+    lat2=lat2*2.0*pi/360.
+    lon2=lon2*2.0*pi/360.
+
+    TanU1=(1-f)*tan(lat1)
+    TanU2=(1-f)*tan(lat2)
+
+    U1=atan(TanU1)
+    U2=atan(TanU2)
+
+    dlon=lon2-lon1
+    last_dlon=-4000000.0                # an impossibe value
+    omega=dlon
+
+    # Iterate the following equations, until there is no significant change in dlon
+    while ( last_dlon < -3000000.0 or dlon != 0 and abs( (last_dlon - dlon)/dlon) > 1.0e-9 ) :
+
+        sqr_sin_sigma = pow( cos(U2) * sin(dlon), 2) + pow( (cos(U1) * sin(U2) - sin(U1) *  cos(U2) * cos(dlon) ), 2 )
+        Sin_sigma = sqrt( sqr_sin_sigma )
+        Cos_sigma = sin(U1) * sin(U2) + cos(U1) * cos(U2) * cos(dlon)
+        sigma = atan2( Sin_sigma, Cos_sigma )
+        Sin_alpha = cos(U1) * cos(U2) * sin(dlon) / sin(sigma)
+        alpha = asin( Sin_alpha )
+        Cos2sigma_m = cos(sigma) - (2 * sin(U1) * sin(U2) / pow(cos(alpha), 2) )
+        C = (f/16) * pow(cos(alpha), 2) * (4 + f * (4 - 3 * pow(cos(alpha), 2)))
+        last_dlon = dlon
+        dlon = omega + (1-C) * f * sin(alpha) * (sigma + C * sin(sigma) * (Cos2sigma_m + C * cos(sigma) * (-1 + 2 * pow(Cos2sigma_m, 2) )))
+
+        u2 = pow(cos(alpha),2) * (a*a-b*b) / (b*b)
+        A = 1 + (u2/16384) * (4096 + u2 * (-768 + u2 * (320 - 175 * u2)))
+        B = (u2/1024) * (256 + u2 * (-128+ u2 * (74 - 47 * u2)))
+        delta_sigma = B * Sin_sigma * (Cos2sigma_m + (B/4) * (Cos_sigma * (-1 + 2 * pow(Cos2sigma_m, 2) ) - (B/6) * Cos2sigma_m * (-3 + 4 * sqr_sin_sigma) * (-3 + 4 * pow(Cos2sigma_m,2 ) )))
+        
+        dist = b * A * (sigma - delta_sigma)
+        alpha12 = atan2( (cos(U2) * sin(dlon)), (cos(U1) * sin(U2) - sin(U1) * cos(U2) * cos(dlon)))
+        alpha21 = atan2( (cos(U1) * sin(dlon)), (-sin(U1) * cos(U2) + cos(U1) * sin(U2) * cos(dlon)))
+
+
+    if ( alpha12 < 0.0 ) :
+        alpha12 =  alpha12 + (2.0*pi)
+    if ( alpha12 > (2.0*pi) ) :
+        alpha12 = alpha12 - (2.0*pi)
+
+    alpha21 = alpha21 + pi
+
+    if ( alpha21 < 0.0 ) :
+        alpha21 = alpha21 + (2.0*pi)
+    if ( alpha21 > (2.0*pi) ) :
+        alpha21 = alpha21 - (2.0*pi)
+
+    #convert to degrees:
+    alpha12=alpha12*360/(2.0*pi)
+    alpha21=alpha21*360/(2.0*pi)
+
+    return dist,alpha12,alpha21
