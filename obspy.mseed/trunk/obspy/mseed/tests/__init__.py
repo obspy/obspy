08ba98e500c9b28f38cd9227f5f31778b4e4a528
Robert Barsch <barsch@egu.eu>
2009-08-05 18:40:30 +0000

    - removes a old import

--- obspy.mseed/trunk/obspy/mseed/tests/__init__.py
+++ obspy.mseed/trunk/obspy/mseed/tests/__init__.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import obspy, unittest
-from obspy.mseed.tests import test_libmseed, test_core, test_plotting
+from obspy.mseed.tests import test_libmseed, test_core
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(test_core.suite())
     suite.addTest(test_libmseed.suite())
-    #suite.addTest(test_plotting.suite())
     return suite
 
 
