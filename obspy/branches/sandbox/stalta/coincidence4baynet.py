7b7e45b8a8c264883f9a36eee46d5963e12bcda9
Moritz Beyreuther <random.seed@web.de>
2009-11-27 07:33:16 +0000

    Minor change in coincidence

--- obspy/branches/sandbox/stalta/coincidence4baynet.py
+++ obspy/branches/sandbox/stalta/coincidence4baynet.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python
 """
-Build Coincidence for Baynet
+USAGE: ./coincidence4baynet.py file1.pick file2.pick
+
+Build Coincidence for Baynet. file1.pick, file2.pick ...
+are the output of ./stalta4baynet.py and look for station RMOA like:
+
+2009-05-01T00:11:13.499999Z,2009-05-01T00:11:43.499999Z,RMOA
+2009-05-01T15:33:26.299999Z,2009-05-01T15:33:33.515000Z,RMOA
+...
 """
 
 import os, glob, sys
 from obspy.core import UTCDateTime
 from pprint import pprint
 
+if len(sys.argv) == 1:
+    print __doc__
+    sys.exit(1)
+
+
 trigger_list = []
 for file in sys.argv[1:]:
-    for line in open(file):
+    try:
+        f = open(file)
+    except:
+        continue
+    for line in f:
         begin, end, station = line.strip().split(",")
         trigger_list.append([UTCDateTime(begin).timestamp,
                              UTCDateTime(end).timestamp,
