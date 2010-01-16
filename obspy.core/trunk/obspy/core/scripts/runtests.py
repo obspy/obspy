bd10c84b381c8703ccf41c9ce522520c19e43fe9
Moritz Beyreuther <random.seed@web.de>
2010-01-16 05:31:30 +0000

    Refactoring runtests script.
    - reactivated main fkt for setup.py
    - allowing shortcuts for module names, e.g. obspy-runtests gse2 sac
      to run only the tests for gse2 and sac
    - using modules __doc__ for cmd line help (obspy-runtests -h)

--- obspy.core/trunk/obspy/core/scripts/runtests.py
+++ obspy.core/trunk/obspy/core/scripts/runtests.py
@@ -5,75 +5,63 @@ ObsPy Test Suite Module.
 
 All tests in ObsPy are located in the tests directory of the certain
 module. The __init__.py of the tests directory itself as well as every test
-has a function called suite.
+file located in the tests directory has a function called suite. The suite
+function is the one that needs to be called. Running the test verbose
+exposes the names of the available tests.
 
-To run all tests/a single test from the shell/cmd do one of the following::
-
+Examples:
     python runtests.py                         # Run all tests
     python runtests.py -v                      # Verbose output
     python runtests.py obspy.mseed.tests.suite # run tests of obspy.mseed
+    python runtests.py mseed                   # obspy shortcut
+    python runtests.py obspy.core.tests.test_stats.StatsTestCase.test_init
     python -c "import obspy.core; obspy.core.runTests()"            # Run all tests
     python -c "import obspy.core; obspy.core.runTests(verbosity=2)" # Verbose output
-
-To run all tests/a single test inside Python do one of the following::
-
-    import obspy.core
-    obspy.core.runTests()                      # Run all tests
-    obspy.core.runTests(verbosity=1)           # Verbose output
-    obspy.core.runTests(verbosity=2, \\        # Special Test case
-        modules=['obspy.core.tests.test_stats.StatsTestCase.test_init'])
-
-Running the test verbose exposes the names of available tests.
+    python -c "import obspy.core; obspy.core.runTests(verbosity=2,\\# Special Test case
+        tests=['obspy.core.tests.test_stats.StatsTestCase.test_init'])
 """
 
 from optparse import OptionParser
-import sys
-import time
 import unittest
-import obspy
-
 
 DEFAULT_MODULES = ['core', 'gse2', 'mseed', 'sac', 'wav', 'signal', 'imaging',
                    'xseed', 'seisan']
 
-def suite(modules=[]):
+
+def suite(tests=[]):
     """
     The obspy test suite.
     """
-    for id in DEFAULT_MODULES:
-        module = 'obspy.%s.tests' % id
-        try:
-            __import__(module)
-        except ImportError, e:
-            print "Cannot import test suite of module %s" % module
-            print e
-            time.sleep(0.5)
-    if modules == []:
-        modules = ['obspy.%s.tests.suite' % d for d in DEFAULT_MODULES]
-    suite = unittest.TestLoader().loadTestsFromNames(modules)
+    if tests == []:
+        names = ['obspy.%s.tests.suite' % d for d in DEFAULT_MODULES]
+    else:
+        names = []
+        # Search for short cuts in tests, if there are no short cuts
+        # names is equal to tests
+        for test in tests:
+            if test in DEFAULT_MODULES:
+                test = 'obspy.%s.tests.suite' % test
+            names.append(test)
+    # Construct the test suite from the given names. Note modules need not
+    # be imported before in this case
+    suite = unittest.TestLoader().loadTestsFromNames(names)
     return suite
 
 
-def runTests(verbosity=1, modules=[]):
+def runTests(verbosity=1, tests=[]):
     """
     This function runs all available tests in obspy, from Python
 
     :param verbosity: Run tests in verbose mode. 0 quiet, 1 normal, 2 verbose
-    :param modules: List of modules for testing. Default runs all.
+    :param tests: List of tests to run. Defaults to runs all.
                     Example ['obspy.core.tests.suite']
     """
-    unittest.TextTestRunner(verbosity=verbosity).run(suite(modules))
+    unittest.TextTestRunner(verbosity=verbosity).run(suite(tests))
 
 
-if __name__ == "__main__":
+def main():
     usage = "USAGE: %prog [options] modules\n\n" + \
-        """Examples:
-    %prog                               - run default set of tests
-    %prog MyTestSuite                   - run suite 'MyTestSuite'
-    %prog MyTestCase.testSomething      - run MyTestCase.testSomething
-    %prog MyTestCase                    - run all 'test*' test methods
-                                                in MyTestCase
-    """
+            "\n".join(__doc__.split("\n")[3:])
     parser = OptionParser(usage.strip())
     parser.add_option("-v", "--verbose", default=False,
                       action="store_true", dest="verbose",
@@ -82,7 +70,7 @@ if __name__ == "__main__":
                       action="store_true", dest="quiet",
                       help="quiet mode")
     (options, _) = parser.parse_args()
-    # Simulate same behaviour as standard unittest args parsing
+    # set correct verbosity level
     if options.verbose:
         verbosity = 2
     elif options.quiet:
@@ -90,3 +78,11 @@ if __name__ == "__main__":
     else:
         verbosity = 1
     runTests(verbosity, parser.largs)
+
+
+if __name__ == "__main__":
+    # It is not possible to add the code of main directly to here.
+    # This script is automatically installed with name obspy-runtests by
+    # setup.py to the Scripts or bin directory of your Python distribution
+    # setup.py needs a function to which it's scripts can be linked.
+    main()
