bb08e036495d83c88a29a291ff2f6274434ddf7d
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2010-02-02 13:20:12 +0000

    - Fixed #43 for OSX 64bit.

--- obspy.signal/trunk/obspy/signal/tests/test_util.py
+++ obspy.signal/trunk/obspy/signal/tests/test_util.py
@@ -24,48 +24,21 @@ class UtilTestCase(unittest.TestCase):
 
     def test_xcorr(self):
         """
-        Test case for xcorr. 
-        (1) Tests example in ObsPy paper submitted to the Electronic
-        Seismologist section of SRL is still working. The test shouldn't be
-        changed because the reference gets wrong. If xcorr is imported
-        before hand e.g. via a doctest, this test will fail with a
-        segmentation fault.
-        (2) Test case of real way how it should be used. Thus look at
-        example 1, 2, 3 or 4.
         """
-        # test_SRL
-        np.random.seed(815)
-        data1 = np.random.randn(1000).astype('float32')
-        data2 = data1.copy()
-        window_len = 100
-        #
-        path = os.path.dirname(inspect.getsourcefile(self.__class__))
-        name = os.path.join(path, os.pardir, 'lib', lib_name)
-        lib = C.CDLL(name)
-        #
-        shift = C.c_int()
-        coe_p = C.c_double()
-        lib.X_corr(data1.ctypes.data, data2.ctypes.data,
-                   window_len, len(data1), len(data2),
-                   C.byref(shift), C.byref(coe_p))
-
-        self.assertAlmostEquals(0.0, shift.value)
-        self.assertAlmostEquals(1.0, coe_p.value)
-        #
-        # Example 1 - all samples are equal
+        # example 1 - all samples are equal
         np.random.seed(815) # make test reproducable
         tr1 = np.random.randn(10000).astype('float32')
         tr2 = tr1.copy()
         shift, corr = xcorr(tr1, tr2, 100)
         self.assertEquals(shift, 0)
         self.assertAlmostEquals(corr, 1, 2)
-        # Example 2 - all samples are different
+        # example 2 - all samples are different
         tr1 = np.ones(10000, dtype='float32')
         tr2 = np.zeros(10000, dtype='float32')
         shift, corr = xcorr(tr1, tr2, 100)
         self.assertEquals(shift, 0)
         self.assertAlmostEquals(corr, 0, 2)
-        # Example 3 - shift of 10 samples
+        # example 3 - shift of 10 samples
         tr1 = np.random.randn(10000).astype('float32')
         tr2 = np.concatenate((np.zeros(10), tr1[0:-10]))
         shift, corr = xcorr(tr1, tr2, 100)
@@ -74,7 +47,7 @@ class UtilTestCase(unittest.TestCase):
         shift, corr = xcorr(tr2, tr1, 100)
         self.assertEquals(shift, 10)
         self.assertAlmostEquals(corr, 1, 2)
-        # Example 4 - shift of 10 samples + small sine disturbance
+        # example 4 - shift of 10 samples + small sine disturbance
         tr1 = (np.random.randn(10000) * 100).astype('float32')
         var = np.sin(np.arange(10000, dtype='float32') * 0.1)
         tr2 = np.concatenate((np.zeros(10), tr1[0:-10])) * 0.9
@@ -85,6 +58,31 @@ class UtilTestCase(unittest.TestCase):
         shift, corr = xcorr(tr2, tr1, 100)
         self.assertEquals(shift, 10)
         self.assertAlmostEquals(corr, 1, 2)
+
+    def test_SRL(self):
+        """
+        Tests if example in ObsPy paper submitted to the Electronic
+        Seismologist section of SRL is still working. The test shouldn't be
+        changed because the reference gets wrong.
+        """
+        np.random.seed(815)
+        data1 = np.random.randn(1000).astype('float32')
+        data2 = data1.copy()
+        window_len = 100
+
+        path = os.path.dirname(inspect.getsourcefile(self.__class__))
+        name = os.path.join(path, os.pardir, 'lib', lib_name)
+        lib = C.CDLL(name)
+        #
+        shift = C.c_int()
+        coe_p = C.c_double()
+        lib.X_corr(data1.ctypes.data_as(C.c_void_p),
+                   data2.ctypes.data_as(C.c_void_p),
+                   window_len, len(data1), len(data2),
+                   C.byref(shift), C.byref(coe_p))
+
+        self.assertAlmostEquals(0.0, shift.value)
+        self.assertAlmostEquals(1.0, coe_p.value)
 
 def suite():
     return unittest.makeSuite(UtilTestCase, 'test')
