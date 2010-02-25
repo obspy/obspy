8032736f13bc667d5a6f2645f619320e8d1c2294
Robert Barsch <barsch@egu.eu>
2010-02-25 20:57:43 +0000

    tagging 0.3.0

---
+++ obspy.mseed/tags/0.3.0/obspy/mseed/__init__.py
@@ -0,0 +1,209 @@
+# -*- coding: utf-8 -*-
+"""
+obspy.mseed - MiniSEED read and write support
+=============================================
+This module provides read and write support for Mini-SEED waveform data and
+some other convinient methods to handle Mini-SEED files. Most methods are based
+on libmseed, a C library framework by Chad Trabant and interfaced via python
+ctypes.
+
+:copyright: The ObsPy Development Team (devs@obspy.org) & Chad Trabant
+:license: GNU General Public License (GPLv2)
+
+Reading via obspy.core
+----------------------
+Similiar to reading any other waveform data format using obspy.core:
+
+>>> from obspy.core import read
+>>> st = read('COP.BHE.DK.2009.050')
+>>> st
+<obspy.core.stream.Stream object at 0x101700150>
+>>> print st
+1 Trace(s) in Stream:
+DK.COP..BHE | 2009-02-19T00:00:00.035100Z - 2009-02-19T23:59:59.985100Z | 20.0 Hz, 1728000 samples
+
+The format will be determined automatically.
+
+Each trace will have a stats attribute containing the usual information. When
+reading a Mini-SEED file it will have one additional attribute: 'mseed'. This
+attribute contains all Mini-SEED specific attributes which actually is just the
+dataquality.
+
+>>> print st[0].stats
+Stats({
+    'network': 'DK',
+    'mseed': Stats({
+        'dataquality': 'D'
+    }),
+    'station': 'COP',
+    'location': '',
+    'starttime': UTCDateTime(2009, 2, 19, 0, 0, 0, 35100), 
+    'npts': 1728000, 
+    'sampling_rate': 20.0,
+    'endtime': UTCDateTime(2009, 2, 19, 23, 59, 59, 985100), 
+    'channel': 'BHE'
+})
+
+The actual data is stored as numpy.ndarray in the data attribute of each trace.
+
+>>> st[0].data
+array([1085, 1167, 1131, ...,  -19,  -46,  -55], dtype=int32)
+
+Writing via obspy.core
+----------------------
+WARNING: No matter what is written in the Stream[i].stats.mseed['dataquality']
+field, the dataquality in the resulting Mini-SEED file will always be 'D'.
+
+Writing is also done in the usual way.
+
+>>> st.write('Mini-SEED-filename.mseed', format = 'MSEED')
+
+You can also specify several keyword arguments that change the resulting
+Mini-SEED file:
+
+* reclen : Record length in bytes of the resulting Mini-SEED file. The record
+  length needs to be expressible as 2 to the power of X where X is in between
+  and including 8 and 20. If no reclen is given it will default to 4096 bytes.
+* encoding: Encoding of the Mini-SEED file. You can either give the a string or
+  the corresponding number. If no encoding is given it will default to STEIM2.
+  Available encodings:
+  |   o INT16 or 1
+  |   o INT32 or 3
+  |   o STEIM1 or 10
+  |   o STEIM2 or 11 
+* byteorder: Byte order of the Mini-SEED file. 0 will result in a little-endian
+  file and 1 in a big-endian file. Defaults to big-endian. Do not change this
+  if you don't know what you are doing because most other programs can only
+  read big-endian Mini-SEED files.
+* flush: If it is not zero all of the data will be packed into records,
+  otherwise records will only be packed while there are enough data samples to
+  completely fill a record. The default value is -1 and thus every data value
+  will be packed by default.
+* verbose: Controls verbosity of the underlaying libmseed. A value higher than
+  0 will give diagnostic output. Defaults to 0. 
+
+So in order to write a STEIM1 encoded Mini-SEED file with a record_length of
+512 byte do the following:
+
+>>> st.write('out.mseed', format = 'MSEED', reclen = 512, encoding = 'STEIM1')
+
+Additional methods of obspy.mseed
+---------------------------------
+All of the following methods can only be accessed with an instance of the
+libmseed class.
+
+>>> from obspy.mseed import libmseed
+>>> mseed = libmseed()
+>>> mseed
+<obspy.mseed.libmseed.libmseed object at 0x10178ef50>
+
+printFileInformation
+^^^^^^^^^^^^^^^^^^^^
+Prints some informations about the file.
+
+Parameters:
+    * filename = MiniSEED file. 
+
+>>> mseed.printFileInformation('COP.BHE.DK.2009.050')
+Source            Start sample               End sample                 Gap  Hz  Samples
+DK_COP__BHE_D     2009-02-19T00:00:00.035100 2009-02-19T23:59:59.985100 ==   20  1728000
+Total: 1 trace segment(s)
+
+isMSEED
+^^^^^^^
+Tests whether a file is a MiniSEED file or not. Returns True on success or
+False otherwise.
+
+This method only reads the first seven bytes of the file and checks whether it
+is a MiniSEED or fullSEED file. It also is true for fullSEED files because
+libmseed can read the data part of fullSEED files. If the method finds a
+fullSEED file it also checks if it has a data part and returns False otherwise.
+Thus it cannot be used to validate a MiniSEED or SEED file.
+
+Parameters:
+    * filename = MiniSEED file. 
+
+>>> mseed.isMSEED('COP.BHE.DK.2009.050')
+True
+
+getDataQualityFlagsCount
+^^^^^^^^^^^^^^^^^^^^^^^^
+Counts all data quality flags of the given MiniSEED file. This method will
+count all set data quality flag bits in the fixed section of the data header in
+a MiniSEED file and returns the total count for each flag type.
+
+Data quality flags:
+
+========  =================================================
+Bit       Description
+========  =================================================
+[Bit 0]   Amplifier saturation detected (station dependent)
+[Bit 1]   Digitizer clipping detected
+[Bit 2]   Spikes detected
+[Bit 3]   Glitches detected
+[Bit 4]   Missing/padded data present
+[Bit 5]   Telemetry synchronization error
+[Bit 6]   A digital filter may be charging
+[Bit 7]   Time tag is questionable
+========  =================================================
+
+This will only work correctly if each record in the file has the same record
+length.
+
+Parameters:
+    * filename = MiniSEED file. 
+
+>>> mseed.getDataQualityFlagsCount('qualityflags.mseed')
+[9, 8, 7, 6, 5, 4, 3, 2]
+
+getTimingQuality
+^^^^^^^^^^^^^^^^
+Reads timing quality and returns a dictionary containing statistics about it.
+This method will read the timing quality in Blockette 1001 for each record in
+the file if available and return the following statistics:
+Minima, maxima, average, median and upper and lower quantile. It is probably
+pretty safe to set the first_record parameter to True because the timing
+quality is a vendor specific value and thus it will probably be set for each
+record or for none.
+
+Parameters:
+* filename = MiniSEED file.
+* first_record: Determines whether all records are assumed to either have a
+  timing quality in Blockette 1001 or not depending on whether the first
+  records has one. If True and the first records does not have a timing quality
+  it will not parse the whole file. If False is will parse the whole file
+  anyway and search for a timing quality in each record. Defaults to True.
+* rl_autodetection: Determines the auto-detection of the record lengths in the
+  file. If 0 only the length of the first record is detected automatically. All
+  subsequent records are then assumed to have the same record length. If -1 the
+  length of each record is automatically detected. Defaults to -1. 
+
+>>> mseed.getTimingQuality('timingquality.mseed')
+{'average': 50.0,
+ 'lower_quantile': 25.0,
+ 'max': 100.0,
+ 'median': 50.0,
+ 'min': 0.0,
+ 'upper_quantile': 75.0}
+
+getFirstRecordHeaderInfo
+^^^^^^^^^^^^^^^^^^^^^^^^
+Takes a MiniSEED file and returns some header information from the first record.
+
+Returns a dictionary containing some header information from the first record
+of the MiniSEED file only. It returns the location, network, station and
+channel information. The advantage is that this method is very fast but keep in
+mind that it will only parse the first record.
+
+Parameters:
+    * filename = MiniSEED file. 
+
+>>> mseed.getFirstRecordHeaderInfo('COP.BHE.DK.2009.050')
+{'channel': 'BHE', 'location': '', 'network': 'DK', 'station': 'COP'}
+"""
+
+from obspy.core.util import _getVersionString
+from obspy.mseed.libmseed import LibMSEED
+
+
+__version__ = _getVersionString("obspy.mseed")
---
+++ obspy.mseed/tags/0.3.0/obspy/mseed/core.py
@@ -0,0 +1,229 @@
+# -*- coding: utf-8 -*-
+"""
+MSEED bindings to ObsPy core module.
+"""
+
+from obspy.core import Stream, Trace
+from obspy.core.util import NATIVE_BYTEORDER
+from obspy.mseed import LibMSEED
+from obspy.mseed.headers import ENCODINGS
+import numpy as np
+import platform
+
+
+def isMSEED(filename):
+    """
+    Returns true if the file is a Mini-SEED file and false otherwise.
+
+    Parameters
+    ----------
+    filename : string
+        Mini-SEED file to be checked.
+    """
+    __libmseed__ = LibMSEED()
+    return __libmseed__.isMSEED(filename)
+
+
+def readMSEED(filename, headonly=False, starttime=None, endtime=None,
+              reclen= -1, **kwargs):
+    """
+    Reads a given Mini-SEED file and returns an Stream object.
+    
+    This function should NOT be called directly, it registers via the
+    obspy :func:`~obspy.core.stream.read` function, call this instead.
+
+    Parameters
+    ----------
+    filename : string
+        Mini-SEED file to be read.
+    headonly : bool, optional
+        If set to True, read only the head. This is most useful for
+        scanning available data in huge (temporary) data sets.
+    starttime : :class:`~obspy.core.utcdatetime.UTCDateTime`, optional
+        Specify the starttime to read. The remaining records are not
+        extracted. Providing a starttime usually results into faster reading.
+    endtime : :class:`~obspy.core.utcdatetime.UTCDateTime`, optional
+        See description of starttime.
+    reclen : int, optional
+        Record length in bytes of Mini-SEED file to read. This option might
+        be usefull if blockette 10 is missing and thus read cannot
+        determine the reclen automatically.
+
+    Example
+    -------
+    >>> from obspy.core import read # doctest: +SKIP
+    >>> st = read("test.mseed") # doctest: +SKIP
+    """
+    __libmseed__ = LibMSEED()
+    # read MiniSEED file
+    if headonly:
+        trace_list = __libmseed__.readMSHeader(filename, reclen=reclen)
+    else:
+        kwargs['starttime'] = kwargs.get('starttime', None)
+        kwargs['endtime'] = kwargs.get('endtime', None)
+        if starttime or endtime or platform.system() == "Windows":
+            # 4x slower on Mac
+            trace_list = __libmseed__.readMSTracesViaRecords(filename,
+                         starttime=starttime, endtime=endtime, reclen=reclen)
+        else:
+            # problem on windows with big files (>=20 MB)
+            trace_list = __libmseed__.readMSTraces(filename, reclen=reclen)
+    # Create a list containing all the traces.
+    traces = []
+    # Loop over all traces found in the file.
+    for _i in trace_list:
+        # Convert header to be compatible with obspy.core.
+        old_header = _i[0]
+        header = {}
+        # Create a dictionary to specify how to convert keys.
+        convert_dict = {'station': 'station', 'sampling_rate':'samprate',
+                        'npts': 'numsamples', 'network': 'network',
+                        'location': 'location', 'channel': 'channel',
+                        'starttime' : 'starttime', 'endtime' : 'endtime'}
+        # Convert header.
+        for _j, _k in convert_dict.iteritems():
+            header[_j] = old_header[_k]
+        # Dataquality is Mini-SEED only and thus has an extra Stats attribute.
+        header['mseed'] = {}
+        header['mseed']['dataquality'] = old_header['dataquality']
+        # Convert times to obspy.UTCDateTime objects.
+        header['starttime'] = \
+            __libmseed__._convertMSTimeToDatetime(header['starttime'])
+        header['endtime'] = \
+            __libmseed__._convertMSTimeToDatetime(header['endtime'])
+        # Append traces.
+        if headonly:
+            header['npts'] = int((header['endtime'] - header['starttime']) *
+                                   header['sampling_rate'] + 1 + 0.5)
+            traces.append(Trace(header=header))
+        else:
+            traces.append(Trace(header=header, data=_i[1]))
+    return Stream(traces=traces)
+
+
+def writeMSEED(stream, filename, encoding=None, **kwargs):
+    """
+    Write Mini-SEED file from a Stream object.
+
+    All kwargs are passed directly to obspy.mseed.writeMSTraces.
+    This function should NOT be called directly, it registers via the
+    obspy :meth:`~obspy.core.stream.Stream.write` method of an ObsPy
+    Stream object, call this instead.
+
+    Parameters
+    ----------
+    stream_object : :class:`~obspy.core.stream.Stream`
+        A Stream object. Data in stream object must be of type int32.
+        NOTE: They are automatically adapted if necessary
+    filename : string
+        Name of the output file
+    reclen : int, optional
+        Should be set to the desired data record length in bytes
+        which must be expressible as 2 raised to the power of X where X is
+        between (and including) 8 to 20. -1 defaults to 4096
+    encoding : int or string, optional
+        Should be set to one of the following supported
+        Mini-SEED data encoding formats: ASCII (0)*, INT16 (1), INT32 (3), 
+        FLOAT32 (4)*, FLOAT64 (5)*, STEIM1 (10) and STEIM2 (11)*. Default 
+        data types a marked with an asterisk.
+    byteorder : [ 0 or '<' | '1 or '>' | -1], optional
+        Must be either 0 or '<' for LSBF or little-endian, 1 or
+        '>' for MBF or big-endian. -1 defaults to big-endian (1)
+    flush : int, optional
+        If it is not zero all of the data will be packed into 
+        records, otherwise records will only be packed while there are
+        enough data samples to completely fill a record.
+    verbose : int, optional
+         Controls verbosity, a value of zero will result in no 
+        diagnostic output.
+    """
+    # Check if encoding kwarg is set and catch invalid encodings.
+    # XXX: Currently INT24 is not working due to lacking numpy support.
+    encoding_strings = dict([(v[0], k) for (k, v) in ENCODINGS.iteritems()])
+    if not encoding:
+        encoding = -1
+    elif isinstance(encoding, int) and encoding in ENCODINGS:
+        encoding = encoding
+    elif isinstance(encoding, basestring) and encoding in encoding_strings:
+        encoding = encoding_strings[encoding]
+    else:
+        msg = 'Invalid encoding %s. Valid encodings: %s'
+        raise ValueError(msg % (encoding, encoding_strings))
+    # translate byteorder
+    if 'byteorder' in kwargs.keys() and kwargs['byteorder'] not in [0, 1, -1]:
+        if kwargs['byteorder'] == '=':
+            kwargs['byteorder'] = NATIVE_BYTEORDER
+        if kwargs['byteorder'] == '<':
+            kwargs['byteorder'] = 0
+        elif kwargs['byteorder'] == '>':
+            kwargs['byteorder'] = 1
+        else:
+            kwargs['byteorder'] = 1
+    # Catch invalid record length.
+    valid_record_lengths = [256, 512, 1024, 2048, 4096, 8192, 16384, 32768,
+                            65536, 131072, 262144, 524288, 1048576]
+    if 'reclen' in kwargs.keys() and \
+                not kwargs['reclen'] in valid_record_lengths:
+        msg = 'Invalid record length. The record length must be a value\n' + \
+              'of 2 to the power of X where 8 <= X <= 20.'
+        raise ValueError(msg)
+    # libmseed instance.
+    __libmseed__ = LibMSEED()
+    traces = stream.traces
+    trace_list = []
+    convert_dict = {'station': 'station', 'samprate':'sampling_rate',
+                    'numsamples': 'npts', 'network': 'network',
+                    'location': 'location', 'channel': 'channel',
+                    'starttime': 'starttime', 'endtime': 'endtime'}
+    for trace in traces:
+        header = {}
+        for _j, _k in convert_dict.iteritems():
+            header[_j] = trace.stats[_k]
+        # Set data quality to indeterminate (= D) if it is not already set.
+        try:
+            header['dataquality'] = trace.stats['mseed']['dataquality']
+        except:
+            header['dataquality'] = 'D'
+        # Convert UTCDateTime times to Mini-SEED times.
+        header['starttime'] = \
+            __libmseed__._convertDatetimeToMSTime(header['starttime'])
+        header['endtime'] = \
+            __libmseed__._convertDatetimeToMSTime(header['endtime'])
+        # Check that data are numpy.ndarrays
+        if not isinstance(trace.data, np.ndarray):
+            msg = "Unsupported data type %s" % type(trace.data)
+            raise Exception(msg)
+        # autodetect format if no global encoding is given
+        if encoding == -1:
+            if trace.data.dtype.type == np.dtype("int32"):
+                enc = 11
+            elif trace.data.dtype.type == np.dtype("float32"):
+                enc = 4
+            elif trace.data.dtype.type == np.dtype("float64"):
+                enc = 5
+            elif trace.data.dtype.type == np.dtype("int16"):
+                enc = 1
+            elif trace.data.dtype.type == np.dtype('|S1').type:
+                enc = 0
+            else:
+                msg = "Unsupported data type %s" % trace.data.dtype
+                raise Exception(msg)
+            _, sampletype, _ = ENCODINGS[enc]
+        else:
+            # global encoding given
+            enc = encoding
+            id, sampletype, dtype = ENCODINGS[enc]
+            # Check if supported data type
+            if trace.data.dtype.type != dtype:
+                msg = "Data type for encoding %s must be of %s" % (id, dtype)
+                raise Exception(msg)
+        # INT16 needs INT32 data type
+        if enc == 1:
+            trace.data = trace.data.astype(np.int32)
+        header['sampletype'] = sampletype
+        header['encoding'] = enc
+        # Fill the samplecnt attribute.
+        header['samplecnt'] = len(trace.data)
+        trace_list.append([header, trace.data])
+    # Write resulting trace_list to Mini-SEED file.
+    __libmseed__.writeMSTraces(trace_list, outfile=filename, **kwargs)
