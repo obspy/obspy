165b38fab5a8dae206a93b5b0da8a5599adb3148
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2010-08-10 22:11:18 +0000

    Seishub Explorer:  - Enabled component wildcards in the dialog which opens the picker.

--- apps/SeisHubExplorer/gui/call_picker_dialog.py
+++ apps/SeisHubExplorer/gui/call_picker_dialog.py
@@ -19,6 +19,10 @@ class CallPickerDialog(QtGui.QDialog):
         self.command = command
         self._initInterface()
         self._connectSignals()
+        # Toggle the channel wildcards.
+        self._createWildcardCommand()
+        self.wildcards_toggled = True
+        self.command_edit.setText(self.wildcard_command)
 
     def _initInterface(self):
         """
@@ -38,6 +42,9 @@ class CallPickerDialog(QtGui.QDialog):
         self.button_layout = QtGui.QHBoxLayout()
         self.button_layout.setMargin(0)
         self.button_frame.setLayout(self.button_layout)
+        self.channel_wildcards_button = \
+                QtGui.QPushButton('Toggle Component Wildcards')
+        self.button_layout.addWidget(self.channel_wildcards_button)
         spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding,
                                         QtGui.QSizePolicy.Minimum)
         self.button_layout.addItem(spacerItem)
@@ -50,11 +57,62 @@ class CallPickerDialog(QtGui.QDialog):
         self.button_layout.addWidget(self.ok_button)
         self.ok_button.setDefault(True)
 
+    def _createWildcardCommand(self):
+        """
+        Creates a wildcard version of the command where all components will be
+        replaced by wildcards.
+        """
+        # Replaces all components with a wildcards and removes duplicate
+        # entries.
+        channels = self.command.split("'")[1].split(',')
+        channels = [str(channel.split()[0])[:-1] + '*' for channel in channels]
+        channels = list(set(channels))
+        channels.sort()
+        # Recreate the new wildcard command.
+        self.wildcard_command = self.command.split("'")[0] + "'%s'" %\
+            ','.join(channels)
+
+    def toggleChannelWildcards(self):
+        """
+        Toggles the channel wildcards. True by default.
+        """
+        if self.wildcards_toggled:
+            # Check if changed.
+            if str(self.command_edit.text()) != self.wildcard_command:
+                msg =  20 * ' ' + 'Command changed!' + 20 * ' '
+                info = 'All manual changes will be lost. Continue?'
+                box = QtGui.QMessageBox()
+                box.setText(msg)
+                box.setInformativeText(info)
+                box.setStandardButtons(QtGui.QMessageBox.No| QtGui.QMessageBox.Yes)
+                ret_code = box.exec_()
+                if ret_code == QtGui.QMessageBox.No:
+                    return
+            self.command_edit.setText(self.command)
+            self.wildcards_toggled = False
+        else:
+            # Check if changed.
+            if str(self.command_edit.text()) != self.command:
+                msg =  20 * ' ' + 'Command changed!' + 20 * ' '
+                info = 'All manual changes will be lost. Continue?'
+                box = QtGui.QMessageBox()
+                box.setText(msg)
+                box.setInformativeText(info)
+                box.setStandardButtons(QtGui.QMessageBox.No| QtGui.QMessageBox.Yes)
+                ret_code = box.exec_()
+                if ret_code == QtGui.QMessageBox.No:
+                    return
+            self.command_edit.setText(self.wildcard_command)
+            self.wildcards_toggled = True
+
     def reset(self):
         """
         Resets the content of the command edit line.
         """
-        self.command_edit.setText(self.command)
+        if self.wildcards_toggled:
+            self.command_edit.setText(self.wildcard_command)
+        else:
+            self.command_edit.setText(self.command)
 
     def accept(self):
         """
@@ -75,3 +133,5 @@ class CallPickerDialog(QtGui.QDialog):
                                self.reject)
         QtCore.QObject.connect(self.ok_button, QtCore.SIGNAL("clicked()"),
                                self.accept)
+        QtCore.QObject.connect(self.channel_wildcards_button, QtCore.SIGNAL("clicked()"),
+                               self.toggleChannelWildcards)
