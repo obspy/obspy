7d5a4d08feee4045adc833d99c7030544e4140b9
Robert Barsch <barsch@egu.eu>
2010-08-12 14:44:16 +0000

    - hack -> runtest fails - Tobi will clean up ;)

--- obspy.core/trunk/obspy/core/tests/__init__.py
+++ obspy.core/trunk/obspy/core/tests/__init__.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 
-import obspy
-from obspy.core import util, utcdatetime, trace, stream, __init__
+import obspy.core
 from obspy.core.tests import test_stream, test_utcdatetime, test_trace, \
     test_stats, test_waveform_plugins, test_preview, test_util, test_ascii
 import doctest
 import unittest
+import warnings
 
 
 def suite():
     suite = unittest.TestSuite()
-    #for module in [util, utcdatetime, trace, stream, __init__]:
-    for module in [util, utcdatetime, trace, stream]:
-        print type(module)
-        suite.addTest(doctest.DocTestSuite(module))
-        #try:
-        #    suite.addTest(doctest.DocTestSuite(module))
-        #except:
-        #    pass
+    suite.addTest(doctest.DocTestSuite(obspy.core))
+    for module in [obspy.core, obspy.core.util, obspy.core.utcdatetime,
+                   obspy.core.trace, obspy.core.stream]:
+        try:
+            suite.addTest(doctest.DocTestSuite(module))
+        except Exception, e:
+            warnings.warn(str(e))
+            pass
     suite.addTest(test_utcdatetime.suite())
     suite.addTest(test_stats.suite())
     suite.addTest(test_trace.suite())
