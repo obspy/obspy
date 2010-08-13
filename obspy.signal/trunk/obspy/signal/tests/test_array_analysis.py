e081c9ddcd206fa071559091517d055f8acf6549
Martin van Driel <vandriel@erdw.ethz.ch>
2010-08-13 15:29:31 +0000

    added another test for array_rotation_strain using synthetic data with pure dilation

--- obspy.signal/trunk/obspy/signal/tests/test_array_analysis.py
+++ obspy.signal/trunk/obspy/signal/tests/test_array_analysis.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-The Array test suite.
+The array_analysis test suite.
 """
 
 import unittest
@@ -10,17 +10,17 @@ from obspy.signal.array_analysis import array_rotation_strain
 
 class ArrayTestCase(unittest.TestCase):
     """
-    Test cases for Array functions.
+    Test cases for array_analysis functions.
     """
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
-
-    def test_array(self):
-        #    REL OFFSET (M)
-        #    x       y      z     
+    
+    def test_array_rotation(self):
+        # tests function array_rotation_strain with synthetic data with pure
+        # rotation and no strain
         array_coords = np.array([[   0.0,    0.0,   0.0],
                                  [  -5.0,    7.0,   0.0],
                                  [   5.0,    7.0,   0.0],
@@ -44,8 +44,8 @@ class ArrayTestCase(unittest.TestCase):
         ts2 = np.ones((1000, 7)) * np.NaN
         ts3 = np.ones((1000, 7)) * np.NaN
 
-        for stat in np.arange(7):
-            for t in np.arange(1000):
+        for stat in xrange(7):
+            for t in xrange(1000):
                 ts1[t, stat] = -1. * array_coords[stat, 1] * rotz[t]
                 ts2[t, stat] = array_coords[stat, 0] * rotz[t]
                 ts3[t, stat] = array_coords[stat, 1] * rotx[t] - \
@@ -54,9 +54,67 @@ class ArrayTestCase(unittest.TestCase):
         out = array_rotation_strain(subarray, ts1, ts2, ts3, Vp, Vs,
                                     array_coords, sigmau)
 
-        np.testing.assert_array_almost_equal(rotx, out['ts_w1'])
-        np.testing.assert_array_almost_equal(roty, out['ts_w2'])
-        np.testing.assert_array_almost_equal(rotz, out['ts_w3'])
+        np.testing.assert_array_almost_equal(rotx, out['ts_w1'], decimal=12)
+        np.testing.assert_array_almost_equal(roty, out['ts_w2'], decimal=12)
+        np.testing.assert_array_almost_equal(rotz, out['ts_w3'], decimal=12)
+        np.testing.assert_array_almost_equal(np.zeros(1000), out['ts_s'],
+                decimal=15)
+        np.testing.assert_array_almost_equal(np.zeros(1000), out['ts_d'],
+                decimal=15)
+        np.testing.assert_array_almost_equal(np.zeros(1000), out['ts_M'],
+                decimal=12)
+
+    def test_array_dilation(self):
+        # tests function array_rotation_strain with synthetic data with pure
+        # dilation and no rotation or shear strain
+        array_coords = np.array([[   0.0,    0.0,   0.0],
+                                 [  -5.0,    7.0,   0.0],
+                                 [   5.0,    7.0,   0.0],
+                                 [  10.0,    0.0,   0.0],
+                                 [   5.0,   -7.0,   0.0],
+                                 [  -5.0,   -7.0,   0.0],
+                                 [ -10.0,    0.0,   0.0]])
+        subarray = np.array([0, 1, 2, 3, 4, 5, 6])
+        sigmau = 0.0001
+        Vp = 1.93  # km/s
+        Vs = 0.326 # km/s
+        
+        eta = 1 - 2*Vs**2/Vp**2
+
+        dilation = .00001 * np.exp(-1*np.square(np.linspace(-2, 2, 1000))) * \
+                np.sin(np.linspace(-40*np.pi, 40*np.pi, 1000))
+
+        ts1 = np.ones((1000, 7)) * np.NaN
+        ts2 = np.ones((1000, 7)) * np.NaN
+        ts3 = np.ones((1000, 7)) * np.NaN
+
+        for stat in xrange(7):
+            for t in xrange(1000):
+                ts1[t, stat] = array_coords[stat, 0] * dilation[t]
+                ts2[t, stat] = array_coords[stat, 1] * dilation[t]
+                ts3[t, stat] = array_coords[stat, 2] * dilation[t]
+
+        out = array_rotation_strain(subarray, ts1, ts2, ts3, Vp, Vs,
+                                    array_coords, sigmau)
+
+        # remember free surface boundary conditions!
+        # see Spudich et al, 1995, (A2)
+        np.testing.assert_array_almost_equal(dilation * (2-2*eta),
+                out['ts_d'], decimal=12)
+        np.testing.assert_array_almost_equal(dilation * 2 , out['ts_dh'],
+                decimal=12)
+        np.testing.assert_array_almost_equal(abs(dilation * .5 * (1 + \
+                2*eta)), out['ts_s'], decimal=12)
+        np.testing.assert_array_almost_equal(np.zeros(1000), out['ts_sh'],
+                decimal=12)
+        np.testing.assert_array_almost_equal(np.zeros(1000), out['ts_w1'],
+                decimal=15)
+        np.testing.assert_array_almost_equal(np.zeros(1000), out['ts_w2'],
+                decimal=15)
+        np.testing.assert_array_almost_equal(np.zeros(1000), out['ts_w3'],
+                decimal=15)
+        np.testing.assert_array_almost_equal(np.zeros(1000), out['ts_M'],
+                decimal=12)
 
 
 def suite():
