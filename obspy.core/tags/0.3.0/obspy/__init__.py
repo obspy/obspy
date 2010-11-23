c86fc26621ae17883cc06c15ef347e13291f77e9
Robert Barsch <barsch@egu.eu>
2010-11-23 10:53:19 +0000

    - removing unused tags

--- obspy.core/tags/0.3.0/obspy/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# See http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
-    from pkgutil import extend_path
-    __path__ = extend_path(__path__, __name__)
