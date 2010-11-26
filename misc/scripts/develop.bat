a11e4ebe67d5d93ebf4fe80e7d161a70f4785c7d
Robert Barsch <barsch@egu.eu>
2010-11-26 11:30:04 +0000

    - testing svn hooks

--- misc/scripts/develop.bat
+++ misc/scripts/develop.bat
@@ -6,8 +6,8 @@ REM Run vcvars32.bat in VS2008 command line for 32 bit!
 REM Run vcvars64.bat in VS2008 command line for 64 bit!
 REM
 
-set PYTHON="C:\Program Files (x86)\Python2.6\python"
-REM set PYTHON="C:\Program Files\Python26\python"
+REM set PYTHON="C:\Program Files (x86)\Python2.6\python"
+set PYTHON="C:\Program Files\Python26\python"
 
 FOR %%M IN (core mseed gse2 signal imaging arclink fissures sac seisan seishub wav xseed sh) DO (
 cd ..\..
