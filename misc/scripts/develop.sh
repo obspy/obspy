2c1ffa274b5855de99775f2ba9efffa4e2ccd05e
Moritz Beyreuther <random.seed@web.de>
2010-12-28 16:53:22 +0000

    Adding events to develop.sh

--- misc/scripts/develop.sh
+++ misc/scripts/develop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PACKAGES="core gse2 mseed sac seisan sh wav xseed signal imaging arclink \
-seishub fissures db segy"
+seishub fissures db segy events"
 
 # Go from here to ObsPy root directory
 cd ../..
