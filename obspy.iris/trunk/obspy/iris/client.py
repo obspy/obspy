f1ac759bb856b4578e2bf59419a7e96b1ef426a5
Tobias Megies <megies@geophysik.uni-muenchen.de>
2011-01-23 16:36:58 +0000

    iris:
     - first implementations for availability/bulkdataselect webservices
     - client.getWaveform() now working without *and with* wildcards
     - no tests yet
     - very likely a bit quick&dirty, should maybe be cleaned up
     - some queries can include further options like min/max longitude etc. that could also be implemented

--- obspy.iris/trunk/obspy/iris/client.py
+++ obspy.iris/trunk/obspy/iris/client.py
@@ -59,10 +59,96 @@ class Client(object):
         doc = response.read()
         return doc
 
+    def _HTTP_request(self, url, data, headers={}):
+        """
+        Send a HTTP request via urllib2.
+
+        :type url: String
+        :param url: Complete URL of resource
+        :type data: String
+        :param data: Channel list as returned by `availability`-webservice
+        :type headers: dict
+        :param headers: Additional header information for request
+        """
+        headers['User-Agent'] = "ObsPy"
+        # replace special characters 
+        remoteaddr = self.base_url + url
+        req = urllib2.Request(url=remoteaddr, data=data, headers=headers)
+        # timeout exists only for Python >= 2.6
+        if sys.hexversion < 0x02060000:
+            response = urllib2.urlopen(req)
+        else:
+            response = urllib2.urlopen(req, timeout=self.timeout)
+        data = response.read()
+        return data
+
     def getWaveform(self, network, station, location, channel, starttime,
                      endtime, quality='B'):
         """
         Gets a ObsPy Stream object.
+        Wildcards are allowed for `network`, `station`, `location` and
+        `channel`.
+
+        Parameters
+        ----------
+        network : string
+            Network code, e.g. 'IU' or 'I*'.
+        station : string
+            Station code, e.g. 'ANMO' or 'A*'.
+        location : string
+            Location code, e.g. '00' or '*'.
+        channel : string
+            Channel code, e.g. 'BHZ' or 'B*'.
+        starttime : :class:`~obspy.core.utcdatetime.UTCDateTime`
+            Start date and time.
+        endtime : :class:`~obspy.core.utcdatetime.UTCDateTime`
+            End date and time.
+        quality : 'D', 'R', 'Q', 'M' or 'B', optional
+            MiniSEED data quality indicator. M and B (default) are treated the
+            same and indicate best available. If M or B are selected, the
+            output data records will be stamped with a M.
+
+        Returns
+        -------
+            :class:`~obspy.core.stream.Stream`
+        """
+        kwargs = {}
+        kwargs['network'] = str(network)[0:2]
+        kwargs['station'] = str(station)[0:5]
+        if location:
+            kwargs['location'] = str(location)[0:2]
+        else:
+            kwargs['location'] = '--'
+        kwargs['channel'] = str(channel)[0:3]
+        # try to be intelligent in starttime/endtime extension for fetching data
+        try:
+            t_extension = 2.0 / BAND_CODE[kwargs['channel'][0]]
+        except:
+            # use 1 second extension if no proper bandcode info
+            t_extension = 1.0
+        kwargs['starttime'] = (UTCDateTime(starttime) - t_extension).formatIRISWebService()
+        kwargs['endtime'] = (UTCDateTime(endtime) + t_extension).formatIRISWebService()
+        if str(quality).upper() in ['D', 'R', 'Q', 'M', 'B']:
+            kwargs['quality'] = str(quality).upper()
+
+        # single channel request, go via `dataselect`-webservice
+        if all([val.isalnum() for val in (kwargs['network'], kwargs['station'], kwargs['location'], kwargs['channel'])]):
+            st = self.dataselect(**kwargs)
+        # wildcarded channel request, go via `availability`+`bulkdataselect`-webservices
+        else:
+            quality = kwargs.pop("quality", "")
+            bulk = self.availability(**kwargs)
+            st = self.bulkdataselect(bulk, quality)
+
+        st.trim(UTCDateTime(starttime), UTCDateTime(endtime))
+        return st
+        
+
+    def dataselect(self, **kwargs):
+        """
+        Interface for `dataselect`-webservice of IRIS
+        (http://www.iris.edu/ws/dataselect/).
+        Single channel request, no wildcards allowed.
 
         Parameters
         ----------
@@ -73,7 +159,7 @@ class Client(object):
         location : string
             Location code, e.g. '00'.
         channel : string
-            Channel code, e.g. 'EHE', wildcards are not allowed.
+            Channel code, e.g. 'EHE'.
         starttime : :class:`~obspy.core.utcdatetime.UTCDateTime`
             Start date and time.
         endtime : :class:`~obspy.core.utcdatetime.UTCDateTime`
@@ -89,26 +175,6 @@ class Client(object):
         """
         url = '/dataselect/query'
         # build up query
-        kwargs = {}
-        kwargs['net'] = str(network)[0:2]
-        kwargs['sta'] = str(station)[0:5]
-        if location:
-            kwargs['loc'] = str(location)[0:2]
-        else:
-            kwargs['loc'] = '--'
-        kwargs['cha'] = str(channel)[0:3]
-        # try to be intelligent in start/endtime extension for fetching data
-        try:
-            t_extension = 2.0 / BAND_CODE[kwargs['cha'][0]]
-        except:
-            # use 1 second extension if no proper bandcode info
-            t_extension = 1.0
-        kwargs['start'] = UTCDateTime(starttime - t_extension).formatIRISWebService()
-        kwargs['end'] = UTCDateTime(endtime + t_extension).formatIRISWebService()
-        kwargs['start'] = UTCDateTime(starttime - 1).formatIRISWebService()
-        kwargs['end'] = UTCDateTime(endtime + 1).formatIRISWebService()
-        if str(quality).upper() in ['D', 'R', 'Q', 'M', 'B']:
-            kwargs['quality'] = str(quality).upper()
         try:
             data = self._fetch(url, **kwargs)
         except HTTPError:
@@ -128,9 +194,104 @@ class Client(object):
             os.remove(tf.name)
         except:
             pass
-        # trim stream
-        stream.trim(starttime, endtime)
         return stream
+
+    def bulkdataselect(self, bulk, quality=""):
+        """
+        Interface for `bulkdataselect`-webservice of IRIS
+        (http://www.iris.edu/ws/bulkdataselect/).
+
+        Parameters
+        ----------
+        bulk : string
+            List of channels to fetch as returned by :meth:`~obspy.iris.client.Client.availability`
+        quality : 'D', 'R', 'Q', 'M' or 'B', optional
+            MiniSEED data quality indicator. M and B (default) are treated the
+            same and indicate best available. If M or B are selected, the
+            output data records will be stamped with a M.
+
+        Returns
+        -------
+            :class:`~obspy.core.stream.Stream`
+        """
+        url = '/bulkdataselect/query'
+        # quality parameter is optional
+        if quality:
+            bulk = "quality %s\n" % quality.upper() + bulk
+        # build up query
+        try:
+            data = self._HTTP_request(url, data=bulk)
+        except HTTPError:
+            raise Exception("No waveform data available")
+        # create temporary file for writing data
+        tf = NamedTemporaryFile()
+        tf.write(data)
+        # read stream using obspy.mseed
+        tf.seek(0)
+        try:
+            stream = read(tf.name, 'MSEED')
+        except:
+            stream = Stream()
+        tf.close()
+        # remove temporary file:
+        try:
+            os.remove(tf.name)
+        except:
+            pass
+        return stream
+
+    def availability(self, network="*", station="*", location="*", channel="*",
+                      starttime=UTCDateTime()-3600, endtime=UTCDateTime()-3700, output="bulk"):
+        """
+        Interface for `availability`-webservice of IRIS
+        (http://www.iris.edu/ws/availability/).
+        Returns list of available channels that can be requested using the
+        `bulkdataselect`-webservice.
+
+        Parameters
+        ----------
+        network : string
+            Network code, e.g. 'BW', wildcards allowed.
+        station : string
+            Station code, e.g. 'MANZ', wildcards allowed.
+        location : string
+            Location code, e.g. '00', wildcards allowed.
+        channel : string
+            Channel code, e.g. 'EHE', wildcards allowed.
+        starttime : :class:`~obspy.core.utcdatetime.UTCDateTime`
+            Start date and time.
+        endtime : :class:`~obspy.core.utcdatetime.UTCDateTime`
+            End date and time.
+        output : string
+            Either "bulk" or "xml".
+
+        Returns
+        -------
+            String that lists available channels, either as plaintext
+            `bulkdataselect` format (`output="bulk"`) or in xml format
+            (`output="xml"`).
+        """
+        url = '/availability/query'
+        # build up query
+        kwargs = {}
+        kwargs['network'] = str(network)
+        kwargs['station'] = str(station)
+        kwargs['location'] = str(location)
+        kwargs['channel'] = str(channel)
+        try:
+            kwargs['starttime'] = UTCDateTime(starttime).formatIRISWebService()
+        except:
+            kwargs['starttime'] = starttime
+        try:
+            kwargs['endtime'] = UTCDateTime(endtime).formatIRISWebService()
+        except:
+            kwargs['endtime'] = endtime
+        kwargs['output'] = str(output)
+        if not kwargs['output'] in ("bulk", "xml"):
+            msg = "kwarg output must be either 'bulk' or 'xml'."
+            raise ValueError(msg)
+        data = self._fetch(url, **kwargs)
+        return data
 
 
 if __name__ == '__main__':
