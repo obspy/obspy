04d56bf42fe01604e7c15f7d345c33df881b4454
Robert Barsch <barsch@egu.eu>
2011-01-24 10:59:02 +0000

    - huge restructuring of the svn according to #208

--- obspy.sac/branches/bug194/bug194.py
+++
@@ -1,6 +0,0 @@
-from obspy.core import read
-
-st = read("TEMP1", format='SACXY')
-st.write("TEMP1.obspy", format='SACXY')
-#st = read("testxy.sac", format='SACXY')
-#st.write("testxy.sac.obspy", format='SACXY')
