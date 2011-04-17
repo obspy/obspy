866c61c79e797eeb6988f8ac1b4a76cb6eac0fe1
Moritz Beyreuther <random.seed@web.de>
2011-04-17 10:57:47 +0000

    Monkey patch for sphinx autosummary generate, to not include other modules classes and functions

--- trunk/obspy.wav/docs/api/conf.py
+++ trunk/obspy.wav/docs/api/conf.py
@@ -397,6 +397,149 @@ def obspy__str__(self, indent=0, func_role="obj"):
     out = self._str_indent(out, indent)
     return '\n'.join(out)
 
+from sphinx.ext.autosummary import generate
+from sphinx.jinja2glue import BuiltinTemplateLoader
+from jinja2.sandbox import SandboxedEnvironment
+from sphinx.ext.autosummary.generate import _simple_warn, _simple_info, \
+    find_autosummary_in_files, ensuredir, import_by_name, get_documenter, \
+    generate_autosummary_docs
+# monkey patch for Sphinx-1.0.6-py2.7.egg/sphinx/ext/autosummary/generate.py
+# -- Generating output ---------------------------------------------------------
+def obspy_generate_autosummary_docs(sources, output_dir=None, suffix='.rst',
+                                    warn=_simple_warn, info=_simple_info,
+                                    base_path=None, builder=None, template_dir=None):
+
+    showed_sources = list(sorted(sources))
+    if len(showed_sources) > 20:
+        showed_sources = showed_sources[:10] + ['...'] + showed_sources[-10:]
+    info('[autosummary] generating autosummary for: %s' %
+         ', '.join(showed_sources))
+
+    if output_dir:
+        info('[autosummary] writing to %s' % output_dir)
+
+    if base_path is not None:
+        sources = [os.path.join(base_path, filename) for filename in sources]
+
+    # create our own templating environment
+    template_dirs = [os.path.join(os.path.dirname(__file__), 'templates')]
+    if builder is not None:
+        # allow the user to override the templates
+        template_loader = BuiltinTemplateLoader()
+        template_loader.init(builder, dirs=template_dirs)
+    else:
+        if template_dir:
+            template_dirs.insert(0, template_dir)
+        template_loader = FileSystemLoader(template_dirs)
+    template_env = SandboxedEnvironment(loader=template_loader)
+
+    # read
+    items = find_autosummary_in_files(sources)
+
+    # remove possible duplicates
+    items = dict([(item, True) for item in items]).keys()
+
+    # keep track of new files
+    new_files = []
+
+    # write
+    for name, path, template_name in sorted(items):
+        if path is None:
+            # The corresponding autosummary:: directive did not have
+            # a :toctree: option
+            continue
+
+        path = output_dir or os.path.abspath(path)
+        ensuredir(path)
+
+        try:
+            obj, name = import_by_name(name)
+        except ImportError, e:
+            warn('[autosummary] failed to import %r: %s' % (name, e))
+            continue
+
+        fn = os.path.join(path, name + suffix)
+
+        # skip it if it exists
+        if os.path.isfile(fn):
+            continue
+
+        new_files.append(fn)
+
+        f = open(fn, 'w')
+
+        try:
+            doc = get_documenter(obj)
+
+            if template_name is not None:
+                template = template_env.get_template(template_name)
+            else:
+                try:
+                    template = template_env.get_template('autosummary/%s.rst'
+                                                         % doc.objtype)
+                except TemplateNotFound:
+                    template = template_env.get_template('autosummary/base.rst')
+
+            def get_members(obj, typ, include_public=[]):
+                items = [
+                    name for name in dir(obj)
+                    if get_documenter(getattr(obj, name)).objtype == typ
+                ]
+                public = [x for x in items
+                          if x in include_public or not x.startswith('_')]
+                # 3 Lines monkey patch Moritz, do not include imported modules
+                if typ in ['class', 'function']:
+                    public = [x for x in public
+                            if obj.__name__ == getattr(obj, x).__module__]
+                return public, items
+
+            ns = {}
+
+            if doc.objtype == 'module':
+                ns['members'] = dir(obj)
+                ns['functions'], ns['all_functions'] = \
+                                   get_members(obj, 'function')
+                ns['classes'], ns['all_classes'] = \
+                                 get_members(obj, 'class')
+                ns['exceptions'], ns['all_exceptions'] = \
+                                   get_members(obj, 'exception')
+            elif doc.objtype == 'class':
+                ns['members'] = dir(obj)
+                ns['methods'], ns['all_methods'] = \
+                                 get_members(obj, 'method', ['__init__'])
+                ns['attributes'], ns['all_attributes'] = \
+                                 get_members(obj, 'attribute')
+
+            parts = name.split('.')
+            if doc.objtype in ('method', 'attribute'):
+                mod_name = '.'.join(parts[:-2])
+                cls_name = parts[-2]
+                obj_name = '.'.join(parts[-2:])
+                ns['class'] = cls_name
+            else:
+                mod_name, obj_name = '.'.join(parts[:-1]), parts[-1]
+
+            ns['fullname'] = name
+            ns['module'] = mod_name
+            ns['objname'] = obj_name
+            ns['name'] = parts[-1]
+
+            ns['objtype'] = doc.objtype
+            ns['underline'] = len(name) * '='
+
+            rendered = template.render(**ns)
+            f.write(rendered)
+        finally:
+            f.close()
+
+    # descend recursively to new files
+    if new_files:
+        generate_autosummary_docs(new_files, output_dir=output_dir,
+                                  suffix=suffix, warn=warn, info=info,
+                                  base_path=base_path, builder=builder,
+                                  template_dir=template_dir)
+
+generate.generate_autosummary_docs = obspy_generate_autosummary_docs
 NumpyDocString.__init__ = obspy__init__
 SphinxDocString.__str__ = obspy__str__
 SphinxDocString._str_param_list = obspy_str_param_list
