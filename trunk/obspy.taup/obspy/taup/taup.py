20b52db40ccc22c6c9717c267c8841012c556bf3
Chris Scheingraber <chris@mytum.de>
2011-06-17 13:18:43 +0000

    - fixing the last commit :/ taup.getTravelTimes now accepts positive and negative depths

--- trunk/obspy.taup/obspy/taup/taup.py
+++ trunk/obspy.taup/obspy/taup/taup.py
@@ -49,7 +49,7 @@ def getTravelTimes(delta, depth, model='iasp91'):
     # Distance in degree
     delta = C.c_float(delta)
     # Depth in kilometer.
-    depth = abs(C.c_float(depth))
+    depth = C.c_float(abs(depth))
 
     # Max number of phases. Hard coded in the Fortran code. Do not change!
     max = 60
