70fff68e946347cd14f1ab2a7b5236e1f81e8475
Simon Kremers <kremers@geophysik.uni-muenchen.de>
2011-07-07 09:30:49 +0000

    adding calibration module to signal, for now only containing relative calibration (relcalstack)
    +tests for relcal module

---
+++ trunk/obspy.signal/obspy/signal/tests/test_relcal.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+The relcal test suite.
+"""
+
+import os
+import unittest
+import numpy as np
+from obspy.core import read
+from obspy.signal.calibration import relcalstack
+
+class RelcalTestCase(unittest.TestCase):
+    """
+    Relcal test case
+    """
+    def test_relcal_sts2_vs_unknown(self):
+        """
+        Test relative calibration of unknow instrument vs STS2 in the same time
+        range. Window length is set to 20 s, smoothing rate to 10.
+        """
+
+        cwd = os.getcwd()
+        st1 = read('%s/data/ref_STS2'%(cwd))
+        st2 = read('%s/data/ref_unknown'%(cwd))
+        calfile = "%s/data/STS2_simp.cal"%(cwd)
+        
+        freq, amp, phase = relcalstack(st1, st2, calfile, 20, smooth=10)
+
+        # read in the reference responses
+        un_resp = np.loadtxt("data/unknown.resp")
+        kn_resp = np.loadtxt("data/STS2.refResp")
+
+        # test if freq, amp and phase match the reference values
+        np.testing.assert_array_almost_equal(freq, un_resp[:,0],
+                                             decimal=4)
+        np.testing.assert_array_almost_equal(freq, kn_resp[:,0],
+                                             decimal=4)
+        np.testing.assert_array_almost_equal(amp, un_resp[:,1],
+                                             decimal=4)
+        np.testing.assert_array_almost_equal(phase, un_resp[:,2],
+                                             decimal=4)
+
+        os.remove("0438.20.resp")
+        os.remove("STS2.refResp")
+
+def suite():
+    return unittest.makeSuite(RelcalTestCase, 'test')
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
+
