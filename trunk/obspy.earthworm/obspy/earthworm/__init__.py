9bd8108489135e6625a67e643c0f9ae0a58cd808
Tobias Megies <megies@geophysik.uni-muenchen.de>
2011-12-20 11:45:20 +0000

    earthworm: adding short example to docs front page

--- trunk/obspy.earthworm/obspy/earthworm/__init__.py
+++ trunk/obspy.earthworm/obspy/earthworm/__init__.py
@@ -8,6 +8,30 @@ obspy.earthworm - Earthworm Wave Server client for ObsPy.
 :license:
     GNU General Public License (GPLv2)
     (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+
+Basic Usage
+-----------
+The example illustrates how to request and plot 30 seconds of all three
+short period channels (``"BH*"``) of station ``"LON"`` of the Pacific
+Northwest Seismic Network (http://www.pnsn.org/).
+
+        .. rubric:: Example
+
+        >>> from obspy.earthworm import Client
+        >>> from obspy.core import UTCDateTime
+        >>> client = Client("hood.ess.washington.edu", 16021)
+        >>> dt = UTCDateTime() - 2000  # now - 2000 seconds
+        >>> st = client.getWaveform('UW', 'LON', '', 'BH*', dt, dt + 30)
+        >>> st.plot()  # doctest: +SKIP
+
+        .. plot::
+
+            from obspy.earthworm import Client
+            from obspy.core import UTCDateTime
+            client = Client("hood.ess.washington.edu", 16021)
+            dt = UTCDateTime() - 2000
+            st = client.getWaveform('UW', 'LON', '', 'BH*', dt, dt + 30)
+            st.plot()
 """
 
 from obspy.core.util import _getVersionString
