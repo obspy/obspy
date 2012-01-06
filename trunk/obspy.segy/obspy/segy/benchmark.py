69ed58660a9a51248b225561ed96d05643274b43
Robert Barsch <barsch@egu.eu>
2012-01-06 09:41:26 +0000

    - improved size of benchmark data files

--- trunk/obspy.segy/obspy/segy/benchmark.py
+++ trunk/obspy.segy/obspy/segy/benchmark.py
@@ -112,15 +112,14 @@ def plotBenchmark(sufiles, normalize='traces', clip_partial_traces=True,
     .. plot::
 
         from obspy.core.util import getExampleFile
-        files = [getExampleFile('seismic01_analytic_vz.su'),
-                 getExampleFile('seismic01_fdmpi_vz.su'),
+        files = [getExampleFile('seismic01_fdmpi_vz.su'),
                  getExampleFile('seismic01_gemini_vz.su'),
                  getExampleFile('seismic01_sofi2D_transformed_vz.su'),
                  getExampleFile('seismic01_specfem_vz.su')]
         from obspy.segy.segy import readSU
         from obspy.segy.benchmark import plotBenchmark
         sufiles = [readSU(file) for file in files]
-        plotBenchmark(sufiles, title="Homogenous halfspace")
+        plotBenchmark(sufiles, title="Homogenous halfspace", xmax=0.14)
     """
     if not sufiles:
         return
