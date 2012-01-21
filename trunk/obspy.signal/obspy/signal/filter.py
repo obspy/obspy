b1a460d8693488161caa953494db0d57a72f1328
Moritz Beyreuther <random.seed@web.de>
2012-01-21 14:25:07 +0000

    improving filter docs

--- trunk/obspy.signal/obspy/signal/filter.py
+++ trunk/obspy.signal/obspy/signal/filter.py
@@ -187,60 +187,63 @@ def envelope(data):
 
 def remezFIR(data, freqmin, freqmax, samp_rate):
     """
+    .. warning:: This is experimental untested code
+
     The minimax optimal bandpass using Remez algorithm. Zerophase bandpass?
 
     Finite impulse response (FIR) filter whose transfer function minimizes
     the maximum error between the desired gain and the realized gain in the
     specified bands using the remez exchange algorithm
-    """
-    # Remez filter description
-    # ========================
-    #
-    # So, let's go over the inputs that you'll have to worry about.
-    # First is numtaps. This parameter will basically determine how good your
-    # filter is and how much processor power it takes up. If you go for some
-    # obscene number of taps (in the thousands) there's other things to worry
-    # about, but with sane numbers (probably below 30-50 in your case) that is
-    # pretty much what it affects (more taps is better, but more expensive
-    #         processing wise). There are other ways to do filters as well
-    # which require less CPU power if you really need it, but I doubt that you
-    # will. Filtering signals basically breaks down to convolution, and apple
-    # has DSP libraries to do lightning fast convolution I'm sure, so don't
-    # worry about this too much. Numtaps is basically equivalent to the number
-    # of terms in the convolution, so a power of 2 is a good idea, 32 is
-    # probably fine.
-    #
-    # bands has literally your list of bands, so you'll break it up into your
-    # low band, your pass band, and your high band. Something like [0, 99, 100,
-    # 999, 1000, 22049] should work, if you want to pass frequencies between
-    # 100-999 Hz (assuming you are sampling at 44.1 kHz).
-    #
-    # desired will just be [0, 1, 0] as you want to drop the high and low
-    # bands, and keep the middle one without modifying the amplitude.
-    #
-    # Also, specify Hz = 44100 (or whatever).
-    #
-    # That should be all you need; run the function and it will spit out a list
-    # of coefficients [c0, ... c(N-1)] where N is your tap count. When you run
-    # this filter, your output signal y[t] will be computed from the input x[t]
-    # like this (t-N means N samples before the current one):
-    #
-    # y[t] = c0*x[t] + c1*x[t-1] + ... + c(N-1)*x[t-(N-1)]
-    #
-    # After playing around with remez for a bit, it looks like numtaps should
-    # be above 100 for a solid filter. See what works out for you. Eventually,
-    # take those coefficients and then move them over and do the convolution
-    # in C or whatever. Also, note the gaps between the bands in the call to
-    # remez. You have to leave some space for the transition in frequency
-    # response to occur, otherwise the call to remez will complain.
-    #
-    # Source:
-    # http://episteme.arstechnica.com/
-    #         eve/forums/a/tpc/f/6330927813/m/175006289731
-    # See also:
-    # http://aspn.activestate.com/ASPN/Mail/Message/scipy-dev/1592174
-    # http://aspn.activestate.com/ASPN/Mail/Message/scipy-dev/1592172
 
+    ::
+        Remez filter description
+        ========================
+        
+        So, let's go over the inputs that you'll have to worry about.
+        First is numtaps. This parameter will basically determine how good your
+        filter is and how much processor power it takes up. If you go for some
+        obscene number of taps (in the thousands) there's other things to worry
+        about, but with sane numbers (probably below 30-50 in your case) that is
+        pretty much what it affects (more taps is better, but more expensive
+                processing wise). There are other ways to do filters as well
+        which require less CPU power if you really need it, but I doubt that you
+        will. Filtering signals basically breaks down to convolution, and apple
+        has DSP libraries to do lightning fast convolution I'm sure, so don't
+        worry about this too much. Numtaps is basically equivalent to the number
+        of terms in the convolution, so a power of 2 is a good idea, 32 is
+        probably fine.
+        
+        bands has literally your list of bands, so you'll break it up into your
+        low band, your pass band, and your high band. Something like [0, 99, 100,
+        999, 1000, 22049] should work, if you want to pass frequencies between
+        100-999 Hz (assuming you are sampling at 44.1 kHz).
+        
+        desired will just be [0, 1, 0] as you want to drop the high and low
+        bands, and keep the middle one without modifying the amplitude.
+        
+        Also, specify Hz = 44100 (or whatever).
+        
+        That should be all you need; run the function and it will spit out a list
+        of coefficients [c0, ... c(N-1)] where N is your tap count. When you run
+        this filter, your output signal y[t] will be computed from the input x[t]
+        like this (t-N means N samples before the current one):
+        
+        y[t] = c0*x[t] + c1*x[t-1] + ... + c(N-1)*x[t-(N-1)]
+        
+        After playing around with remez for a bit, it looks like numtaps should
+        be above 100 for a solid filter. See what works out for you. Eventually,
+        take those coefficients and then move them over and do the convolution
+        in C or whatever. Also, note the gaps between the bands in the call to
+        remez. You have to leave some space for the transition in frequency
+        response to occur, otherwise the call to remez will complain.
+        
+        Source:
+        http://episteme.arstechnica.com/
+                eve/forums/a/tpc/f/6330927813/m/175006289731
+        See also:
+        http://aspn.activestate.com/ASPN/Mail/Message/scipy-dev/1592174
+        http://aspn.activestate.com/ASPN/Mail/Message/scipy-dev/1592172
+    """
     #take 10% of freqmin and freqmax as """corners"""
     flt = freqmin - 0.1 * freqmin
     fut = freqmax + 0.1 * freqmax
@@ -252,6 +255,8 @@ def remezFIR(data, freqmin, freqmax, samp_rate):
 
 def lowpassFIR(data, freq, samp_rate, winlen=2048):
     """
+    .. warning:: This is experimental untested code
+
     FIR-Lowpass Filter
 
     Filter data by passing data only below a certain frequency.
@@ -262,16 +267,19 @@ def lowpassFIR(data, freq, samp_rate, winlen=2048):
     :param winlen: Window length for filter in samples, must be power of 2;
         Default 2048
     :return: Filtered data.
+
+    ::
+        > There is not currently an FIR-filter design program in SciPy.
+        > One should be constructed as it is not hard to implement (of
+        > course making it generic with all the options you might want
+        > would take some time).
+        >
+        > What kind of window are you currently using?
+        > 
+        > For your purposes this is what I would do:
+        > SRC: Travis Oliphant
+        > http://aspn.activestate.com/ASPN/Mail/Message/scipy-user/2009409]
     """
-    # There is not currently an FIR-filter design program in SciPy.  One
-    # should be constructed as it is not hard to implement (of course making
-    # it generic with all the options you might want would take some time).
-    #
-    # What kind of window are you currently using?
-    #
-    # For your purposes this is what I would do:
-    # SRC: Travis Oliphant
-    # http://aspn.activestate.com/ASPN/Mail/Message/scipy-user/2009409]
     #
     #winlen = 2**11 #2**10 = 1024; 2**11 = 2048; 2**12 = 4096
     #give frequency bins in Hz and sample spacing
@@ -288,6 +296,8 @@ def lowpassFIR(data, freq, samp_rate, winlen=2048):
 
 def integerDecimation(data, decimation_factor):
     """
+    .. warning:: This is experimental untested code
+
     Downsampling by applying a simple integer decimation.
 
     Make sure that no signal is present in frequency bands above the new
