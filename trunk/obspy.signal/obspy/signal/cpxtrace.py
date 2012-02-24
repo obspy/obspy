fixed bug in calculation of time derivatives, profiled individual routines, updated corresponding email adredd

trunk/obspy.signal/obspy/signal/cpxtrace.py
@@ -2,7 +2,7 @@
 #--------------------------------------------------------------------
 # Filename: cpxtrace.py
 #   Author: Conny Hammer
-#    Email: conny@geo.uni-potsdam.de
+#    Email: conny.hammer@geo.uni-potsdam.de
 #
 # Copyright (C) 2008-2012 Conny Hammer
 #-------------------------------------------------------------------
@@ -70,7 +70,8 @@ def normEnvelope(data, fs, smoothie, fk):
     :param data: Data to make normalized envelope of.
     :param fs: Sampling frequency.
     :param smoothie: Window length for moving average.
-    :param fk: Filter coefficients for computing time derivative.
+    :param fk: Coefficients for calculating time derivatives
+        (calculated via central difference).
     :return: **Anorm[, dAnorm]** - Normalized envelope of input data, Time
         derivative of normalized envelope (windowed only).
     """
@@ -80,15 +81,19 @@ def normEnvelope(data, fs, smoothie, fk):
         i = 0
         Anorm = np.zeros(x[1].shape[0], dtype='float64')
         for row in x[1]:
-            A_win_smooth = util.smooth(row, smoothie)
+            A_win_smooth = util.smooth(row, int(np.floor(len(row)/3)))
             # Differentiation of original signal, dA/dt
             #A_win_add = append(append([row[0]]*(size(fk)/2),row),
             #                      [row[size(row)-1]]*(size(fk)/2))
             # Better, because faster, calculation of A_win_add
-            A_win_add = np.hstack(([row[0]] * (size(fk) // 2), row,
-                                  [row[size(row) - 1]] * (size(fk) // 2)))
+            A_win_add = np.hstack(([A_win_smooth[0]] * (size(fk) // 2), 
+                                   A_win_smooth, 
+                                   [A_win_smooth[size(A_win_smooth) - 1]] * \
+                                   (size(fk) // 2)))
             t = signal.lfilter(fk, 1, A_win_add)
-            t = t[size(fk) // 2:(size(t) - size(fk) // 2)]
+            #t = t[size(fk) // 2:(size(t) - size(fk) // 2)]
+            # correct start and end values of time derivative
+            t = t[size(fk)-1:size(t)]
             A_win_smooth[A_win_smooth < 1] = 1
             # (dA/dt) / 2*PI*smooth(A)*fs/2
             t_ = t / (2. * pi * (A_win_smooth) * (fs / 2.0))
@@ -98,19 +103,30 @@ def normEnvelope(data, fs, smoothie, fk):
             Anorm[i] = ((np.exp(np.mean(t_))) - 1) * 100
             i = i + 1
         #Anorm = util.smooth(Anorm,smoothie)
-        Anorm_add = np.append(np.append([Anorm[0]] * (size(fk) // 2), Anorm),
-                              [Anorm[size(Anorm) - 1]] * (size(fk) // 2))
+        #Anorm_add = np.append(np.append([Anorm[0]] * (size(fk) // 2), Anorm),
+        #                      [Anorm[size(Anorm) - 1]] * (size(fk) // 2))
+        # faster alternative to calculate Anorm_add
+        Anorm_add = np.hstack(([Anorm[0]] * (np.size(fk) // 2), Anorm, \
+                  [Anorm[np.size(Anorm) - 1]] * (np.size(fk) // 2))) 
         dAnorm = signal.lfilter(fk, 1, Anorm_add)
-        dAnorm = dAnorm[size(fk) // 2:(size(dAnorm) - size(fk) // 2)]
+        # correct start and end values of time derivative
+        dAnorm = dAnorm[size(fk)-1:size(dAnorm)]
+        #dAnorm = dAnorm[size(fk) // 2:(size(dAnorm) - size(fk) // 2)]
         return Anorm, dAnorm
     else:
         Anorm = np.zeros(1, dtype='float64')
         A_win_smooth = util.smooth(x[1], smoothie)
         # Differentiation of original signal, dA/dt
-        A_win_add = np.append(np.append([x[1][0]] * (size(fk) // 2), x[1]),
-                              [x[1][size(x[1]) - 1]] * (size(fk) // 2))
+        #A_win_add = np.append(np.append([x[1][0]] * (size(fk) // 2), x[1]),
+        #                      [x[1][size(x[1]) - 1]] * (size(fk) // 2))
+        # Better, because faster, calculation of A_win_add
+        A_win_add = np.hstack(([A_win_smooth[0]] * (size(fk) // 2), \
+                  A_win_smooth, [A_win_smooth[size(A_win_smooth) - 1]] * \
+                  (size(fk) // 2)))
         t = signal.lfilter(fk, 1, A_win_add)
-        t = t[size(fk) // 2:(size(t) - size(fk) // 2)]
+        #t = t[size(fk) // 2:(size(t) - size(fk) // 2)]
+        # correct start and end values of time derivative
+        t = t[size(fk)-1:size(t)]
         A_win_smooth[A_win_smooth < 1] = 1
         # (dA/dt) / 2*PI*smooth(A)*fs/2
         t_ = t / (2. * pi * (A_win_smooth) * (fs / 2.0))
@@ -134,7 +150,8 @@ def centroid(data, fk):
 
     :type data: :class:`~numpy.ndarray`
     :param data: Data to determine centroid time of.
-    :param fk: Filter coefficients for computing time derivative.
+    :param fk: Coefficients for calculating time derivatives
+        (calculated via central difference).
     :return: **centroid[, dcentroid]** - Centroid time input data, Time
         derivative of centroid time (windowed only).
     """
@@ -151,13 +168,18 @@ def centroid(data, fk):
                 if (t >= half):
                     frac = (half - (t - sum(row[0:k - 1]))) / \
                         (t - (t - sum(row[0:k - 1])))
-                    centroid[i] = (float(k) + float(frac)) / float(size(row))
+                    centroid[i] = (float(k-1) + float(frac)) / float(size(row))
                     break
             i = i + 1
-        centroid_add = np.append(np.append([centroid[0]] * (size(fk) // 2), \
-            centroid), [centroid[size(centroid) - 1]] * (size(fk) // 2))
+        #centroid_add = np.append(np.append([centroid[0]] * (size(fk) // 2), \
+        #    centroid), [centroid[size(centroid) - 1]] * (size(fk) // 2))
+        centroid_add = np.hstack(([centroid[0]] * (np.size(fk) // 2), \
+                  centroid, [centroid[np.size(centroid) - 1]] * \
+                  (np.size(fk) // 2)))
         dcentroid = signal.lfilter(fk, 1, centroid_add)
-        dcentroid = dcentroid[size(fk) // 2:(size(dcentroid) - size(fk) // 2)]
+        #dcentroid = dcentroid[size(fk) // 2:(size(dcentroid) - size(fk) // 2)]
+        # correct start and end values of time derivative
+        dcentroid = dcentroid[ size(fk)-1 : size(dcentroid)]
         return centroid, dcentroid
     else:
         centroid = np.zeros(1, dtype='float64')
@@ -185,7 +207,8 @@ def instFreq(data, fs, fk):
     :type data: :class:`~numpy.ndarray`
     :param data: Data to determine instantaneous frequency of.
     :param fs: Sampling frequency.
-    :param fk: Filter coefficients for computing time derivative.
+    :param fk: Coefficients for calculating time derivatives
+        (calculated via central difference).
     :return: **omega[, domega]** - Instantaneous frequency of input data, Time
         derivative of instantaneous frequency (windowed only).
     """
@@ -196,35 +219,60 @@ def instFreq(data, fs, fk):
         for row in x[0]:
             f = np.real(row)
             h = np.imag(row)
-            f_add = np.append(np.append([f[0]] * (size(fk) // 2), f),
-                                  [f[size(f) - 1]] * (size(fk) // 2))
+            #f_add = np.append(np.append([f[0]] * (size(fk) // 2), f),
+            #                      [f[size(f) - 1]] * (size(fk) // 2))
+            # faster alternative to calculate f_add
+            f_add = np.hstack(([f[0]] * (np.size(fk) // 2), f, \
+                  [f[np.size(f) - 1]] * (np.size(fk) // 2))) 
             fd = signal.lfilter(fk, 1, f_add)
-            fd = fd[size(fk) // 2:(size(fd) - size(fk) // 2)]
-            h_add = np.append(np.append([h[0]] * (size(fk) // 2), h),
-                                  [h[size(h) - 1]] * (size(fk) // 2))
+            #fd = fd[size(fk) // 2:(size(fd) - size(fk) // 2)]
+            # correct start and end values of time derivative
+            fd = fd[size(fk)-1 :size(fd)]
+            #h_add = np.append(np.append([h[0]] * (size(fk) // 2), h),
+            #                      [h[size(h) - 1]] * (size(fk) // 2))
+            # faster alternative to calculate h_add
+            h_add = np.hstack(([h[0]] * (np.size(fk) // 2), h, \
+                  [h[np.size(h) - 1]] * (np.size(fk) // 2)))
             hd = signal.lfilter(fk, 1, h_add)
-            hd = hd[size(fk) // 2:(size(hd) - size(fk) // 2)]
+            #hd = hd[size(fk) // 2:(size(hd) - size(fk) // 2)]
+            # correct start and end values of time derivative
+            hd = hd[size(fk)-1 :size(hd)]
             omega_win = abs(((f * hd - fd * h) / (f * f + h * h)) * \
                              fs / 2 / pi)
             omega[i] = np.median(omega_win)
             i = i + 1
-        omega_add = np.append(np.append([omega[0]] * (size(fk) // 2), omega),
-                              [omega[size(omega) - 1]] * (size(fk) // 2))
+        #omega_add = np.append(np.append([omega[0]] * (size(fk) // 2), omega),
+        #                      [omega[size(omega) - 1]] * (size(fk) // 2))
+        # faster alternative to calculate omega_add
+        omega_add = np.hstack(([omega[0]] * (np.size(fk) // 2), omega, \
+                  [omega[np.size(omega) - 1]] * (np.size(fk) // 2)))
         domega = signal.lfilter(fk, 1, omega_add)
-        domega = domega[size(fk) // 2:(size(domega) - size(fk) // 2)]
+        #domega = domega[size(fk) // 2:(size(domega) - size(fk) // 2)]
+        # correct start and end values of time derivative
+        domega = domega[ size(fk)-1 : size(domega)]
         return omega, domega
     else:
         omega = np.zeros(size(x[0]), dtype='float64')
         f = np.real(x[0])
         h = np.imag(x[0])
-        f_add = np.append(np.append([f[0]] * (size(fk) // 2), f),
-                              [f[size(f) - 1]] * (size(fk) // 2))
+        #f_add = np.append(np.append([f[0]] * (size(fk) // 2), f),
+        #                      [f[size(f) - 1]] * (size(fk) // 2))
+        # faster alternative to calculate f_add
+        f_add = np.hstack(([f[0]] * (np.size(fk) // 2), f, \
+                [f[np.size(f) - 1]] * (np.size(fk) // 2)))
         fd = signal.lfilter(fk, 1, f_add)
-        fd = fd[size(fk) // 2:(size(fd) - size(fk) // 2)]
-        h_add = np.append(np.append([h[0]] * (size(fk) // 2), h),
-                              [h[size(h) - 1]] * (size(fk) // 2))
+        #fd = fd[size(fk) // 2:(size(fd) - size(fk) // 2)]
+        # correct start and end values of time derivative
+        fd = fd[size(fk)-1 :size(fd)]
+        #h_add = np.append(np.append([h[0]] * (size(fk) // 2), h),
+        #                      [h[size(h) - 1]] * (size(fk) // 2))
+        # faster alternative to calculate h_add
+        h_add = np.hstack(([h[0]] * (np.size(fk) // 2), h, \
+                  [h[np.size(h) - 1]] * (np.size(fk) // 2)))
         hd = signal.lfilter(fk, 1, h_add)
-        hd = hd[size(fk) // 2:(size(hd) - size(fk) // 2)]
+        #hd = hd[size(fk) // 2:(size(hd) - size(fk) // 2)]
+        # correct start and end values of time derivative
+        hd = hd[size(fk)-1 :size(hd)]
         omega = abs(((f * hd - fd * h) / (f * f + h * h)) * fs / 2 / pi)
         return omega
 
@@ -249,23 +297,38 @@ def instBwith(data, fs, fk):
         sigma = np.zeros(x[1].shape[0], dtype='float64')
         i = 0
         for row in x[1]:
-            A_win_add = np.append(np.append([row[0]] * (size(fk) // 2), row),
-                                  [row[size(row) - 1]] * (size(fk) // 2))
+            #A_win_add = np.append(np.append([row[0]] * (size(fk) // 2), row),
+            #                      [row[size(row) - 1]] * (size(fk) // 2))
+            # faster alternative to calculate A_win_add
+            A_win_add = np.hstack(([row[0]] * (np.size(fk) // 2), row, \
+                  [row[np.size(row) - 1]] * (np.size(fk) // 2)))
             t = signal.lfilter(fk, 1, A_win_add)
-            t = t[size(fk) // 2:(size(t) - size(fk) // 2)]
+            #t = t[size(fk) // 2:(size(t) - size(fk) // 2)]
+            # correct start and end values
+            t = t[size(fk)-1:size(t)]
             sigma_win = abs((t * fs) / (row * 2 * pi))
             sigma[i] = np.median(sigma_win)
             i = i + 1
-        sigma_add = np.append(np.append([sigma[0]] * (size(fk) // 2), sigma),
-                          [sigma[size(sigma) - 1]] * (size(fk) // 2))
+        #sigma_add = np.append(np.append([sigma[0]] * (size(fk) // 2), sigma),
+        #                  [sigma[size(sigma) - 1]] * (size(fk) // 2))
+        # faster alternative to calculate sigma_add
+        sigma_add = np.hstack(([sigma[0]] * (np.size(fk) // 2), sigma, \
+                  [sigma[np.size(sigma) - 1]] * (np.size(fk) // 2)))
         dsigma = signal.lfilter(fk, 1, sigma_add)
-        dsigma = dsigma[size(fk) // 2:(size(dsigma) - size(fk) // 2)]
+        #dsigma = dsigma[size(fk) // 2:(size(dsigma) - size(fk) // 2)]
+        # correct start and end values
+        dsigma = dsigma[size(fk)-1:size(dsigma)]
         return sigma, dsigma
     else:
         sigma = np.zeros(size(x[0]), dtype='float64')
-        A_win_add = np.append(np.append([x[1][0]] * (size(fk) // 2), x[1]),
-                              [x[1][size(x[1]) - 1]] * (size(fk) // 2))
+        #A_win_add = np.append(np.append([x[1][0]] * (size(fk) // 2), x[1]),
+        #                      [x[1][size(x[1]) - 1]] * (size(fk) // 2))
+        # faster alternative to calculate A_win_add
+        A_win_add = np.hstack(([row[0]] * (np.size(fk) // 2), row, \
+                  [row[np.size(row) - 1]] * (np.size(fk) // 2)))
         t = signal.lfilter(fk, 1, A_win_add)
-        t = t[size(fk) // 2:(size(t) - size(fk) // 2)]
+        #t = t[size(fk) // 2:(size(t) - size(fk) // 2)]
+        # correct start and end values
+        t = t[size(fk)-1:size(t)] 
         sigma = abs((t * fs) / (x[1] * 2 * pi))
         return sigma

trunk/obspy.signal/obspy/signal/freqattributes.py
@@ -2,7 +2,7 @@
 #------------------------------------------------------------------
 # Filename: freqattributes.py
 #   Author: Conny Hammer
-#    Email: conny@geo.uni-potsdam.de
+#    Email: conny.hammer@geo.uni-potsdam.de
 #
 # Copyright (C) 2008-2012 Conny Hammer
 #------------------------------------------------------------------
@@ -101,7 +101,8 @@ def cfrequency(data, fs, smoothie, fk):
     :param data: Data to estimate central frequency from.
     :param fs: Sampling frequency in Hz.
     :param smoothie: Factor for smoothing the result.
-    :param fk: Filter coefficients for computing time derivative.
+    :param fk: Coefficients for calculating time derivatives
+        (calculated via central difference).
     :return: **cfreq[, dcfreq]** - Central frequency, Time derivative of center
         frequency (windowed only).
     """
@@ -118,11 +119,16 @@ def cfrequency(data, fs, smoothie, fk):
             cfreq[i] = np.sqrt(np.sum(pow(freqaxis, 2) * Px) / (sum(Px)))
             i = i + 1
         cfreq = util.smooth(cfreq, smoothie)
-        cfreq_add = \
-                np.append(np.append([cfreq[0]] * (np.size(fk) // 2), cfreq),
-                [cfreq[np.size(cfreq) - 1]] * (np.size(fk) // 2))
+        #cfreq_add = \
+        #        np.append(np.append([cfreq[0]] * (np.size(fk) // 2), cfreq),
+        #        [cfreq[np.size(cfreq) - 1]] * (np.size(fk) // 2))
+        # faster alternative
+        cfreq_add = np.hstack(([cfreq[0]] * (np.size(fk) // 2), cfreq, \
+                  [cfreq[np.size(cfreq) - 1]] * (np.size(fk) // 2)))
         dcfreq = signal.lfilter(fk, 1, cfreq_add)
-        dcfreq = dcfreq[np.size(fk) // 2:(np.size(dcfreq) - np.size(fk) // 2)]
+        #dcfreq = dcfreq[np.size(fk) // 2:(np.size(dcfreq) - np.size(fk) // 2)]
+        # correct start and end values of time derivative
+        dcfreq = dcfreq[np.size(fk)-1:np.size(dcfreq)]        
         return cfreq, dcfreq
     else:
         Px_wm = welch(data, np.hamming(len(data)), util.nextpow2(len(data)))
@@ -146,7 +152,8 @@ def bwith(data, fs, smoothie, fk):
     :param data: Data to make envelope of.
     :param fs: Sampling frequency in Hz.
     :param smoothie: Factor for smoothing the result.
-    :param fk: Filter coefficients for computing time derivative.
+    :param fk: Coefficients for calculating time derivatives
+        (calculated via central difference).
     :return: **bwith[, dbwithd]** - Bandwidth, Time derivative of predominant
         period (windowed only).
     """
@@ -163,11 +170,16 @@ def bwith(data, fs, smoothie, fk):
             [mdist_ind, _mindist] = min(enumerate(minfc), key=itemgetter(1))
             bwith[i] = freqaxis[mdist_ind]
             i = i + 1
-        bwith_add = \
-                np.append(np.append([bwith[0]] * (np.size(fk) // 2), bwith),
-                [bwith[np.size(bwith) - 1]] * (np.size(fk) // 2))
+        #bwith_add = \
+        #        np.append(np.append([bwith[0]] * (np.size(fk) // 2), bwith),
+        #        [bwith[np.size(bwith) - 1]] * (np.size(fk) // 2))
+        # faster alternative
+        bwith_add = np.hstack(([bwith[0]] * (np.size(fk) // 2), bwith, \
+                [bwith[np.size(bwith) - 1]] * (np.size(fk) // 2)))
         dbwith = signal.lfilter(fk, 1, bwith_add)
-        dbwith = dbwith[np.size(fk) // 2:(np.size(dbwith) - np.size(fk) // 2)]
+        #dbwith = dbwith[np.size(fk) // 2:(np.size(dbwith) - np.size(fk) // 2)]
+        # correct start and end values of time derivative
+        dbwith = dbwith[np.size(fk)-1:]
         bwith = util.smooth(bwith, smoothie)
         dbwith = util.smooth(dbwith, smoothie)
         return bwith, dbwith
@@ -192,7 +204,8 @@ def domperiod(data, fs, smoothie, fk):
     :param data: Data to determine predominant period of.
     :param fs: Sampling frequency in Hz.
     :param smoothie: Factor for smoothing the result.
-    :param fk: Filter coefficients for computing time derivative.
+    :param fk: Coefficients for calculating time derivatives
+        (calculated via central difference).
     :return: **dperiod[, ddperiod]** - Predominant period, Time derivative of
         predominant period (windowed only).
     """
@@ -210,11 +223,16 @@ def domperiod(data, fs, smoothie, fk):
             [mdist_ind, _mindist] = max(enumerate(abs(row)), key=itemgetter(1))
             dperiod[i] = freqaxis[mdist_ind]
             i = i + 1
-        dperiod_add = np.append(np.append([dperiod[0]] * (np.size(fk) // 2), \
-            dperiod), [dperiod[np.size(dperiod) - 1]] * (np.size(fk) // 2))
+        #dperiod_add = np.append(np.append([dperiod[0]] * (np.size(fk) // 2), \
+        #    dperiod), [dperiod[np.size(dperiod) - 1]] * (np.size(fk) // 2))
+        # faster alternative
+        dperiod_add = np.hstack(([dperiod[0]] * (np.size(fk) // 2), dperiod, \
+                [dperiod[np.size(dperiod) - 1]] * (np.size(fk) // 2)))
         ddperiod = signal.lfilter(fk, 1, dperiod_add)
-        ddperiod = ddperiod[np.size(fk) / \
-            2:(np.size(ddperiod) - np.size(fk) // 2)]
+        #ddperiod = ddperiod[np.size(fk) / \
+        #    2:(np.size(ddperiod) - np.size(fk) // 2)]
+        # correct start and end values of time derivative
+        ddperiod = ddperiod[np.size(fk)-1:]
         dperiod = util.smooth(dperiod, smoothie)
         ddperiod = util.smooth(ddperiod, smoothie)
         return dperiod, ddperiod

trunk/obspy.signal/obspy/signal/hoctavbands.py
@@ -2,7 +2,7 @@
 #-------------------------------------------------------------------
 # Filename: hoctavbands.py
 #   Author: Conny Hammer
-#    Email: conny@geo.uni-potsdam.de
+#    Email: conny.hammer@geo.uni-potsdam.de
 #
 # Copyright (C) 2008-2012 Conny Hammer
 #-------------------------------------------------------------------
@@ -57,13 +57,13 @@ def sonogram(data, fs, fc1, nofb, no_win):
     z = np.zeros([len(c[:, 1]), nofb])
     z_tot = np.zeros(len(c[:, 1]))
     hob = np.zeros([no_win, nofb])
-    for k in range(no_win):
-        for j in range(len(c[1, :])):
+    for k in xrange(no_win):
+        for j in xrange(len(c[1, :])):
             z_tot[k] = z_tot[k] + pow(np.abs(c[k, j]), 2)
-        for i in range(nofb):
+        for i in xrange(nofb):
             start = int(round(fmin[i] * nfft * 1. / float(fs), 0))
             end = int(round(fmax[i] * nfft * 1. / float(fs), 0)) + 1
-            for j in range(start, end):
+            for j in xrange(start, end):
                 z[k, i] = z[k, i] + pow(np.abs(c[k, j - 1]), 2)
             hob[k, i] = np.log(z[k, i] / z_tot[k])
     return hob

trunk/obspy.signal/obspy/signal/util.py
@@ -157,29 +157,41 @@ def smooth(x, smoothie):
     """
     size_x = np.size(x)
     suma = np.zeros(size_x)
-    if smoothie > 1:
+    if smoothie > 0:
         if (len(x) > 1 and len(x) < size_x):
             #out_add = append(append([x[0,:]]*smoothie,x,axis=0),
             #                     [x[(len(x)-1),:]]*smoothie,axis=0)
-            out_add = (np.append([x[0, :]] * int(smoothie), x, axis=0))
+            #out_add = (np.append([x[0, :]]*int(smoothie), x, axis=0))
+            out_add = np.vstack(( [x[0, :]] * int(smoothie), x, \
+                     [x[(len(x)-1), :]] * int(smoothie) ))
             help = np.transpose(out_add)
-            out = signal.lfilter(np.ones(smoothie) / smoothie, 1, help)
+            #out = signal.lfilter(np.ones(smoothie) / smoothie, 1, help)
+            out = signal.lfilter(np.hstack((np.ones(smoothie) / (2*smoothie), \
+                     0,np.ones(smoothie) / (2*smoothie))), 1, help)
             out = np.transpose(out)
-            out = out[smoothie:len(out), :]
+            #out = out[smoothie:len(out), :]
+            out = out[2*smoothie:len(out), :]
             #out = filter(ones(1,smoothie)/smoothie,1,out_add)
             #out[1:smoothie,:] = []
         else:
-            out_add = np.append(np.append([x[0]] * smoothie, x),
-                               [x[size_x - 1]] * smoothie)
-            for i in xrange(smoothie, len(x) + smoothie):
-                sum = 0
-                for k in xrange(-smoothie, smoothie):
-                    sum = sum + out_add[i + k]
-                    suma[i - smoothie] = float(sum) / (2 * smoothie)
-                    out = suma
-                    out[0:smoothie] = out[smoothie]
-                    out[size_x - 1 - smoothie:size_x] = \
-                        out[size_x - 1 - smoothie]
+            #out_add = np.append(np.append([x[0]] * smoothie, x),
+            #                   [x[size_x - 1]] * smoothie)
+            out_add = np.hstack(( [x[0]] * int(smoothie), x, \
+                     [x[(len(x)-1)]] * int(smoothie) ))
+            out = signal.lfilter(np.hstack((np.ones(smoothie) / (2*smoothie), \
+                     0,np.ones(smoothie) / (2*smoothie))), 1, out_add)
+            out = out[2*smoothie:len(out)]
+            out[0:smoothie] = out[smoothie]
+            out[len(out)-smoothie:len(out)] = out[len(out)-smoothie-1]
+            #for i in xrange(smoothie, len(x) + smoothie):
+            #    sum = 0
+            #    for k in xrange(-smoothie, smoothie):
+            #        sum = sum + out_add[i + k]
+            #        suma[i - smoothie] = float(sum) / (2 * smoothie)
+            #        out = suma
+            #        out[0:smoothie] = out[smoothie]
+            #        out[size_x - 1 - smoothie:size_x] = \
+            #            out[size_x - 1 - smoothie]
     else:
         out = x
     return out