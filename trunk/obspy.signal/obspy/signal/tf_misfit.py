1cf57888490f2f553a2034fa1a24fb0c4e9ce0e6
Martin van Driel <vandriel@erdw.ethz.ch>
2012-03-14 11:05:19 +0000

    - local docbuilding helps to write running doctests/plots ;)

--- trunk/obspy.signal/obspy/signal/tf_misfit.py
+++ trunk/obspy.signal/obspy/signal/tf_misfit.py
@@ -873,6 +873,7 @@ def plotTfMisfits(st1, st2, dt=0.01, fmin=1., fmax=10., nf=100, w0=6,
     .. rubric:: Example
 
     For a signal with pure phase error
+
     .. seealso:: [Kristekova2006]_, Fig.(4)
 
     >>> import numpy as np
@@ -886,53 +887,42 @@ def plotTfMisfits(st1, st2, dt=0.01, fmin=1., fmax=10., nf=100, w0=6,
     >>> f1 = 2.
     >>> phi1 = 0.
     >>> phase_shift = 0.1
-    >>> H = lambda t: (np.sign(t) + 1)/ 2
-    >>> S1 = lambda t: (A1 * (t - t1) * np.exp(-2*(t - t1)) *
-    ...                np.cos(2. * np.pi * f1 * (t - t1) + phi1 * np.pi) *
-    ...                H(t - t1))
+    >>> H1 = (np.sign(t - t1) + 1)/ 2
+    >>> st1 = (A1 * (t - t1) * np.exp(-2*(t - t1)) *
+    ...       np.cos(2. * np.pi * f1 * (t - t1) + phi1 * np.pi) * H1)
+    >>> # Reference signal
+    >>> st2 = st1.copy()
     >>> # Distorted signal:
     >>> # generate analytical signal (hilbert transform) and add phase shift
-    >>> st1 = S1(t)
     >>> st1 = hilbert(st1)
     >>> st1 = np.real(np.abs(st1) * np.exp((np.angle(st1) + 
     ...                                     phase_shift * np.pi) * 1j))
-    >>> # Reference signal
-    >>> st2 = S1(t)
     >>> plotTfMisfits(st1, st2, dt=dt, fmin=1., fmax=10.) # doctest: +SKIP
 
     .. plot::
 
         import numpy as np
-        from obspy.signal.tf_misfit import plotTfMisfits
         from scipy.signal import hilbert
-
+        from obspy.signal.tf_misfit import plotTfMisfits
         tmax = 6.
         dt = 0.01
         npts = int(tmax / dt + 1)
         t = np.linspace(0., tmax, npts)
-
         A1 = 4.
         t1 = 2.
         f1 = 2.
         phi1 = 0.
-
         phase_shift = 0.1
-
-        H = lambda t: (np.sign(t) + 1)/ 2
-        S1 = lambda t: (A1 * (t - t1) * np.exp(-2*(t - t1)) *
-                        np.cos(2. * np.pi * f1 * (t - t1) + phi1 * np.pi) *
-                        H(t - t1))
-
+        H1 = (np.sign(t - t1) + 1)/ 2
+        st1 = (A1 * (t - t1) * np.exp(-2*(t - t1)) *
+              np.cos(2. * np.pi * f1 * (t - t1) + phi1 * np.pi) * H1)
+        # Reference signal
+        st2 = st1.copy()
         # Distorted signal:
         # generate analytical signal (hilbert transform) and add phase shift
-        st1 = S1(t)
         st1 = hilbert(st1)
         st1 = np.real(np.abs(st1) * np.exp((np.angle(st1) + 
                                             phase_shift * np.pi) * 1j))
-
-        # Reference signal
-        st2 = S1(t)
-
         plotTfMisfits(st1, st2, dt=dt, fmin=1., fmax=10.)
     """
     npts = st1.shape[-1]
@@ -1159,6 +1149,52 @@ def plotTfGofs(st1, st2, dt=0.01, fmin=1., fmax=10., nf=100, w0=6,
 
     :return: If show is False, returns a maplotlib.pyplot.figure object (single
         component data) or a list of figure objects (multi component data)
+    
+    .. rubric:: Example
+
+    For a signal with pure amplitude error
+
+    >>> import numpy as np
+    >>> from scipy.signal import hilbert
+    >>> tmax = 6.
+    >>> dt = 0.01
+    >>> npts = int(tmax / dt + 1)
+    >>> t = np.linspace(0., tmax, npts)
+    >>> A1 = 4.
+    >>> t1 = 2.
+    >>> f1 = 2.
+    >>> phi1 = 0.
+    >>> phase_shift = 0.1
+    >>> H1 = (np.sign(t - t1) + 1)/ 2
+    >>> st1 = (A1 * (t - t1) * np.exp(-2*(t - t1)) *
+    ...       np.cos(2. * np.pi * f1 * (t - t1) + phi1 * np.pi) * H1)
+    >>> # Reference signal
+    >>> st2 = st1.copy()
+    >>> # Distorted signal:
+    >>> st1 = st1 * 3.
+    >>> plotTfGofs(st1, st2, dt=dt, fmin=1., fmax=10.) # doctest: +SKIP
+
+    .. plot::
+
+        import numpy as np
+        from obspy.signal.tf_misfit import plotTfGofs
+        tmax = 6.
+        dt = 0.01
+        npts = int(tmax / dt + 1)
+        t = np.linspace(0., tmax, npts)
+        A1 = 4.
+        t1 = 2.
+        f1 = 2.
+        phi1 = 0.
+        phase_shift = 0.1
+        H1 = (np.sign(t - t1) + 1)/ 2
+        st1 = (A1 * (t - t1) * np.exp(-2*(t - t1)) *
+              np.cos(2. * np.pi * f1 * (t - t1) + phi1 * np.pi) * H1)
+        # Reference signal
+        st2 = st1.copy()
+        # Distorted signal:
+        st1 = st1 * 3.
+        plotTfGofs(st1, st2, dt=dt, fmin=1., fmax=10.)
     """
     npts = st1.shape[-1]
     tmax = (npts - 1) * dt
