617931ca653631f03db104a6f2929af852f2a449
Robert Barsch <barsch@egu.eu>
2012-03-20 16:32:30 +0000

    - updated develop.bat - deleting testing.bat as we have "obspy-runtests"  for "end users", imho devs should call/debug tests scripts directly or use "python setup.py test" on single failing modules

--- trunk/misc/scripts/develop.bat
+++ trunk/misc/scripts/develop.bat
@@ -1,31 +1,71 @@
 @echo off
 SETLOCAL
 
+IF [%1]==[] (
+    set MODE=MS Visual C++ 2008 and MinGW
+) else (
+    set MODE=MinGW only
+)
+echo Build Mode: %MODE%
+
+if defined PYTHON (
+    echo PYTHON=%PYTHON%
+) else (
+    set PYTHON=python.exe
+    echo PYTHON=python.exe
+)
+
+if defined BUILD_FLAGS (
+    set BUILD_FLAGS=-q build -c mingw32
+    echo BUILD_FLAGS=%BUILD_FLAGS%
+) else (
+    set BUILD_FLAGS=-q
+    echo BUILD_FLAGS=-q
+)
+
+REM add build mode
+IF [%1]==[] (
+    set BUILD_FLAGS=%BUILD_FLAGS% build
+) else (
+    set BUILD_FLAGS=%BUILD_FLAGS% build -c mingw32
+)
+
 REM
 REM Run vcvars32.bat in VS2008 command line for 32 bit!
 REM Run vcvars64.bat in VS2008 command line for 64 bit!
 REM
 
+if defined VCINSTALLDIR (
+    echo Microsoft Visual C++ 2008 found
+    echo * VSINSTALLDIR="%VSINSTALLDIR%"
+    echo * VCINSTALLDIR="%VCINSTALLDIR%"
+) else (
+    echo MS Visual C++ 2008 not found
+)
+
 REM
-REM Set your correct python interpreter here or just use virtualenv.
-set PYTHON=python
+REM Add mingw to path in order to find those executables
 REM
+
+echo Check for MinGW executables
+<nul (set/p z="* gcc: ")
+WHERE /F gcc.exe
+<nul (set/p z="* gfortran (32bit): ")
+WHERE /F gfortran.exe
+<nul (set/p z="* gfortran (64bit): ")
+WHERE /F x86_64-w64-mingw32-gfortran.exe
+
+echo.
 
 cd ..\..
 
-echo The Python interpreter is set to: %PYTHON%
-echo.
 
 FOR /f %%M IN ('dir /B /AD obspy.*') DO (
 cd %%M
 echo === %%M ===
-%PYTHON% setup.py -q clean --all >NUL 2>NUL
-IF [%1]==[] (
-  %PYTHON% setup.py -q build develop
-) ELSE (
-  %PYTHON% setup.py -q build -c mingw32 develop
-)
-%PYTHON% setup.py -q clean --all >NUL 2>NUL
+%PYTHON% setup.py %BUILD_FLAGS% clean --all >NUL 2>NUL
+%PYTHON% setup.py %BUILD_FLAGS% develop
+%PYTHON% setup.py %BUILD_FLAGS% clean --all >NUL 2>NUL
 echo OK
 cd ..
 )
