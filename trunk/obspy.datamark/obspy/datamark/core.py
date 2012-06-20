e9f56a2d3831d90a1053eb94adcd382fdfb6a6d1
Tobias Megies <megies@geophysik.uni-muenchen.de>
2012-06-20 12:45:35 +0000

    datamark: using with statement and warnings

--- trunk/obspy.datamark/obspy/datamark/core.py
+++ trunk/obspy.datamark/obspy/datamark/core.py
@@ -4,6 +4,8 @@ DATAMARK bindings to ObsPy core module.
 Thomas Lecocq based on others (refs will be included in the release version)
 """
 
+from __future__ import with_statement
+import warnings
 from obspy.core import Trace, UTCDateTime, Stream
 import numpy as np
 
@@ -38,76 +40,76 @@ def readDATAMARK(filename, century="20", **kwargs):  # @UnusedVariable
     output = {}
 
     # read datamark file
-    fpin = open(filename, "rb")
+    with open(filename, "rb") as fpin:
+        fpin.seek(0, 2)
+        sz = fpin.tell()
+        fpin.seek(0)
+        leng = 0
+        status0 = 0
+        start = 0
+        while leng < sz:
+            pklen = fpin.read(4)
+            if len(pklen) == 0:
+                break  # EOF
+            leng = 4
+            truelen = np.fromstring(pklen, '>i')[0]  # equiv to Str4Int
+            buff = fpin.read(6)
+            leng += 6
 
-    fpin.seek(0, 2)
-    sz = fpin.tell()
-    fpin.seek(0)
-    leng = 0
-    status0 = 0
-    start = 0
-    while leng < sz:
-        pklen = fpin.read(4)
-        if len(pklen) == 0:
-            break  # EOF
-        leng = 4
-        truelen = np.fromstring(pklen, '>i')[0]  # equiv to Str4Int
-        buff = fpin.read(6)
-        leng += 6
+            yy = "%s%x" % (century, np.fromstring(buff[0], dtype='b')[0])
+            mm = "%x" % np.fromstring(buff[1], dtype='b')[0]
+            dd = "%x" % np.fromstring(buff[2], dtype='b')[0]
+            hh = "%x" % np.fromstring(buff[3], dtype='b')[0]
+            mi = "%x" % np.fromstring(buff[4], dtype='b')[0]
+            sec = "%x" % np.fromstring(buff[5], dtype='b')[0]
 
-        yy = "%s%x" % (century, np.fromstring(buff[0], dtype='b')[0])
-        mm = "%x" % np.fromstring(buff[1], dtype='b')[0]
-        dd = "%x" % np.fromstring(buff[2], dtype='b')[0]
-        hh = "%x" % np.fromstring(buff[3], dtype='b')[0]
-        mi = "%x" % np.fromstring(buff[4], dtype='b')[0]
-        sec = "%x" % np.fromstring(buff[5], dtype='b')[0]
+            date = UTCDateTime(int(yy), int(mm), int(dd), int(hh), int(mi),
+                               int(sec))
+            if start == 0:
+                start = date
+            if status0 == 0:
+                sdata = None
+            while leng < truelen:
+                buff = fpin.read(4)
+                leng += 4
+                _flag = np.fromstring(buff[0], dtype='b')[0]
+                chanum = np.fromstring(buff[1], dtype='b')[0]
+                datawide = np.fromstring(buff[2], dtype='b')[0] >> 4
+                srate = np.fromstring(buff[3], dtype='b')[0]
+                xlen = (srate - 1) * datawide
+                idata00 = fpin.read(4)
+                leng += 4
+                idata22 = np.fromstring(idata00, '>i')[0]
 
-        date = UTCDateTime(int(yy), int(mm), int(dd), int(hh), int(mi),
-                           int(sec))
-        if start == 0:
-            start = date
-        if status0 == 0:
-            sdata = None
-        while leng < truelen:
-            buff = fpin.read(4)
-            leng += 4
-            _flag = np.fromstring(buff[0], dtype='b')[0]
-            chanum = np.fromstring(buff[1], dtype='b')[0]
-            datawide = np.fromstring(buff[2], dtype='b')[0] >> 4
-            srate = np.fromstring(buff[3], dtype='b')[0]
-            xlen = (srate - 1) * datawide
-            idata00 = fpin.read(4)
-            leng += 4
-            idata22 = np.fromstring(idata00, '>i')[0]
-
-            if chanum in output:
-                output[chanum].append(idata22)
-            else:
-                output[chanum] = [idata22, ]
-            sdata = fpin.read(xlen)
-            leng += xlen
-
-            if len(sdata) < xlen:
-                fpin.seek(-(xlen - len(sdata)), 1)
-                sdata += fpin.read(xlen - len(sdata))
-                print "This shouldn't happen, it's weird..."
-            for i in range((xlen / datawide)):
-                idata2 = 0
-                if datawide == 1:
-                    idata2 = np.fromstring(sdata[i:i + 1], 'b')[0]
-                elif datawide == 2:
-                    idata2 = np.fromstring(sdata[2 * i:2 * (i + 1)], '>h')[0]
-                elif datawide == 3:
-                    idata2 = np.fromstring(sdata[3 * i:3 * (i + 1)] + ' ',
-                                           '>i')[0] >> 8
-                elif datawide == 4:
-                    idata2 = np.fromstring(sdata[4 * i:4 * (i + 1)], '>i')[0]
+                if chanum in output:
+                    output[chanum].append(idata22)
                 else:
-                    print "WOOOW, SORRY,DATAWIDE IS NOT 1, 2, 3 or 4"
-                    print datawide
-                idata22 += idata2
-                output[chanum].append(idata22)
-    fpin.close()
+                    output[chanum] = [idata22, ]
+                sdata = fpin.read(xlen)
+                leng += xlen
+
+                if len(sdata) < xlen:
+                    fpin.seek(-(xlen - len(sdata)), 1)
+                    sdata += fpin.read(xlen - len(sdata))
+                    msg = "This shouldn't happen, it's weird..."
+                    warnings.warning(msg)
+                for i in range((xlen / datawide)):
+                    idata2 = 0
+                    if datawide == 1:
+                        idata2 = np.fromstring(sdata[i:i + 1], 'b')[0]
+                    elif datawide == 2:
+                        idata2 = np.fromstring(sdata[2 * i:2 * (i + 1)], '>h')[0]
+                    elif datawide == 3:
+                        idata2 = np.fromstring(sdata[3 * i:3 * (i + 1)] + ' ',
+                                               '>i')[0] >> 8
+                    elif datawide == 4:
+                        idata2 = np.fromstring(sdata[4 * i:4 * (i + 1)], '>i')[0]
+                    else:
+                        msg = "DATAWIDE is %s " % datawide + \
+                              "but only values of 1, 2, 3 or 4 are supported."
+                        warnings.warn(msg)
+                    idata22 += idata2
+                    output[chanum].append(idata22)
 
     traces = []
     for i in output.keys():
