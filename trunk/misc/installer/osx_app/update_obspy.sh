aad788632e22ddb18c99ae7c9b5dd913371478c8
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2012-06-28 14:44:31 +0000

    Updated the OSX installer scripts and application.

--- trunk/misc/installer/osx_app/update_obspy.sh
+++ trunk/misc/installer/osx_app/update_obspy.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env sh
-
 PREFIX=/Applications/ObsPy.app/Contents/MacOS
 BINS=$PREFIX/bin
 
-# Uninstall old ipython version and install new version
-$BINS/pip uninstall -y ipython
-$BINS/pip install ipython==0.12
-
-# Uninstall old ObsPy version
-$BINS/pip uninstall -y obspy.core
+# Install latest ObsPy version.
 $BINS/pip uninstall -y obspy.arclink
+$BINS/pip uninstall -y obspy.core
+$BINS/pip uninstall -y obspy.datamark
 $BINS/pip uninstall -y obspy.db
 $BINS/pip uninstall -y obspy.earthworm
+$BINS/pip uninstall -y obspy.fissures
 $BINS/pip uninstall -y obspy.gse2
 $BINS/pip uninstall -y obspy.imaging
 $BINS/pip uninstall -y obspy.iris
 $BINS/pip uninstall -y obspy.mseed
 $BINS/pip uninstall -y obspy.neries
+$BINS/pip uninstall -y obspy.realtime
 $BINS/pip uninstall -y obspy.sac
 $BINS/pip uninstall -y obspy.seg2
 $BINS/pip uninstall -y obspy.segy
@@ -28,24 +26,25 @@ $BINS/pip uninstall -y obspy.taup
 $BINS/pip uninstall -y obspy.wav
 $BINS/pip uninstall -y obspy.xseed
 
-
 # Install latest ObsPy version.
-$BINS/pip install obspy.core==0.6.2
-$BINS/pip install obspy.arclink==0.5.1
-$BINS/pip install obspy.db==0.5.1
-$BINS/pip install obspy.earthworm==0.1.0
-$BINS/pip install obspy.gse2==0.5.1
-$BINS/pip install obspy.imaging==0.5.1
-$BINS/pip install obspy.iris==0.5.1
-$BINS/pip install obspy.mseed==0.6.1
-$BINS/pip install obspy.neries==0.5.1
-$BINS/pip install obspy.sac==0.5.1
-$BINS/pip install obspy.seg2==0.1.1
-$BINS/pip install obspy.segy==0.5.2
-$BINS/pip install obspy.seisan==0.5.1
-$BINS/pip install obspy.seishub==0.5.1
-$BINS/pip install obspy.sh==0.5.2
-$BINS/pip install obspy.signal==0.6.1
-$BINS/pip install obspy.taup==0.5.1
-$BINS/pip install obspy.wav==0.5.1
-$BINS/pip install obspy.xseed==0.5.2
+$BINS/pip install --no-deps obspy.arclink==0.7.1
+$BINS/pip install --no-deps obspy.core==0.7.1
+$BINS/pip install --no-deps obspy.datamark==0.1.0
+$BINS/pip install --no-deps obspy.db==0.7.0
+$BINS/pip install --no-deps obspy.earthworm==0.1.0
+$BINS/pip install --no-deps obspy.gse2==0.7.0
+$BINS/pip install --no-deps obspy.imaging==0.7.0
+$BINS/pip install --no-deps obspy.iris==0.7.0
+$BINS/pip install --no-deps obspy.mseed==0.7.0
+$BINS/pip install --no-deps obspy.neries==0.7.0
+$BINS/pip install --no-deps obspy.realtime==0.1.0
+$BINS/pip install --no-deps obspy.sac==0.7.0
+$BINS/pip install --no-deps obspy.seg2==0.7.0
+$BINS/pip install --no-deps obspy.segy==0.5.2
+$BINS/pip install --no-deps obspy.seisan==0.5.1
+$BINS/pip install --no-deps obspy.seishub==0.5.1
+$BINS/pip install --no-deps obspy.sh==0.5.2
+$BINS/pip install --no-deps obspy.signal==0.7.0
+$BINS/pip install --no-deps obspy.taup==0.7.0
+$BINS/pip install --no-deps obspy.wav==0.5.1
+$BINS/pip install --no-deps obspy.xseed==0.7.0
