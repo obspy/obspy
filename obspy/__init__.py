49e10e1145307c7a2eac6de4d2a1b84fd234d3cc
Tobias Megies <megies@geophysik.uni-muenchen.de>
2012-10-14 18:51:41 +0200

    single package refactoring
    removing old __init__.pys, new one for 'from obspy import' (back to the
    roots ;))

---
+++ obspy/__init__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------
+#  Purpose: Convenience imports for obspy
+#   Author: Robert Barsch
+#           Moritz Beyreuther
+#           Lion Krischer
+#           Tobias Megies
+#
+# Copyright (C) 2008-2012 Robert Barsch, Moritz Beyreuther, Lion Krischer,
+#                         Tobias Megies
+#------------------------------------------------------------------------------
+"""
+ObsPy: A Python Toolbox for seismology/seismological observatories
+==================================================================
+
+ObsPy is an open-source project dedicated to provide a Python framework for
+processing seismological data. It provides parsers for common file formats
+and seismological signal processing routines which allow the manipulation of
+seismological time series.
+
+The goal of the ObsPy project is to facilitate rapid application development
+for seismology.
+
+:copyright:
+    The ObsPy Development Team (devs@obspy.org)
+:license:
+    GNU Lesser General Public License, Version 3
+    (http://www.gnu.org/copyleft/lesser.html)
+"""
+
+# don't change order
+from obspy.core.utcdatetime import UTCDateTime
+from obspy.core.util.base import _getVersionString
+from obspy.core.trace import Stats, Trace
+from obspy.core.stream import Stream, read
+
+
+__version__ = _getVersionString("obspy.core")
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(exclude_empty=True)
