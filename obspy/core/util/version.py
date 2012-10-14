f6807b045fb05e46d3468c57238bee1158c597fd
Tobias Megies <megies@geophysik.uni-muenchen.de>
2012-10-14 21:39:18 +0200

    single package refactoring
    adapting version getter module

--- obspy/core/util/version.py
+++ obspy/core/util/version.py
@@ -33,7 +33,11 @@
 
 __all__ = ("get_git_version")
 
+import os
 from subprocess import Popen, PIPE
+
+VERSION_FILE = os.abspath(os.path.join(os.path.dirname(__file__),
+                                       "RELEASE-VERSION"))
 
 
 def call_git_describe(abbrev=4):
@@ -43,60 +47,44 @@ def call_git_describe(abbrev=4):
         p.stderr.close()
         line = p.stdout.readlines()[0]
         return line.strip()
-
     except:
         return None
 
 
 def read_release_version():
     try:
-        f = open("RELEASE-VERSION", "r")
-
-        try:
-            version = f.readlines()[0]
-            return version.strip()
-
-        finally:
-            f.close()
-
+        version = open("RELEASE-VERSION", "r").readlines()[0]
+        return version.strip()
     except:
         return None
 
 
 def write_release_version(version):
-    f = open("RELEASE-VERSION", "w")
-    f.write("%s\n" % version)
-    f.close()
+    open("RELEASE-VERSION", "w").write("%s\n" % version)
 
 
 def get_git_version(abbrev=4):
     # Read in the version that's currently in RELEASE-VERSION.
-
     release_version = read_release_version()
 
     # First try to get the current version using “git describe”.
-
     version = call_git_describe(abbrev)
 
     # If that doesn't work, fall back on the value that's in
     # RELEASE-VERSION.
-
     if version is None:
         version = release_version
 
     # If we still don't have anything, that's an error.
-
     if version is None:
         raise ValueError("Cannot find the version number!")
 
     # If the current version is different from what's in the
     # RELEASE-VERSION file, update the file to be current.
-
     if version != release_version:
         write_release_version(version)
 
     # Finally, return the current version.
-
     return version
 
 
