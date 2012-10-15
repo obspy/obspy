4d8264c9b9f55ba80e562aa1f49745893924cffb
Tobias Megies <megies@geophysik.uni-muenchen.de>
2012-10-15 18:15:01 +0200

    debian packaging
    fix for previous commits

--- misc/scripts/build_all_debs.sh
+++ misc/scripts/build_all_debs.sh
@@ -15,15 +15,15 @@ mkdir -p $BASEDIR
 echo '#############' >> $LOG
 echo "#### `date`" >> $LOG
 
-git clone https://github.com/obspy/obspy.git $GITDIR &>> $LOG
-cd $GITDIR &>> $LOG
+git clone https://github.com/obspy/obspy.git $GITDIR 2>&1 >> $LOG
+cd $GITDIR 2>&1 >> $LOG
 
 for DIST in squeeze lucid natty oneiric precise; do
     for ARCH in i386 amd64; do
         DISTARCH=${DIST}_${ARCH}
         echo "#### $DISTARCH" >> $LOG
-        git clean -fxd &>> $LOG
-        echo "cd $DEBSCRIPTDIR; ./deb__build_debs.sh &>> $LOG" | schroot -c $DISTARCH &>> $LOG
-        mv $PACKAGEDIR/* $BASEDIR &>> $LOG
+        git clean -fxd 2>&1 >> $LOG
+        echo "cd $DEBSCRIPTDIR; ./deb__build_debs.sh &>> $LOG" | schroot -c $DISTARCH 2>&1 >> $LOG
+        mv $PACKAGEDIR/* $BASEDIR 2>&1 >> $LOG
     done
 done
