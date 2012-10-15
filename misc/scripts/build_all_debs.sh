cd49e7a2e124b37c96304390223b3acc95b0eab9
Tobias Megies <megies@geophysik.uni-muenchen.de>
2012-10-15 18:00:10 +0200

    debian packaging
    fixes for previous commit

--- misc/scripts/build_all_debs.sh
+++ misc/scripts/build_all_debs.sh
@@ -10,19 +10,20 @@ LOG=$BASEDIR/build_all_debs.log
 BUILDDIR=/tmp/python-obspy_build
 PACKAGEDIR=$BUILDDIR/packages
 
+rm -rf $BASEDIR
+mkdir -p $BASEDIR
 echo '#############' >> $LOG
 echo "#### `date`" >> $LOG
 
-rm -rf $BUILDDIR
-git clone https://github.com/obspy/obspy.git $GITDIR
-cd $GITDIR
+git clone https://github.com/obspy/obspy.git $GITDIR &>> $LOG
+cd $GITDIR &>> $LOG
 
 for DIST in squeeze lucid natty oneiric precise; do
     for ARCH in i386 amd64; do
         DISTARCH=${DIST}_${ARCH}
         echo "#### $DISTARCH" >> $LOG
-        git clean -fxd
-        echo "cd $DEBSCRIPTDIR; ./deb__build_debs.sh &>> $LOG" | schroot -c $DISTARCH
-        mv $PACKAGEDIR/* $BASEDIR
+        git clean -fxd &>> $LOG
+        echo "cd $DEBSCRIPTDIR; ./deb__build_debs.sh &>> $LOG" | schroot -c $DISTARCH &>> $LOG
+        mv $PACKAGEDIR/* $BASEDIR &>> $LOG
     done
 done
