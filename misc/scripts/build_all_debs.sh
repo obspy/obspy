086f963052ac12478cba6971b10c4839f7b65be8
Tobias Megies <megies@geophysik.uni-muenchen.de>
2012-10-15 17:53:35 +0200

    debian packaging
    adapt schroot batch build script for debian packages to git

--- misc/scripts/build_all_debs.sh
+++ misc/scripts/build_all_debs.sh
@@ -2,28 +2,27 @@
 # Automated script to build all packages for all distributions.
 # schroot environments have to be set up accordingly beforehand.
 
-DIR=/tmp/debian
-TAGSDIR=$DIR/tags
-LOG=$DIR/build_all_debs.log
+BASEDIR=/tmp/python-obspy_buildall
+GITDIR=$BASEDIR/git
+DEBSCRIPTDIR=$GITDIR/misc/debian
+LOG=$BASEDIR/build_all_debs.log
+
+BUILDDIR=/tmp/python-obspy_build
+PACKAGEDIR=$BUILDDIR/packages
 
 echo '#############' >> $LOG
-echo `date` >> $LOG
+echo "#### `date`" >> $LOG
 
-rm -rf $DIR
-mkdir $HOME/build_all_debs
-
-svn checkout --non-interactive --trust-server-cert https://svn.obspy.org/trunk/misc/debian $DIR
+rm -rf $BUILDDIR
+git clone https://github.com/obspy/obspy.git $GITDIR
+cd $GITDIR
 
 for DIST in squeeze lucid natty oneiric precise; do
     for ARCH in i386 amd64; do
         DISTARCH=${DIST}_${ARCH}
-        echo "$DISTARCH" >> $LOG
-        rm -rf $TAGSDIR
-        svn checkout --non-interactive --trust-server-cert https://svn.obspy.org/tags $TAGSDIR
-        svn revert --non-interactive --trust-server-cert $DIR/control
-        echo "cd $DIR; ./deb__build_debs.sh &>> $LOG" | schroot -c $DISTARCH
-        mv $DIR/packages/* $HOME/build_all_debs/
+        echo "#### $DISTARCH" >> $LOG
+        git clean -fxd
+        echo "cd $DEBSCRIPTDIR; ./deb__build_debs.sh &>> $LOG" | schroot -c $DISTARCH
+        mv $PACKAGEDIR/* $BASEDIR
     done
 done
-
-cp $LOG $HOME/
