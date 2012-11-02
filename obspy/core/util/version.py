a74c97f6070126bcfc693d10744df500070c8057
Tobias Megies <megies@geophysik.uni-muenchen.de>
2012-11-02 01:18:29 +0100

    fix calling "git describe" from inside repo for getting version number

--- obspy/core/util/version.py
+++ obspy/core/util/version.py
@@ -43,7 +43,7 @@ VERSION_FILE = os.path.abspath(os.path.join(os.path.dirname(__file__),
 def call_git_describe(abbrev=4):
     try:
         p = Popen(['git', 'describe', '--dirty', '--abbrev=%d' % abbrev],
-                  stdout=PIPE, stderr=PIPE)
+                  cwd=os.path.dirname(VERSION_FILE), stdout=PIPE, stderr=PIPE)
         p.stderr.close()
         line = p.stdout.readlines()[0]
         return line.strip()
