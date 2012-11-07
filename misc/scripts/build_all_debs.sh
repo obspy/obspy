c7bb168d997ab00388476eecebfee922c64b731d
Tobias Megies <megies@geophysik.uni-muenchen.de>
2012-11-07 17:25:29 +0100

    deb: safer exiting of schroot session + new ubuntu

--- misc/scripts/build_all_debs.sh
+++ misc/scripts/build_all_debs.sh
@@ -18,12 +18,15 @@ echo "#### `date`" >> $LOG
 git clone https://github.com/obspy/obspy.git $GITDIR 2>&1 >> $LOG
 cd $GITDIR 2>&1 >> $LOG
 
-for DIST in squeeze wheezy lucid natty oneiric precise; do
+for DIST in squeeze wheezy lucid natty oneiric precise quantal; do
     for ARCH in i386 amd64; do
         DISTARCH=${DIST}_${ARCH}
         echo "#### $DISTARCH" >> $LOG
         git clean -fxd 2>&1 >> $LOG
-        echo "cd $DEBSCRIPTDIR; ./deb__build_debs.sh &>> $LOG" | schroot -c $DISTARCH 2>&1 >> $LOG
+        cd /tmp  # can make problems to enter schroot environment from a folder not present in the schroot
+        SCHROOT_SESSION=$(schroot --begin-session -c $DISTARCH)
+        echo "cd $DEBSCRIPTDIR; ./deb__build_debs.sh &>> $LOG" | schroot --run-session -c "$SCHROOT_SESSION" 2>&1 >> $LOG
+        schroot -f --end-session -c "$SCHROOT_SESSION" 2>&1 >> $LOG
         mv $PACKAGEDIR/* $BASEDIR 2>&1 >> $LOG
     done
 done
