82dfc53801135c368456d4b811dc1f82bc596e35
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2013-02-26 18:09:02 +0100

    PEP8

--- obspy/realtime/signal.py
+++ obspy/realtime/signal.py
@@ -31,16 +31,20 @@ _PI = math.pi
 _TWO_PI = 2.0 * math.pi
 _MIN_FLOAT_VAL = 1.0e-20
 
+
 def offset(trace, offset=0.0, rtmemory_list=None):
     """
     Add the specified offset to the data.
 
     :type trace: :class:`~obspy.core.trace.Trace`
-    :param trace: :class:`~obspy.core.trace.Trace` object to append to this RtTrace
+    :param trace: :class:`~obspy.core.trace.Trace` object to append to this
+        RtTrace
     :type offset: float, optional
     :param offset: offset (default is 0.0)
-    :type rtmemory_list: list of :class:`~obspy.realtime.rtmemory.RtMemory`, optional
-    :param rtmemory_list: Persistent memory used by this process for specified trace
+    :type rtmemory_list: list of :class:`~obspy.realtime.rtmemory.RtMemory`,
+        optional
+    :param rtmemory_list: Persistent memory used by this process for specified
+        trace
     :rtype: Numpy :class:`numpy.ndarray`
     :return: Processed trace data from appended Trace object
     """
@@ -51,7 +55,6 @@ def offset(trace, offset=0.0, rtmemory_list=None):
 
     trace.data += offset
     return trace.data
-
 
 
 def scale(trace, factor=1.0, rtmemory_list=None):  # @UnusedVariable
@@ -434,7 +437,7 @@ def mwpIntegral(trace, max_time, ref_time, mem_time=1.0, gain=1.0,
         index = ioffset_mwp_min + np.size(rtmemory.input)
         rtmemory.output[_AMP_AT_PICK] = rtmemory.input[index]
     elif ioffset_mwp_min < -(np.size(rtmemory.input)) \
-        and not rtmemory.output[_HAVE_USED_MEMORY]:
+            and not rtmemory.output[_HAVE_USED_MEMORY]:
         msg = "mem_time not large enough to buffer required input data."
         raise ValueError(msg)
     if ioffset_mwp_min < 0 and rtmemory.output[_HAVE_USED_MEMORY]:
@@ -470,7 +473,7 @@ def mwpIntegral(trace, max_time, ref_time, mem_time=1.0, gain=1.0,
         # check displacement polarity
         if disp_amp >= 0.0:  # pos
             # check if past extremum
-            if  polarity < 0:  # passed from neg to pos displacement
+            if polarity < 0:  # passed from neg to pos displacement
                 mwp_int_int_sum *= -1.0
                 mwp_int_int_sum = 0
             polarity = 1
@@ -526,28 +529,30 @@ def calculateMwpMag(peak, epicentral_distance):
         mwp_mag = (2.0 / 3.0) * (math.log10(moment) - 9.1)
     return mwp_mag
 
+
 def kurtosis(trace, win=3.0, rtmemory_list=None):
     """
-    Apply recursive kurtosis calculation on data. 
-    
+    Apply recursive kurtosis calculation on data.
+
     Recursive kurtosis is computed using the [ChassandeMottin2002]_
     formulation adjusted to give the kurtosis of a gaussian distribution = 0.0.
 
     :type trace: :class:`~obspy.core.trace.Trace`
-    :param trace: :class:`~obspy.core.trace.Trace` object to append to this RtTrace
+    :param trace: :class:`~obspy.core.trace.Trace` object to append to this
+        RtTrace
     :type win: float, optional
     :param win: window length in seconds for the kurtosis (default is 3.0 s)
-    :type rtmemory_list: list of :class:`~obspy.realtime.rtmemory.RtMemory`, optional
-    :param rtmemory_list: Persistent memory used by this process for specified trace
+    :type rtmemory_list: list of :class:`~obspy.realtime.rtmemory.RtMemory`,
+        optional
+    :param rtmemory_list: Persistent memory used by this process for specified
+        trace
     :rtype: Numpy :class:`numpy.ndarray`
     :return: Processed trace data from appended Trace object
     """
-
-
     if not isinstance(trace, Trace):
         msg = "Trace parameter must be an obspy.core.trace.Trace object."
         raise ValueError(msg)
-    
+
     # if this is the first appended trace, the rtmemory_list will be None
     if not rtmemory_list:
         rtmemory_list = [RtMemory(), RtMemory(), RtMemory()]
@@ -558,14 +563,14 @@ def kurtosis(trace, win=3.0, rtmemory_list=None):
         return sample
 
     # get simple info from trace
-    npts=len(sample)
-    dt=trace.stats.delta
+    npts = len(sample)
+    dt = trace.stats.delta
 
     # set some constants for the kurtosis calulation
-    C1 = dt/float(win)
+    C1 = dt / float(win)
     a1 = 1.0 - C1
-    C2 = (1.0 - a1*a1)/2.0
-    bias = -3*C1 - 3.0
+    C2 = (1.0 - a1 * a1) / 2.0
+    bias = -3 * C1 - 3.0
 
     # prepare the output array
     kappa4 = np.empty(npts, sample.dtype)
@@ -581,23 +586,23 @@ def kurtosis(trace, win=3.0, rtmemory_list=None):
     # that needs carrying over
     # initialize mu1_last to 0
     if not rtmemory_mu1.initialized:
-        memory_size_input  = 1
+        memory_size_input = 1
         memory_size_output = 0
-        rtmemory_mu1.initialize(sample.dtype, memory_size_input,\
+        rtmemory_mu1.initialize(sample.dtype, memory_size_input,
                                 memory_size_output, 0, 0)
 
     # initialize mu2_last (sigma) to 1
     if not rtmemory_mu2.initialized:
-        memory_size_input  = 1
+        memory_size_input = 1
         memory_size_output = 0
-        rtmemory_mu2.initialize(sample.dtype, memory_size_input,\
+        rtmemory_mu2.initialize(sample.dtype, memory_size_input,
                                 memory_size_output, 1, 0)
 
     # initialize k4_bar_last to 0
     if not rtmemory_k4_bar.initialized:
-        memory_size_input  = 1
+        memory_size_input = 1
         memory_size_output = 0
-        rtmemory_k4_bar.initialize(sample.dtype, memory_size_input,\
+        rtmemory_k4_bar.initialize(sample.dtype, memory_size_input,
                                 memory_size_output, 0, 0)
 
     mu1_last = rtmemory_mu1.input[0]
@@ -606,20 +611,18 @@ def kurtosis(trace, win=3.0, rtmemory_list=None):
 
     # do recursive kurtosis
     for i in xrange(npts):
-        mu1 = a1*mu1_last + C1*sample[i]
-        dx2 = (sample[i]-mu1_last)*(sample[i]-mu1_last)
-        mu2 = a1*mu2_last + C2*dx2
+        mu1 = a1 * mu1_last + C1 * sample[i]
+        dx2 = (sample[i] - mu1_last) * (sample[i] - mu1_last)
+        mu2 = a1 * mu2_last + C2 * dx2
         dx2 = dx2 / mu2_last
-        k4_bar = (1+C1 - 2*C1*dx2)*k4_bar_last + C1 * dx2 * dx2
+        k4_bar = (1 + C1 - 2 * C1 * dx2) * k4_bar_last + C1 * dx2 * dx2
         kappa4[i] = k4_bar + bias
-        mu1_last=mu1
-        mu2_last=mu2
-        k4_bar_last=k4_bar
+        mu1_last = mu1
+        mu2_last = mu2
+        k4_bar_last = k4_bar
 
     rtmemory_mu1.input[0] = mu1_last
     rtmemory_mu2.input[0] = mu2_last
     rtmemory_k4_bar.input[0] = k4_bar_last
 
     return kappa4
-
-
