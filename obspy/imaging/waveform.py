aed3a935f2c9d0618cfe71f07b5d241528491938
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2013-03-17 22:09:09 +0100

    Creating copy of stream prior to plotting.
    Some convenience functionality inside the plotting routine operates
    inplace. This prevents changing the original data. Fixes #529.

--- obspy/imaging/waveform.py
+++ obspy/imaging/waveform.py
@@ -65,6 +65,7 @@ class WaveformPlotting(object):
         if len(self.stream) < 1:
             msg = "Empty stream object"
             raise IndexError(msg)
+        self.stream = self.stream.copy()
         # Type of the plot.
         self.type = kwargs.get('type', 'normal')
         # Start- and endtimes of the plots.
@@ -327,8 +328,6 @@ class WaveformPlotting(object):
         """
         Extend the seismogram.
         """
-        # Create a copy of the stream because it might be operated on.
-        self.stream = self.stream.copy()
         # Merge and trim to pad.
         self.stream.merge()
         if len(self.stream) != 1:
@@ -559,8 +558,6 @@ class WaveformPlotting(object):
 
         Slow and high memory consumption for large datasets.
         """
-        # Copy to avoid any changes to original data.
-        trace = [tr.copy() for tr in trace]
         if len(trace) > 1:
             stream = Stream(traces=trace)
             # Merge with 'interpolation'. In case of overlaps this method will
