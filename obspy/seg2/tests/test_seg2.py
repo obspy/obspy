1afd8beb2f39f6ead59bda131c1441fd915ef6e4
Tobias Megies <megies@geophysik.uni-muenchen.de>
2013-03-24 15:56:24 +0100

    seg2: add a test to check parsing of SEG2 headers

--- obspy/seg2/tests/test_seg2.py
+++ obspy/seg2/tests/test_seg2.py
@@ -5,8 +5,55 @@ The obspy.seg2 test suite.
 
 import numpy as np
 from obspy import read
+from obspy.core import AttribDict
 import os
 import unittest
+
+
+TRACE1_HEADER = {'ACQUISITION_DATE': '07/JAN/2013',
+                 'ACQUISITION_DATE_UTC': '07/JAN/2013',
+                 'ACQUISITION_TIME': '10:30:41',
+                 'ACQUISITION_TIME_MICROSECONDS': '0',
+                 'ACQUISITION_TIME_MICROSECONDS_UTC': '0',
+                 'ACQUISITION_TIME_UTC': '09:30:41',
+                 'APPLIED_STANDARD': '0',
+                 'BATTERY_LEVEL': '99 0 30.35 4.123',
+                 'CHANNEL_NUMBER': '1',
+                 'CLIENT': 'CLIENT',
+                 'CLOCK_SOURCE': '1',
+                 'COMPANY': 'COMPANY',
+                 'DESCALING_FACTOR': '2.17378e-05',
+                 'DEVICE_NAME': 'VIPA 15',
+                 'DEVICE_SERIAL_NO': '01-0000143912a3',
+                 'DIFFERENCE_TO_REAL_TIME': '0.000000',
+                 'FIRMWARE_VERSION': '0.0.125',
+                 'FIRST_SAMPLE_NO': '8000',
+                 'HIGH_CUT_FILTER': '0 0',
+                 'INSTRUMENT': 'DMT_VIPA_01-0000143912a3',
+                 'LAST_SAMPLE_NO': '9999',
+                 'LOCATION': 'LOCATION',
+                 'LOW_CUT_FILTER': '10.000000 12.000000',
+                 'NETWORK_NAME': 'BANK',
+                 'NOTE': AttribDict({'Comment': ''}),
+                 'OBSERVER': 'OBSERVER',
+                 'REAL_TIME_AVAILABLE': 'FALSE',
+                 'REGISTRATION_DIRECTION': 'X',
+                 'SAMPLE_INTERVAL': '0.00100000',
+                 'SCALE_UNIT': 'mm/s',
+                 'SECONDS_SINCE_LAST_GPS_SYNC': '-1',
+                 'SENSOR_CALIB_DATE': '21/8/12',
+                 'SENSOR_FC': '4.500000',
+                 'SENSOR_TYPE_ID': '1',
+                 'SENSOR_TYPE_NAME': 'DMT-3D/DIN',
+                 'STATION_CODE': 'BA1',
+                 'STATION_NAME': 'DMT-BANK',
+                 'SYSTEM_T0': '0',
+                 'TIME_ZONE': 'CET',
+                 'TRACE_TYPE': 'SEISMIC_DATA',
+                 'TRIGGER_LEVEL': '2.00000000',
+                 'TRIGGER_SAMPLE_NO': '8000',
+                 'TRIGGER_SOURCE': '0',
+                 'UNITS': 'METERS'}
 
 
 class SEG2TestCase(unittest.TestCase):
@@ -34,6 +81,9 @@ class SEG2TestCase(unittest.TestCase):
             scaled_data = tr.data * float(tr.stats.seg2.DESCALING_FACTOR) * 1e3
             self.assertTrue(np.allclose(scaled_data, result, rtol=1e-7,
                                         atol=1e-7))
+        # test seg2 specific header values
+        # (trace headers include SEG2 file header)
+        self.assertEqual(st[0].stats.seg2, TRACE1_HEADER)
 
 
 def suite():
