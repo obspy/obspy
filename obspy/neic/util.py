a9fc9841eff2e24a420bbca6a4195df823b8514f
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2013-05-02 22:00:01 +0200

    Added neic module.
    Thanks to David Ketchum.

---
+++ obspy/neic/util.py
@@ -0,0 +1,39 @@
+import string
+from datetime import datetime
+
+def asctime():
+	"""
+	asctime() returns the current time as a string hh:mm:ss
+	"""
+	a = str(datetime.utcnow())
+	return a[11:19]
+def ascdate():
+	"""
+	ascdate() returns the current date at yy/mm/dd
+	"""
+	a = str(datetime.utcnow())
+	return a[2:10]
+	
+def dsecs(dt):
+	"""
+	Given a timedelta object compute it as double seconds.
+	"""
+	d = dt.days*86400.
+	d = d + dt.seconds
+	d = d + dt.microseconds/1000000.
+	return d
+
+def getProperty(file, key):
+	"""
+	Given a property filename get the value of the given key
+	"""
+	file = open(file, 'r')
+	lines = file.readlines()
+	for i in range(len(lines)):
+		lines[i] = string.strip(lines[i])
+		if lines[i][0:len(key)] == key:
+			ans = lines[i][len(key)+1:]
+			return ans
+	return ""
+
+
