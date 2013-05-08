dead4798cedd5e567b40e8887519591dfc01a4f3
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2013-05-08 10:30:22 +0200

    New helper function.
    Added a new helper function converting a vector of unit length given in
    dip and azimuth to a unit vector in the vertical, south, east coordinate
    system.

--- obspy/signal/rotate.py
+++ obspy/signal/rotate.py
@@ -2,10 +2,10 @@
 #-------------------------------------------------------------------
 # Filename: rotate.py
 #  Purpose: Various Seismogram Rotation Functions
-#   Author: Tobias Megies, Tom Richter
+#   Author: Tobias Megies, Tom Richter, Lion Krischer
 #    Email: tobias.megies@geophysik.uni-muenchen.de
 #
-# Copyright (C) 2009-2012 Tobias Megies, Tom Richter
+# Copyright (C) 2009-2013 Tobias Megies, Tom Richter, Lion Krischer
 #---------------------------------------------------------------------
 """
 Various Seismogram Rotation Functions
@@ -16,8 +16,8 @@ Various Seismogram Rotation Functions
     GNU Lesser General Public License, Version 3
     (http://www.gnu.org/copyleft/lesser.html)
 """
-
 from math import pi, sin, cos
+import numpy as np
 
 
 def rotate_NE_RT(n, e, ba):
@@ -122,3 +122,64 @@ def rotate_LQT_ZNE(l, q, t, ba, inc):
     n = -l * sin(inc) * cos(ba) + q * cos(inc) * cos(ba) + t * sin(ba)
     e = -l * sin(inc) * sin(ba) + q * cos(inc) * sin(ba) - t * cos(ba)
     return z, n, e
+
+
+def _dip_azimuth2ZSE_base_vector(dip, azimuth):
+    """
+    Helper function converting a vector described with azimuth and dip of unit
+    length to a vector in the ZSE (vertical, south, east) base.
+
+    The definition of azimuth and dip is according to the SEED reference
+    manual, as are the following examples (they use rounding for small
+    numerical inaccuracies - also positive and negative zero are treated as
+    equal):
+
+    >>> r = lambda x: np.array([_i if _i != -0.0 else 0.0\
+        for _i in np.round(x, 10)])
+    >>> r(_dip_azimuth2ZSE_base_vector(-90, 0)) #doctest: +NORMALIZE_WHITESPACE
+    array([ 1., 0., 0.])
+    >>> r(_dip_azimuth2ZSE_base_vector(90, 0)) #doctest: +NORMALIZE_WHITESPACE
+    array([-1., 0., 0.])
+    >>> r(_dip_azimuth2ZSE_base_vector(0, 0)) #doctest: +NORMALIZE_WHITESPACE
+    array([ 0., -1., 0.])
+    >>> r(_dip_azimuth2ZSE_base_vector(0, 180)) #doctest: +NORMALIZE_WHITESPACE
+    array([ 0., 1., 0.])
+    >>> r(_dip_azimuth2ZSE_base_vector(0, 90)) #doctest: +NORMALIZE_WHITESPACE
+    array([ 0., 0., 1.])
+    >>> r(_dip_azimuth2ZSE_base_vector(0, 270)) #doctest: +NORMALIZE_WHITESPACE
+    array([ 0., 0., -1.])
+    """
+    # Convert both to radian.
+    dip = np.deg2rad(dip)
+    azimuth = np.deg2rad(azimuth)
+
+    # Define the rotation axis for the dip.
+    c1 = 0.0
+    c2 = 0.0
+    c3 = -1.0
+    # Now the dip rotation matrix.
+    dip_rotation_matrix = np.cos(dip) * \
+        np.matrix(((1.0, 0.0, 0.0), (0.0, 1.0, 0.0), (0.0, 0.0, 1.0))) + \
+        (1 - np.cos(dip)) * np.matrix(((c1 * c1, c1 * c2, c1 * c3),
+                                         (c2 * c1, c2 * c2, c2 * c3),
+                                         (c3 * c1, c3 * c2, c3 * c3))) + \
+        np.sin(dip) * np.matrix(((0, -c3, c2), (c3, 0, -c1), (-c2, c1, 0)))
+    # Do the same for the azimuth.
+    c1 = -1.0
+    c2 = 0.0
+    c3 = 0.0
+    azimuth_rotation_matrix = np.cos(azimuth) * \
+        np.matrix(((1.0, 0.0, 0.0), (0.0, 1.0, 0.0), (0.0, 0.0, 1.0))) + \
+        (1 - np.cos(azimuth)) * np.matrix(((c1 * c1, c1 * c2, c1 * c3),
+                                         (c2 * c1, c2 * c2, c2 * c3),
+                                         (c3 * c1, c3 * c2, c3 * c3))) + \
+        np.sin(azimuth) * np.matrix(((0, -c3, c2), (c3, 0, -c1), (-c2, c1, 0)))
+
+    # Now simply rotate a north pointing unit vector with both matrixes.
+    temp = np.array([azimuth_rotation_matrix.dot([0.0, -1.0, 0.0])])[0][0]
+    return np.array(dip_rotation_matrix.dot(temp))[0]
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(exclude_empty=True)
