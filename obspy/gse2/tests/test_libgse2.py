1922e18a7a0f6883afe015a7e27dadb306af4bc5
mbyt <random.seed@web.de>
2013-05-25 06:57:55 +0200

    correcting test file name
    see #538

--- obspy/gse2/tests/test_libgse2.py
+++ obspy/gse2/tests/test_libgse2.py
@@ -170,12 +170,12 @@ class LibGSE2TestCase(unittest.TestCase):
         np.testing.assert_array_equal(data[-4:],
                                       np.array([-139, -153, -169, -156]))
 
-    def test_corruptHead(self):
+    def test_brokenHead(self):
         """
         Tests that gse2 files with n_samps=0 will not end up with a
         segmentation fault
         """
-        f = open(os.path.join(self.path, 'corrupt_head.gse2'), 'rb')
+        f = open(os.path.join(self.path, 'broken_head.gse2'), 'rb')
         self.assertRaises(ChksumError, libgse2.read, f)
 
 
