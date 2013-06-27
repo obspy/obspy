a2e2c116e6054200c915423631af88a7231b0eb5
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2013-06-27 12:17:59 +0200

    Making setup.py work with OSX compilers.
    The clang compiler shipping with newer OSX versions does not honor the
    LIBRARY_PATH variable and thus cannot find the gfortran libraries. This
    adds a workaround. Works with OSX 10.7 and the latest official XCode
    clang version. Also tested with a local gcc installation. Works with
    Python 2.6.8 and 2.7.5. Tested with NumPy 1.6.2 and 1.7.1 for each. All
    other dependencies where the latest versions from pypi.

--- setup.py
+++ setup.py
@@ -365,11 +365,7 @@ else:
         UnixCCompiler.linker_so = ["gfortran"]
         self.compiler_so = ["gfortran"]
         cc_args = ['-c', '-fno-underscoring']
-        if sys.platform == 'darwin':
-            self.compiler_so = _darwin_compiler_fixup(self.compiler_so,
-                                                      cc_args)
-        else:
-            cc_args.append('-fPIC')
+        cc_args.append('-fPIC')
         try:
             self.spawn(self.compiler_so + [src, '-o', obj] + cc_args)
         except DistutilsExecError:
@@ -597,6 +593,14 @@ def setupLibTauP():
     """
     Prepare building of Fortran extensions.
     """
+    # The clang version shipping with OSX does not honor the $LIBRARY_PATH
+    # environment variable so it cannot find libgfortran. Simple add all
+    # paths in there to the linker. Only on darwin.
+    if sys.platform == "darwin" and "LIBRARY_PATH" in os.environ:
+        extra_link_args = ["-L%s" % _i.strip() for _i in
+            os.environ["LIBRARY_PATH"].split(":")]
+    else:
+        extra_link_args = []
     # create library name
     if IS_DEVELOP:
         lib_name = 'libtaup-%s-%s-py%s' % (
@@ -608,6 +612,7 @@ def setupLibTauP():
     src = os.path.join('obspy', 'taup', 'src') + os.sep
     lib = MyExtension(lib_name,
                       libraries=['gfortran'],
+                      extra_link_args=extra_link_args,
                       sources=[src + 'emdlv.f', src + 'libtau.f',
                                src + 'ttimes_subrout.f'])
     return lib
