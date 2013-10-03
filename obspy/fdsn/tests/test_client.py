c2e71fb56ca987b7839809bcdfec45b424ff7eeb
Tobias Megies <megies@geophysik.uni-muenchen.de>
2013-10-03 23:21:40 +0200

    fdsn: start adding tests for example queries

--- obspy/fdsn/tests/test_client.py
+++ obspy/fdsn/tests/test_client.py
@@ -9,10 +9,26 @@ The obspy.fdsn.client test suite.
     GNU Lesser General Public License, Version 3
     (http://www.gnu.org/copyleft/lesser.html)
 """
+from obspy import readEvents, UTCDateTime
 from obspy.fdsn import Client
 from obspy.fdsn.client import build_url
+from obspy.fdsn.header import DEFAULT_USER_AGENT
 import os
 import unittest
+
+
+USER_AGENT = "ObsPy (test suite) " + " ".join(DEFAULT_USER_AGENT.split())
+
+
+def failmsg(got, expected):
+    if isinstance(got, str) and isinstance(expected, str):
+        from difflib import Differ
+        diff = Differ().compare(got.splitlines(True),
+                                expected.splitlines(True))
+        diff = "".join([l for l in diff if l[0] in "-+?"])
+        return "\nDiff:\n%s" % diff
+    else:
+        return "\nGot:\n%s\nExpected:\n%s" % (str(got), str(expected))
 
 
 class ClientTestCase(unittest.TestCase):
@@ -22,6 +38,8 @@ class ClientTestCase(unittest.TestCase):
     def setUp(self):
         # directory where the test files are located
         self.path = os.path.dirname(__file__)
+        self.datapath = os.path.join(self.path, "data")
+        self.client = Client(base_url="IRIS", user_agent=USER_AGENT)
 
     def test_url_building(self):
         """
@@ -56,7 +74,61 @@ class ClientTestCase(unittest.TestCase):
         """
         Tests the automatic discovery of services with the IRIS endpoint.
         """
-        client = Client(base_url="IRIS")
+        pass
+
+    def test_IRIS_example_queries(self):
+        """
+        Tests the (sometimes modified) example queries given on IRIS webpage.
+        """
+        client = self.client
+
+        # event example queries
+        queries = [
+            dict(eventid=609301),
+            dict(starttime=UTCDateTime("2011-01-07T01:00:00"),
+                 endtime=UTCDateTime("2011-01-07T02:00:00"),
+                 catalog="NEIC PDE"),
+            dict(starttime=UTCDateTime("2011-01-07T14:00:00"),
+                 endtime=UTCDateTime("2011-01-08T00:00:00"), minlatitude=15,
+                 maxlatitude=40, minlongitude=-170, maxlongitude=170,
+                 includeallmagnitudes=True, minmagnitude=4,
+                 orderby="magnitude"),
+            ]
+        result_files = ["events_by_eventid.xml",
+                        "events_by_time.xml",
+                        "events_by_misc.xml",
+                        ]
+        for query, filename in zip(queries, result_files):
+            got = client.get_events(**query)
+            file_ = os.path.join(self.datapath, filename)
+            #got.write(file_, "QUAKEML")
+            expected = readEvents(file_)
+            self.assertEqual(got, expected, failmsg(got, expected))
+
+        # station example queries
+        queries = [
+            dict(latitude=-56.1, longitude=-26.7, maxradius=15),
+            dict(startafter=UTCDateTime("2003-01-07"),
+                 endbefore=UTCDateTime("2011-02-07"), minlatitude=15,
+                 maxlatitude=55, minlongitude=170, maxlongitude=-170),
+            dict(starttime=UTCDateTime("2013-01-01"), network="IU",
+                 sta="ANMO", level="channel"),
+            dict(starttime=UTCDateTime("2013-01-01"), network="IU", sta="A*",
+                 location="00", level="channel", format="text"),
+            ]
+        result_files = ["stations_by_latlon.xml",
+                        "stations_by_misc.xml",
+                        "stations_by_station.xml",
+                        "stations_by_station_wildcard.xml",
+                        ]
+        for query, filename in zip(queries, result_files):
+            got = client.get_stations(**query)
+            file_ = os.path.join(self.datapath, filename)
+            with open(file_, "wt") as fh:
+                fh.write(got)
+            with open(file_) as fh:
+                expected = fh.read()
+            self.assertEqual(got, expected, failmsg(got, expected))
 
 
 def suite():
