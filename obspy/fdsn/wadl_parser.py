2b0d9cee6891a005d1ecedd7bb5e5344b61b46ea
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2013-10-04 13:02:45 +0200

    IRIS fixed the bug. Remove workaround.

--- obspy/fdsn/wadl_parser.py
+++ obspy/fdsn/wadl_parser.py
@@ -120,8 +120,7 @@ class WADLParser(object):
             param_type = float
         elif param_type in ["xs:long", "xs:int", "xs:integer"]:
             param_type = int
-        # XXX: Remove sboolean once iris fixes it!
-        elif param_type in ["xs:boolean", "xs:sboolean"]:
+        elif param_type  == "xs:boolean":
             param_type = bool
         else:
             msg = "Unknown parameter type '%s' in WADL." % param_type
