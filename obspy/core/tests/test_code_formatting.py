81b878147e1d5691b9d54ab22806922c0c559141
Tobias Megies <megies@geophysik.uni-muenchen.de>
2013-10-28 09:11:23 +0100

    make flake8 check reusable

--- obspy/core/tests/test_code_formatting.py
+++ obspy/core/tests/test_code_formatting.py
@@ -5,7 +5,9 @@ from obspy.core.util.decorator import skipIf
 import fnmatch
 import inspect
 import os
+import sys
 import unittest
+import StringIO
 
 EXCLUDE_FILES = [
     "*/__init__.py",
@@ -20,6 +22,41 @@ else:
     HAS_FLAKE8 = flake8.__version__ >= '2'
 
 
+def check_flake8():
+    if not HAS_FLAKE8:
+        raise Exception('flake8 is required to check code formatting')
+    import flake8.main
+    test_dir = os.path.abspath(inspect.getfile(inspect.currentframe()))
+    obspy_dir = os.path.dirname(os.path.dirname(os.path.dirname(test_dir)))
+    error_count = 0
+    file_count = 0
+    untracked_files = get_untracked_files_from_git() or []
+    sys.stdout = StringIO.StringIO()
+    for dirpath, _, filenames in os.walk(obspy_dir):
+        filenames = [_i for _i in filenames if
+                     os.path.splitext(_i)[-1] == os.path.extsep + "py"]
+        if not filenames:
+            continue
+        for py_file in filenames:
+            py_file = os.path.join(dirpath, py_file)
+            # ignore untracked files
+            if os.path.abspath(py_file) in untracked_files:
+                continue
+
+            # Check files that do not match any exclusion pattern
+            for exclude_pattern in EXCLUDE_FILES:
+                if fnmatch.fnmatch(py_file, exclude_pattern):
+                    break
+            else:
+                file_count += 1
+                if flake8.main.check_file(py_file):
+                    error_count += 1
+    sys.stdout.seek(0)
+    message = sys.stdout.read()
+    sys.stdout = sys.__stdout__
+    return error_count, message, file_count
+
+
 class CodeFormattingTestCase(unittest.TestCase):
     """
     Test codebase for compliance with the flake8 tool.
@@ -29,35 +66,9 @@ class CodeFormattingTestCase(unittest.TestCase):
         """
         Test codebase for compliance with the flake8 tool.
         """
-        if not HAS_FLAKE8:
-            raise Exception('flake8 is required to check code formatting')
-        import flake8.main
-        test_dir = os.path.abspath(inspect.getfile(inspect.currentframe()))
-        obspy_dir = os.path.dirname(os.path.dirname(os.path.dirname(test_dir)))
-        error_count = 0
-        file_count = 0
-        untracked_files = get_untracked_files_from_git() or []
-        for dirpath, _, filenames in os.walk(obspy_dir):
-            filenames = [_i for _i in filenames if
-                         os.path.splitext(_i)[-1] == os.path.extsep + "py"]
-            if not filenames:
-                continue
-            for py_file in filenames:
-                py_file = os.path.join(dirpath, py_file)
-                # ignore untracked files
-                if os.path.abspath(py_file) in untracked_files:
-                    continue
-
-                # Check files that do not match any exclusion pattern
-                for exclude_pattern in EXCLUDE_FILES:
-                    if fnmatch.fnmatch(py_file, exclude_pattern):
-                        break
-                else:
-                    file_count += 1
-                    if flake8.main.check_file(py_file):
-                        error_count += 1
+        error_count, message, file_count = check_flake8()
         self.assertTrue(file_count > 10)
-        self.assertEqual(error_count, 0)
+        self.assertEqual(error_count, 0, message)
 
 
 def suite():
