742d8dc1b9a5b336f1811ce7233a89e5a1f51e97
Mark Williams <mark.c.will@gmail.com>
2013-10-30 10:30:03 -0700

    Cosmetic pep8 fixes

--- obspy/core/json.py
+++ obspy/core/json.py
@@ -4,17 +4,17 @@ JSON write support
 
 JavaScript Object Notation is a text-based open standard designed for
 human-readable data interchange. The JSON format is often used for serializing
-and transmitting structured data over a network connection. It is used 
+and transmitting structured data over a network connection. It is used
 primarily to transmit data between a server and web application, serving as an
 alternative to XML.
 
 This module provides:
 ---------------------
-Default : a class to create a "default" function accepted by the 
+Default : a class to create a "default" function accepted by the
 python json module Encoder classes, valid for obspy.core.event objects
 
 get_dump_kwargs : function that wraps a Default function and some other params
-into a dictionary suitable for passing to json.dumps. 
+into a dictionary suitable for passing to json.dumps.
 
 Example
 -------
@@ -24,18 +24,18 @@ Example
 >>> s = json.dumps(c, default=d)
 
 """
-from obspy.core.event import ( AttribDict, Catalog, UTCDateTime, 
-    ResourceIdentifier )
+from obspy.core.event import (AttribDict, Catalog, UTCDateTime,
+    ResourceIdentifier)
 
 
 class Default(object):
     """
     Class to create a "default" function for the json.dump* functions
     which is passed to the JSONEncoder.
-    
+
     """
-    _catalog_attrib = ('events','comments', 'description','creation_info',
-        'resource_id')
+    _catalog_attrib = ('events', 'comments', 'description', 'creation_info',
+                       'resource_id')
 
     OMIT_NULLS = None
     TIME_FORMAT = None
@@ -55,7 +55,7 @@ class Default(object):
     def __call__(self, obj):
         """
         Deal with obspy event objects in JSON Encoder
-        
+
         This function can be passed to the json module's
         'default' keyword parameter
 
@@ -66,14 +66,14 @@ class Default(object):
             # Map to a serializable dict
             # Leave out nulls, empty strings, list, dicts, except for numbers
             if self.OMIT_NULLS:
-                return { k:v for k,v in obj.iteritems() if v or v == 0 } 
+                return {k: v for k, v in obj.iteritems() if v or v == 0}
             else:
-                return { k:v for k,v in obj.iteritems() }
+                return {k: v for k, v in obj.iteritems()}
         elif isinstance(obj, Catalog):
             # Catalog isn't a dict
-            return { k : getattr(obj, k) for k in self._catalog_attrib 
-                if getattr(obj,k) 
-            }
+            return {k: getattr(obj, k) for k in self._catalog_attrib
+                    if getattr(obj, k)
+                    }
         elif isinstance(obj, UTCDateTime):
             if self.TIME_FORMAT is None:
                 return str(obj)
@@ -92,11 +92,9 @@ def get_dump_kwargs(minify=True, no_nulls=True, **kwargs):
 
     :param bool minify: Use no spaces between separators (True)
     :param bool no_nulls: Omit null values and empty sequences/mappings (True)
-    
+
     """
     if minify:
-        kwargs["separators"] =  (',',':')
+        kwargs["separators"] = (',', ':')
     kwargs["default"] = Default(omit_nulls=no_nulls)
     return kwargs
-
-
--- obspy/core/tests/test_json.py
+++ obspy/core/tests/test_json.py
@@ -7,11 +7,12 @@ import json
 
 warnings.filterwarnings("ignore")
 
+
 class JSONTestCase(unittest.TestCase):
     """Test JSON module classes and functions"""
     def setUp(self):
         self.path = os.path.join(os.path.dirname(__file__), 'data')
-        qml_file  = os.path.join(self.path, 'qml-example-1.2-RC3.xml')
+        qml_file = os.path.join(self.path, 'qml-example-1.2-RC3.xml')
         c = readQuakeML(qml_file)
         self.event = c.events[0]
 
@@ -20,45 +21,41 @@ class JSONTestCase(unittest.TestCase):
         self.assertIsInstance(s, str)
         j = json.loads(s)
         self.assertIsInstance(j, dict)
-    
+
     def test_default(self):
         """Test Default function class"""
         default = Default()
-        self.assertTrue( hasattr( default, '_catalog_attrib') )
-        self.assertTrue( hasattr( default, 'OMIT_NULLS') )
-        self.assertTrue( hasattr( default, 'TIME_FORMAT') )
-        self.assertTrue( hasattr( default, '__init__') )
-        self.assertTrue( hasattr( default, '__call__') )
-        
+        self.assertTrue(hasattr(default, '_catalog_attrib'))
+        self.assertTrue(hasattr(default, 'OMIT_NULLS'))
+        self.assertTrue(hasattr(default, 'TIME_FORMAT'))
+        self.assertTrue(hasattr(default, '__init__'))
+        self.assertTrue(hasattr(default, '__call__'))
         s = json.dumps(self.event, default=default)
         self.verify_json(s)
-    
+
     def test_get_dump_kwargs(self):
         """Test getting kwargs for json.dumps"""
         kw = get_dump_kwargs()
-        self.assertTrue( 'default' in kw )
-        self.assertTrue( 'separators' in kw )
-        self.assertIsInstance( kw['default'], Default )
-        self.assertTrue( kw['default'].OMIT_NULLS )
-        self.assertEqual( kw['separators'], (',',':') )
-        
+        self.assertTrue('default' in kw)
+        self.assertTrue('separators' in kw)
+        self.assertIsInstance(kw['default'], Default)
+        self.assertTrue(kw['default'].OMIT_NULLS)
+        self.assertEqual(kw['separators'], (',', ':'))
         s1 = json.dumps(self.event, **kw)
         self.verify_json(s1)
-        
         kw = get_dump_kwargs(minify=False, no_nulls=False)
-        self.assertTrue( 'default' in kw )
-        self.assertTrue( 'separators' not in kw )
-        self.assertIsInstance( kw['default'], Default )
-        self.assertFalse( kw['default'].OMIT_NULLS )
-        
+        self.assertTrue('default' in kw)
+        self.assertTrue('separators' not in kw)
+        self.assertIsInstance(kw['default'], Default)
+        self.assertFalse(kw['default'].OMIT_NULLS)
         s2 = json.dumps(self.event, **kw)
         self.verify_json(s2)
-        
         # Compacted version is smaller
-        self.assertLess( len(s1), len(s2) )
+        self.assertLess(len(s1), len(s2))
 
     def tearDown(self):
         del self.event
+
 
 def suite():
     return unittest.makeSuite(JSONTestCase, 'test')
@@ -66,4 +63,3 @@ def suite():
 
 if __name__ == '__main__':
     unittest.main(defaultTest='suite')
-
