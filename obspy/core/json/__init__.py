c7ac35cca06e8318ac34576ea9178d988350e932
Mark Williams <mark.c.will@gmail.com>
2013-10-30 11:41:05 -0700

    Refactored to subpackage format due to json namespace collision

---
+++ obspy/core/json/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+JSON write support
+
+JavaScript Object Notation is a text-based open standard designed for
+human-readable data interchange. The JSON format is often used for serializing
+and transmitting structured data over a network connection. It is used
+primarily to transmit data between a server and web application, serving as an
+alternative to XML.
+
+See the module obspy.core.json.default for documentation
+
+"""
+from default import Default, get_dump_kwargs
--- obspy/core/json/default.py
+++ obspy/core/json/default.py
@@ -1,12 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-JSON write support
-
-JavaScript Object Notation is a text-based open standard designed for
-human-readable data interchange. The JSON format is often used for serializing
-and transmitting structured data over a network connection. It is used
-primarily to transmit data between a server and web application, serving as an
-alternative to XML.
+JSON Encoder default function
 
 This module provides:
 ---------------------
