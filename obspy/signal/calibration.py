@mbyt: theoretically correct computation for 99c5a978b105ff2a2b0 proposed by @jwassermann. no adaptions of tests necessary.
(the conjugate was computed for the wrong one of the two signals in the
cross spectrum, making a manual shift by 180 degree necessary.)

obspy/signal/calibration.py
@@ -87,10 +87,8 @@ def relcalstack(st1, st2, calib_file, window_len, overlap_frac=0.5, smooth=0,
     auto, _freq, _t = \
         spectral_helper(tr1, tr1, NFFT=nfft, Fs=sampfreq, noverlap=noverlap)
     cross, freq, _t = \
-        spectral_helper(tr1, tr2, NFFT=nfft, Fs=sampfreq, noverlap=noverlap)
+        spectral_helper(tr2, tr1, NFFT=nfft, Fs=sampfreq, noverlap=noverlap)
 
-    # 180 degree phase shift
-    cross.imag *= -1.0
     res = (cross / auto).sum(axis=1) * gg
 
     # The first item might be zero. Problems with phase calculations.