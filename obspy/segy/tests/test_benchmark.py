6e40c0938d82b090d8cbec75f399493e4fa630aa
Tobias Megies <megies@geophysik.uni-muenchen.de>
2013-11-14 17:53:28 +0100

    image tests: higher tolerance for 3 tests on very old matplotlib
    I checked all three, the axis box gets plotted slightly differently for them on matplotlib 1.1.1 but that's not our problem.

--- obspy/segy/tests/test_benchmark.py
+++ obspy/segy/tests/test_benchmark.py
@@ -5,12 +5,16 @@ The obspy.segy benchmark test suite.
 
 from obspy.core.util.testing import ImageComparison, HAS_COMPARE_IMAGE
 from obspy.core.util.decorator import skipIf
+from obspy.core.util.base import getMatplotlibVersion
 from obspy.segy.benchmark import plotBenchmark
 import glob
 import os
 import unittest
 import warnings
 import numpy as np
+
+
+MATPLOTLIB_VERSION = getMatplotlibVersion()
 
 
 class BenchmarkTestCase(unittest.TestCase):
@@ -22,11 +26,15 @@ class BenchmarkTestCase(unittest.TestCase):
         """
         Test benchmark plot.
         """
+        reltol = 1
+        if MATPLOTLIB_VERSION < [1, 2, 0]:
+            reltol = 2
         path = os.path.join(os.path.dirname(__file__), 'data')
         path_images = os.path.join(os.path.dirname(__file__), "images")
         sufiles = sorted(glob.glob(os.path.join(path, 'seismic01_*_vz.su')))
         # new temporary file with PNG extension
-        with ImageComparison(path_images, 'test_plotBenchmark.png') as ic:
+        with ImageComparison(path_images, 'test_plotBenchmark.png',
+                             reltol=reltol) as ic:
             # generate plot
             with warnings.catch_warnings(record=True) as w:
                 warnings.resetwarnings()
