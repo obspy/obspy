867ae1f8e882c72c47a3adfce7756e5eb35ca340
mbyt <random.seed@web.de>
2013-11-23 18:56:30 +0100

    cleaning up calc_resp hacking

--- obspy/signal/src/evalresp/blub.py
+++
@@ -1,6 +0,0 @@
-from obspy.station import read_inventory
-import numpy as np
-
-inv = read_inventory("./IRIS_single_channel_with_response.xml")
-stage = inv[0][0][0].response.get_evalresp_response()
-print np.abs(stage).ptp()
