b921173985ecc462314a250c3536e5f9780815c0
Robert Barsch <barsch@egu.eu>
2013-12-07 01:45:57 +0100

    pep8

--- obspy/taup/taup.py
+++ obspy/taup/taup.py
@@ -8,9 +8,9 @@ import os
 import platform
 
 
-lib_name = 'libtau_%s_%s_py%s' % (platform.system(),
-        platform.architecture()[0], ''.join([str(i) for i in
-        platform.python_version_tuple()[:2]]))
+lib_name = 'libtau_%s_%s_py%s' % \
+    (platform.system(), platform.architecture()[0],
+     ''.join([str(i) for i in platform.python_version_tuple()[:2]]))
 
 # Import libtau in a platform specific way.
 try:
@@ -23,7 +23,6 @@ except ImportError:
     if platform.system() == "Windows":
         import ctypes as C
         from distutils import sysconfig
-        import numpy as np
         lib_extension, = sysconfig.get_config_vars('SO')
         libtau = C.CDLL(os.path.join(os.path.dirname(__file__), os.pardir,
                         'lib', lib_name + lib_extension))
@@ -41,11 +40,11 @@ except ImportError:
             dddp = np.zeros(60, 'float32', flags)
 
             libtau.ttimes_(C.byref(delta), C.byref(depth), modnam, phase_names,
-                   tt.ctypes.data_as(C.POINTER(C.c_float)),
-                   toang.ctypes.data_as(C.POINTER(C.c_float)),
-                   dtdd.ctypes.data_as(C.POINTER(C.c_float)),
-                   dtdh.ctypes.data_as(C.POINTER(C.c_float)),
-                   dddp.ctypes.data_as(C.POINTER(C.c_float)))
+                           tt.ctypes.data_as(C.POINTER(C.c_float)),
+                           toang.ctypes.data_as(C.POINTER(C.c_float)),
+                           dtdd.ctypes.data_as(C.POINTER(C.c_float)),
+                           dtdh.ctypes.data_as(C.POINTER(C.c_float)),
+                           dddp.ctypes.data_as(C.POINTER(C.c_float)))
             phase_names = np.array([p.value for p in phase_names])
             return phase_names, tt, toang, dtdd, dtdh, dddp
 
