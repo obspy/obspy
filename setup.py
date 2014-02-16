646fce6ff7c14d2094aa8b503616a793ded0dd29
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2014-02-16 15:26:44 +0100

    suds-jurko instead of suds.
    This distribution should work for Python 2 and Python 3.

--- setup.py
+++ setup.py
@@ -91,9 +91,8 @@ INSTALL_REQUIRES = [
     'scipy',
     'matplotlib',
     'lxml',
-    'sqlalchemy']
-    #'suds']
-    #'suds>=0.4.0']
+    'sqlalchemy',
+    'suds-jurko']
 EXTRAS_REQUIRE = {
     'tests': ['flake8>=2',
               'nose']}
