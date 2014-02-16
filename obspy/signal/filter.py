2d7c77c218a157d2697242050065c31a73a81ba0
mbyt <random.seed@web.de>
2014-02-16 14:15:55 +0100

    correcting filter docstrings
    closes #723

--- obspy/signal/filter.py
+++ obspy/signal/filter.py
@@ -35,8 +35,7 @@ def bandpass(data, freqmin, freqmax, df, corners=4, zerophase=False):
     :param freqmin: Pass band low corner frequency.
     :param freqmax: Pass band high corner frequency.
     :param df: Sampling rate in Hz.
-    :param corners: Filter corners. Note: This is twice the value of PITSA's
-        filter sections
+    :param corners: Filter corners / orders.
     :param zerophase: If True, apply filter once forwards and once backwards.
         This results in twice the number of corners but zero phase shift in
         the resulting filtered trace.
@@ -74,8 +73,7 @@ def bandstop(data, freqmin, freqmax, df, corners=4, zerophase=False):
     :param freqmin: Stop band low corner frequency.
     :param freqmax: Stop band high corner frequency.
     :param df: Sampling rate in Hz.
-    :param corners: Filter corners. Note: This is twice the value of PITSA's
-        filter sections
+    :param corners: Filter corners / orders.
     :param zerophase: If True, apply filter once forwards and once backwards.
         This results in twice the number of corners but zero phase shift in
         the resulting filtered trace.
@@ -112,8 +110,7 @@ def lowpass(data, freq, df, corners=4, zerophase=False):
     :param data: Data to filter, type numpy.ndarray.
     :param freq: Filter corner frequency.
     :param df: Sampling rate in Hz.
-    :param corners: Filter corners. Note: This is twice the value of PITSA's
-        filter sections
+    :param corners: Filter corners / orders.
     :param zerophase: If True, apply filter once forwards and once backwards.
         This results in twice the number of corners but zero phase shift in
         the resulting filtered trace.
@@ -146,8 +143,7 @@ def highpass(data, freq, df, corners=4, zerophase=False):
     :param data: Data to filter, type numpy.ndarray.
     :param freq: Filter corner frequency.
     :param df: Sampling rate in Hz.
-    :param corners: Filter corners. Note: This is twice the value of PITSA's
-        filter sections
+    :param corners: Filter corners / orders.
     :param zerophase: If True, apply filter once forwards and once backwards.
         This results in twice the number of corners but zero phase shift in
         the resulting filtered trace.
