obspy/obspy

commit 710d467fbeae23f2c82cee83a7a588eb1f23e866

Py3k: taup `futurize -0 -w .`

obspy/taup/taup.py
@@ -2,6 +2,9 @@
 """
 obspy.taup - Travel time calculation tool
 """
+from __future__ import division
+from __future__ import unicode_literals
+from future.builtins import str
 import inspect
 import numpy as np
 import os
@@ -187,7 +190,7 @@ def travelTimePlot(min_degree=0, max_degree=360, npoints=1000,
                     data[phase][1].append(np.NaN)
                     data[phase][0].append(degree)
     # Plot and some formatting.
-    for key, value in data.iteritems():
+    for key, value in data.items():
         plt.plot(value[0], value[1], '.', label=key)
     plt.grid()
     plt.xlabel('Distance (degrees)')

obspy/taup/__init__.py
@@ -13,6 +13,7 @@ The library iaspei-tau is written in Fortran and interfaced via Python ctypes.
 :license:
     Unknown
 """
+from __future__ import unicode_literals
 
 # Convenience imports.
 from obspy.taup.taup import getTravelTimes, travelTimePlot

obspy/taup/tests/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
 import unittest
 from obspy.core.util import add_doctests, add_unittests
 

obspy/taup/tests/test_taup.py
@@ -2,6 +2,9 @@
 """
 The obspy.taup test suite.
 """
+from __future__ import unicode_literals
+from future.builtins import range
+from future.builtins import open
 from obspy.taup.taup import getTravelTimes
 import os
 import unittest

commit 06eab0ea4ec8ffacbcce9ccb9abce3a05b106884

commit 710d467 broke obspy.taup on Windows - ctypes bindings to libtau do not like unicode filenames

obspy/taup/taup.py
@@ -42,7 +42,8 @@ except ImportError:
             dtdh = np.zeros(60, 'float32', flags)
             dddp = np.zeros(60, 'float32', flags)
 
-            libtau.ttimes_(C.byref(delta), C.byref(depth), modnam, phase_names,
+            libtau.ttimes_(C.byref(delta), C.byref(depth),
+                           modnam.encode('ascii'), phase_names,
                            tt.ctypes.data_as(C.POINTER(C.c_float)),
                            toang.ctypes.data_as(C.POINTER(C.c_float)),
                            dtdd.ctypes.data_as(C.POINTER(C.c_float)),