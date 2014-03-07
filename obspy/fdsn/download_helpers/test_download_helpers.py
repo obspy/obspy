13e639ca0f09e1cb570618e818664be3aca20425
kasra-hosseini <hosseini@geophysik.uni-muenchen.de>
2014-03-07 18:28:37 +0100

    Test for filter_availability

--- obspy/fdsn/download_helpers/test_download_helpers.py
+++ obspy/fdsn/download_helpers/test_download_helpers.py
@@ -13,6 +13,7 @@ from __future__ import unicode_literals
 from future import standard_library  # NOQA
 import unittest
 
+from obspy.fdsn.download_helpers.utils import filter_availability
 
 from obspy.fdsn.download_helpers.download_helpers import \
     _filter_channel_priority
@@ -22,11 +23,35 @@ class DownloadHelpersUtilTestCase(unittest.TestCase):
     """
     Test cases for utility functionality for the download helpers.
     """
-    def test_something(self):
+    def test_filter_availability(self):
         """
         Does not yet do anything.
         """
-        self.assertTrue(True)
+        dict_client1 =\
+          {'BW.FURT': {'channels': ['.BHE', '.BHN', '.BHZ', '00.LHE', '00.LHE'],
+          'elevation_in_m': 10.0,
+          'latitude': 1.0,
+          'longitude': 2.0}
+          }
+        dict_client2 =\
+          {'BE1.FURT': {'channels': ['.BHE', '.BHN', '.BHZ', '00.LHE', '00.LHE'],
+          'elevation_in_m': 10.0,
+          'latitude': 1.2,
+          'longitude': 2.0},
+          'BE2.FURT': {'channels': ['.BHE', '.BHN', '.BHZ', '00.LHE', '00.LHE'],
+          'elevation_in_m': 10.0,
+          'latitude': -1.2,
+          'longitude': 2.0},
+          'BE3.FURT': {'channels': ['.BHE', '.BHN', '.BHZ', '00.LHE', '00.LHE'],
+          'elevation_in_m': 10.0,
+          'latitude': 1.0001,
+          'longitude': 2.0}
+          }
+        output = filter_availability(dict_client1, dict_client2, 
+                                    dist_threshold=20)
+        self.assertEqual(dict_client1, output)
+
+
 
     def test_channel_priority_filtering(self):
         """
