2f2815de3cbf4d04c19b85a8927bab84b96387c7
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2014-04-14 11:09:38 +0200

    Added test for reading a file with multiple events.

--- obspy/ndk/tests/test_ndk.py
+++ obspy/ndk/tests/test_ndk.py
@@ -28,6 +28,44 @@ class NDKTestCase(unittest.TestCase):
 
         self.assertEqual(cat, ref_cat)
 
+    def test_read_multiple_events(self):
+        """
+        Tests the reading of multiple events in one file. The file has been
+        edited to test a variety of settings.
+        """
+        filename = os.path.join(self.datapath, "multiple_events.ndk")
+        cat = read_ndk(filename)
+
+        self.assertEqual(len(cat), 6)
+
+        # Test the type of moment tensor inverted for.
+        self.assertEqual([i.focal_mechanisms[0].moment_tensor.inversion_type
+                          for i in cat],
+                         ["general", "zero trace", "double couple"] * 2)
+
+        # Test the type and duration of the moment rate function.
+        self.assertEqual(
+            [i.focal_mechanisms[0].moment_tensor.source_time_function.type
+             for i in cat],
+            ["triangle", "box car"] * 3)
+        self.assertEqual(
+            [i.focal_mechanisms[0].moment_tensor.source_time_function.duration
+             for i in cat],
+            [2.6, 7.4, 9.0, 1.8, 2.0, 1.6])
+
+        # Test the type of depth setting.
+        self.assertEqual([i.preferred_origin().depth_type for i in cat],
+                         ["from moment tensor inversion", "from location",
+                          "from modeling of broad-band P waveforms"] * 2)
+
+        # Check the solution type.
+        for event in cat[:3]:
+            self.assertTrue("Standard" in
+                            event.focal_mechanisms[0].comments[0].text)
+        for event in cat[3:]:
+            self.assertTrue("Quick" in
+                            event.focal_mechanisms[0].comments[0].text)
+
 
 def suite():
     return unittest.makeSuite(NDKTestCase, 'test')
