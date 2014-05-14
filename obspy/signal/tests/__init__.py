af95797b6b25ee924c6b1014cc1e5d1e46bd26c8
Robert Barsch <barsch@egu.eu>
2014-05-14 23:21:28 +0200

    remove check for deprecation warning which was already purged ;)

--- obspy/signal/tests/__init__.py
+++ obspy/signal/tests/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-import warnings
 import unittest
 from obspy.core.util import add_doctests, add_unittests
 
@@ -11,13 +10,8 @@ MODULE_NAME = "obspy.signal"
 
 def suite():
     suite = unittest.TestSuite()
-    with warnings.catch_warnings(record=True):
-        warnings.filterwarnings(
-            'ignore', 'Module obspy.signal.psd is deprecated! '
-            'Use obspy.signal.spectral_estimation instead or import directly '
-            '"from obspy.signal import ...".', category=DeprecationWarning)
-        add_doctests(suite, MODULE_NAME)
-        add_unittests(suite, MODULE_NAME)
+    add_doctests(suite, MODULE_NAME)
+    add_unittests(suite, MODULE_NAME)
     return suite
 
 
