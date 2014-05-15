cede5034791cd7e830cbea59989b00bddc0ec98c
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2014-05-15 09:07:35 +0200

    Cleaning up version.py encoding issues.

--- obspy/core/util/version.py
+++ obspy/core/util/version.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 from __future__ import unicode_literals
-from future.utils import PY3
 # Author: Douglas Creager <dcreager@dcreager.net>
 # This file is placed into the public domain.
 
@@ -37,6 +36,7 @@ from future.utils import PY3
 __all__ = ("get_git_version")
 
 # NO IMPORTS FROM OBSPY IN THIS FILE! (file gets used at installation time)
+import io
 import os
 import inspect
 from subprocess import Popen, PIPE
@@ -54,24 +54,22 @@ def call_git_describe(abbrev=4):
         p = Popen(['git', 'rev-parse', '--show-toplevel'],
                   cwd=OBSPY_ROOT, stdout=PIPE, stderr=PIPE)
         p.stderr.close()
-        path = p.stdout.readlines()[0].strip()
+        path = p.stdout.readline().decode().strip()
         p.stdout.close()
     except:
         return None
     path = os.path.normpath(path)
-    if PY3:
-        path = path.decode()
     if path != OBSPY_ROOT:
         return None
     try:
         p = Popen(['git', 'describe', '--dirty', '--abbrev=%d' % abbrev,
                    '--always', '--tags'],
                   cwd=OBSPY_ROOT, stdout=PIPE, stderr=PIPE)
+
         p.stderr.close()
-        line = p.stdout.readlines()[0]
-        if PY3:
-            line = line.decode().rstrip()
+        line = p.stdout.readline.decode()
         p.stdout.close()
+
         # (this line prevents official releases)
         # should work again now, see #482 and obspy/obspy@b437f31
         if "-" not in line and "." not in line:
@@ -83,15 +81,16 @@ def call_git_describe(abbrev=4):
 
 def read_release_version():
     try:
-        version = open(VERSION_FILE, "rb").readlines()[0]
-        return version.strip().decode()
+        with io.open(VERSION_FILE, "rt") as fh:
+            version = fh.readlines()[0]
+        return version.strip()
     except:
         return None
 
 
 def write_release_version(version):
-    open(VERSION_FILE, "wb").write(
-        ("%s\n" % version).encode('ascii', 'strict'))
+    with io.open(VERSION_FILE, "wb") as fh:
+        fh.write(("%s\n" % version).encode('ascii', 'strict'))
 
 
 def get_git_version(abbrev=4):
