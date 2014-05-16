12dd9151eebf649caeb3ab6794eac80c8a554716
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2014-05-16 09:55:57 +0200

    Test to enforce the same future import in every single file.
    This should guarantee a consitent behaviour across the code
    base.

--- obspy/core/tests/test_code_formatting.py
+++ obspy/core/tests/test_code_formatting.py
@@ -3,9 +3,11 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from future.builtins import *  # NOQA
 
+import inspect
 from obspy.core.util.decorator import skipIf
 from obspy.core.util.testing import check_flake8
 import os
+import re
 import unittest
 
 
@@ -13,6 +15,7 @@ class CodeFormattingTestCase(unittest.TestCase):
     """
     Test codebase for compliance with the flake8 tool.
     """
+
     @skipIf('OBSPY_NO_FLAKE8' in os.environ, 'flake8 check disabled')
     def test_flake8(self):
         """
@@ -25,8 +28,56 @@ class CodeFormattingTestCase(unittest.TestCase):
         self.assertEqual(error_count, 0, message)
 
 
+class FutureUsageTestCase(unittest.TestCase):
+    def test_future_imports_in_every_file(self):
+        """
+        Tests that every single Python file includes the appropriate future
+        headers to enforce consistent behaviour.
+        """
+        test_dir = os.path.abspath(inspect.getfile(inspect.currentframe()))
+        obspy_dir = os.path.dirname(os.path.dirname(os.path.dirname(test_dir)))
+        print(os.path.abspath(obspy_dir))
+
+        future_import_line = (
+            "from __future__ import (absolute_import, division, "
+            "print_function, unicode_literals)")
+        builtins_line = "from future.builtins import *  # NOQA"
+
+        future_imports_pattern = re.compile(
+            r"^from __future__ import \(absolute_import,\s*"
+            r"division,\s*print_function,\s*unicode_literals\)$",
+            flags=re.MULTILINE)
+
+        builtin_pattern = re.compile(
+            r"^from future\.builtins import \*  # NOQA$",
+            flags=re.MULTILINE)
+
+        failures = []
+        # Walk the obspy directory.
+        for dirpath, _, filenames in os.walk(obspy_dir):
+            # Find all Python files.
+            filenames = [os.path.abspath(os.path.join(dirpath, i)) for i in
+                         filenames if i.endswith(".py")]
+            for filename in filenames:
+                with open(filename, "rt") as fh:
+                    content = fh.read()
+
+                    if re.search(future_imports_pattern, content) is None:
+                        failures.append("File '%s' misses imports: %s" %
+                                        (filename, future_import_line))
+
+                    if re.search(builtin_pattern, content) is None:
+                        failures.append("File '%s' misses imports: %s" %
+                                        (filename, builtins_line))
+        self.assertEqual(len(failures), 0, "\n" + "\n".join(failures))
+
+
 def suite():
-    return unittest.makeSuite(CodeFormattingTestCase, 'test')
+
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(CodeFormattingTestCase, 'test'))
+    suite.addTest(unittest.makeSuite(FutureUsageTestCase, 'test'))
+    return suite
 
 
 if __name__ == '__main__':
