74b5a57f7818134ef864c0dedaec33cdd025f943
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2014-06-21 01:10:42 -0400

    Use argparse in obspy-print.

--- obspy/core/scripts/print.py
+++ obspy/core/scripts/print.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-USAGE: obspy-print [ -f format ] file1 file2 ...
-
 Print stream information for waveform data in local files
 """
 from __future__ import (absolute_import, division, print_function,
@@ -11,27 +9,30 @@ from future.builtins import *  # NOQA
 
 from obspy import read, Stream
 from obspy import __version__
-from optparse import OptionParser
+from argparse import ArgumentParser
 
 
 def main():
-    parser = OptionParser(__doc__.strip(), version="%prog " + __version__)
-    parser.add_option("-f", default=None, type="string",
-                      dest="format", help="Waveform format.")
-    parser.add_option("-n", "--nomerge", default=True, action="store_false",
-                      dest="merge", help="Switch off cleanup merge.")
-    parser.add_option("-g", "--print-gaps", default=False, action="store_true",
-                      dest="print_gaps",
-                      help="Switch on printing of gap information.")
+    parser = ArgumentParser(prog='obspy-print', description=__doc__.strip())
+    parser.add_argument('-V', '--version', action='version',
+                        version='%(prog)s ' + __version__)
+    parser.add_argument('-f', '--format', default=None,
+                        help='Waveform format (slightly faster if specified).')
+    parser.add_argument('-n', '--nomerge', action='store_false',
+                        dest='merge', help='Switch off cleanup merge.')
+    parser.add_argument('-g', '--print-gaps', action='store_true',
+                        help='Switch on printing of gap information.')
+    parser.add_argument('files', nargs='+',
+                        help='Files to process.')
+    args = parser.parse_args()
 
-    (options, args) = parser.parse_args()
     st = Stream()
-    for arg in args:
-        st += read(arg, format=options.format)
-    if options.merge:
+    for f in args.files:
+        st += read(f, format=args.format)
+    if args.merge:
         st.merge(-1)
     print(st)
-    if options.print_gaps:
+    if args.print_gaps:
         print()
         st.printGaps()
 
