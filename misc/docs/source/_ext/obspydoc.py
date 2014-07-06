6b51a50be1e412d4af5499e4ed3fe8b2e0a103ec
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2014-07-06 04:11:14 -0400

    doc: Fix the citations list styling.
    It seems to be impossible to get the class onto the reference label
    using pure reST, so we have to modify the HTML after it's created.

--- misc/docs/source/_ext/obspydoc.py
+++ misc/docs/source/_ext/obspydoc.py
@@ -1,4 +1,5 @@
 
+import re
 
 def post_process_html(app, pagename, templatename, context, doctree):
     try:
@@ -7,6 +8,24 @@ def post_process_html(app, pagename, templatename, context, doctree):
     except:
         pass
 
+    if not doctree or not doctree.has_name('citations'):
+        return
+
+    # Fix citations list
+    body = re.sub(
+        r'<td><table class="first last docutils citation" '
+        r'''frame="void" id="(?P<tag>[A-Za-z0-9]+)" rules="none">
+<colgroup><col class="label" /><col /></colgroup>
+<tbody valign="top">
+<tr><td class="label">(?P<content>\[[A-Za-z0-9]+\])</td><td></td></tr>
+</tbody>
+</table>
+</td>''',
+        r'<td id="\g<tag>">'
+        r'<span class="label label-default">\g<content></span>'
+        r'</td>',
+        context['body'])
+    context['body'] = body
 
 def setup(app):
     app.connect('html-page-context', post_process_html)
