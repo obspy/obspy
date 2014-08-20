6242008b22de8534414fe5d50ce8d725f1ee29e7
Tobias Megies <megies@geophysik.uni-muenchen.de>
2014-08-20 21:17:28 +0200

    safer handling of path of test file

--- obspy/core/tests/test_trace.py
+++ obspy/core/tests/test_trace.py
@@ -1746,18 +1746,13 @@ class TraceTestCase(unittest.TestCase):
         This also tests the interpolation in obspy.signal. No need to repeat
         the same test twice I guess.
         """
-        path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(
-            __file__))), 'sac', 'tests', 'data')
         # Load the prepared data. The data has been created using SAC.
-        org_tr = read(os.path.join(
-            path,
-            "interpolation_test_random_waveform_delta_0.01_npts_50.sac"))[0]
-        interp_delta_0_003 = read(os.path.join(
-            path,
-            "interpolation_test_interpolated_delta_0.003.sac"))[0]
-        interp_delta_0_077 = read(os.path.join(
-            path,
-            "interpolation_test_interpolated_delta_0.077.sac"))[0]
+        file_ = "interpolation_test_random_waveform_delta_0.01_npts_50.sac"
+        org_tr = read("/path/to/%s" % file_)[0]
+        file_ = "interpolation_test_interpolated_delta_0.003.sac"
+        interp_delta_0_003 = read("/path/to/%s" % file_)[0]
+        file_ = "interpolation_test_interpolated_delta_0.077.sac"
+        interp_delta_0_077 = read("/path/to/%s" % file_)[0]
 
         # Perform the same interpolation as in Python with ObsPy.
         int_tr = org_tr.copy().interpolate(sampling_rate=1.0 / 0.003,
