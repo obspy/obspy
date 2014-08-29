21fc817dc72b117f92cec475d4f7d90a8dc872f7
Tobias Megies <megies@geophysik.uni-muenchen.de>
2014-08-29 18:51:58 +0200

    deb: again.. try to fix squeeze
    (gaaah python-support has some trigger linking files around but ignoring
    symlinks)

--- debian.python_distutils/_dh_link_data_files.sh
+++ debian.python_distutils/_dh_link_data_files.sh
@@ -3,17 +3,15 @@
 CODENAME=`lsb_release -cs`
 
 rm -rf debian/python-obspy.links debian/python3-obspy.links
-for DIRTYPE in data images
-do
-    for FILE in `find debian/tmp -type f -regex ".*/python2.*/obspy/.*?/tests/${DIRTYPE}/.*"`
+if [ "$CODENAME" != "squeeze" ]
+then
+    for DIRTYPE in data images
     do
-        SOURCE=`echo $FILE | sed 's#.*-packages/obspy/#usr/share/obspy/#'`
-        if [ "$CODENAME" = "squeeze" ]
-        then
-            DESTINATION=`echo $FILE | sed 's#.*-packages/obspy/#usr/share/pyshared/obspy/##'`
-        else
+        for FILE in `find debian/tmp -type f -regex ".*/python2.*/obspy/.*?/tests/${DIRTYPE}/.*"`
+        do
+            SOURCE=`echo $FILE | sed 's#.*-packages/obspy/#usr/share/obspy/#'`
             DESTINATION=`echo $FILE | sed 's#debian/tmp/##'`
-        fi
-        echo $SOURCE $DESTINATION >> debian/python-obspy.links
+            echo $SOURCE $DESTINATION >> debian/python-obspy.links
+        done
     done
-done
+fi
--- debian/_dh_install_data_files.sh
+++ debian/_dh_install_data_files.sh
@@ -5,7 +5,12 @@ do
     for DIR in `ls -d debian/tmp/usr/lib/python2*/*-packages/obspy/*/tests/${DIRTYPE}`
     do
         MOD=`echo $DIR | sed 's#.*obspy/##' | sed 's#/.*##'`
-        TARGET=usr/share/obspy/${MOD}/tests/${DIRTYPE}
+        if [ "$CODENAME" = "squeeze" ]
+        then
+            TARGET=usr/share/pyshared/obspy/${MOD}/tests/${DIRTYPE}
+        else
+            TARGET=usr/share/obspy/${MOD}/tests/${DIRTYPE}
+        fi
         dh_installdirs -p python-obspy-dbg ${TARGET}
         dh_install -p python-obspy-dbg ${DIR}/* ${TARGET}
     done
