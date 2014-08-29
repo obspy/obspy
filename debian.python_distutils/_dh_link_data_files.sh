893631f69c546528e0d924b1176f03930dc3e35b
Tobias Megies <megies@geophysik.uni-muenchen.de>
2014-08-29 17:43:16 +0200

    deb: fix previous commit

--- debian.python_distutils/_dh_link_data_files.sh
+++ debian.python_distutils/_dh_link_data_files.sh
@@ -10,7 +10,7 @@ do
         echo $SOURCE $DESTINATION >> debian/python-obspy.links
     done
     # for squeeze:
-    for FILE in `find debian/tmp -type f -regex "usr/share/pyshared/obspy/.*?/tests/${DIRTYPE}/.*"`
+    for FILE in `find debian/tmp -type f -regex ".*/usr/share/pyshared/obspy/.*?/tests/${DIRTYPE}/.*"`
     do
         SOURCE=`echo $FILE | sed 's#.*pyshared/obspy/#usr/share/obspy/#'`
         DESTINATION=`echo $FILE | sed 's#debian/tmp/##'`
