7b4dc54e7b27bd360e7a7b7965c2991c5777da06
Tobias Megies <megies@geophysik.uni-muenchen.de>
2014-08-29 18:00:40 +0200

    deb: try to fix squeeze build yet again

--- debian.python_distutils/_dh_link_data_files.sh
+++ debian.python_distutils/_dh_link_data_files.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
+CODENAME=`lsb_release -cs`
+
 rm -rf debian/python-obspy.links debian/python3-obspy.links
 for DIRTYPE in data images
 do
     for FILE in `find debian/tmp -type f -regex ".*/python2.*/obspy/.*?/tests/${DIRTYPE}/.*"`
     do
         SOURCE=`echo $FILE | sed 's#.*-packages/obspy/#usr/share/obspy/#'`
-        DESTINATION=`echo $FILE | sed 's#debian/tmp/##'`
-        echo $SOURCE $DESTINATION >> debian/python-obspy.links
-    done
-    # for squeeze:
-    for FILE in `find debian/tmp -type f -regex ".*/usr/share/pyshared/obspy/.*?/tests/${DIRTYPE}/.*"`
-    do
-        SOURCE=`echo $FILE | sed 's#.*pyshared/obspy/#usr/share/obspy/#'`
-        DESTINATION=`echo $FILE | sed 's#debian/tmp/##'`
+        if [ "$CODENAME" == "squeeze" ]
+        then
+            DESTINATION=`echo $FILE | sed 's#.*-packages/obspy/#usr/share/pyshared/obspy/##'`
+        else
+            DESTINATION=`echo $FILE | sed 's#debian/tmp/##'`
+        fi
         echo $SOURCE $DESTINATION >> debian/python-obspy.links
     done
 done
