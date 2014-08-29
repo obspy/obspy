b9b896c9140715923706b1d6952e11f208241af6
Tobias Megies <megies@geophysik.uni-muenchen.de>
2014-08-29 17:29:04 +0200

    deb: try to fix packaging for squeeze

--- debian.pybuild/_dh_link_data_files.sh
+++ debian.pybuild/_dh_link_data_files.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 
-#DIRS=`ls -d debian/python-obspy/usr/lib/python2*/*-packages/obspy/*/tests/data` `ls -d debian/python-obspy/usr/lib/python2*/*-packages/obspy/*/tests/images`
-#for DIR in $DIRS
-#do
-#    SOURCEDIR=`echo $DIR | sed 's#debian/python-obspy/##'`
-#    TARGETDIR=`echo $SOURCEDIR | sed 's#.*?-packages/obspy/#usr/share/obspy/#'`
-#    for FILE in `ls $DIR`
-#    do
-#        FILENAME=`echo $FILE | sed 's#.*/##'`
-#        echo "${TARGETDIR}/$FILENAME ${SOURCEDIR}/$FILENAME" >> python-obspy.links
-#    done
-#done
-#
-
-#for DIR in `ls debian/python-obspy/usr/lib/python2*/*-packages/obspy/*/tests/data debian/python-obspy/usr/lib/python2*/*-packages/obspy/*/tests/images`
-#do
-#    SOURCEFOLDER=`echo $DIR | sed 's#-*?-packages/obspy/#usr/share/obspy/#'`
-#    LINKFOLDER=`echo $DIR | sed 's#debian/python-obspy/##'`
-#    for FILE in `ls ${DIR}/*`
-#    do
-#        LINK=`echo $FILE | sed 's#debian/python-obspy-data/usr/share/obspy/##'`
-#        FILE=`echo $FILE | sed 's#.*?-packages/obspy/##'`
-#        echo "usr/share/obspy/$FILE `echo $DIR | sed 's#debian/python-obspy/##'`${FILE}" >> python-obspy.links
-#    done
-#done
-
 rm -rf debian/python-obspy.links debian/python3-obspy.links
 for DIRTYPE in data images
 do
@@ -41,20 +16,3 @@ do
         echo $SOURCE $DESTINATION >> debian/python3-obspy.links
     done
 done
-
-#dh_link -p python-obspy $LINKFILES
-#
-#for DIR in DIRS=`ls -d debian/python3-obspy/usr/lib/python3*/*-packages/obspy/*/tests/*`
-#do
-#    SOURCEDIR=`echo $DIR | sed 's#debian/python3-obspy/##'`
-#    TARGETDIR=`echo $SOURCEDIR | sed 's#.*?-packages/obspy/#usr/share/obspy/#'`
-#    dh_link -p python-obspy ${TARGETDIR}/* ${SOURCEDIR}
-#done
-
-#rm -rf python3-obspy.links
-#for FILE in `ls debian/python-obspy/usr/lib/python3*/*-packages/obspy/*/tests/data/* debian/python-obspy/usr/lib/python3*/*-packages/obspy/*/tests/images/*`
-#do
-#    LINK=`echo $FILE | sed 's#debian/python3-obspy/##'`
-#    FILE=`echo $FILE | sed 's#.*?-packages/obspy/##'`
-#    echo "usr/share/obspy/$FILE $LINK" >> python3-obspy.links
-#done
--- debian.python_distutils/_dh_link_data_files.sh
+++ debian.python_distutils/_dh_link_data_files.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 
-#DIRS=`ls -d debian/python-obspy/usr/lib/python2*/*-packages/obspy/*/tests/data` `ls -d debian/python-obspy/usr/lib/python2*/*-packages/obspy/*/tests/images`
-#for DIR in $DIRS
-#do
-#    SOURCEDIR=`echo $DIR | sed 's#debian/python-obspy/##'`
-#    TARGETDIR=`echo $SOURCEDIR | sed 's#.*?-packages/obspy/#usr/share/obspy/#'`
-#    for FILE in `ls $DIR`
-#    do
-#        FILENAME=`echo $FILE | sed 's#.*/##'`
-#        echo "${TARGETDIR}/$FILENAME ${SOURCEDIR}/$FILENAME" >> python-obspy.links
-#    done
-#done
-#
-
-#for DIR in `ls debian/python-obspy/usr/lib/python2*/*-packages/obspy/*/tests/data debian/python-obspy/usr/lib/python2*/*-packages/obspy/*/tests/images`
-#do
-#    SOURCEFOLDER=`echo $DIR | sed 's#-*?-packages/obspy/#usr/share/obspy/#'`
-#    LINKFOLDER=`echo $DIR | sed 's#debian/python-obspy/##'`
-#    for FILE in `ls ${DIR}/*`
-#    do
-#        LINK=`echo $FILE | sed 's#debian/python-obspy-data/usr/share/obspy/##'`
-#        FILE=`echo $FILE | sed 's#.*?-packages/obspy/##'`
-#        echo "usr/share/obspy/$FILE `echo $DIR | sed 's#debian/python-obspy/##'`${FILE}" >> python-obspy.links
-#    done
-#done
-
 rm -rf debian/python-obspy.links debian/python3-obspy.links
 for DIRTYPE in data images
 do
@@ -34,27 +9,11 @@ do
         DESTINATION=`echo $FILE | sed 's#debian/tmp/##'`
         echo $SOURCE $DESTINATION >> debian/python-obspy.links
     done
-    for FILE in `find debian/tmp -type f -regex ".*/python3.*/obspy/.*?/tests/${DIRTYPE}/.*"`
+    # for squeeze:
+    for FILE in `find debian/tmp -type f -regex "usr/share/pyshared/obspy/.*?/tests/${DIRTYPE}/.*"`
     do
-        SOURCE=`echo $FILE | sed 's#.*-packages/obspy/#usr/share/obspy/#'`
+        SOURCE=`echo $FILE | sed 's#.*pyshared/obspy/#usr/share/obspy/#'`
         DESTINATION=`echo $FILE | sed 's#debian/tmp/##'`
-        echo $SOURCE $DESTINATION >> debian/python3-obspy.links
+        echo $SOURCE $DESTINATION >> debian/python-obspy.links
     done
 done
-
-#dh_link -p python-obspy $LINKFILES
-#
-#for DIR in DIRS=`ls -d debian/python3-obspy/usr/lib/python3*/*-packages/obspy/*/tests/*`
-#do
-#    SOURCEDIR=`echo $DIR | sed 's#debian/python3-obspy/##'`
-#    TARGETDIR=`echo $SOURCEDIR | sed 's#.*?-packages/obspy/#usr/share/obspy/#'`
-#    dh_link -p python-obspy ${TARGETDIR}/* ${SOURCEDIR}
-#done
-
-#rm -rf python3-obspy.links
-#for FILE in `ls debian/python-obspy/usr/lib/python3*/*-packages/obspy/*/tests/data/* debian/python-obspy/usr/lib/python3*/*-packages/obspy/*/tests/images/*`
-#do
-#    LINK=`echo $FILE | sed 's#debian/python3-obspy/##'`
-#    FILE=`echo $FILE | sed 's#.*?-packages/obspy/##'`
-#    echo "usr/share/obspy/$FILE $LINK" >> python3-obspy.links
-#done
