c0c86d859c755f2b4cfd36d3fa6c155acca33f09
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2014-10-06 12:27:25 +0200

    More descriptive setup.py script.

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Setup script for the waveform solver input file generator.
+Setup script for TauPy.
 
 :copyright:
-    Lion Krischer (krischer@geophysik.uni-muenchen.de), 2013
+    Nicolas Rothenhäusler (n.rothenhaeusler@gmail.com)
+    Lion Krischer (krischer@geophysik.uni-muenchen.de), 2013-2014
 :license:
     GNU General Public License, Version 3
     (http://www.gnu.org/copyleft/gpl.html)
 """
-import inspect
-import os
 from setuptools import setup, find_packages
 
 setup_config = dict(
@@ -18,10 +17,29 @@ setup_config = dict(
     version="0.0.1a",
     description="Python port of TauP",
     packages=find_packages(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or ' +
+        'Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Physics'],
+    url="https://github.com/krischer/TauPy",
     license="GNU General Public License, version 3 (GPLv3)",
     platforms="OS Independent",
+    requires=["numpy"],
+    include_package_data=True,
+     # this is needed for "easy_install taupy==dev"
+    download_url=("https://github.com/krischer/TauPy/zipball/master"
+                  "#egg=obspy=dev"),
 )
 
 
 if __name__ == "__main__":
-    setup(**setup_config, requires=['numpy'])
+    setup(**setup_config)
