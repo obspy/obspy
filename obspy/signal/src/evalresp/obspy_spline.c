0df6a85860b6d151a1453f921878813b10a88d06
mbyt <random.seed@web.de>
2014-10-20 09:34:25 +0200

    beautifying

--- obspy/signal/src/evalresp/obspy_spline.c
+++ obspy/signal/src/evalresp/obspy_spline.c
@@ -15,7 +15,6 @@
  */
 
 #include <stdlib.h>
-#include <stdio.h>
 #include <math.h>
 #include <float.h>
 #include "spline.h"
--- obspy/signal/src/evalresp/obspy_spline.h
+++ obspy/signal/src/evalresp/obspy_spline.h
@@ -1,9 +1,9 @@
  /* Copyright (C) ObsPy Development Team, 2014.
- *
- * This file is licensed under the terms of the GNU Lesser General Public
- * License, Version 3 (http://www.gnu.org/copyleft/lesser.html).
- *
- */
+  *
+  * This file is licensed under the terms of the GNU Lesser General Public
+  * License, Version 3 (http://www.gnu.org/copyleft/lesser.html).
+  *
+  */
 
 char *evr_spline(int num_points, double *t, double *y, double tension,
                                  double k, double *xvals_arr, int num_xvals,
