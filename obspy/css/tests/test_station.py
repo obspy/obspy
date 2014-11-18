8cca7f731a54e2bf43ff4a946edf5745622a991d
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2014-11-17 19:59:31 -0500

    Add CSS format export of Inventories.

---
+++ obspy/css/tests/test_station.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Test suite for the CSS station writer.
+
+:copyright:
+    The ObsPy Development Team (devs@obspy.org)
+:license:
+    GNU Lesser General Public License, Version 3
+    (http://www.gnu.org/copyleft/lesser.html)
+"""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from future.builtins import *  # NOQA
+
+import fnmatch
+import inspect
+import os
+import shutil
+import tempfile
+import unittest
+
+import obspy.station
+
+
+class CSSStationTestCase(unittest.TestCase):
+    """
+    """
+    def setUp(self):
+        # Most generic way to get the actual data directory.
+        self.data_dir = os.path.join(os.path.dirname(os.path.abspath(
+            inspect.getfile(inspect.currentframe()))), 'data', 'station')
+
+    def test_write_default(self):
+        """
+        Test that writing of a CSS station database with all possible
+        relations works.
+        """
+        fname = 'default'
+
+        inv = obspy.station.read_inventory()
+
+        tempdir = tempfile.mkdtemp(prefix='obspy-')
+
+        try:
+            inv.write(os.path.join(tempdir, fname), format='CSS')
+
+            expected_files = sorted(name for name in os.listdir(self.data_dir)
+                                    if fnmatch.fnmatch(name, fname + '.*'))
+            actual_files = sorted(os.listdir(tempdir))
+            self.assertEqual(expected_files, actual_files)
+
+            for expected, actual in zip(expected_files, actual_files):
+                with open(os.path.join(self.data_dir, expected), 'rb') as f:
+                    expected_text = f.readlines()
+                with open(os.path.join(tempdir, actual), 'rb') as f:
+                    actual_text = f.readlines()
+
+                self.assertEqual(expected_text, actual_text)
+
+        finally:
+            shutil.rmtree(tempdir)
+
+
+def suite():
+    return unittest.makeSuite(CSSStationTestCase, "test")
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
