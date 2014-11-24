380941050394d7f88c5bc39c25705349af895986
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2014-11-24 14:59:43 +0100

    Refactored the restrictions to their own file.

--- obspy/fdsn/download_helpers/__init__.py
+++ obspy/fdsn/download_helpers/__init__.py
@@ -359,6 +359,7 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from future.builtins import *  # NOQA
 
-from .download_helpers import DownloadHelper, Restrictions
+from .download_helpers import DownloadHelper
+from .restrictions import Restrictions
 from .domain import Domain, RectangularDomain, CircularDomain, GlobalDomain
 from .utils import format_report
