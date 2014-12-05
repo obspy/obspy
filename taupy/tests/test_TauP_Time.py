fc7706a7881e3c6bcbb5067a278778f1e5bc3d54
Rothenhouser <n.rothenhaeusler@gmail.com>
2014-12-05 17:26:58 +0100

    Merge the tests for TauP_Time.

--- taupy/tests/test_TauP_Time.py
+++ taupy/tests/test_TauP_Time.py
@@ -1,53 +1,117 @@
-import unittest, sys, subprocess, os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This file tests the TauP_Time utility  against the original TauPy using
+both the high-level tau interface of TauPy and the java-like old script-based
+interface.
+"""
+import inspect
+import os
+import unittest
+import sys
+import subprocess
+
+from taupy.tau import TauPyModel
 from taupy.TauP_Time import TauP_Time
+
+# Most generic way to get the data folder path.
+DATA = os.path.join(os.path.dirname(os.path.abspath(
+    inspect.getfile(inspect.currentframe()))), "data", "TauP_test_data")
+
+
+def parse_taup_time_output(filename):
+    with open(filename, "rt") as fh:
+        data_started = False
+        arrivals = []
+        for line in fh:
+            line = line.strip()
+            if not line:
+                continue
+            if line.startswith("-------"):
+                data_started = True
+                continue
+            if data_started is False:
+                continue
+            l = [_i.strip() for _i in line.split() if _i != "="]
+            arrivals.append({
+                "distance": float(l[0]),
+                "depth": float(l[1]),
+                "phase_name": str(l[2]),
+                "time": float(l[3]),
+                "ray_param": float(l[4]),
+                "takeoff": float(l[5]),
+                "incident": float(l[6]),
+                "purist_distance": float(l[7]),
+                "purist_name": str(l[8]),
+            })
+    return arrivals
+
+
+def compare_arrivals_with_taup_time_output(arrivals, filename):
+    """
+    """
+    filename = os.path.join(DATA, filename)
+    expected_arrivals = parse_taup_time_output(filename)
+
+    arrivals = [
+        {
+            "distance": _i.getModuloDistDeg(),
+            "depth": _i.sourceDepth,
+            "phase_name": _i.phase.name,
+            "time": _i.time,
+            "ray_param": _i.rayParam_sec_degree,
+            "takeoff": _i.takeoffAngle,
+            "incident": _i.incidentAngle,
+            "purist_distance": _i.getDistDeg(),
+            "purist_name": _i.puristName
+        } for _i in arrivals]
+
+    # Sort both by time.
+    expected_arrivals = sorted(expected_arrivals, key=lambda x: x["time"])
+    arrivals = sorted(arrivals, key=lambda x: x["time"])
+
+    assert len(expected_arrivals) == len(arrivals)
+
+    for e_arr, arr in zip(expected_arrivals, arrivals):
+        assert sorted(e_arr.keys()) == sorted(arr.keys())
+        for key, value in e_arr.items():
+            if isinstance(value, float):
+                # Estimate the precision in the taup output.
+                v = str(value)
+                prec = len(v) - v.find(".") - 1
+                assert value == round(arr[key], prec)
+            else:
+                assert value == arr[key]
+
+
+def test_all_phases_iasp91_35_deg_distance():
+    """
+    Tests tauptime at 35 degree distance, phases ttall.
+    """
+    model = TauPyModel("iasp91")
+    tts = model.get_travel_time(source_depth_in_km=10.0,
+                                distance_in_degree=35.0)
+    compare_arrivals_with_taup_time_output(
+        tts, "taup_time_-h_10_-ph_ttall_-deg_35")
 
 
 class TestTauPTime(unittest.TestCase):
-
-    # def test_main(self):
-    #     tt = TauP_Time(["S", "P"], "iasp91", 200, 57.4)
-    #     tt.run()
-    #     self.assertEqual(tt.tMod.tauBranches[0][3].maxRayParam,
-    #                      742.2891566265059)
-    #     # Of course tMod should be correct, if TauP_Create works as it should.
-    #     # As far as I could tell, it does, even though it's a bit difficult
-    #     # to compare to the java all at once and there may be some rounding
-    #     # differences some 10 digits behind the comma.
-    #
-    #     self.assertEqual(tt.tModDepth.sourceDepth, 200)
-    #     # Checking the corected tModDepth will be difficult again...
-    #     # Also calls recalcPhases, so check phases here...
-    #     self.assertEqual(tt.tMod.tauBranches[0][7],
-    #                      tt.tModDepth.tauBranches[0][8])
-    #     self.assertEqual(tt.tModDepth.tauBranches[0][8].maxRayParam,
-    #                      109.7336675261915)
-    #     self.assertEqual(tt.tMod.rayParams, tt.tModDepth.rayParams)
-    #     self.assertEqual(tt.tModDepth.noDisconDepths[0], 200)
-    #     self.assertEqual(len(tt.tModDepth.sMod.PLayers), 562)
-    #     self.assertEqual(tt.tModDepth.sMod.PLayers[105].topP,
-    #                      745.9964237713114)
-    #     self.assertEqual(tt.tModDepth, tt.phases[0].tMod)
-    #     self.assertEqual(len(tt.tModDepth.sMod.criticalDepths), 9)
-        # Following is commented because sometimes phases[0] is P, sometimes S.
-        # Not sure why. Testing result is pretty unpredictable, most recently
-        # it worked via nosetests but not in the IDE, but it has also been the
-        # other way round. ???????????????????????????
-        # self.assertTrue(all(len(x) == 234 for x in (
-        #     tt.phases[0].dist, tt.phases[0].time, tt.phases[0].rayParams)))
-        # self.assertTrue(all(len(x) == 179 for x in (
-        #     tt.phases[1].dist, tt.phases[1].time, tt.phases[1].rayParams)))
-        # self.assertEqual(tt.phases[1].puristName, "P")
-        # # ought to be ...02 but close enough
-        # self.assertEqual(tt.phases[0].dist[232], 1.7004304436716804)
-        # self.assertEqual(tt.phases[1].dist[121], 0.9912173304550589)
-        # tt.calculate(tt.degrees)
-        # self.assertEqual(tt.arrivals[1].time, 1028.9304953527787)
+    # For some reason this test throws nosetests off if not in the unittest
+    # framwork like the test above...?
+    def test_all_phases_ak135_35_deg_distance(self):
+        """
+        Tests tauptime at 35 degree distance for the ak135 model, phases ttall.
+        """
+        model = TauPyModel("ak135")
+        tts = model.get_travel_time(source_depth_in_km=10.0,
+                                    distance_in_degree=35.0)
+        compare_arrivals_with_taup_time_output(
+            tts, "taup_time_-h_10_-ph_ttall_-deg_35_-mod_ak135")
 
     def test_range(self):
         """
         Check taup_time output for a range of inputs against the Java output.
         """
-        # todo: add checks of Pdiff, Pn, kmps?
         if not os.path.isfile("data/java_tauptime_testoutput"):
             subprocess.call("./generate_tauptime_output.sh", shell=True)
         stdout = sys.stdout
