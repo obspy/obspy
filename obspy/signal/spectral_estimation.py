e5826cca0e14d879a1feb1f4de49110ba428bbc3
aringler-usgs <aringler@usgs.gov>
2015-01-20 13:25:59 -0700

    Made changes to make it pep8 compatible.

--- obspy/signal/spectral_estimation.py
+++ obspy/signal/spectral_estimation.py
@@ -34,6 +34,7 @@ from obspy.signal import cosTaper
 from obspy.signal.util import prevpow2
 from obspy.signal.invsim import pazToFreqResp
 
+
 MATPLOTLIB_VERSION = getMatplotlibVersion()
 
 dtiny = np.finfo(0.0).tiny
@@ -644,7 +645,6 @@ class PPSD():
         spec, _freq = psd(tr.data, self.nfft, self.sampling_rate,
                           detrend=mlab.detrend_linear, window=fft_taper,
                           noverlap=self.nlap)
-        
         # leave out first entry (offset)
         spec = spec[1:]
 
@@ -659,21 +659,18 @@ class PPSD():
             spec /= paz['sensitivity']**2
         else:
             #Get the complex response from the pole/zero model
-            resp = pazToFreqResp(paz['poles'],paz['zeros'],
-                paz['gain']*paz['sensitivity'],self.sampling_rate,
-                nfft=self.nfft)
+            resp = pazToFreqResp(paz['poles'], paz['zeros'],
+                                 paz['gain']*paz['sensitivity'],
+                                 self.sampling_rate, nfft=self.nfft)
             resp = resp[1:]
             resp = resp[::-1]
             #Now get the amplitude response (squared)
-            respamp = np.absolute(resp*np.conjugate(resp))
+            respamp = np.absolute(resp * np.conjugate(resp))
             #Make omega with the same conventions as spec
-            w = 2.0 *math.pi*_freq[1:]
+            w = 2.0 * math.pi * _freq[1:]
             w = w[::-1]
-            #Here we do the response removal 
+            #Here we do the response removal
             spec = (w**2) * spec / respamp
-
-
-
         # avoid calculating log of zero
         idx = spec < dtiny
         spec[idx] = dtiny
