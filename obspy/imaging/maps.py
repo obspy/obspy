obspy/obspy

commit f3aa3cf937ad170bfca69c11309452ccee024200

avoid plt.sca() for older matplotlibs

obspy/imaging/maps.py
@@ -144,7 +144,6 @@ def plot_basemap(lons, lats, size, color, labels=None,
     if show_colorbar:
         map_ax = fig.add_axes([ax_x0, 0.13, ax_width, 0.77])
         cm_ax = fig.add_axes([ax_x0, 0.05, ax_width, 0.05])
-        plt.sca(map_ax)
     else:
         ax_y0, ax_height = 0.05, 0.85
         if projection == "local":
@@ -251,11 +250,11 @@ def plot_basemap(lons, lats, size, color, labels=None,
                 # large values if it cannot project a point.
                 if xpt > 1e25:
                     continue
-                plt.text(xpt, ypt, name, weight="heavy",
-                         color="k", zorder=100, **path_effect_kwargs)
+                map_ax.text(xpt, ypt, name, weight="heavy",
+                            color="k", zorder=100, **path_effect_kwargs)
         elif len(lons) == 1:
-            plt.text(x[0], y[0], labels[0], weight="heavy", color="k",
-                     **path_effect_kwargs)
+            map_ax.text(x[0], y[0], labels[0], weight="heavy", color="k",
+                        **path_effect_kwargs)
 
     scatter = bmap.scatter(x, y, marker=marker, s=size, c=color,
                            zorder=10, cmap=colormap)

commit 1476488eba2222169eaf1ebc98fad134857c89fa

Fixing issue introduced in f3aa3cf.

obspy/imaging/maps.py
@@ -159,11 +159,11 @@ def plot_basemap(lons, lats, size, color, labels=None,
         map_ax = fig.add_axes([ax_x0, ax_y0, ax_width, ax_height])
 
     if projection == 'cyl':
-        bmap = Basemap(resolution=resolution)
+        bmap = Basemap(resolution=resolution, ax=map_ax)
     elif projection == 'ortho':
         bmap = Basemap(projection='ortho', resolution=resolution,
                        area_thresh=1000.0, lat_0=np.mean(lats),
-                       lon_0=np.mean(lons))
+                       lon_0=np.mean(lons), ax=map_ax)
     elif projection == 'local':
         if min(lons) < -150 and max(lons) > 150:
             max_lons = max(np.array(lons) % 360)
@@ -199,7 +199,7 @@ def plot_basemap(lons, lats, size, color, labels=None,
 
         bmap = Basemap(projection='aeqd', resolution=resolution,
                        area_thresh=1000.0, lat_0=lat_0, lon_0=lon_0,
-                       width=width, height=height)
+                       width=width, height=height, ax=map_ax)
         # not most elegant way to calculate some round lats/lons
 
         def linspace2(val1, val2, N):

obspy/core/tests/test_event.py
@@ -458,7 +458,7 @@ class CatalogTestCase(unittest.TestCase):
         parameters.
         """
         cat = readEvents()
-        reltol = 1
+        reltol = 1.5
         # Basemap smaller 1.0.4 has a serious issue with plotting. Thus the
         # tolerance must be much higher.
         if BASEMAP_VERSION < [1, 0, 4]: