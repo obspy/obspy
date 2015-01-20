4fe30ddaa65adda9267e98d901f4247b812be548
aringler-usgs <aringler@usgs.gov>
2015-01-20 14:13:22 -0700

    Fixed more pep8 issues using flake8.

--- obspy/signal/spectral_estimation.py
+++ obspy/signal/spectral_estimation.py
@@ -614,18 +614,18 @@ class PPSD():
             # in case of rotational data just remove sensitivity
             spec /= paz['sensitivity']**2
         else:
-            #Get the complex response from the pole/zero model
+            # Get the complex response from the pole/zero model
             resp = pazToFreqResp(paz['poles'], paz['zeros'],
                                  paz['gain']*paz['sensitivity'],
                                  self.sampling_rate, nfft=self.nfft)
             resp = resp[1:]
             resp = resp[::-1]
-            #Now get the amplitude response (squared)
+            # Now get the amplitude response (squared)
             respamp = np.absolute(resp * np.conjugate(resp))
-            #Make omega with the same conventions as spec
+            # Make omega with the same conventions as spec
             w = 2.0 * math.pi * _freq[1:]
             w = w[::-1]
-            #Here we do the response removal
+            # Here we do the response removal
             spec = (w**2) * spec / respamp
         # avoid calculating log of zero
         idx = spec < dtiny
