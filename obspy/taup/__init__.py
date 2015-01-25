0fba5396b80c285f35551ec3980a5dd2c5f0e37d
Tobias Megies <megies@geophysik.uni-muenchen.de>
2015-01-25 17:59:50 +0100

    moving TauPy directory structure to ObsPy dir structure

--- obspy/taup/__init__.py
+++ obspy/taup/__init__.py
@@ -12,6 +12,13 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from future.builtins import *  # NOQA
 
+import inspect
+import os
+
+# Most generic way to get the data directory.
+__DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(inspect.getfile(
+    inspect.currentframe()))), "data")
+
 # Convenience imports.
 from obspy.taup.taup import getTravelTimes, travelTimePlot
 
