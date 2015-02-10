e468ea02198e0d0b7b72612740be5292eab15c6a
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2015-02-10 17:40:33 +0100

    pep 8

--- obspy/taup/slowness_layer.py
+++ obspy/taup/slowness_layer.py
@@ -3,7 +3,6 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from future.builtins import *  # NOQA
-from future.utils import native_str
 
 import math
 
