2635d68e759064212389228d44332255dd731e83
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2015-02-17 09:10:06 +0100

    Basic raypath plots.

--- obspy/taup/tau.py
+++ obspy/taup/tau.py
@@ -4,6 +4,9 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from future.builtins import *  # NOQA
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 from .tau_model import TauModel
 from .taup_create import TauP_Create
 from .taup_path import TauP_Path
@@ -11,14 +14,35 @@ from .taup_pierce import TauP_Pierce
 from .taup_time import TauP_Time
 
 
+# From colorbrewer2.org
+COLORS = [
+    "#1f78b4",
+    "#33a02c",
+    "#e31a1c",
+    "#ff7f00",
+    "#6a3d9a",
+    "#a6cee3",
+    "#b2df8a",
+    "#fb9a99",
+    "#fdbf6f",
+    "#cab2d6"]
+
+
 class Arrivals(list):
     """
-    Class that is returned by the interface methods.
-    """
-    __slots__ = ["arrivals"]
+    List of arrivals returned by the
+    methods of the :class:`~obspy.taup.tau.TauPyModel` class.
 
-    def __init__(self, arrivals):
+    :param arrivals: List of arrivals.
+    :param model: The model used to calculate the arrivals.
+    :param distance: The requested distance.
+    """
+    __slots__ = ["model", "distance"]
+
+    def __init__(self, arrivals, model, distance):
         super(Arrivals, self).__init__()
+        self.model = model
+        self.distance = distance
         self.extend(arrivals)
 
     def __str__(self):
@@ -30,6 +54,69 @@ class Arrivals(list):
 
     def __repr__(self):
         return "[%s]" % (", ".join([repr(_i) for _i in self]))
+
+    def plot(self, plot_type="spherical", plot_all=False):
+        """
+        Plot the raypaths if any.
+
+        :param plot_type: Either ``"spherical"`` or ``"cartesian"``.
+            A spherical plot is always global whereas a cartesian one can
+            also be local.
+        :type plot_type: str
+        :param plot_all: By default only rays that match the requested
+            distance are plotted and rays arriving at 360 - x degrees are
+            not shown.
+        :type plot_all: bool
+        """
+        arrivals = []
+        for _i in self:
+            if _i.path is None:
+                continue
+            if plot_all is False:
+                dist = np.rad2deg(_i.dist)
+                if (dist - self.distance) / dist > 1E-5:
+                    continue
+            arrivals.append(_i)
+        if not arrivals:
+            raise ValueError("Can only plot arrivals with calculated ray "
+                             "paths.")
+        discons = self.model.sMod.vMod.getDisconDepths()
+        if plot_type == "spherical":
+            ax = plt.subplot(111, polar=True)
+            ax.set_theta_zero_location('N')
+            ax.set_theta_direction(-1)
+            ax.set_xticks([])
+            ax.set_yticks([])
+            for _i, ray in enumerate(arrivals):
+                ax.plot(ray.path["dist"], 6371.0 - ray.path["depth"],
+                        color=COLORS[_i % len(COLORS)], label=ray.name,
+                        lw=1.5)
+            ax.set_xticks(np.pi / 180.0 * np.linspace(180, -180, 8,
+                                                      endpoint=False))
+            ax.set_yticks(6371 - discons)
+            ax.xaxis.set_major_formatter(plt.NullFormatter())
+            ax.yaxis.set_major_formatter(plt.NullFormatter())
+            ax.set_rmax(6371.0)
+            ax.set_rmin(0.0)
+            plt.legend(loc="upper left")
+        elif plot_type == "cartesian":
+            plt.gca().invert_yaxis()
+            for _i, ray in enumerate(arrivals):
+                plt.plot(np.rad2deg(ray.path["dist"]), ray.path["depth"],
+                         color=COLORS[_i % len(COLORS)], label=ray.name,
+                         lw=1.5)
+            plt.ylabel("Depth [km]")
+            plt.legend()
+            plt.xlabel("Distance [deg]")
+            x = plt.xlim()
+            y = plt.ylim()
+            for depth in discons:
+                if not (y[1] <= depth <= y[0]):
+                    continue
+                plt.hlines(depth, x[0], x[1], color="0.5", zorder=-1)
+        else:
+            raise NotImplementedError
+        plt.show()
 
 
 class TauPyModel(object):
@@ -76,7 +163,8 @@ class TauPyModel(object):
         tt = TauP_Time(self.model, phase_list, source_depth_in_km,
                        distance_in_degree)
         tt.run()
-        return Arrivals(tt.arrivals)
+        return Arrivals(sorted(tt.arrivals, key=lambda x: x.time),
+                        model=self.model, distance=distance_in_degree)
 
     def get_pierce_points(self, source_depth_in_km, distance_in_degree,
                           phase_list=("ttall",)):
@@ -98,7 +186,8 @@ class TauPyModel(object):
         pp = TauP_Pierce(self.model, phase_list, source_depth_in_km,
                          distance_in_degree)
         pp.run()
-        return Arrivals(pp.arrivals)
+        return Arrivals(sorted(pp.arrivals, key=lambda x: x.time),
+                        model=self.model, distance=distance_in_degree)
 
     def get_ray_paths(self, source_depth_in_km, distance_in_degree=None,
                       phase_list=("ttall,")):
@@ -120,7 +209,8 @@ class TauPyModel(object):
         rp = TauP_Path(self.model, phase_list, source_depth_in_km,
                        distance_in_degree)
         rp.run()
-        return Arrivals(rp.arrivals)
+        return Arrivals(sorted(rp.arrivals, key=lambda x: x.time),
+                        model=self.model, distance=distance_in_degree)
 
 
 def create_taup_model(model_name, output_dir, input_dir):
