358246f0683c2bac31122969ec88d917aeeba709
Tobias Megies <megies@geophysik.uni-muenchen.de>
2015-03-12 14:38:07 +0100

    trivial refactoring

--- setup.py
+++ setup.py
@@ -109,12 +109,12 @@ KEYWORDS = [
     'instrument simulation', 'IRIS', 'kelunji', 'kinemetrics', 'magnitude',
     'MiniSEED', 'misfit', 'mopad', 'MSEED', 'NDK', 'NERA', 'NERIES',
     'NonLinLoc', 'NLLOC', 'observatory', 'ORFEUS', 'PDAS', 'picker',
-    'processing', 'PQLX', 'Q', 'real time', 'realtime', 'RESP', 'response
-    file', 'RT', 'SAC', 'SEED', 'SeedLink', 'SEG-2', 'SEG Y', 'SEISAN',
-    'SeisHub', 'Seismic Handler', 'seismology', 'seismogram', 'seismograms',
-    'signal', 'slink', 'spectrogram', 'StationXML', 'taper', 'taup', 'travel
-    time', 'trigger', 'VERCE', 'WAV', 'waveform', 'WaveServer', 'WaveServerV',
-    'WebDC', 'web service', 'Winston', 'XML-SEED', 'XSEED']
+    'processing', 'PQLX', 'Q', 'real time', 'realtime', 'RESP',
+    'response file', 'RT', 'SAC', 'SEED', 'SeedLink', 'SEG-2', 'SEG Y',
+    'SEISAN', 'SeisHub', 'Seismic Handler', 'seismology', 'seismogram',
+    'seismograms', 'signal', 'slink', 'spectrogram', 'StationXML', 'taper',
+    'taup', 'travel time', 'trigger', 'VERCE', 'WAV', 'waveform', 'WaveServer',
+    'WaveServerV', 'WebDC', 'web service', 'Winston', 'XML-SEED', 'XSEED']
 
 INSTALL_REQUIRES = [
     'future>=0.12.4',
