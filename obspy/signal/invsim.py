5582920dd082678c55d30ff535effe7ad810bbef
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2015-03-13 17:24:00 +0100

    Plot to document the taper.

--- obspy/signal/invsim.py
+++ obspy/signal/invsim.py
@@ -157,9 +157,44 @@ def c_sac_taper(freqs, flimit):
     :param freqs: frequency vector to use
     :type freqs: :class:`numpy.ndarray`
     :param flimit: sequence containing the 4 frequency limits
-    :type flimit: list/tuple of 4 floats
+    :type flimit: tuple of 4 floats
     :returns: taper
     :rtype: :class:`numpy.ndarray`
+
+    The `flimit` parameter is a tuple of four frequency values `(f1, f2,
+    f3, f4)`, the following plots illustrates the concept:
+
+    .. plot::
+
+        import matplotlib.pylab as plt
+        import numpy as np
+        from obspy.signal.invsim import c_sac_taper
+
+        plt.figure(figsize=(10, 3))
+
+        freqs = np.logspace(-2.01, 0, 2000)
+
+        plt.vlines([0.015, 0.03, 0.2, 0.4], -0.1, 1.3, color="#89160F")
+        plt.semilogx(freqs, c_sac_taper(freqs, (0.015, 0.03, 0.2, 0.4)),
+                     lw=2, color="#4C72B0")
+
+        props = {
+            "bbox": dict(facecolor='white', edgecolor="0.5",
+                 boxstyle="square,pad=0.2"),
+            "va": "top", "ha": "center", "color": "#89160F",
+            "size": "large"}
+        plt.text(0.015, 1.25, "f1", **props)
+        plt.text(0.03, 1.25, "f2", **props)
+        plt.text(0.2, 1.25, "f3", **props)
+        plt.text(0.4, 1.25, "f4", **props)
+
+        plt.xlim(freqs[0], freqs[-1])
+        plt.ylim(-0.1, 1.3)
+        plt.ylabel("Taper Amplitude")
+        plt.xlabel("Frequency [Hz]")
+        plt.grid()
+        plt.tight_layout()
+        plt.show()
     """
     fl1, fl2, fl3, fl4 = flimit
     taper = np.zeros_like(freqs)
