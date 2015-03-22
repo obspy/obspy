3f24fee5fc2f810d518ec6428cf82f016cc1bd2a
mbyt <random.seed@web.de>
2015-03-22 22:16:48 +0100

    test earthworm solely on pubavo1.wr.usgs.gov:16022

--- obspy/earthworm/tests/test_client.py
+++ obspy/earthworm/tests/test_client.py
@@ -22,11 +22,18 @@ class ClientTestCase(unittest.TestCase):
     def setUp(self):
         # Monkey patch: set lower default precision of all UTCDateTime objects
         UTCDateTime.DEFAULT_PRECISION = 4
-        self.client = Client("pele.ess.washington.edu", 16017, timeout=7)
+        self.client = Client("pubavo1.wr.usgs.gov", 16022, timeout=30.0)
 
     def tearDown(self):
         # restore default precision of all UTCDateTime objects
         UTCDateTime.DEFAULT_PRECISION = 6
+
+    @skip_on_network_error
+    def test_availability(self):
+        data = self.client.availability()
+        seeds = ["%s.%s.%s.%s" % (net, sta, loc, cha)
+                 for net, sta, loc, cha, _start, _stop in data]
+        self.assertTrue('AV.ACH.--.EHE' in seeds)
 
     @skip_on_network_error
     def test_getWaveform(self):
@@ -34,52 +41,52 @@ class ClientTestCase(unittest.TestCase):
         Tests getWaveform method.
         """
         client = self.client
-        start = UTCDateTime(2013, 1, 17)
-        end = start + 30
+        start = UTCDateTime(2015, 3, 22)
+        end = start + 1.0
         # example 1 -- 1 channel, cleanup
-        stream = client.getWaveform('UW', 'TUCA', '', 'BHZ', start, end)
+        stream = client.getWaveform('AV', 'ACH', '--', 'EHE', start, end)
         self.assertEqual(len(stream), 1)
         delta = stream[0].stats.delta
         trace = stream[0]
-        self.assertTrue(len(trace) == 1201)
+        self.assertEqual(len(trace), 101)
         self.assertTrue(trace.stats.starttime >= start - delta)
         self.assertTrue(trace.stats.starttime <= start + delta)
         self.assertTrue(trace.stats.endtime >= end - delta)
         self.assertTrue(trace.stats.endtime <= end + delta)
-        self.assertEqual(trace.stats.network, 'UW')
-        self.assertEqual(trace.stats.station, 'TUCA')
+        self.assertEqual(trace.stats.network, 'AV')
+        self.assertEqual(trace.stats.station, 'ACH')
         self.assertEqual(trace.stats.location, '')
-        self.assertEqual(trace.stats.channel, 'BHZ')
+        self.assertEqual(trace.stats.channel, 'EHE')
         # example 2 -- 1 channel, no cleanup
-        stream = client.getWaveform('UW', 'TUCA', '', 'BHZ', start, end,
+        stream = client.getWaveform('AV', 'ACH', '--', 'EHE', start, end,
                                     cleanup=False)
         self.assertTrue(len(stream) >= 2)
         summed_length = sum(len(tr) for tr in stream)
-        self.assertTrue(summed_length == 1201)
+        self.assertEqual(summed_length, 101)
         self.assertTrue(stream[0].stats.starttime >= start - delta)
         self.assertTrue(stream[0].stats.starttime <= start + delta)
         self.assertTrue(stream[-1].stats.endtime >= end - delta)
         self.assertTrue(stream[-1].stats.endtime <= end + delta)
         for trace in stream:
-            self.assertEqual(trace.stats.network, 'UW')
-            self.assertEqual(trace.stats.station, 'TUCA')
+            self.assertEqual(trace.stats.network, 'AV')
+            self.assertEqual(trace.stats.station, 'ACH')
             self.assertEqual(trace.stats.location, '')
-            self.assertEqual(trace.stats.channel, 'BHZ')
+            self.assertEqual(trace.stats.channel, 'EHE')
         # example 3 -- component wildcarded with '?'
-        stream = client.getWaveform('UW', 'TUCA', '', 'BH?', start, end)
+        stream = client.getWaveform('AV', 'ACH', '--', 'EH?', start, end)
         self.assertEqual(len(stream), 3)
         for trace in stream:
-            self.assertTrue(len(trace) == 1201)
+            self.assertEqual(len(trace), 101)
             self.assertTrue(trace.stats.starttime >= start - delta)
             self.assertTrue(trace.stats.starttime <= start + delta)
             self.assertTrue(trace.stats.endtime >= end - delta)
             self.assertTrue(trace.stats.endtime <= end + delta)
-            self.assertEqual(trace.stats.network, 'UW')
-            self.assertEqual(trace.stats.station, 'TUCA')
+            self.assertEqual(trace.stats.network, 'AV')
+            self.assertEqual(trace.stats.station, 'ACH')
             self.assertEqual(trace.stats.location, '')
-        self.assertEqual(stream[0].stats.channel, 'BHZ')
-        self.assertEqual(stream[1].stats.channel, 'BHN')
-        self.assertEqual(stream[2].stats.channel, 'BHE')
+        self.assertEqual(stream[0].stats.channel, 'EHZ')
+        self.assertEqual(stream[1].stats.channel, 'EHN')
+        self.assertEqual(stream[2].stats.channel, 'EHE')
 
     @skip_on_network_error
     def test_saveWaveform(self):
@@ -88,60 +95,30 @@ class ClientTestCase(unittest.TestCase):
         """
         # initialize client
         client = self.client
-        start = UTCDateTime(2013, 1, 17)
-        end = start + 30
+        start = UTCDateTime(2015, 3, 22)
+        end = start + 1.0
         with NamedTemporaryFile() as tf:
             testfile = tf.name
             # 1 channel, cleanup (using SLIST to avoid dependencies)
-            client.saveWaveform(testfile, 'UW', 'TUCA', '', 'BHZ', start, end,
+            client.saveWaveform(testfile, 'AV', 'ACH', '--', 'EHE', start, end,
                                 format="SLIST")
             stream = read(testfile)
         self.assertEqual(len(stream), 1)
         delta = stream[0].stats.delta
         trace = stream[0]
-        self.assertTrue(len(trace) == 1201)
+        self.assertEqual(len(trace), 101)
         self.assertTrue(trace.stats.starttime >= start - delta)
         self.assertTrue(trace.stats.starttime <= start + delta)
         self.assertTrue(trace.stats.endtime >= end - delta)
         self.assertTrue(trace.stats.endtime <= end + delta)
-        self.assertEqual(trace.stats.network, 'UW')
-        self.assertEqual(trace.stats.station, 'TUCA')
+        self.assertEqual(trace.stats.network, 'AV')
+        self.assertEqual(trace.stats.station, 'ACH')
         self.assertEqual(trace.stats.location, '')
-        self.assertEqual(trace.stats.channel, 'BHZ')
-
-    @skip_on_network_error
-    def test_availability(self):
-        data = self.client.availability()
-        seeds = ["%s.%s.%s.%s" % (d[0], d[1], d[2], d[3]) for d in data]
-        self.assertTrue('UW.TUCA.--.BHZ' in seeds)
-
-
-class ClientUSGSTestCase(unittest.TestCase):
-    """
-    Minimal test suite using an alternative wave server located at
-    pubavo1.wr.usgs.gov:16022
-    """
-    def setUp(self):
-        # Monkey patch: set lower default precision of all UTCDateTime objects
-        UTCDateTime.DEFAULT_PRECISION = 4
-        self.client = Client("pubavo1.wr.usgs.gov", 16022, timeout=30.0)
-
-    def tearDown(self):
-        # restore default precision of all UTCDateTime objects
-        UTCDateTime.DEFAULT_PRECISION = 6
-
-    @skip_on_network_error
-    def test_availabilityAndGetWaveform(self):
-        response = self.client.availability("AV", "ACH", channel="EHE")
-        self.assertGreaterEqual(len(response), 1)
-        t = UTCDateTime()
-        st = self.client.getWaveform('AV', 'ACH', '--', 'EHE', t - 120, t - 60)
-        self.assertGreaterEqual(len(st), 1)
+        self.assertEqual(trace.stats.channel, 'EHE')
 
 
 def suite():
     suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(ClientUSGSTestCase, 'test'))
     suite.addTest(unittest.makeSuite(ClientTestCase, 'test'))
     return suite
 
