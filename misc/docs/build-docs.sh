249b7c61cd3bd4be783a2cb989ce62635ba885e5
mbyt <random.seed@web.de>
2015-03-29 17:31:01 +0200

    freezing pep8 version for doc building
    see http://tests.obspy.org/23456/#35

--- misc/docs/build-docs.sh
+++ misc/docs/build-docs.sh
@@ -71,12 +71,12 @@ export PATH=$BASEDIR/bin:$PATH
 
 # run develop.sh
 cd $GITDIR
-# export LDFLAGS="-lgcov"
+# export LDFLAGS="-lgcov"  # coverage on C code (make c_coverage)
 $BASEDIR/bin/python setup.py develop --verbose
 
 # keep some packages up to date
 $BASEDIR/bin/pip install --upgrade pip
-$BASEDIR/bin/pip install --upgrade --no-deps pep8 flake8
+$BASEDIR/bin/pip install --upgrade --no-deps pep8==1.5.7 flake8
 
 # make docs
 cd $GITDIR/misc/docs
