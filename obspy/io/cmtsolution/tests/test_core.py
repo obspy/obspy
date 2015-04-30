c7dc2e032535a7ec2680a980bba58285234b9abf
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2015-04-30 17:24:05 +0200

    Added reading and writing test

--- obspy/io/cmtsolution/tests/test_core.py
+++ obspy/io/cmtsolution/tests/test_core.py
@@ -8,24 +8,47 @@ import inspect
 import os
 import unittest
 
-from obspy.io.cmtsolution.core import is_cmtsolution, read_cmtsolution
-
+import obspy
+from obspy.core.util.base import NamedTemporaryFile
 
 
 class CmtsolutionTestCase(unittest.TestCase):
     """
-    Test suite for obspy.io.cmtsolution
+    Test suite for obspy.io.cmtsolution.
+
+    The tests usually directly utilize the registered function with the
+    read_events() to also test the integration.
     """
     def setUp(self):
         self.path = os.path.dirname(os.path.abspath(inspect.getfile(
             inspect.currentframe())))
         self.datapath = os.path.join(self.path, "data")
 
-    def test_read_cmtsolution(self):
+    def test_read_and_write_cmtsolution_from_files(self):
         """
-        Test reading cmtsolution files.
+        Tests that reading and writing CMTSOLUTIONS file does not change
+        anything.
         """
-        pass
+        filename = os.path.join(self.datapath, "CMTSOLUTION")
+        with open(filename, "rt") as fh:
+            data = fh.read()
+
+        cat = obspy.read_events(filename)
+
+        with NamedTemporaryFile() as tf:
+            temp_filename = tf.name
+
+        try:
+            cat.write(temp_filename, format="CMTSOLUTION")
+            with open(temp_filename, "rt") as fh:
+                new_data = fh.read()
+        finally:
+            try:
+                os.remove(temp_filename)
+            except:
+                pass
+
+        self.assertEqual(data, new_data)
 
 
 def suite():
