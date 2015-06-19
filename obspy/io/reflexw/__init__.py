fcbe9467027e4eb803792e2e507018b32355e05a
Tobias Megies <megies@geophysik.uni-muenchen.de>
2015-06-19 11:14:46 +0200

    reflexw: add __init__.py

---
+++ obspy/io/reflexw/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+obspy.io.reflexw - Read support for waveforms in Reflexw format
+===============================================================
+
+This module provides read support for waveform data in the file format used by
+the Reflexw software suite (http://www.sandmeier-geo.de/reflexw.html).
+
+:copyright:
+    The ObsPy Development Team (devs@obspy.org)
+:license:
+    GNU Lesser General Public License, Version 3
+    (http://www.gnu.org/copyleft/lesser.html)
+"""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from future.builtins import *  # NOQA
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(exclude_empty=True)
