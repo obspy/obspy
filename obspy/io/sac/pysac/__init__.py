f7a1548bbadc9c8a92b2ecac5dadffd80e823eeb
Jonathan MacCarthy <jkmacc@lanl.gov>
2015-09-09 22:53:48 -0600

    Add future stuff to pysac init.

--- obspy/io/sac/pysac/__init__.py
+++ obspy/io/sac/pysac/__init__.py
@@ -1,1 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from future.builtins import *  # NOQA
+
 from .sactrace import SACTrace
