77ad3070f38f5a88d6e47c72da1f06c8c08ca3b3
Matthias <matthias@matthias-Latitude-E6520.(none)>
2015-11-24 21:07:39 +0100

    surface or quiver plots

--- obspy/imaging/radpattern.py
+++ obspy/imaging/radpattern.py
@@ -8,6 +8,7 @@
 This script contains the following functions to compute and plot radiation patterns:
     farfield_p
     farfield_s
+    plot_3drpattern
 
 :copyright:
     The ObsPy Development Team (devs@obspy.org)
@@ -19,11 +20,10 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from future.builtins import *  # NOQA @UnusedWildImport
 
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-def farfield_p_unitsphere(mt,nlats=30):
+def plot_3drpattern(mt, kind='both_sphere'):
     """
     Returns the P farfield radiation pattern on a unit sphere grid
 
@@ -34,6 +34,8 @@ def farfield_p_unitsphere(mt,nlats=30):
         x,y,z equals North,East,Down convention is as follows: Mrr=Mzz,
         Mtt=Mxx, Mpp=Myy, Mrt=Mxz, Mrp=-Myz, Mtp=-Mxy.
 
+    :param kind: can be 'p_quiver, s_quiver, both_quiver, p_sphere, s_sphere'
+
     :return: 3D grid point array with shape [3,npts] that contains
              the sperical grid points
 
@@ -42,15 +44,129 @@ def farfield_p_unitsphere(mt,nlats=30):
 
     based on Aki & Richards Eq 4.29
     """
-    ndim = 3
-    Mpq = fullmt(mt)
 
-    #---- make spherical grid ----
-    colats = np.linspace(0.,np.pi,nlats)
+    #---- compute and plot radiation pattern ----
+    vlength = 0.1 #length of vectors
+    nlat = 30 #points for quiver sphere
+
+    if kind=='p_quiver':
+        #---- precompute even spherical grid and directional cosine array ----
+        points = spherical_grid(nlat=nlat)
+
+        #---- get radiation pattern ----
+        disp = farfield_p(mt,points)
+        
+        #---- plot ----
+        fig = plt.figure()
+        ax  = fig.add_subplot(111, projection='3d')
+        ax.quiver(points[0],points[1],points[2],disp[0],disp[1],disp[2],length=vlength)
+
+    elif kind=='p_sphere':
+        #---- lat/lon sphere ----
+        u = np.linspace(0, 2 * np.pi, 200)
+        v = np.linspace(0, np.pi, 200)
+
+        x = np.outer(np.cos(u), np.sin(v))
+        y = np.outer(np.sin(u), np.sin(v))
+        z = np.outer(np.ones(np.size(u)), np.cos(v))
+
+        points = np.vstack( (x.flatten(),y.flatten(),z.flatten()) )
+
+        #---- get radiation pattern ----
+        disp = farfield_p(mt,points)
+        magn = np.sum(disp*points,axis=0)
+        magn /= np.max(np.abs(magn))
+
+        #---- compute colours and displace points for normalized vectors ----
+        norm  = plt.Normalize(-1.,1.)
+        cmap = plt.get_cmap('bwr')
+        x *= (1.+np.abs(magn.reshape(x.shape))/2.)
+        y *= (1.+np.abs(magn.reshape(x.shape))/2.)
+        z *= (1.+np.abs(magn.reshape(x.shape))/2.)
+        colors = np.array([cmap(norm(val)) for val in magn]).reshape(x.shape[0],x.shape[1],4)
+
+        #---- plot ----
+        fig = plt.figure()
+        ax  = fig.add_subplot(111, projection='3d')
+        ax.plot_surface(x, y, z, rstride=4, cstride=4, facecolors=colors)
+
+    elif kind=='s_quiver':
+        #---- precompute even spherical grid and directional cosine array ----
+        points = spherical_grid(nlat=nlat)
+
+        #---- get radiation pattern ----
+        disp = farfield_s(mt,points)
+
+        #---- plot ----
+        fig = plt.figure()
+        ax  = fig.add_subplot(111, projection='3d')
+        ax.quiver(points[0],points[1],points[2],disp[0],disp[1],disp[2],length=vlength)
+
+    elif kind=='s_sphere':
+        #---- lat/lon sphere ----
+        u = np.linspace(0, 2 * np.pi, 200)
+        v = np.linspace(0, np.pi, 200)
+
+        x = np.outer(np.cos(u), np.sin(v))
+        y = np.outer(np.sin(u), np.sin(v))
+        z = np.outer(np.ones(np.size(u)), np.cos(v))
+
+        points = np.vstack( (x.flatten(),y.flatten(),z.flatten()) )
+
+        #---- get radiation pattern ----
+        disp = farfield_s(mt,points)
+        magn = np.sum(disp*disp,axis=0)
+        magn /= np.max(np.abs(magn))
+
+        #---- compute colours and displace points for normalized vectors ----
+        norm  = plt.Normalize(-1.,1.)
+        cmap = plt.get_cmap('bwr')
+        x *= (1.+np.abs(magn.reshape(x.shape))/2.)
+        y *= (1.+np.abs(magn.reshape(x.shape))/2.)
+        z *= (1.+np.abs(magn.reshape(x.shape))/2.)
+        colors = np.array([cmap(norm(val)) for val in magn]).reshape(x.shape[0],x.shape[1],4)
+
+        #---- plot ----
+        fig = plt.figure()
+        ax  = fig.add_subplot(111, projection='3d')
+        ax.plot_surface(x, y, z, rstride=4, cstride=4, facecolors=colors)
+
+    elif kind=='both_quiver':
+        #---- precompute even spherical grid and directional cosine array ----
+        points = spherical_grid(nlat=nlat)
+
+        #---- get radiation pattern ----
+        dispp = farfield_p(mt,points)
+        disps = farfield_s(mt,points)
+
+        #---- plot ----
+        fig = plt.figure()
+        ax  = fig.add_subplot(111, projection='3d')
+        qp = ax.quiver(points[0],points[1],points[2],dispp[0],dispp[1],dispp[2],length=vlength)
+
+        fig = plt.figure()
+        ax  = fig.add_subplot(111, projection='3d')
+        qs = ax.quiver(points[0],points[1],points[2],disps[0],disps[1],disps[2],length=vlength)
+        qs.set_array(normp)
+    else:
+        raise NotImplementedError('{:s} not implemented yet'.format(kind))
+
+    plt.show()
+
+
+def spherical_grid(nlat=30):
+    """
+    generates a simple equal area spherical grid
+    """
+
+    ndim = 3
+    colats = np.linspace(0.,np.pi,nlat)
     norms = np.sin(colats)
-    nlons = (nlats*norms+1).astype(int)
+    nlons = (nlat*norms+1).astype(int) #scale number of point with latitude
+
+    #---- make colat/lon grid ----
     colatgrid,longrid = [],[]
-    for ilat in range(nlats):
+    for ilat in range(nlat):
         nlon = nlons[ilat]
         dlon = 2.*np.pi/nlon
         lons = np.arange(0.,2.*np.pi,dlon)
@@ -65,12 +181,42 @@ def farfield_p_unitsphere(mt,nlats=30):
     points[1] = np.sin(colatgrid)*np.sin(longrid)
     points[2] = np.cos(colatgrid)
 
+    return points
+
+
+def farfield_p(mt,points):
+    """
+    Returns the P farfield radiation pattern
+
+    :param mt: Focal mechanism NM x 6 (Mxx, Myy, Mzz, Mxy, Mxz, Myz - the
+        six independent components of the moment tensor)
+
+    :param points: 3D vector array with shape [3,npts] (x,y,z) or [2,npts] (theta,phi)
+                   The normalized displacement of the moment tensor source is computed
+                   at these points.
+
+    :return: 3D vector array with shape [3,npts] that contains the
+             displacement vector for each grid point
+
+    based on Aki & Richards Eq 4.29
+    """
+    ndim,npoints = points.shape
+    if ndim==2:
+        #points are given as theta,phi
+        points = np.empty( (3,npoints) )
+        points[0] = np.sin(points[0])*np.cos(points[1])
+        points[1] = np.sin(points[0])*np.sin(points[1])
+        points[2] = np.cos(points[0])
+    elif ndim==3:
+        #points are given as x,y,z, (same system as the moment tensor)
+        pass
+    else:
+        raise ValueError('points should have shape 2 x npoints or 3 x npoints')
+    Mpq = fullmt(mt)
+    
     #---- precompute directional cosine array ----
     dists = np.sqrt(points[0]*points[0]+points[1]*points[1]+points[2]*points[2])
-    gammas = np.empty( (ndim,npoints) )
-    gammas[0] = points[0]/dists
-    gammas[1] = points[1]/dists
-    gammas[2] = points[2]/dists
+    gammas = points/dists
 
     #---- initialize displacement array ----
     disp   = np.empty( (ndim,npoints) )
@@ -84,56 +230,32 @@ def farfield_p_unitsphere(mt,nlats=30):
       for n in range(ndim):
           disp[n,ipoint] = gamma[n]*np.sum(gammatimesmt.flatten())
 
-    return points,disp
+    return disp
 
-def farfield_s_unitsphere(mt,nlats=30):
+def farfield_s(mt,points):
     """
-    Returns the S farfield radiation pattern on a unit sphere grid
+    Returns the S farfield radiation pattern
 
-    :param mt: Focal mechanism NM x 6 (M11, M22, M33, M12, M13, M23 - the
-        six independent components of the moment tensor, where the coordinate
-        system is 1,2,3 = Up,South,East which equals r,theta,phi -
-        Harvard/Global CMT convention). The relation to Aki and Richards
-        x,y,z equals North,East,Down convention is as follows: Mrr=Mzz,
-        Mtt=Mxx, Mpp=Myy, Mrt=Mxz, Mrp=-Myz, Mtp=-Mxy.
+    :param mt: Focal mechanism NM x 6 (Mxx, Myy, Mzz, Mxy, Mxz, Myz - the
+        six independent components of the moment tensor)
 
-    :return: 3D grid point array with shape [3,npts] that contains
-             the sperical grid points
+    :param points: 3D vector array with shape [3,npts] (x,y,z) or [2,npts] (theta,phi)
+                   The normalized displacement of the moment tensor source is computed
+                   at these points.
 
-             3D vector array with shape [3,npts] that contains the
+    :return: 3D vector array with shape [3,npts] that contains the
              displacement vector for each grid point
 
     based on Aki & Richards Eq 4.29
     """
-    ndim = 3
+    ndim,npoints = points.shape
+    if ndim!=3:
+        raise ValueError('point array should have shape [3,npoints]')
     Mpq = fullmt(mt)
-
-    #---- make spherical grid ----
-    colats = np.linspace(0.,np.pi,nlats)
-    norms = np.sin(colats)
-    nlons = (nlats*norms+1).astype(int)
-    colatgrid,longrid = [],[]
-    for ilat in range(nlats):
-        nlon = nlons[ilat]
-        dlon = 2.*np.pi/nlon
-        lons = np.arange(0.,2.*np.pi,dlon)
-        for ilon in range(nlon):
-            colatgrid.append(colats[ilat])
-            longrid.append(lons[ilon])
-    npoints = len(longrid)
-
-    #---- get cartesian coordinates of spherical grid ----
-    points = np.empty( (ndim,npoints) )
-    points[0] = np.sin(colatgrid)*np.cos(longrid)
-    points[1] = np.sin(colatgrid)*np.sin(longrid)
-    points[2] = np.cos(colatgrid)
-
+    
     #---- precompute directional cosine array ----
     dists = np.sqrt(points[0]*points[0]+points[1]*points[1]+points[2]*points[2])
-    gammas = np.empty( (ndim,npoints) )
-    gammas[0] = points[0]/dists
-    gammas[1] = points[1]/dists
-    gammas[2] = points[2]/dists
+    gammas = points/dists
 
     #---- initialize displacement array ----
     disp   = np.empty( (ndim,npoints) )
@@ -150,8 +272,9 @@ def farfield_s_unitsphere(mt,nlats=30):
               psum += (gamma[n]*gamma[p] - deltanp)*Mp[p]
           disp[n,ipoint] = psum
 
-    return points,disp
+    return disp
 
+#---- get full moment tensor ----
 def fullmt(mt):
     mt_full = np.array( ([[mt[0],mt[3],mt[4]],
                           [mt[3],mt[1],mt[5]],
--- obspy/imaging/tests/test_radpattern.py
+++ obspy/imaging/tests/test_radpattern.py
@@ -9,7 +9,7 @@ from future.builtins import *  # NOQA
 import unittest
 
 import matplotlib.pyplot as plt
-from obspy.imaging.radpattern import farfieldP,farfieldS
+from obspy.imaging.radpattern import plot_3drpattern
 from mpl_toolkits.mplot3d import Axes3D
 
 
@@ -22,24 +22,11 @@ class RadPatternTestCase(unittest.TestCase):
         """
         Tests to plot P/S wave farfield radiation pattern
         """
-        vlength = 0.05
         mt = [0.91, -0.89, -0.02, 1.78, -1.55, 0.47]
-        pointsp,dispp = farfieldP(mt)
-        pointss,disps = farfieldS(mt)
-
-        fig = plt.figure()
-        ax  = fig.add_subplot(111, projection='3d')
-        ax.quiver(pointsp[0],pointsp[1],pointsp[2],dispp[0],dispp[1],dispp[2],length=vlength)
-
-        fig = plt.figure()
-        ax  = fig.add_subplot(111, projection='3d')
-        ax.quiver(pointss[0],pointss[1],pointss[2],disps[0],disps[1],disps[2],length=vlength)
-
-        plt.show()
+        plot_3drpattern(mt,kind='p_sphere')
 
 def suite():
     return unittest.makeSuite(RadPatternTestCase, 'test')
-
 
 if __name__ == '__main__':
     unittest.main(defaultTest='suite')
