3796cab8f0584494c837b1844e24c9f0da4a2471
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2016-01-15 19:51:32 +0100

    Mock tests for more methods.

--- obspy/clients/syngine/tests/test_client.py
+++ obspy/clients/syngine/tests/test_client.py
@@ -9,6 +9,7 @@ from future.builtins import *  # NOQA @UnusedWildImport
 import unittest
 
 from obspy.core.compatibility import mock
+from obspy.core.util.misc import CatchOutput
 from obspy.clients.syngine import Client
 from obspy.clients.base import DEFAULT_TESTING_USER_AGENT
 
@@ -47,6 +48,50 @@ class ClientTestCase(unittest.TestCase):
                          'http://service.iris.edu/irisws/syngine/1/info')
         self.assertEqual(p.call_args[1]["params"],
                          {'model': 'test_model'})
+        self.assertEqual(p.call_args[1]["headers"],
+                         {'User-Agent': DEFAULT_TESTING_USER_AGENT})
+
+    def test_get_available_models_mock(self):
+        with mock.patch("requests.get") as p:
+            p.return_value = RequestsMockResponse()
+            self.c.get_available_models()
+
+        self.assertEqual(p.call_count, 1)
+        self.assertEqual(p.call_args[0][0],
+                         'http://service.iris.edu/irisws/syngine/1/models')
+        self.assertEqual(p.call_args[1]["params"], None)
+        self.assertEqual(p.call_args[1]["headers"],
+                         {'User-Agent': DEFAULT_TESTING_USER_AGENT})
+
+    def test_print_model_information(self):
+        with mock.patch("requests.get") as p:
+            p.return_value = RequestsMockResponse()
+            p.return_value._json = {"a": "b"}
+
+            with CatchOutput() as out:
+                self.c.print_model_information()
+
+        self.assertEqual(out.stdout, b"{'a': 'b'}\n")
+
+        self.assertEqual(p.call_count, 1)
+        self.assertEqual(p.call_args[0][0],
+                         'http://service.iris.edu/irisws/syngine/1/models')
+        self.assertEqual(p.call_args[1]["params"], None)
+        self.assertEqual(p.call_args[1]["headers"],
+                         {'User-Agent': DEFAULT_TESTING_USER_AGENT})
+
+    def test_get_service_version_mock(self):
+        with mock.patch("requests.get") as p:
+            p.return_value = RequestsMockResponse()
+            p.return_value.text = "1.2.3"
+            version = self.c.get_service_version()
+
+        self.assertEqual(version, "1.2.3")
+
+        self.assertEqual(p.call_count, 1)
+        self.assertEqual(p.call_args[0][0],
+                         'http://service.iris.edu/irisws/syngine/1/version')
+        self.assertEqual(p.call_args[1]["params"], None)
         self.assertEqual(p.call_args[1]["headers"],
                          {'User-Agent': DEFAULT_TESTING_USER_AGENT})
 
