c9b11b8074ac3a58e550c52b81f477064347e5ae
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2016-01-22 14:08:24 +0100

    More documentation work.

--- obspy/clients/syngine/__init__.py
+++ obspy/clients/syngine/__init__.py
@@ -44,7 +44,7 @@ IU.ANMO.SE.MXE | 2010-02-27T06:35:14... - ... | 4.0 Hz, 15520 samples
     st.plot()
 
 The available parameters are explained in detail in the
-:meth:`~obspy.clients.Syngine.client.Client.get_waveforms()` method and on
+:meth:`~obspy.clients.syngine.client.Client.get_waveforms()` method and on
 the `Syngine <http://ds.iris.edu/ds/products/syngine/>`_ website.
 
 
@@ -71,20 +71,45 @@ Bulk Requests
 -------------
 
 It is also possible to send requests for multiple user-defined stations. See
-the :meth:`~obspy.clients.Syngine.client.Client.get_waveforms_bulk()` method
+the :meth:`~obspy.clients.syngine.client.Client.get_waveforms_bulk()` method
 for details. This example specifies a bunch of receiver coordinates and
 requests seismograms for all of these.
 
 >>> bulk = [{"latitude": 10.1, "longitude": 12.2, "stacode": "AA"},
-...         {"latitude": 14.5, "longitude": 154.0, "stacode": "BB"}]
+...         {"latitude": 14.5, "longitude": 10.0, "stacode": "BB"}]
 >>> st = client.get_waveforms_bulk(
 ...     model="ak135f_5s", eventid="GCMT:C201002270634A",
 ...     bulk=bulk, starttime="P-10", endtime="P+20")
 >>> print(st)  # doctest: +ELLIPSIS
-3 Trace(s) in Stream:
+6 Trace(s) in Stream:
 XX.AA.SE.MXZ | 2010-02-27T06:48:11.500000Z - ... | 4.0 Hz, 120 samples
 XX.AA.SE.MXN | 2010-02-27T06:48:11.500000Z - ... | 4.0 Hz, 120 samples
 XX.AA.SE.MXE | 2010-02-27T06:48:11.500000Z - ... | 4.0 Hz, 120 samples
+XX.BB.SE.MXZ | 2010-02-27T06:48:15.250000Z - ... | 4.0 Hz, 120 samples
+XX.BB.SE.MXN | 2010-02-27T06:48:15.250000Z - ... | 4.0 Hz, 120 samples
+XX.BB.SE.MXE | 2010-02-27T06:48:15.250000Z - ... | 4.0 Hz, 120 samples
+
+
+Other Useful Methods
+--------------------
+
+Use the :meth:`~obspy.clients.syngine.client.Client.get_available_models()`
+method for a list of all available methods including some meta-information.
+
+>>> client.get_available_models()  # doctest: +SKIP
+{'ak135f_1s': {'components': 'vertical only',
+  'default_components': 'Z',
+  'default_dt': '0.05',
+  'description': 'ak135 with density & Q of Montagner & Kennet(1996)',
+  'length': 1815.00690721715,
+  'max_event_depth': 750000,
+  'max_period': '~100',
+  'max_sampling_period': '0.255383',
+  'min_period': 1.04999995231628},
+ 'ak135f_2s': ...
+ ...
+ }
+
 """
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
