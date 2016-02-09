e2c8f12792dc9e36b826d0e03989e097c0e3a2cb
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2016-02-09 23:35:34 +0100

    Forgot to rename things in the taup tests.

--- obspy/taup/tests/test_split_model.py
+++ obspy/taup/tests/test_split_model.py
@@ -22,89 +22,91 @@ class SplitTauModelTestCase(unittest.TestCase):
     def test_split(self):
         depth = 110
         tau_model = TauModel.from_file('iasp91')
-        splitTMod = tau_model.split_branch(depth)
+        split_t_mod = tau_model.split_branch(depth)
         self.assertEqual(tau_model.tau_branches.shape[1] + 1,
-                         splitTMod.tau_branches.shape[1])
+                         split_t_mod.tau_branches.shape[1])
         self.assertEqual(len(tau_model.ray_params) + 2,
-                         len(splitTMod.ray_params))
+                         len(split_t_mod.ray_params))
 
         branch_count = tau_model.tau_branches.shape[1]
         split_branch_index = tau_model.find_branch(depth)
 
-        new_P_ray_param = splitTMod.s_mod.get_slowness_layer(
-            splitTMod.s_mod.layer_number_above(depth, True), True)['bot_p']
-        new_S_ray_param = splitTMod.s_mod.get_slowness_layer(
-            splitTMod.s_mod.layer_number_above(depth, False), False)['bot_p']
+        new_p_ray_param = split_t_mod.s_mod.get_slowness_layer(
+            split_t_mod.s_mod.layer_number_above(depth, True), True)['bot_p']
+        new_s_ray_param = split_t_mod.s_mod.get_slowness_layer(
+            split_t_mod.s_mod.layer_number_above(depth, False), False)['bot_p']
 
-        pIndex = sIndex = len(splitTMod.ray_params)
-        ray_params = splitTMod.ray_params
+        p_index = s_index = len(split_t_mod.ray_params)
+        ray_params = split_t_mod.ray_params
         for j in range(len(ray_params)):
-            if new_P_ray_param == ray_params[j]:
-                pIndex = j
-            if new_S_ray_param == ray_params[j]:
-                sIndex = j
+            if new_p_ray_param == ray_params[j]:
+                p_index = j
+            if new_s_ray_param == ray_params[j]:
+                s_index = j
 
-        self.assertTrue(pIndex == len(splitTMod.ray_params) or sIndex < pIndex)
+        self.assertTrue(p_index == len(split_t_mod.ray_params) or
+                        s_index < p_index)
 
         for b in range(branch_count):
             orig = tau_model.get_tau_branch(b, True)
-            depthBranch = None
             if b < split_branch_index:
-                depthBranch = splitTMod.get_tau_branch(b, True)
-                self.assertGreater(depthBranch.dist[pIndex], 0)
-                self.assertGreater(depthBranch.time[pIndex], 0)
+                depth_branch = split_t_mod.get_tau_branch(b, True)
+                self.assertGreater(depth_branch.dist[p_index], 0)
+                self.assertGreater(depth_branch.time[p_index], 0)
             elif b > split_branch_index:
-                depthBranch = splitTMod.get_tau_branch(b + 1, True)
-                self.assertAlmostEqual(depthBranch.dist[pIndex], 0,
+                depth_branch = split_t_mod.get_tau_branch(b + 1, True)
+                self.assertAlmostEqual(depth_branch.dist[p_index], 0,
                                        delta=0.00000001)
-                self.assertAlmostEqual(depthBranch.time[pIndex], 0,
+                self.assertAlmostEqual(depth_branch.time[p_index], 0,
                                        delta=0.00000001)
             else:
                 # the split one
                 continue
 
-            np.testing.assert_allclose(orig.dist[0:sIndex],
-                                       depthBranch.dist[0:sIndex],
+            np.testing.assert_allclose(orig.dist[0:s_index],
+                                       depth_branch.dist[0:s_index],
                                        atol=0.00000001)
-            np.testing.assert_allclose(orig.time[0:sIndex],
-                                       depthBranch.time[0:sIndex],
+            np.testing.assert_allclose(orig.time[0:s_index],
+                                       depth_branch.time[0:s_index],
                                        atol=0.00000001)
             orig_len = len(orig.dist)
-            if sIndex < orig_len:
-                self.assertEqual(orig_len + 2, len(depthBranch.dist))
-                np.testing.assert_allclose(orig.dist[sIndex:pIndex - 1],
-                                           depthBranch.dist[sIndex + 1:pIndex],
-                                           atol=0.00000001)
-                np.testing.assert_allclose(orig.time[sIndex:pIndex - 1],
-                                           depthBranch.time[sIndex + 1:pIndex],
-                                           atol=0.00000001)
+            if s_index < orig_len:
+                self.assertEqual(orig_len + 2, len(depth_branch.dist))
                 np.testing.assert_allclose(
-                    orig.dist[pIndex:orig_len-sIndex-2],
-                    depthBranch.dist[pIndex+2:orig_len-sIndex],
+                    orig.dist[s_index:p_index - 1],
+                    depth_branch.dist[s_index + 1:p_index],
                     atol=0.00000001)
                 np.testing.assert_allclose(
-                    orig.time[pIndex:orig_len-sIndex-2],
-                    depthBranch.time[pIndex+2:orig_len-sIndex],
+                    orig.time[s_index:p_index - 1],
+                    depth_branch.time[s_index + 1:p_index],
+                    atol=0.00000001)
+                np.testing.assert_allclose(
+                    orig.dist[p_index:orig_len-s_index-2],
+                    depth_branch.dist[p_index+2:orig_len-s_index],
+                    atol=0.00000001)
+                np.testing.assert_allclose(
+                    orig.time[p_index:orig_len-s_index-2],
+                    depth_branch.time[p_index+2:orig_len-s_index],
                     atol=0.00000001)
 
         # now check branch split
         orig = tau_model.get_tau_branch(split_branch_index, True)
-        above = splitTMod.get_tau_branch(split_branch_index, True)
-        below = splitTMod.get_tau_branch(split_branch_index + 1, True)
+        above = split_t_mod.get_tau_branch(split_branch_index, True)
+        below = split_t_mod.get_tau_branch(split_branch_index + 1, True)
         self.assertAlmostEqual(above.min_ray_param, below.max_ray_param, 8)
         for i in range(len(above.dist)):
-            if i < sIndex:
+            if i < s_index:
                 self.assertAlmostEqual(orig.dist[i], above.dist[i],
                                        delta=0.000000001)
-            elif i == sIndex:
+            elif i == s_index:
                 # new value should be close to average of values to either side
                 self.assertAlmostEqual((orig.dist[i - 1] + orig.dist[i]) / 2,
                                        above.dist[i], delta=0.00001)
-            elif i > sIndex and i < pIndex:
+            elif i > s_index and i < p_index:
                 self.assertAlmostEqual(orig.dist[i - 1],
                                        above.dist[i] + below.dist[i],
                                        delta=0.000000001)
-            elif i == pIndex:
+            elif i == p_index:
                 # new value should be close to average of values to either side
                 self.assertAlmostEqual(
                     (orig.dist[i - 2] + orig.dist[i - 1]) / 2,
