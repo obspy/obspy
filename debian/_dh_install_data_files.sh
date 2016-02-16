e4973041f13538f48e414a85910aef93952f4ecd
Tobias Megies <megies@geophysik.uni-muenchen.de>
2016-02-16 17:51:57 +0100

    deb: fix file copying again

--- debian/_dh_install_data_files.sh
+++ debian/_dh_install_data_files.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 
-for TESTSDIR in `find . -type d -wholename './debian/tmp/usr/lib/python2*/*-packages/obspy/*/tests'`
+for TESTSDIR in `find . -type d -regextype posix-egrep -regex './debian/tmp/usr/lib/python2.*/.*-packages/obspy/.*/tests/(data|images)'`
 do
-    for DIRTYPE in data images
-    do
-        SUFFIX=`echo $TESTSDIR | sed 's#.*-packages/##'`
-        TARGET=usr/share/${SUFFIX}/${DIRTYPE}
-        # check if there's files that need copying, otherwise the build complains about the empty folder..
-        if [ `ls ${TESTSDIR}/${DIRTYPE}/* &> /dev/null; echo $?` -eq 0 ]
-        then
-            dh_installdirs -p python-obspy-dbg ${TARGET}
-            dh_install -p python-obspy-dbg ${TESTSDIR}/${DIRTYPE}/* ${TARGET}
-        fi
-    done
+    SUFFIX=`echo $TESTSDIR | sed 's#.*-packages/##'`
+    TARGET=usr/share/${SUFFIX}
+    dh_installdirs -p python-obspy-dbg ${TARGET}
+    dh_install -p python-obspy-dbg ${TESTSDIR}/* ${TARGET}
 done
