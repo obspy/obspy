cf57ce4d22f1eea383ce60d14ff7cd25aa92a28c
Tobias Megies <megies@geophysik.uni-muenchen.de>
2016-03-12 13:26:47 +0100

    avoid test fail on new miniseed test file in core

--- obspy/core/tests/test_waveform_plugins.py
+++ obspy/core/tests/test_waveform_plugins.py
@@ -188,7 +188,10 @@ class WaveformPluginsTestCase(unittest.TestCase):
             os.path.join('io', 'reftek', 'tests', 'data',
                          '2015282_225051_0ae4c_1_2.msd'),
             os.path.join('io', 'reftek', 'tests', 'data',
-                         '2015282_225051_0ae4c_1_3.msd')]
+                         '2015282_225051_0ae4c_1_3.msd'),
+            os.path.join('core', 'tests', 'data',
+                         'ffbx_unrotated_gaps.mseed'),
+        ]
         formats_ep = _get_default_eps('obspy.plugin.waveform', 'isFormat')
         formats = list(formats_ep.values())
         # Get all the test directories.
