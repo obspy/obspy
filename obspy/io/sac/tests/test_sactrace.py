002140637521b0074c6aa01ead51c65b3bb948f7
Jonathan MacCarthy <jkmacc@lanl.gov>
2016-06-19 10:16:46 -0600

    Remove unused import

--- obspy/io/sac/tests/test_sactrace.py
+++ obspy/io/sac/tests/test_sactrace.py
@@ -2,7 +2,6 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from future.builtins import *  # NOQA
-from future.utils import native_str
 
 import os
 import unittest
