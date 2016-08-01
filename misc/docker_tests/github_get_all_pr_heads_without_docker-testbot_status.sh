2649e42b725b9940502f23ba25b92aac2f4b26e5
Tobias Megies <megies@geophysik.uni-muenchen.de>
2016-08-01 17:55:38 +0200

    docker fetch commits to build for PRs: also get repo information for each head tip

--- misc/docker_tests/github_get_all_pr_heads_without_docker-testbot_status.sh
+++ misc/docker_tests/github_get_all_pr_heads_without_docker-testbot_status.sh
@@ -9,7 +9,21 @@ rm $LOG
 DATA=`curl --silent -H "Authorization: token ${OBSPY_COMMIT_STATUS_TOKEN}" --request GET 'https://api.github.com/repos/obspy/obspy/pulls?state=open&sort=updated&direction=desc&per_page=100'`
 echo $DATA >> $LOG
 echo "" >> $LOG
-COMMITS=`echo $DATA | grep -s --only-matching --extended-regexp '"statuses_url": "https://api.github.com/repos/obspy/obspy/statuses/([0-9a-z]{40})",' | sed 's#.*/##' | sed 's#".*##'`
+# echo without quotes gets rid of line breaks, which makes regex simpler
+# than get rid of spaces and tabs with sed
+# than extract all matches with grep, one per line
+#    ..assuming the following structure in the returned json:
+#      ...
+#      "statuses_url": "https://api.github.com/repos/obspy/obspy/statuses/dd58fbbc33a8d8ce6f1a76013a5daa9e4a7db72a",
+#      "head": {
+#        "label": "obspy:docker_rm_old_logs",
+#        "ref": "docker_rm_old_logs",
+#        "sha": "dd58fbbc33a8d8ce6f1a76013a5daa9e4a7db72a",
+#        "user": {
+#          "login": "obspy",
+#      ...
+# finally extract the interesting parts with sed and regex
+COMMITS=`echo $DATA | sed 's#\s##g' | grep -s --only-matching --extended-regexp '"head":{"label":"[^"]*","ref":"[^"]*","sha":"[a-z0-9]{40}"' | sed 's#.*"label":"\([^:]*\):.*"sha":"\([a-z0-9]\{40\}\)"#\1:\2#'`
 echo $COMMITS >> $LOG
 echo "" >> $LOG
 
@@ -17,7 +31,9 @@ echo "" >> $LOG
 # returns 0 if no such status exists, i.e. build needed
 # returns 1 if such status exists, i.e. no build needed
 commit_needs_build() {
-    curl --silent --show-error --no-buffer -H "Authorization: token ${OBSPY_COMMIT_STATUS_TOKEN}" --request GET "https://api.github.com/repos/obspy/obspy/commits/$1/status" 2>> $LOG | grep -q -s '"context":[ ]*"docker-testbot"' >> $LOG 2>&1
+    REPO_SHA=(${1//:/ })
+    SHA=${REPO_SHA[1]}
+    curl --silent --show-error --no-buffer -H "Authorization: token ${OBSPY_COMMIT_STATUS_TOKEN}" --request GET "https://api.github.com/repos/obspy/obspy/commits/${SHA}/status" 2>> $LOG | grep -q -s '"context":[ ]*"docker-testbot"' >> $LOG 2>&1
     if [ $? = 0 ]; then return 1; else return 0; fi
 }
 
