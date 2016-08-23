6f6a598a083dd9e998e72aec12b9e201953689cb
Tobias Megies <megies@geophysik.uni-muenchen.de>
2016-08-23 11:38:42 +0200

    minor updates for docker testbot scripts

--- misc/docker_tests/cronjob_docker_tests.sh
+++ misc/docker_tests/cronjob_docker_tests.sh
@@ -1,12 +1,13 @@
 # this script is currently used in a Debian Linux virtual machine's crontab
 # (which gets started headless once per day):
 # 
+# # a personal github authorization token with "repo:status" OAuth scope has to be provided to work around anonymous github API limits
 # OBSPY_COMMIT_STATUS_TOKEN=....
-# @reboot mv $HOME/docker_testbot.log $HOME/docker_testbot.log.old; bash /path/to/cronjob_docker_tests.sh > $HOME/docker_testbot.log 2>&1
+# @reboot mv $HOME/docker_testbot.log $HOME/docker_testbot.log.old; bash /path/to/dedicated/obspy/checkout/misc/docker_tests/cronjob_docker_tests.sh > $HOME/docker_testbot.log 2>&1
 
 # sleep for some time, so that the cronjob can be killed if the VM is entered
 # for some manual tampering
-sleep 60
+sleep 300
 # keep VM up to date
 sudo aptitude update && sudo aptitude upgrade -y
 
@@ -23,11 +24,11 @@ OBSPY_DOCKER=$HOME/obspy_docker_tests
 export OBSPY_DOCKER_TEST_SOURCE_TREE="clone"
 
 # run docker tests on any commit that is the tip of a pull request and that
-# does not have a docker testbot commit status
+# does not have a docker testbot commit status (or has status "pending")
 cd ${OBSPY_DOCKER} || exit 1
-git fetch origin
+git fetch origin  # this assumes "origin" is the remote alias for the obspy/obspy main repository!
 git clean -fdx
-git checkout master
+git checkout master  # "master" should be set up to track "master" branch of obspy/obspy
 git pull
 git reset --hard FETCH_HEAD
 git clean -fdx
@@ -35,28 +36,25 @@ cd ${OBSPY_DOCKER}/misc/docker_tests/
 TARGETS=`bash github_get_all_pr_heads_without_docker-testbot_status.sh`
 for TARGET in $TARGETS
 do
-    # TARGET is e.g. '1002-krischer:966a4a93b3376866c9fade5ded73a7d6e3a20492'
-    PR_REPO_SHA=(${TARGET//-/ })
+    echo "##### WORKING ON TARGET: ${TARGET}"
+    # only allowed special character in github user names is dash ('-'),
+    # so we use underscore as a separator after the issue number
+    # TARGET is e.g. 1397_andres-h:3f9d48fdaad19051e7f8993dc81119f379d1041b
+    PR_REPO_SHA=(${TARGET//_/ })
     PR=${PR_REPO_SHA[0]}
     REPO_SHA=${PR_REPO_SHA[1]}
+    echo "##### RUNNING DOCKER TESTS FOR TARGET: ${REPO_SHA}"
     bash run_obspy_tests.sh -t${REPO_SHA} -e"--pr-url=https://github.com/obspy/obspy/pull/${PR}"
 done
-
-# run docker tests on maintenance_1.0.x and master
-for BRANCH in maintenance_1.0.x master
+# run docker tests on maintenance_1.0.x and master as well
+for BRANCH in obspy:maintenance_1.0.x obspy:master
 do
-    cd ${OBSPY_DOCKER} || exit 1
-    git fetch origin
-    git clean -fdx
-    git checkout $BRANCH
-    git pull
-    git reset --hard FETCH_HEAD
-    git clean -fdx
-    cd ${OBSPY_DOCKER}/misc/docker_tests/
-    bash run_obspy_tests.sh
+    echo "##### RUNNING DOCKER TESTS FOR BRANCH: ${BRANCH}"
+    bash run_obspy_tests.sh -t$BRANCH
 done
 
-# build and test debian packages
+
+# build and test debian packages (needs proper Debian buildbot set up in VM first)
 cd $HOME
 ## run tests in chroots
 #bash $HOME/schroot_testrun.sh -f obspy -t master
--- misc/docker_tests/github_get_all_pr_heads_without_docker-testbot_status.sh
+++ misc/docker_tests/github_get_all_pr_heads_without_docker-testbot_status.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-mkdir -p logs
-LOG=logs/github_get_all_pr_heads_without_docker-testbot_status.log
+LOGDIR=logs
+LOG=$LOGDIR/github_get_all_pr_heads_without_docker-testbot_status.log
 
-rm $LOG
+rm -f $LOG
+
+mkdir -p $LOGDIR
 
 # get a list of commit hashes for open pull requests
 DATA=`curl --silent -H "Authorization: token ${OBSPY_COMMIT_STATUS_TOKEN}" --request GET 'https://api.github.com/repos/obspy/obspy/pulls?state=open&sort=updated&direction=desc&per_page=100'`
+echo "GitHub API PR data:" >> $LOG
 echo $DATA >> $LOG
 echo "" >> $LOG
 # echo without quotes gets rid of line breaks, which makes regex simpler
@@ -25,7 +28,10 @@ echo "" >> $LOG
 #      ...
 # finally extract the interesting parts with sed and regex
 TARGETS=`echo $DATA | sed 's#\s##g' | grep -s --only-matching --extended-regexp '"comments_url":"[^"]*","statuses_url":"[^"]*","head":{"label":"[^"]*","ref":"[^"]*","sha":"[a-z0-9]{40}"' | sed 's#.*/issues/\([0-9]*\)/comments".*"head":{"label":"\([^:]*\):.*"sha":"\([a-z0-9]\{40\}\)"#\1_\2:\3#'`
+# only special character allowed by github in user names is dash ('-'),
+# so we use underscore as a separator after the PR number
 # output is PRNUMBER_FORK:SHA
+echo "All PR targets:" >> $LOG
 echo $TARGETS >> $LOG
 echo "" >> $LOG
 
@@ -39,10 +45,12 @@ target_needs_build() {
     REPO_SHA=(${REPO_SHA//:/ })
     SHA=${REPO_SHA[1]}
     STATUS=`curl --silent --show-error --no-buffer -H "Authorization: token ${OBSPY_COMMIT_STATUS_TOKEN}" --request GET "https://api.github.com/repos/obspy/obspy/commits/${SHA}/status" 2>> $LOG`
+    # if status is "pending" the commit/PR also needs a build..
     echo $STATUS | sed 's#\s##g' | grep -s --only-matching --extended-regexp '"state":[^}]*"context":"docker-testbot"' | grep -q -v "pending"
     if [ $? = 0 ]; then return 1; else return 0; fi
 }
 
+echo "PR targets that need a build:" >> $LOG
 for TARGET in $TARGETS
 do
     if target_needs_build $TARGET
