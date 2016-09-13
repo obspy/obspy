bab8dfefe6d3400b3d5e5a30d2d260d8a688e843
Jonathan MacCarthy <jkmacc@lanl.gov>
2016-09-13 15:58:26 -0600

    Employ module for managing http proxy
    The NEIC Client class now queries the user's environment using
    urllib.getproxies(), and passes those setting to functions imported from
    httpproxy.py, so that neic.Client uses a proxy-enabled socket.  If no
    proxies are present in the user environment, behavior is the same as
    previous.
    
    Flake8
    
    Flake8
    
    Fix str check for addr type

--- obspy/clients/httpproxy.py
+++ obspy/clients/httpproxy.py
@@ -10,6 +10,9 @@ License:
 J. MacCarthy, modified from https://gist.github.com/frxstrem/4487802
 
 """
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from future.builtins import *  # NOQA
 
 import socket
 from base64 import b64encode
@@ -33,11 +36,11 @@ def get_proxy_tuple():
 
 def valid_address(addr):
     """ Verify that an IP/port tuple is valid """
-    TF = (isinstance(addr, (list, tuple)) and
-          len(addr) == 2 and
-          isinstance(addr[0], str) and
-          isinstance(addr[1], (int, long)))
-    return TF
+    is_valid = (isinstance(addr, (list, tuple)) and
+                len(addr) == 2 and
+                isinstance(addr[0].decode(), (str, unicode)) and
+                isinstance(addr[1], int))
+    return is_valid
 
 
 def http_proxy_connect(address, proxy, auth=None, headers={}):
