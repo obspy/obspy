fe9e200970847ab3efa5d35e7eaedf878c58d7ac
Tobias Megies <megies@geophysik.uni-muenchen.de>
2016-11-20 14:02:52 +0100

    deb packaging: fix building man pages
     - also copy py2 .so's to source tree for help2man
     - shift man page generation back
    
    we need to copy so files to source tree before man page generation,
    otherwise it will fail
    
     - ignore warnings about timestamped gzip files
    
    warning shows up for every .gz in our package and that makes it hard to
    see really significant warnings/errors.
    the gzip files this lintian tag is warning about are not zipped during
    package building but are in our tests/data/ directories, so the warning
    is not even true (the build is still reproducible, as the timestamps are
    the same across builds).
    
    fixes #1272

--- debian/pybuild/_copy_lib_extensions_to_source_tree.sh
+++ debian/pybuild/_copy_lib_extensions_to_source_tree.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+for FILE in `ls debian/tmp/usr/lib/python2.7/*-packages/obspy/lib/lib*`
+do
+    # copy to original source directory, otherwise Help2Man exits with error
+    # because command line script can't be executed (it is tried to be executed
+    # in source directory it seems)
+    cp $FILE obspy/lib/
+done
 for FILE in `ls debian/tmp/usr/lib/python3*/*-packages/obspy/lib/lib*`
 do
     # copy to original source directory, otherwise Help2Man exits with error
