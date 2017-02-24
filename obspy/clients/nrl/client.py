a2b36cf09b8f0d162c1dde35aebdece555ed4784
Lloyd Carothers <lloyd@passcal.nmt.edu>
2017-02-24 16:24:15 -0700

    NRL client, with possible cursor

---
+++ obspy/clients/nrl/client.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+obspy.clients.nrl.client.py for navigating, and picking responses from the
+Nominal Response Library.
+
+:copyright:
+    Lloyd Carothers IRIS/PASSCAL, 2016
+:license:
+    GNU Lesser General Public License, Version 3
+    (http://www.gnu.org/copyleft/lesser.html)
+'''
