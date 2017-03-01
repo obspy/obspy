2d6040683acc155af88859e0711283dd77c9481e
Lloyd Carothers <lloyd@passcal.nmt.edu>
2017-02-28 17:37:54 -0700

    NRL refactor. Almost done.

--- obspy/clients/nrl/tests/test_nrl.py
+++ obspy/clients/nrl/tests/test_nrl.py
@@ -7,7 +7,7 @@ import unittest
 
 import os
 
-from obspy.clients.nrl.client import NRL
+from obspy.clients.nrl.client import NRL, LocalNRL, RemoteNRL
 
 
 class NRLTestCase(unittest.TestCase):
@@ -21,57 +21,25 @@ class NRLTestCase(unittest.TestCase):
                                            'IRIS',
                                            )
         self._nrl_local = NRL(root=self.local_nrl_root)
-        self._nrl_online = NRL(root='http://ds.iris.edu/NRL/')
+        self._nrl_online = NRL(root='http://ds.iris.edu/NRL')
+        self._nrl_default = NRL()
 
-    def test_local_path(self):
-        nrl = self._nrl_local
-        # Test Datalogger
-        path = nrl.datalogger_path(('REF TEK', 'RT 130 & 130-SMA',
-                                    nrl.GAIN, nrl.SR),
-                                   gain=1, sr=1)
-        self.assertEqual(os.path.basename(path), 'RESP.XX.NR001..LHZ.130.1.1')
-        # Test sensor
-        path = nrl.sensor_path(['Guralp', 'CMG-3T',
-                                '120s - 50 Hz', '1500'])
-        self.assertEqual(os.path.basename(path),
-                         'RESP.XX.NS007..BHZ.CMG3T.120.1500')
+        # For Lloyd delete
+        self._nrl_full_local = NRL(root=os.path.join(os.path.dirname(__file__),
+                                           'data',
+                                           'IRIS_full' ))
 
-    def test_url_path(self):
-        nrl = self._nrl_online
-        path = nrl.datalogger_path(('REF TEK', 'RT 130 & 130-SMA',
-                                    nrl.GAIN, nrl.SR),
-                                   gain=1, sr=1)
-        self.assertEqual(os.path.basename(path), 'RESP.XX.NR001..LHZ.130.1.1')
-        path = nrl.sensor_path(['Guralp', 'CMG-3T',
-                                '120s - 50 Hz', '1500'])
-        self.assertEqual(os.path.basename(path),
-                         'RESP.XX.NS007..BHZ.CMG3T.120.1500')
+    def test_obj_types(self):
+        self.assertIsInstance(self._nrl_local, NRL)
+        self.assertIsInstance(self._nrl_local, LocalNRL)
+        self.assertIsInstance(self._nrl_online, NRL)
+        self.assertIsInstance(self._nrl_online, RemoteNRL)
+        self.assertIsInstance(self._nrl_default, NRL)
+        self.assertIsInstance(self._nrl_default, RemoteNRL)
 
-    def test_resp(self):
-        # these functions return the contents of RESPs
-        nrl = self._nrl_online
-        resp = nrl.datalogger_from_short('q330',
-                                         gain=1, sr=1)
-        self.assertRegexpMatches(resp, r'B050F03\s*Station:\s*NQ001')
-        # self.assert
-        nrl = self._nrl_local
-        resp = nrl.sensor_from_short('cmg3t')
-        self.assertRegexpMatches(resp, r'B050F03\s*Station:\s*NS007')
-
-    def test_all_shortcut_path(self):
-        nrl = self._nrl_local
-        for sc in nrl.dl_shortcuts:
-            resp_path = nrl.datalogger_path_from_short(sc, gain=1, sr=1)
-            self.assertTrue(os.path.basename(resp_path).startswith('RESP.XX'))
-        for sc in nrl.sensor_shortcuts:
-            resp_path = nrl.sensor_path_from_short(sc)
-            self.assertTrue(os.path.basename(resp_path).startswith('RESP.XX'))
-
-    def test_get_inv_response(self):
-        nrl = self._nrl_online
-        response = nrl.get_response(
-            sensor_keys=['Nanometrics', 'Trillium Compact', '120 s'],
-            datalogger_keys=['REF TEK', 'RT 130 & 130-SMA', '1', '200'])
+    def test_get_response(self):
+        nrl = self._nrl_full_local
+        nrl.get_response
 
 
 def suite():
