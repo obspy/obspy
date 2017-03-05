a472efba40bb83133e224adc568673d67ed09592
Robert Barsch <barsch@egu.eu>
2017-03-05 15:35:26 +0100

    prevent "DeprecationWarning: object of type <class 'float'> cannot be safely interpreted as an integer."

--- obspy/core/tests/test_response.py
+++ obspy/core/tests/test_response.py
@@ -195,7 +195,7 @@ class ResponseTestCase(unittest.TestCase):
 
         sampling_rate = 40.0
         t_samp = 1.0 / sampling_rate
-        nfft = 100.0
+        nfft = 100
 
         cpx_response, freq = inv[0][0][0].response.get_evalresp_response(
             t_samp=t_samp, nfft=nfft, output="VEL", start_stage=None,
@@ -236,7 +236,7 @@ class ResponseTestCase(unittest.TestCase):
         # list.
         sampling_rate = 45.0
         t_samp = 1.0 / sampling_rate
-        nfft = 100.0
+        nfft = 100
 
         with self.assertRaises(ValueError) as e:
             inv[0][0][0].response.get_evalresp_response(
