1f9685619a54dcd5384af31e901dc6fd90e4b823
ThomasLecocq <thomas.lecocq@oma.be>
2017-03-14 15:43:29 +0100

    intercept to intercept_origin and flipping behaviour

--- obspy/signal/regression.py
+++ obspy/signal/regression.py
@@ -26,16 +26,18 @@ import scipy.optimize
 import numpy as np
 
 
-def linear_regression(xdata, ydata, weights=None, p0=None, intercept=False):
+def linear_regression(xdata, ydata, weights=None, p0=None,
+                      intercept_origin=True):
     """ Use linear least squares to fit a function, f, to data. This method
     is a generalized version of :meth:`scipy.optimize.minpack.curve_fit`;
     allowing for Ordinary Least Square and Weighted Least Square regressions:
 
-    * OLS without intercept : ``linear_regression(xdata, ydata)``
-    * OLS with intercept : ``linear_regression(xdata, ydata, intercept=True)``
-    * WLS without intercept : ``linear_regression(xdata, ydata, weights)``
-    * WLS with intercept : ``linear_regression(xdata, ydata, weights,
-      intercept=True)``
+    * OLS with origin intercept : ``linear_regression(xdata, ydata)``
+    * OLS with any intercept : ``linear_regression(xdata, ydata,
+      intercept_origin=False)``
+    * WLS with origin intercept : ``linear_regression(xdata, ydata, weights)``
+    * WLS with any intercept : ``linear_regression(xdata, ydata, weights,
+      intercept_origin=False)``
 
     If the expected values of slope (and intercept) are different from 0.0,
     provide the p0 value(s).
@@ -48,11 +50,12 @@ def linear_regression(xdata, ydata, weights=None, p0=None, intercept=False):
      1/sigma.
     :param p0: Initial guess for the parameters. If None, then the initial
      values will all be 0 (Different from SciPy where all are 1)
-    :param intercept: If False: solves y=a*x ; if True: solves y=a*x+b.
+    :param intercept_origin: If True: solves y=a*x (default); if False:
+     solves y=a*x+b.
 
     :rtype: tuple
-    :returns: (slope, intercept, std_slope, std_intercept) if `intercept` is
-     `True` or (slope, std_slope) if `False`
+    :returns: (slope, std_slope) if `intercept_origin` is `True`;
+     (slope, intercept, std_slope, std_intercept) if `False`.
     """
     if weights is not None:
         sigma = 1./weights
@@ -60,12 +63,20 @@ def linear_regression(xdata, ydata, weights=None, p0=None, intercept=False):
         sigma = None
 
     if p0 is None:
-        if intercept:
-            p0 = [0.0, 0.0]
-        else:
+        if intercept_origin:
             p0 = 0.0
+        else:
+            p0 = [0.0, 0.0]
 
-    if intercept:
+    if intercept_origin:
+        p, cov = scipy.optimize.curve_fit(lambda x, a: a * x,
+                                          xdata, ydata, p0, sigma=sigma,
+                                          xtol=1e-20)
+        slope = p[0]
+        std_slope = np.sqrt(cov[0, 0])
+        return slope, std_slope
+
+    else:
         p, cov = scipy.optimize.curve_fit(lambda x, a, b: a * x + b,
                                           xdata, ydata, p0, sigma=sigma,
                                           xtol=1e-20)
@@ -73,11 +84,3 @@ def linear_regression(xdata, ydata, weights=None, p0=None, intercept=False):
         std_slope = np.sqrt(cov[0, 0])
         std_intercept = np.sqrt(cov[1, 1])
         return slope, intercept, std_slope, std_intercept
-
-    else:
-        p, cov = scipy.optimize.curve_fit(lambda x, a: a * x,
-                                          xdata, ydata, p0, sigma=sigma,
-                                          xtol=1e-20)
-        slope = p[0]
-        std_slope = np.sqrt(cov[0, 0])
-        return slope, std_slope
