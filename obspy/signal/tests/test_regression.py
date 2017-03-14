6d0ebe1f0700e8b5767c60535b826b5ee4192082
ThomasLecocq <thomas.lecocq@oma.be>
2017-03-14 15:44:42 +0100

    regression tests updated with flipped behaviour

--- obspy/signal/tests/test_regression.py
+++ obspy/signal/tests/test_regression.py
@@ -29,41 +29,41 @@ class RegressionTestCase(unittest.TestCase):
 
     def test_weight_intercept(self):
         """
-        Test for regression with weight and intercept
+        Test for regression with weight and any intercept
         """
         ref_result = [1.103354807117634, 5.5337860049233898,
                       0.076215945728448836, 0.54474791667622224]
         result = linear_regression(self.x, self.y, self.weights,
-                                   intercept=True)
+                                   intercept_origin=False)
         np.testing.assert_equal(len(result), 4)
         np.testing.assert_allclose(result, ref_result)
 
     def test_weight_nointercept(self):
         """
-        Test for regression with weight and no intercept
+        Test for regression with weight and intercept at origin
         """
         ref_result = [1.8461448748925857, 0.075572144774706959]
         result = linear_regression(self.x, self.y, self.weights,
-                                   intercept=False)
+                                   intercept_origin=True)
         np.testing.assert_equal(len(result), 2)
         np.testing.assert_allclose(result, ref_result)
 
     def test_noweight_intercept(self):
         """
-        Test for regression with no weight and intercept
+        Test for regression with no weight and any intercept
         """
         ref_result = [1.0161734373705231, 6.090329180877835,
                       0.053630481620317534, 0.28630842447712868]
-        result = linear_regression(self.x, self.y, intercept=True)
+        result = linear_regression(self.x, self.y, intercept_origin=False)
         np.testing.assert_equal(len(result), 4)
         np.testing.assert_allclose(result, ref_result)
 
     def test_noweight_nointercept(self):
         """
-        Test for regression with no weight and no intercept
+        Test for regression with no weight and intercept at origin
         """
         ref_result = [1.9778043606670241, 0.20639881421917514]
-        result = linear_regression(self.x, self.y, intercept=False)
+        result = linear_regression(self.x, self.y, intercept_origin=True)
         np.testing.assert_equal(len(result), 2)
         np.testing.assert_allclose(result, ref_result)
 
