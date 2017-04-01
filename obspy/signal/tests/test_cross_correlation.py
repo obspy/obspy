9bb359e02a458df202adb255f0667c274e27f91e
Lion Krischer <lion.krischer@gmail.com>
2017-04-01 23:11:04 +0200

    ignore the deprecation warnings.

--- obspy/signal/tests/test_cross_correlation.py
+++ obspy/signal/tests/test_cross_correlation.py
@@ -10,8 +10,10 @@ import ctypes as C
 import numpy as np
 import os
 import unittest
+import warnings
 
 from obspy import UTCDateTime, read
+from obspy.core.util.deprecation_helpers import ObsPyDeprecationWarning
 from obspy.core.util.libnames import _load_cdll
 from obspy.core.util.testing import ImageComparison
 from obspy.signal.cross_correlation import (correlate, xcorr_pick_correction,
@@ -37,39 +39,41 @@ class CrossCorrelationTestCase(unittest.TestCase):
         """
         This tests the old, deprecated xcorr() function.
         """
-        # example 1 - all samples are equal
-        np.random.seed(815)  # make test reproducible
-        tr1 = np.random.randn(10000).astype(np.float32)
-        tr2 = tr1.copy()
-        shift, corr = xcorr(tr1, tr2, 100)
-        self.assertEqual(shift, 0)
-        self.assertAlmostEqual(corr, 1, 2)
-        # example 2 - all samples are different
-        tr1 = np.ones(10000, dtype=np.float32)
-        tr2 = np.zeros(10000, dtype=np.float32)
-        shift, corr = xcorr(tr1, tr2, 100)
-        self.assertEqual(shift, 0)
-        self.assertAlmostEqual(corr, 0, 2)
-        # example 3 - shift of 10 samples
-        tr1 = np.random.randn(10000).astype(np.float32)
-        tr2 = np.concatenate((np.zeros(10), tr1[0:-10]))
-        shift, corr = xcorr(tr1, tr2, 100)
-        self.assertEqual(shift, -10)
-        self.assertAlmostEqual(corr, 1, 2)
-        shift, corr = xcorr(tr2, tr1, 100)
-        self.assertEqual(shift, 10)
-        self.assertAlmostEqual(corr, 1, 2)
-        # example 4 - shift of 10 samples + small sine disturbance
-        tr1 = (np.random.randn(10000) * 100).astype(np.float32)
-        var = np.sin(np.arange(10000, dtype=np.float32) * 0.1)
-        tr2 = np.concatenate((np.zeros(10), tr1[0:-10])) * 0.9
-        tr2 += var
-        shift, corr = xcorr(tr1, tr2, 100)
-        self.assertEqual(shift, -10)
-        self.assertAlmostEqual(corr, 1, 2)
-        shift, corr = xcorr(tr2, tr1, 100)
-        self.assertEqual(shift, 10)
-        self.assertAlmostEqual(corr, 1, 2)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=ObsPyDeprecationWarning)
+            # example 1 - all samples are equal
+            np.random.seed(815)  # make test reproducible
+            tr1 = np.random.randn(10000).astype(np.float32)
+            tr2 = tr1.copy()
+            shift, corr = xcorr(tr1, tr2, 100)
+            self.assertEqual(shift, 0)
+            self.assertAlmostEqual(corr, 1, 2)
+            # example 2 - all samples are different
+            tr1 = np.ones(10000, dtype=np.float32)
+            tr2 = np.zeros(10000, dtype=np.float32)
+            shift, corr = xcorr(tr1, tr2, 100)
+            self.assertEqual(shift, 0)
+            self.assertAlmostEqual(corr, 0, 2)
+            # example 3 - shift of 10 samples
+            tr1 = np.random.randn(10000).astype(np.float32)
+            tr2 = np.concatenate((np.zeros(10), tr1[0:-10]))
+            shift, corr = xcorr(tr1, tr2, 100)
+            self.assertEqual(shift, -10)
+            self.assertAlmostEqual(corr, 1, 2)
+            shift, corr = xcorr(tr2, tr1, 100)
+            self.assertEqual(shift, 10)
+            self.assertAlmostEqual(corr, 1, 2)
+            # example 4 - shift of 10 samples + small sine disturbance
+            tr1 = (np.random.randn(10000) * 100).astype(np.float32)
+            var = np.sin(np.arange(10000, dtype=np.float32) * 0.1)
+            tr2 = np.concatenate((np.zeros(10), tr1[0:-10])) * 0.9
+            tr2 += var
+            shift, corr = xcorr(tr1, tr2, 100)
+            self.assertEqual(shift, -10)
+            self.assertAlmostEqual(corr, 1, 2)
+            shift, corr = xcorr(tr2, tr1, 100)
+            self.assertEqual(shift, 10)
+            self.assertAlmostEqual(corr, 1, 2)
 
     def test_srl_xcorr(self):
         """
