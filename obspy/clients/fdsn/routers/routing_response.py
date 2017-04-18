977ff3b86c2b829859f661a774bf6072a23836a2
celsoReyes <celso.reyes@sed.ethz.ch>
2017-04-18 10:55:40 +0200

    doctests pass. station retrieval works at basic level

--- obspy/clients/fdsn/routers/routing_response.py
+++ obspy/clients/fdsn/routers/routing_response.py
@@ -9,8 +9,16 @@ class RoutingResponse(object):
     def __len__(self):
         return len(self.request_lines)
 
+    def __str__(self):
+        if len(self) != 1:
+            line_or_lines = " lines"
+        else:
+            line_or_lines = " line"
+        return self.code + ", with " + str(len(self)) + line_or_lines
+
     def add_request_line(self, request_line):
-        raise NotImplementedError
+        '''override this'''
+        self.request_lines.append(request_line)
 
 
 
