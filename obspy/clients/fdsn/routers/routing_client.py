f4d4e07fcfd8ab4388b97e5067b5b0b2db3a181c
Tobias Megies <megies@users.noreply.github.com>
2017-05-08 11:32:42 +0200

    import cleanups

--- obspy/clients/fdsn/routers/routing_client.py
+++ obspy/clients/fdsn/routers/routing_client.py
@@ -33,15 +33,16 @@ and fails the many assumptions built into the Client class.
 """
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
+from future.builtins import *  # NOQA
+from future.utils import string_types
+
+import logging
+import multiprocessing as mp
 try:
     import Queue as queue
 except ImportError:
     import queue
-# import sys
-import logging
-import multiprocessing as mp
-from future.builtins import *  # NOQA
-from future.utils import string_types
+
 from obspy.clients.fdsn import Client
 from obspy.clients.fdsn.routers.fedcatalog_parser import (RoutingResponse,
                                                           FDSNBulkRequests)
