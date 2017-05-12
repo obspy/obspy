88e68f03f88beb2ff182c8b82cfb33988ee7de09
Schneider, S.A. (Simon) <s.a.schneider@uu.nl>
2017-05-12 11:20:44 +0200

    AH format: refactoring of packing routines

--- obspy/io/ah/core.py
+++ obspy/io/ah/core.py
@@ -237,195 +237,6 @@ def _write_ah1(stream, filename):
     :param filename: open file, or file-like object
 
     """
-    def _pack_trace_with_ah_dict(trace, packer):
-
-        # station info
-        packer.pack_int(6)
-        packer.pack_fstring(6, tr.stats.ah.station.code.encode('utf-8'))
-        packer.pack_int(6)
-        try:
-            packer.pack_fstring(6, tr.stats.channel.encode('utf-8'))
-        except:
-            packer.pack_fstring(6, tr.stats.ah.station.channel.encode('utf-8'))
-
-        packer.pack_int(8)
-        packer.pack_fstring(8, tr.stats.ah.station.type.encode('utf-8'))
-        packer.pack_float(tr.stats.ah.station.latitude)
-        packer.pack_float(tr.stats.ah.station.longitude)
-        packer.pack_float(tr.stats.ah.station.elevation)
-        packer.pack_float(tr.stats.ah.station.gain)
-        packer.pack_float(tr.stats.ah.station.normalization)
-
-        poles = tr.stats.ah.station.poles
-        zeros = tr.stats.ah.station.zeros
-
-        # Poles and Zeros
-        packer.pack_float(len(poles))
-        packer.pack_float(0)
-        packer.pack_float(len(zeros))
-        packer.pack_float(0)
-
-        for _i in range(1, 30):
-            try:
-                r, i = poles[_i].real, poles[_i].imag
-            except IndexError:
-                r, i = 0, 0
-            packer.pack_float(r)
-            packer.pack_float(i)
-
-            try:
-                r, i = zeros[_i].real, zeros[_i].imag
-            except IndexError:
-                r, i = 0, 0
-            packer.pack_float(r)
-            packer.pack_float(i)
-
-        # event info
-        packer.pack_float(tr.stats.ah.event.latitude)
-        packer.pack_float(tr.stats.ah.event.longitude)
-        packer.pack_float(tr.stats.ah.event.depth)
-        try:
-            packer.pack_int(tr.stats.ah.event.origin_time.year)
-            packer.pack_int(tr.stats.ah.event.origin_time.month)
-            packer.pack_int(tr.stats.ah.event.origin_time.day)
-            packer.pack_int(tr.stats.ah.event.origin_time.hour)
-            packer.pack_int(tr.stats.ah.event.origin_time.minute)
-            packer.pack_float(tr.stats.ah.event.origin_time.second)
-        except:
-            packer.pack_int(0)
-            packer.pack_int(0)
-            packer.pack_int(0)
-            packer.pack_int(0)
-            packer.pack_int(0)
-            packer.pack_float(0)
-
-        packer.pack_int(80)
-        packer.pack_fstring(80, tr.stats.ah.event.comment.encode('utf-8'))
-
-        # record info
-        dtype = tr.stats.ah.record.type
-        packer.pack_int(dtype)
-        ndata = tr.stats.npts
-        packer.pack_uint(ndata)
-        packer.pack_float(tr.stats.ah.record.delta)
-        packer.pack_float(tr.stats.ah.record.max_amplitude)
-        packer.pack_int(tr.stats.ah.record.start_time.year)
-        packer.pack_int(tr.stats.ah.record.start_time.month)
-        packer.pack_int(tr.stats.ah.record.start_time.day)
-        packer.pack_int(tr.stats.ah.record.start_time.hour)
-        packer.pack_int(tr.stats.ah.record.start_time.minute)
-        packer.pack_float(tr.stats.ah.record.start_time.second)
-        packer.pack_float(tr.stats.ah.record.abscissa_min)
-        packer.pack_int(80)
-        packer.pack_fstring(80, tr.stats.ah.record.comment.encode('utf-8'))
-        packer.pack_int(202)
-        packer.pack_fstring(202, tr.stats.ah.record.log.encode('utf-8'))
-
-        # # extras
-        packer.pack_array(tr.stats.ah.extras, packer.pack_float)
-
-        # pack data using dtype from record info
-        if dtype == 1:
-            # float
-            packer.pack_farray(ndata, tr.data, packer.pack_float)
-        elif dtype == 6:
-            # double
-            packer.pack_farray(ndata, tr.data, packer.pack_double)
-        else:
-            # e.g. 3 (vector), 2 (complex), 4 (tensor)
-            msg = 'Unsupported AH v1 record type %d'
-            raise NotImplementedError(msg % (dtype))
-
-        return packer
-
-    def _pack_trace_wout_ah_dict(trace, packer):
-        """
-        Entry are packed in the same order as shown in
-        _pack_trace_with_ah_dict .The missing information
-        is replaced with zeros
-        station info
-        """
-        packer.pack_int(6)
-        packer.pack_fstring(6, tr.stats.station.encode('utf-8'))
-        packer.pack_int(6)
-        packer.pack_fstring(6, tr.stats.channel.encode('utf-8'))
-        packer.pack_int(8)
-        packer.pack_fstring(8, 'null'.encode('utf-8'))
-        # There is no information about latitude, longitude, elevation,
-        # gain and normalization in the basic stream object,  are set to 0
-        packer.pack_float(0)
-        packer.pack_float(0)
-        packer.pack_float(0)
-        packer.pack_float(0)
-        packer.pack_float(0)
-
-        # Poles and Zeros are not provided by stream object, are set to 0
-        for _i in range(0, 30):
-            packer.pack_float(0)
-            packer.pack_float(0)
-            packer.pack_float(0)
-            packer.pack_float(0)
-
-        # event info
-        packer.pack_float(0)
-        packer.pack_float(0)
-        packer.pack_float(0)
-        packer.pack_int(0)
-        packer.pack_int(0)
-        packer.pack_int(0)
-        packer.pack_int(0)
-        packer.pack_int(0)
-        packer.pack_float(0)
-
-        packer.pack_int(80)
-        packer.pack_fstring(80, 'null'.encode('utf-8'))
-
-        # record info
-        dtype = type(tr.data[0])
-        if '32' in str(dtype):
-            dtype = 1
-        elif '64' in str(dtype):
-            dtype = 6
-
-        packer.pack_int(dtype)
-        ndata = tr.stats.npts
-        packer.pack_uint(ndata)
-        packer.pack_float(tr.stats.delta)
-        packer.pack_float(max(tr.data))
-        packer.pack_int(tr.stats.starttime.year)
-        packer.pack_int(tr.stats.starttime.month)
-        packer.pack_int(tr.stats.starttime.day)
-        packer.pack_int(tr.stats.starttime.hour)
-        packer.pack_int(tr.stats.starttime.minute)
-
-        sec = tr.stats.starttime.second
-        msec = tr.stats.starttime.microsecond
-        starttime_second = float(str(sec) + '.' + str(msec))
-        packer.pack_float(starttime_second)
-
-        packer.pack_float(0)
-        packer.pack_int(80)
-        packer.pack_fstring(80, 'null'.encode('utf-8'))
-        packer.pack_int(202)
-        packer.pack_fstring(202, 'null'.encode('utf-8'))
-
-        # # extras
-        packer.pack_array(np.zeros(21).tolist(), packer.pack_float)
-
-        # pack data using dtype from record info
-        if dtype == 1:
-            # float
-            packer.pack_farray(ndata, tr.data, packer.pack_float)
-        elif dtype == 6:
-            # double
-            packer.pack_farray(ndata, tr.data, packer.pack_double)
-        else:
-            # e.g. 3 (vector), 2 (complex), 4 (tensor)
-            msg = 'Unsupported AH v1 record type %d'
-            raise NotImplementedError(msg % (dtype))
-
-        return packer
-
     packer = xdrlib.Packer()
 
     for tr in stream:
@@ -436,6 +247,197 @@ def _write_ah1(stream, filename):
 
     with open(filename, 'wb') as fh:
         fh.write(packer.get_buffer())
+
+
+def _pack_trace_with_ah_dict(tr, packer):
+
+    # station info
+    packer.pack_int(6)
+    packer.pack_fstring(6, tr.stats.ah.station.code.encode('utf-8'))
+    packer.pack_int(6)
+    try:
+        packer.pack_fstring(6, tr.stats.channel.encode('utf-8'))
+    except:
+        packer.pack_fstring(6, tr.stats.ah.station.channel.encode('utf-8'))
+
+    packer.pack_int(8)
+    packer.pack_fstring(8, tr.stats.ah.station.type.encode('utf-8'))
+    packer.pack_float(tr.stats.ah.station.latitude)
+    packer.pack_float(tr.stats.ah.station.longitude)
+    packer.pack_float(tr.stats.ah.station.elevation)
+    packer.pack_float(tr.stats.ah.station.gain)
+    packer.pack_float(tr.stats.ah.station.normalization)
+
+    poles = tr.stats.ah.station.poles
+    zeros = tr.stats.ah.station.zeros
+
+    # Poles and Zeros
+    packer.pack_float(len(poles))
+    packer.pack_float(0)
+    packer.pack_float(len(zeros))
+    packer.pack_float(0)
+
+    for _i in range(1, 30):
+        try:
+            r, i = poles[_i].real, poles[_i].imag
+        except IndexError:
+            r, i = 0, 0
+        packer.pack_float(r)
+        packer.pack_float(i)
+
+        try:
+            r, i = zeros[_i].real, zeros[_i].imag
+        except IndexError:
+            r, i = 0, 0
+        packer.pack_float(r)
+        packer.pack_float(i)
+
+    # event info
+    packer.pack_float(tr.stats.ah.event.latitude)
+    packer.pack_float(tr.stats.ah.event.longitude)
+    packer.pack_float(tr.stats.ah.event.depth)
+    try:
+        packer.pack_int(tr.stats.ah.event.origin_time.year)
+        packer.pack_int(tr.stats.ah.event.origin_time.month)
+        packer.pack_int(tr.stats.ah.event.origin_time.day)
+        packer.pack_int(tr.stats.ah.event.origin_time.hour)
+        packer.pack_int(tr.stats.ah.event.origin_time.minute)
+        packer.pack_float(tr.stats.ah.event.origin_time.second)
+    except:
+        packer.pack_int(0)
+        packer.pack_int(0)
+        packer.pack_int(0)
+        packer.pack_int(0)
+        packer.pack_int(0)
+        packer.pack_float(0)
+
+    packer.pack_int(80)
+    packer.pack_fstring(80, tr.stats.ah.event.comment.encode('utf-8'))
+
+    # record info
+    dtype = tr.stats.ah.record.type
+    packer.pack_int(dtype)
+    ndata = tr.stats.npts
+    packer.pack_uint(ndata)
+    packer.pack_float(tr.stats.ah.record.delta)
+    packer.pack_float(tr.stats.ah.record.max_amplitude)
+    packer.pack_int(tr.stats.ah.record.start_time.year)
+    packer.pack_int(tr.stats.ah.record.start_time.month)
+    packer.pack_int(tr.stats.ah.record.start_time.day)
+    packer.pack_int(tr.stats.ah.record.start_time.hour)
+    packer.pack_int(tr.stats.ah.record.start_time.minute)
+    packer.pack_float(tr.stats.ah.record.start_time.second)
+    packer.pack_float(tr.stats.ah.record.abscissa_min)
+    packer.pack_int(80)
+    packer.pack_fstring(80, tr.stats.ah.record.comment.encode('utf-8'))
+    packer.pack_int(202)
+    packer.pack_fstring(202, tr.stats.ah.record.log.encode('utf-8'))
+
+    # # extras
+    packer.pack_array(tr.stats.ah.extras, packer.pack_float)
+
+    # pack data using dtype from record info
+    if dtype == 1:
+        # float
+        packer.pack_farray(ndata, tr.data, packer.pack_float)
+    elif dtype == 6:
+        # double
+        packer.pack_farray(ndata, tr.data, packer.pack_double)
+    else:
+        # e.g. 3 (vector), 2 (complex), 4 (tensor)
+        msg = 'Unsupported AH v1 record type %d'
+        raise NotImplementedError(msg % (dtype))
+
+    return packer
+
+
+def _pack_trace_wout_ah_dict(tr, packer):
+    """
+    Entry are packed in the same order as shown in
+    _pack_trace_with_ah_dict .The missing information
+    is replaced with zeros
+    station info
+    """
+    packer.pack_int(6)
+    packer.pack_fstring(6, tr.stats.station.encode('utf-8'))
+    packer.pack_int(6)
+    packer.pack_fstring(6, tr.stats.channel.encode('utf-8'))
+    packer.pack_int(8)
+    packer.pack_fstring(8, 'null'.encode('utf-8'))
+    # There is no information about latitude, longitude, elevation,
+    # gain and normalization in the basic stream object,  are set to 0
+    packer.pack_float(0)
+    packer.pack_float(0)
+    packer.pack_float(0)
+    packer.pack_float(0)
+    packer.pack_float(0)
+
+    # Poles and Zeros are not provided by stream object, are set to 0
+    for _i in range(0, 30):
+        packer.pack_float(0)
+        packer.pack_float(0)
+        packer.pack_float(0)
+        packer.pack_float(0)
+
+    # event info
+    packer.pack_float(0)
+    packer.pack_float(0)
+    packer.pack_float(0)
+    packer.pack_int(0)
+    packer.pack_int(0)
+    packer.pack_int(0)
+    packer.pack_int(0)
+    packer.pack_int(0)
+    packer.pack_float(0)
+
+    packer.pack_int(80)
+    packer.pack_fstring(80, 'null'.encode('utf-8'))
+
+    # record info
+    dtype = type(tr.data[0])
+    if '32' in str(dtype):
+        dtype = 1
+    elif '64' in str(dtype):
+        dtype = 6
+
+    packer.pack_int(dtype)
+    ndata = tr.stats.npts
+    packer.pack_uint(ndata)
+    packer.pack_float(tr.stats.delta)
+    packer.pack_float(max(tr.data))
+    packer.pack_int(tr.stats.starttime.year)
+    packer.pack_int(tr.stats.starttime.month)
+    packer.pack_int(tr.stats.starttime.day)
+    packer.pack_int(tr.stats.starttime.hour)
+    packer.pack_int(tr.stats.starttime.minute)
+
+    sec = tr.stats.starttime.second
+    msec = tr.stats.starttime.microsecond
+    starttime_second = float(str(sec) + '.' + str(msec))
+    packer.pack_float(starttime_second)
+
+    packer.pack_float(0)
+    packer.pack_int(80)
+    packer.pack_fstring(80, 'null'.encode('utf-8'))
+    packer.pack_int(202)
+    packer.pack_fstring(202, 'null'.encode('utf-8'))
+
+    # # extras
+    packer.pack_array(np.zeros(21).tolist(), packer.pack_float)
+
+    # pack data using dtype from record info
+    if dtype == 1:
+        # float
+        packer.pack_farray(ndata, tr.data, packer.pack_float)
+    elif dtype == 6:
+        # double
+        packer.pack_farray(ndata, tr.data, packer.pack_double)
+    else:
+        # e.g. 3 (vector), 2 (complex), 4 (tensor)
+        msg = 'Unsupported AH v1 record type %d'
+        raise NotImplementedError(msg % (dtype))
+
+    return packer
 
 
 def _read_ah2(filename):
