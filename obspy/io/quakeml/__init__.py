227b7b48471c30032f0a752894b8ef95c589371a
Lion Krischer <lion.krischer@gmail.com>
2017-08-11 12:25:56 +0200

    Adding missing one-line descriptions.

--- obspy/io/quakeml/__init__.py
+++ obspy/io/quakeml/__init__.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
+"""
+obspy.io.quakeml - Read and write support for QuakeML files
+===========================================================
+
+:copyright:
+    The ObsPy Development Team (devs@obspy.org)
+:license:
+    GNU Lesser General Public License, Version 3
+    (https://www.gnu.org/copyleft/lesser.html)
+"""
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from future.builtins import *  # NOQA
