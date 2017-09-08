430fddaabd16c541a7eb78f7530506b6b515103a
Kasper Van wijk <kvan637@sc-phy-324060.phy.auckland.ac.nz>
2017-09-08 23:29:25 +1200

    updated the travel_time_plot to the new wrapper function format.

--- misc/docs/source/tutorial/code_snippets/travel_time_plot.py
+++ misc/docs/source/tutorial/code_snippets/travel_time_plot.py
@@ -1,5 +1,3 @@
-from obspy.taup.taup import travelTimePlot
-
-
-travelTimePlot(min_degree=0, max_degree=50, phases=['P', 'S', 'PP'],
-               depth=120, model='iasp91')
+from obspy.taup import traveltime_plot
+traveltime_plot(min_degree=0, max_degree=50, phases=['P', 'S', 'PP'],
+                source_depth=120, model='iasp91')
