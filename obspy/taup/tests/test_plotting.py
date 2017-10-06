3efc10c52088fdeedc4be229cafe9d8405dd6c30
Tobias Megies <megies@geophysik.uni-muenchen.de>
2017-10-06 15:10:50 +0200

    update taup plot tests

--- obspy/taup/tests/test_plotting.py
+++ obspy/taup/tests/test_plotting.py
@@ -9,11 +9,14 @@ from future.builtins import *  # NOQA
 
 import os
 import unittest
+import warnings
 
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 from obspy.core.util.testing import ImageComparison
-from obspy.taup import TauPyModel
+from obspy.core.util.deprecation_helpers import ObsPyDeprecationWarning
+from obspy.taup import TauPyModel, plot_travel_times
 
 
 class TauPyPlottingTestCase(unittest.TestCase):
@@ -121,12 +124,26 @@ class TauPyPlottingTestCase(unittest.TestCase):
         Same as test_cartesian_many_phases but this time no phases
         travelling the other way around the globe are plotted.
         """
+        arrivals = self.model.get_ray_paths(500, 140)
         with ImageComparison(self.image_dir,
                              "cartesian_many_phases_one_way.png") as ic:
-            self.model.get_ray_paths(500, 140).plot_rays(plot_type="cartesian",
-                                                         plot_all=False,
-                                                         show=False)
+            arrivals.plot_rays(plot_type="cartesian", plot_all=False,
+                               show=False)
             plt.savefig(ic.name)
+        # check warning message on deprecated routine
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")
+            with ImageComparison(self.image_dir,
+                                 "cartesian_many_phases_one_way.png") as ic:
+                # default for legend was True in legacy method
+                arrivals.plot(plot_type="cartesian", plot_all=False,
+                              show=False, legend=False)
+                plt.savefig(ic.name)
+            self.assertEqual(len(w), 1)
+            self.assertEqual(
+                str(w[0].message), 'The plot() function is deprecated. Please '
+                'use arrivals.plot_rays()')
+            self.assertEqual(w[0].category, ObsPyDeprecationWarning)
 
     def test_plot_travel_times(self):
         """
@@ -137,6 +154,24 @@ class TauPyPlottingTestCase(unittest.TestCase):
             self.model.get_ray_paths(10, 100,
                                      phase_list=("ttbasic",)).plot_times(
                                          show=False, legend=True)
+            plt.savefig(ic.name)
+
+    def test_plot_travel_times_convenience(self):
+        """
+        Travel time plot for many phases at multiple epicentral distances,
+        convenience function
+        """
+        with ImageComparison(
+                self.image_dir,
+                "traveltimes_many_phases_multiple_degrees.png") as ic:
+            # base line image looks awkward with points at the left/right end
+            # of the plot but that's only due to the "classic" style sheet used
+            # in tests. default mpl2 style makes room around plotted artists
+            # larger point size to ensure plot can fail properly if points should move
+            mpl.rcParams['lines.markersize'] = 20
+            plot_travel_times(10, phase_list=("P", "S", "SKS", "PP"),
+                              min_degrees=40, max_degrees=60, show=False,
+                              legend=True, npoints=4)
             plt.savefig(ic.name)
 
 
