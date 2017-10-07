fb42ed889508c28b7b0c0eaaf065c115c7d8f048
Tobias Megies <megies@geophysik.uni-muenchen.de>
2017-10-07 09:42:54 +0200

    taup travel time plot: add image test for option plot_all=False

--- obspy/taup/tests/test_plotting.py
+++ obspy/taup/tests/test_plotting.py
@@ -174,6 +174,20 @@ class TauPyPlottingTestCase(unittest.TestCase):
                               min_degrees=40, max_degrees=60, show=False,
                               legend=True, npoints=4)
             plt.savefig(ic.name)
+        # now test option plot_all=False
+        with ImageComparison(
+                self.image_dir,
+                "traveltimes_plot_all_False.png") as ic:
+            # base line image looks awkward with points at the left/right end
+            # of the plot but that's only due to the "classic" style sheet used
+            # in tests. default mpl2 style makes room around plotted artists
+            # larger point size to ensure plot can fail properly if points
+            # should move
+            mpl.rcParams['lines.markersize'] = 200
+            plot_travel_times(10, phase_list=("SSS",), min_degrees=150,
+                              max_degrees=200, npoints=4, legend=False,
+                              plot_all=False)
+            plt.savefig(ic.name)
 
     def test_ray_plot_mismatching_axes_type_warnings(self):
         """
