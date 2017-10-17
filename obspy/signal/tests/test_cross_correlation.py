abfe88d96b973e073609bcb08770f05c55b661be
trichter <tom.eulenfeld@bgr.de>
2017-10-17 15:54:28 +0200

    remove xcorr test

--- obspy/signal/tests/test_cross_correlation.py
+++ obspy/signal/tests/test_cross_correlation.py
@@ -16,7 +16,7 @@ from obspy.signal.cross_correlation import (
     correlate, correlate_template, correlate_stream_template,
     correlation_detector,
     xcorr_pick_correction, xcorr_3c, xcorr_max,
-    xcorr, _xcorr_padzeros, _xcorr_slice, _find_peaks)
+    _xcorr_padzeros, _xcorr_slice, _find_peaks)
 from obspy.signal.trigger import coincidence_trigger
 
 
@@ -33,46 +33,6 @@ class CrossCorrelationTestCase(unittest.TestCase):
         self.a = np.sin(np.linspace(0, 10, 101))
         self.b = 5 * np.roll(self.a, 5)
         self.c = 5 * np.roll(self.a[:81], 5)
-
-    def test_xcorr(self):
-        """
-        This tests the old, deprecated xcorr() function.
-        """
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore", category=ObsPyDeprecationWarning)
-            # example 1 - all samples are equal
-            np.random.seed(815)  # make test reproducible
-            tr1 = np.random.randn(10000).astype(np.float32)
-            tr2 = tr1.copy()
-            shift, corr = xcorr(tr1, tr2, 100)
-            self.assertEqual(shift, 0)
-            self.assertAlmostEqual(corr, 1, 2)
-            # example 2 - all samples are different
-            tr1 = np.ones(10000, dtype=np.float32)
-            tr2 = np.zeros(10000, dtype=np.float32)
-            shift, corr = xcorr(tr1, tr2, 100)
-            self.assertEqual(shift, 0)
-            self.assertAlmostEqual(corr, 0, 2)
-            # example 3 - shift of 10 samples
-            tr1 = np.random.randn(10000).astype(np.float32)
-            tr2 = np.concatenate((np.zeros(10), tr1[0:-10]))
-            shift, corr = xcorr(tr1, tr2, 100)
-            self.assertEqual(shift, -10)
-            self.assertAlmostEqual(corr, 1, 2)
-            shift, corr = xcorr(tr2, tr1, 100)
-            self.assertEqual(shift, 10)
-            self.assertAlmostEqual(corr, 1, 2)
-            # example 4 - shift of 10 samples + small sine disturbance
-            tr1 = (np.random.randn(10000) * 100).astype(np.float32)
-            var = np.sin(np.arange(10000, dtype=np.float32) * 0.1)
-            tr2 = np.concatenate((np.zeros(10), tr1[0:-10])) * 0.9
-            tr2 += var
-            shift, corr = xcorr(tr1, tr2, 100)
-            self.assertEqual(shift, -10)
-            self.assertAlmostEqual(corr, 1, 2)
-            shift, corr = xcorr(tr2, tr1, 100)
-            self.assertEqual(shift, 10)
-            self.assertAlmostEqual(corr, 1, 2)
 
     def test_correlate_deprecated_domain_keyword(self):
 
