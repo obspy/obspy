686e9e30bc1f1901f8d287384e7a9249413aee8a
derrick <djachambeador@gmail.com>
2017-11-12 21:03:51 -0800

    replace str with native_str in Stats _type dict

--- obspy/core/trace.py
+++ obspy/core/trace.py
@@ -150,10 +150,10 @@ class Stats(AttribDict):
     _refresh_keys = {'delta', 'sampling_rate', 'starttime', 'npts'}
     # dict of required types for certain attrs
     _types = {
-        'network': (str, native_str),
-        'station': (str, native_str),
-        'location': (str, native_str),
-        'channel': (str, native_str),
+        'network': native_str,
+        'station': native_str,
+        'location': native_str,
+        'channel': native_str,
     }
 
     def __init__(self, header={}):
