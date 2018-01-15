b3a478e03f1b5ecff34d7c36cbe1aa9eaebd9bbf
derrick <djachambeador@gmail.com>
2018-01-15 14:03:25 -0800

    move warnings import under future import

--- obspy/core/tests/test_utcdatetime.py
+++ obspy/core/tests/test_utcdatetime.py
@@ -2,14 +2,13 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-import warnings
-
 from future.builtins import *  # NOQA @UnusedWildImport
 
 import copy
 import datetime
 import itertools
 import unittest
+import warnings
 
 import numpy as np
 
