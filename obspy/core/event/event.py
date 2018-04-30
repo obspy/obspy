867321a477cd7f7ba3de0e97ffabfb1b94e0978f
derrick <djachambeador@gmail.com>
2018-04-29 20:22:11 -0700

    fix issue #2119

--- obspy/core/event/event.py
+++ obspy/core/event/event.py
@@ -114,9 +114,11 @@ class Event(__Event):
         origin = None
         if self.origins:
             origin = self.preferred_origin() or self.origins[0]
-            out += '%s | %+7.3f, %+8.3f' % (origin.time,
-                                            origin.latitude,
-                                            origin.longitude)
+            # get lat, lon, time and handle if any are None (#2119)
+            lat, lon, time = origin.latitude, origin.longitude, origin.time
+            lat_str = '%+7.3f' % lat if lat is not None else ''
+            lon_str = '%+8.3f' % lon if lon is not None else ''
+            out += '%s | %s, %s' % (time, lat_str, lon_str)
         if self.magnitudes:
             magnitude = self.preferred_magnitude() or self.magnitudes[0]
             out += ' | %s %-2s' % (magnitude.mag,
