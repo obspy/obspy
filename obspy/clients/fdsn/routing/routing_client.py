a67a98487e34627e5df49fcd9ce2c23e7297d23e
Lion Krischer <lion.krischer@gmail.com>
2018-05-05 00:34:35 +0200

    Catch and report unexpected exception while downloading routed data.

--- obspy/clients/fdsn/routing/routing_client.py
+++ obspy/clients/fdsn/routing/routing_client.py
@@ -19,6 +19,8 @@ from multiprocessing.dummy import Pool as ThreadPool
 
 import decorator
 import io
+import sys
+import traceback
 import warnings
 
 from obspy.core.compatibility import (urlparse, string_types,
@@ -85,6 +87,17 @@ def _assert_attach_response_not_in_kwargs(f, *args, **kwargs):
     if "attach_response" in kwargs:
         raise ValueError("The `attach_response` argument is not supported")
     return f(*args, **kwargs)
+
+
+def _try_download_bulk(r):
+    try:
+        return _download_bulk(r)
+    except Exception:
+        reason = "".join(traceback.format_exception(*sys.exc_info()))
+        warnings.warn(
+            "Failed to download data of type '%s' from '%s' due to: \n%s" % (
+                r["data_type"], r["endpoint"], reason))
+        return None
 
 
 def _download_bulk(r):
@@ -252,7 +265,7 @@ class BaseRoutingClient(HTTPClient):
                 "kwargs": kwargs,
                 "credentials": self.credentials})
         pool = ThreadPool(processes=len(dl_requests))
-        results = pool.map(_download_bulk, dl_requests)
+        results = pool.map(_try_download_bulk, dl_requests)
 
         # Merge all results into a single object.
         if data_type == "waveform":
