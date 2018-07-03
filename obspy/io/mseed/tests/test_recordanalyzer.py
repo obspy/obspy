8a33ea9b608b4e0effcfd61fd2168c72137a9127
Lion Krischer <lion.krischer@gmail.com>
2018-07-03 13:34:45 +0200

    Correctly deal with sr fact + mult in record analyzer
    Added a test and also eased the testing of the record analyzer in
    general.

--- obspy/io/mseed/tests/test_recordanalyzer.py
+++ obspy/io/mseed/tests/test_recordanalyzer.py
@@ -13,6 +13,7 @@ from obspy.io.mseed.scripts.recordanalyzer import main as obspy_recordanalyzer
 
 class RecordAnalyserTestCase(unittest.TestCase):
     def setUp(self):
+        self.maxDiff = None
         self.test_file = os.path.join(os.path.dirname(__file__),
                                       'data',
                                       'timingquality.mseed')
@@ -27,37 +28,37 @@ Record Offset: 0 byte
 Header Endianness: Big Endian
 
 FIXED SECTION OF DATA HEADER
-	Sequence number: 763445
-	Data header/quality indicator: D
-	Station identifier code: BGLD
-	Location identifier: 
-	Channel identifier: EHE
-	Network code: BW
-	Record start time: 2007-12-31T23:59:59.915000Z
-	Number of samples: 412
-	Sample rate factor: 200
-	Sample rate multiplier: 1
-	Activity flags: 0
-	I/O and clock flags: 0
-	Data quality flags: 0
-	Number of blockettes that follow: 2
-	Time correction: -1500
-	Beginning of data: 64
-	First blockette: 48
+    Sequence number: 763445
+    Data header/quality indicator: D
+    Station identifier code: BGLD
+    Location identifier:
+    Channel identifier: EHE
+    Network code: BW
+    Record start time: 2007-12-31T23:59:59.915000Z
+    Number of samples: 412
+    Sample rate factor: 200
+    Sample rate multiplier: 1
+    Activity flags: 0
+    I/O and clock flags: 0
+    Data quality flags: 0
+    Number of blockettes that follow: 2
+    Time correction: -1500
+    Beginning of data: 64
+    First blockette: 48
 
 BLOCKETTES
-	1000:	Encoding Format: 10
-		Word Order: 1
-		Data Record Length: 9
-	1001:	Timing quality: 55
-		mu_sec: 0
-		Frame count: -73
+    1000:    Encoding Format: 10
+        Word Order: 1
+        Data Record Length: 9
+    1001:    Timing quality: 55
+        mu_sec: 0
+        Frame count: -73
 
 CALCULATED VALUES
-	Corrected Starttime: 2007-12-31T23:59:59.765000Z
+    Corrected Starttime: 2007-12-31T23:59:59.765000Z
 
 ''' % (self.test_file,)  # noqa
-        self.assertEqual(expected, out.stdout)  # noqa
+        self.assertEqual(expected, out.stdout.replace("\t", "    "))  # noqa
 
     def test_second_record(self):
         with CatchOutput() as out:
@@ -69,37 +70,37 @@ Record Offset: 512 byte
 Header Endianness: Big Endian
 
 FIXED SECTION OF DATA HEADER
-	Sequence number: 763446
-	Data header/quality indicator: D
-	Station identifier code: BGLD
-	Location identifier: 
-	Channel identifier: EHE
-	Network code: BW
-	Record start time: 2008-01-01T00:00:01.975000Z
-	Number of samples: 412
-	Sample rate factor: 200
-	Sample rate multiplier: 1
-	Activity flags: 0
-	I/O and clock flags: 0
-	Data quality flags: 0
-	Number of blockettes that follow: 2
-	Time correction: -1500
-	Beginning of data: 64
-	First blockette: 48
+    Sequence number: 763446
+    Data header/quality indicator: D
+    Station identifier code: BGLD
+    Location identifier:
+    Channel identifier: EHE
+    Network code: BW
+    Record start time: 2008-01-01T00:00:01.975000Z
+    Number of samples: 412
+    Sample rate factor: 200
+    Sample rate multiplier: 1
+    Activity flags: 0
+    I/O and clock flags: 0
+    Data quality flags: 0
+    Number of blockettes that follow: 2
+    Time correction: -1500
+    Beginning of data: 64
+    First blockette: 48
 
 BLOCKETTES
-	1000:	Encoding Format: 10
-		Word Order: 1
-		Data Record Length: 9
-	1001:	Timing quality: 70
-		mu_sec: 0
-		Frame count: -73
+    1000:    Encoding Format: 10
+        Word Order: 1
+        Data Record Length: 9
+    1001:    Timing quality: 70
+        mu_sec: 0
+        Frame count: -73
 
 CALCULATED VALUES
-	Corrected Starttime: 2008-01-01T00:00:01.825000Z
+    Corrected Starttime: 2008-01-01T00:00:01.825000Z
 
 ''' % (self.test_file,)  # noqa
-        self.assertEqual(expected, out.stdout)  # noqa
+        self.assertEqual(expected, out.stdout.replace("\t", "    "))  # noqa
 
     def test_record_with_data_offset_zero(self):
         """
@@ -118,35 +119,35 @@ Record Offset: 512 byte
 Header Endianness: Big Endian
 
 FIXED SECTION OF DATA HEADER
-	Sequence number: 1
-	Data header/quality indicator: D
-	Station identifier code: PANIX
-	Location identifier: 
-	Channel identifier: LHZ
-	Network code: CH
-	Record start time: 2016-08-21T01:43:37.000000Z
-	Number of samples: 0
-	Sample rate factor: 1
-	Sample rate multiplier: 1
-	Activity flags: 4
-	I/O and clock flags: 0
-	Data quality flags: 0
-	Number of blockettes that follow: 2
-	Time correction: 0
-	Beginning of data: 0
-	First blockette: 48
+    Sequence number: 1
+    Data header/quality indicator: D
+    Station identifier code: PANIX
+    Location identifier:
+    Channel identifier: LHZ
+    Network code: CH
+    Record start time: 2016-08-21T01:43:37.000000Z
+    Number of samples: 0
+    Sample rate factor: 1
+    Sample rate multiplier: 1
+    Activity flags: 4
+    I/O and clock flags: 0
+    Data quality flags: 0
+    Number of blockettes that follow: 2
+    Time correction: 0
+    Beginning of data: 0
+    First blockette: 48
 
 BLOCKETTES
-	1000:	Encoding Format: 11
-		Word Order: 1
-		Data Record Length: 9
-	201:	NOT YET IMPLEMENTED
+    1000:    Encoding Format: 11
+        Word Order: 1
+        Data Record Length: 9
+    201:    NOT YET IMPLEMENTED
 
 CALCULATED VALUES
-	Corrected Starttime: 2016-08-21T01:43:37.000000Z
+    Corrected Starttime: 2016-08-21T01:43:37.000000Z
 
 ''' % (filename,)  # noqa
-        self.assertEqual(expected, out.stdout)  # noqa
+        self.assertEqual(expected, out.stdout.replace("\t", "    "))  # noqa
 
         with CatchOutput() as out:
             obspy_recordanalyzer(['-n', '2', filename])
@@ -157,37 +158,84 @@ Record Offset: 1024 byte
 Header Endianness: Big Endian
 
 FIXED SECTION OF DATA HEADER
-	Sequence number: 189
-	Data header/quality indicator: D
-	Station identifier code: PANIX
-	Location identifier: 
-	Channel identifier: LHZ
-	Network code: CH
-	Record start time: 2016-08-21T01:45:31.000000Z
-	Number of samples: 262
-	Sample rate factor: 1
-	Sample rate multiplier: 1
-	Activity flags: 68
-	I/O and clock flags: 32
-	Data quality flags: 0
-	Number of blockettes that follow: 2
-	Time correction: 0
-	Beginning of data: 64
-	First blockette: 48
+    Sequence number: 189
+    Data header/quality indicator: D
+    Station identifier code: PANIX
+    Location identifier:
+    Channel identifier: LHZ
+    Network code: CH
+    Record start time: 2016-08-21T01:45:31.000000Z
+    Number of samples: 262
+    Sample rate factor: 1
+    Sample rate multiplier: 1
+    Activity flags: 68
+    I/O and clock flags: 32
+    Data quality flags: 0
+    Number of blockettes that follow: 2
+    Time correction: 0
+    Beginning of data: 64
+    First blockette: 48
 
 BLOCKETTES
-	1000:	Encoding Format: 11
-		Word Order: 1
-		Data Record Length: 9
-	1001:	Timing quality: 100
-		mu_sec: 0
-		Frame count: 7
+    1000:    Encoding Format: 11
+        Word Order: 1
+        Data Record Length: 9
+    1001:    Timing quality: 100
+        mu_sec: 0
+        Frame count: 7
 
 CALCULATED VALUES
-	Corrected Starttime: 2016-08-21T01:45:31.000000Z
+    Corrected Starttime: 2016-08-21T01:45:31.000000Z
 
 ''' % (filename,)  # noqa
-        self.assertEqual(expected, out.stdout)  # noqa
+        self.assertEqual(expected, out.stdout.replace("\t", "    "))  # noqa
+
+    def test_record_with_negative_sr_fact_and_mult(self):
+        """
+        Regression tests as there was an issue with the record analyzer for
+        negative sampling rates and factors.
+        """
+        filename = os.path.join(
+            os.path.dirname(__file__), 'data',
+            'single_record_negative_sr_fact_and_mult.mseed')
+
+        with CatchOutput() as out:
+            obspy_recordanalyzer([filename])
+
+        expected = '''FILE: %s
+Record Number: 0
+Record Offset: 0 byte
+Header Endianness: Big Endian
+
+FIXED SECTION OF DATA HEADER
+    Sequence number: 4
+    Data header/quality indicator: M
+    Station identifier code: TNV
+    Location identifier:
+    Channel identifier: VHZ
+    Network code: MN
+    Record start time: 1991-02-21T23:50:00.430000Z
+    Number of samples: 60
+    Sample rate factor: -10
+    Sample rate multiplier: -1
+    Activity flags: 0
+    I/O and clock flags: 0
+    Data quality flags: 0
+    Number of blockettes that follow: 1
+    Time correction: 0
+    Beginning of data: 64
+    First blockette: 48
+
+BLOCKETTES
+    1000:    Encoding Format: 10
+        Word Order: 1
+        Data Record Length: 12
+
+CALCULATED VALUES
+    Corrected Starttime: 1991-02-21T23:50:00.430000Z
+
+''' % (filename,)  # noqa
+        self.assertEqual(expected, out.stdout.replace("\t", "    "))  # noqa
 
 
 def suite():
