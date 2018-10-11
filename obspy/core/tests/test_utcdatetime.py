c124d9ea5e76bdd17d75a526f98a8569bc2b4f7e
derrick <djachambeador@gmail.com>
2018-10-10 21:02:47 -0700

    tests for new behavior

--- obspy/core/tests/test_utcdatetime.py
+++ obspy/core/tests/test_utcdatetime.py
@@ -1448,26 +1448,34 @@ class UTCDateTimeTestCase(unittest.TestCase):
         kwargs = dict(year=2017, month=9, day=18, hour=0, minute=0, second=0)
         base_utc = partial(UTCDateTime, **kwargs)
         # ensure hour can exceed 23 and is equal to the day ticking forward
-        utc = base_utc(hour=25)
+        utc = base_utc(hour=25, strict=False)
         self.assertEqual(utc, base_utc(day=19, hour=1))
         # ensure minute can exceed 60
-        utc = base_utc(minute=61)
+        utc = base_utc(minute=61, strict=False)
         self.assertEqual(utc, base_utc(hour=1, minute=1))
         # ensure second can exceed 60
-        utc = base_utc(second=120)
+        utc = base_utc(second=120, strict=False)
         self.assertEqual(utc, base_utc(minute=2))
         # ensure not all kwargs are required for overflow behavior
-        utc = UTCDateTime(year=2017, month=9, day=18, second=60)
+        utc = UTCDateTime(year=2017, month=9, day=18, second=60, strict=False)
         self.assertEqual(utc, base_utc(minute=1))
+        # test for combination of args and kwargs
+        utc1 = UTCDateTime(2017, 5, 4, second=120, strict=False)
+        utc2 = UTCDateTime(2017, 5, 4, minute=2)
+        self.assertEqual(utc1, utc2)
+        # if strict == True a ValueError should be raised
+        with self.assertRaises(ValueError) as e:
+            base_utc(hour=60)
+        self.assertIn('hour must be in', str(e.exception))
 
     def test_hour_minute_second_overflow_with_replace(self):
         """
         The replace method should also support the changes described in #2222.
         """
         utc = UTCDateTime('2017-09-18T00:00:00')
-        self.assertEqual(utc.replace(hour=25), utc + 25 * 3600)
-        self.assertEqual(utc.replace(minute=1000), utc + 1000 * 60)
-        self.assertEqual(utc.replace(second=60), utc + 60)
+        self.assertEqual(utc.replace(hour=25, strict=0), utc + 25 * 3600)
+        self.assertEqual(utc.replace(minute=1000, strict=0), utc + 1000 * 60)
+        self.assertEqual(utc.replace(second=60, strict=0), utc + 60)
 
 
 def suite():
