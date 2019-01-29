e32dc718115c10f69707eb06b5f2e3174e4d68a2
Louisa Murray-Bergquist <louisa.murray.bergquist@mail.utoronto.ca>
2019-01-29 16:29:39 +0100

    Fixing bug #1887 in get_waveforms
    Trims traces to start and end times.

--- obspy/clients/fdsn/tests/test_client.py
+++ obspy/clients/fdsn/tests/test_client.py
@@ -308,7 +308,18 @@ class ClientTestCase(unittest.TestCase):
         """
         Fixes #1887
         """
-        self.assertEqual(1, 0)
+        from obspy.clients import fdsn
+        c = fdsn.client.Client(
+            service_mappings={'dataselect': \
+                    'http://eida.ipgp.fr/fdsnws/dataselect/1'}, debug=True)
+        starttime=UTCDateTime('2016-11-01T00:00:00')
+        endtime=UTCDateTime('2016-11-01T00:00:10')
+        stream = c.get_waveforms('G', 'PEL', '*', 'LHZ', starttime,endtime)
+        trace = stream[0]
+        t1= trace.stats.starttime
+        t2= trace.stats.endtime
+        self.assertEqual(starttime, t1)
+        self.assertEqual(endtime,t2)
 
     def test_service_discovery_iris(self):
         """
