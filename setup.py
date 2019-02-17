6c2fd9f39c0ead8b616d5240b73f1e0a5c9f17d4
Tobias Megies <megies@geophysik.uni-muenchen.de>
2019-02-17 13:34:13 +0100

    add Python 3.7 in setup.py metadata for pypi

--- setup.py
+++ setup.py
@@ -793,6 +793,7 @@ def setupPackage():
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
             'Topic :: Scientific/Engineering',
             'Topic :: Scientific/Engineering :: Physics'],
         keywords=KEYWORDS,
