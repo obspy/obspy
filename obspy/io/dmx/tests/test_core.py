d8c1965b927a3e0e2a86fb0f7205b7b486ec64c4
ThomasLecocq <thlecocq@gmail.com>
2019-09-16 16:38:32 +0200

    test data content

--- obspy/io/dmx/tests/test_core.py
+++ obspy/io/dmx/tests/test_core.py
@@ -10,6 +10,8 @@ from future.builtins import *  # NOQA
 import os
 import unittest
 
+import numpy as np
+
 from obspy import read
 from obspy.core.utcdatetime import UTCDateTime
 from obspy.io.dmx.core import _read_dmx
@@ -74,6 +76,22 @@ class CoreTestCase(unittest.TestCase):
         self.assertEqual(st[0].id, "ETNA.EMPL..Z")
         for tr in st:
             self.assertEqual(tr.stats.station, "EMPL")
+
+    def test_check_data_content_sum(self):
+        """
+        Read files and passing a station keyword argument.
+        """
+        filename = os.path.join(self.path, '131114_090600.dmx')
+        # 1
+        st = read(filename)
+        st.verify()
+        self.assertEqual(st[0].data.sum(), -90928.0)
+        np.testing.assert_array_equal(
+            st[0].data[:5], [ 148., 238., 133., 9., -92.])
+
+        self.assertEqual(st[1].data.sum(), 3003120.0)
+        np.testing.assert_array_equal(
+            st[1].data[:5], [537., 721., 844., 924., 977.])
 
 
 def suite():
