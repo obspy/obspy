ae807b89ec5b468ddccbb6d06c37281ac90ce986
Tobias Megies <megies@geophysik.uni-muenchen.de>
2019-09-20 10:39:01 +0200

    isFormat: unify scradec

--- obspy/io/scardec/core.py
+++ obspy/io/scardec/core.py
@@ -28,6 +28,7 @@ from obspy.core.event import (Catalog, Comment, Event, EventDescription,
                               Origin, Magnitude, FocalMechanism, MomentTensor,
                               Tensor, SourceTimeFunction, NodalPlane,
                               NodalPlanes)
+from obspy.core.util.decorator import file_format_check
 from obspy.geodetics import FlinnEngdahl
 
 
@@ -75,16 +76,18 @@ def _buffer_proxy(filename_or_buf, function, reset_fp=True,
             return function(fh, *args, **kwargs)
 
 
-def _is_scardec(filename_or_buf):
+@file_format_check
+def _is_scardec(filename_or_buf, **kwargs):
     """
     Checks if the file is a SCARDEC file.
 
-    :param filename_or_buf: File to test.
-    :type filename_or_buf: str or file-like object.
+    :type filename_or_buf: :class:`io.BytesIOBase`
+    :param filename_or_buf: Open file or file-like object to be checked
+    :rtype: bool
+    :return: ``True`` if SCARDEC file.
     """
     try:
-        return _buffer_proxy(filename_or_buf, _internal_is_scardec,
-                             reset_fp=True)
+        return _internal_is_scardec(filename_or_buf)
     # Happens for example when passing the data as a string which would be
     # interpreted as a filename.
     except OSError:
