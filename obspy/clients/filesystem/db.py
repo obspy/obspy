02fd2901fbf0bdcf6088ab206fa6fd78b0cb5935
Lion Krischer <lion.krischer@gmail.com>
2019-10-09 13:18:19 +0200

    Caching the table bases because sqlalchemy does not like the same declarative base to be declared multiple times.

--- obspy/clients/filesystem/db.py
+++ obspy/clients/filesystem/db.py
@@ -21,76 +21,85 @@ from sqlalchemy.schema import PrimaryKeyConstraint
 
 Base = declarative_base()
 
+# Every declarative class should only be instantiated once. Thus we just use a
+# simple cache to be able to cache by table name.
+_table_cache = {"index_tables": {}, "summary_tables": {}}
+
 
 def get_tsindex_table(table_name='tsindex'):
 
-    class TSIndexTable(Base):
-        """
-        DB table containing tsindex.
-        """
-        __tablename__ = table_name
-        __table_args__ = (
-            PrimaryKeyConstraint('network', 'station',
-                                 'location', 'channel',
-                                 'quality', 'version',
-                                 'starttime', 'endtime'),
-            {'keep_existing': True}
-        )
+    if table_name not in _table_cache["index_tables"]:
+        class TSIndexTable(Base):
+            """
+            DB table containing tsindex.
+            """
+            __tablename__ = table_name
+            __table_args__ = (
+                PrimaryKeyConstraint('network', 'station',
+                                     'location', 'channel',
+                                     'quality', 'version',
+                                     'starttime', 'endtime'),
+                {'keep_existing': True}
+            )
 
-        network = Column(String)
-        station = Column(String)
-        location = Column(String)
-        channel = Column(String)
-        quality = Column(String(1))
-        version = Column(Integer)
-        starttime = Column(String)
-        endtime = Column(String)
-        samplerate = Column(Float)
-        filename = Column(String)
-        byteoffset = Column(Integer)
-        bytes = Column(Integer)
-        hash = Column(String)
-        timeindex = Column(String)
-        timespans = Column(String)
-        timerates = Column(String)
-        format = Column(String)
-        filemodtime = Column(String)
-        updated = Column(String)
-        scanned = Column(String)
+            network = Column(String)
+            station = Column(String)
+            location = Column(String)
+            channel = Column(String)
+            quality = Column(String(1))
+            version = Column(Integer)
+            starttime = Column(String)
+            endtime = Column(String)
+            samplerate = Column(Float)
+            filename = Column(String)
+            byteoffset = Column(Integer)
+            bytes = Column(Integer)
+            hash = Column(String)
+            timeindex = Column(String)
+            timespans = Column(String)
+            timerates = Column(String)
+            format = Column(String)
+            filemodtime = Column(String)
+            updated = Column(String)
+            scanned = Column(String)
 
-        def __repr__(self):
-            return "<TSIndexTable('%s %s %s %s %s %s %s')>" % \
-                    (self.network, self.station, self.location,
-                     self.channel, self.starttime, self.endtime,
-                     self.filemodtime)
-    return TSIndexTable
+            def __repr__(self):
+                return "<TSIndexTable('%s %s %s %s %s %s %s')>" % \
+                        (self.network, self.station, self.location,
+                         self.channel, self.starttime, self.endtime,
+                         self.filemodtime)
+
+        _table_cache["index_tables"][table_name] = TSIndexTable
+    return _table_cache["index_tables"][table_name]
 
 
 def get_tsindex_summary_table(table_name='tsindex_summary'):
+    if table_name not in _table_cache["summary_tables"]:
+        class TSIndexSummaryTable(Base):
+            """
+            DB table containing tsindex.
+            """
+            __tablename__ = table_name
+            __table_args__ = (
+                PrimaryKeyConstraint('network', 'station',
+                                     'location', 'channel',
+                                     'earliest', 'latest'),
+                {'keep_existing': True}
+            )
 
-    class TSIndexSummaryTable(Base):
-        """
-        DB table containing tsindex.
-        """
-        __tablename__ = table_name
-        __table_args__ = (
-            PrimaryKeyConstraint('network', 'station',
-                                 'location', 'channel',
-                                 'earliest', 'latest'),
-            {'keep_existing': True}
-        )
+            network = Column(String)
+            station = Column(String)
+            location = Column(String)
+            channel = Column(String)
+            earliest = Column(String)
+            latest = Column(String)
+            updt = Column(String,
+                          default=datetime.datetime.utcnow)
 
-        network = Column(String)
-        station = Column(String)
-        location = Column(String)
-        channel = Column(String)
-        earliest = Column(String)
-        latest = Column(String)
-        updt = Column(String,
-                      default=datetime.datetime.utcnow)
+            def __repr__(self):
+                return "<TSIndexSummaryTable('%s %s %s %s %s %s %s')>" % \
+                        (self.network, self.station, self.location,
+                         self.channel, self.earliest, self.latest, self.updt)
 
-        def __repr__(self):
-            return "<TSIndexSummaryTable('%s %s %s %s %s %s %s')>" % \
-                    (self.network, self.station, self.location,
-                     self.channel, self.earliest, self.latest, self.updt)
-    return TSIndexSummaryTable
+        _table_cache["summary_tables"][table_name] = TSIndexSummaryTable
+    return _table_cache["summary_tables"][table_name]
