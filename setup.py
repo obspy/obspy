d32bd1d263b0187f9bff5dad0cefba46e920b786
Damian Kula <Heavelock@gmail.com>
2019-11-25 15:57:30 +0100

    Adds Py3.8 compatibility to setup.py

--- setup.py
+++ setup.py
@@ -801,6 +801,7 @@ def setupPackage():
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
             'Topic :: Scientific/Engineering',
             'Topic :: Scientific/Engineering :: Physics'],
         keywords=KEYWORDS,
