441ae6a2d73060c3153b3c12af9212da7b74749b
Tobias Megies <megies@geophysik.uni-muenchen.de>
2020-01-29 17:43:19 +0100

    console_scripts: avoid hickup on Python 2
    see
    https://github.com/conda-forge/obspy-feedstock/pull/31#issuecomment-579767081
    
    Just work around some weird erroneous SyntaxError check due to a module
    being named "print.py", unfortunately

---
+++ obspy/scripts/_print.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Print stream information for waveform data in local files.
+"""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+from .print import main
+
+main = main
