1e4735f1c8ea8dd2173ad4e73ac0e519a0016cfa
Damian Kula <dkula@unistra.fr>
2020-10-07 10:12:23 +0200

    Adds py3.9 to setup.py
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- setup.py
+++ setup.py
@@ -801,6 +801,7 @@ def setupPackage():
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
             'Topic :: Scientific/Engineering',
             'Topic :: Scientific/Engineering :: Physics'],
         keywords=KEYWORDS,
