b5d0ec78dedd4135074fc3f2c0bba1bb647e42fd
trichter <tom.eulenfeld@bgr.de>
2022-01-10 09:45:06 +0100

    ask without report or no-report flag, do not write json report file

--- obspy/scripts/runtests.py
+++ obspy/scripts/runtests.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#:copyright:
+#    The ObsPy Development Team (devs@obspy.org)
+#:license:
+#    GNU Lesser General Public License, Version 3
+#    (https://www.gnu.org/copyleft/lesser.html)
 """
 A command-line program that runs all ObsPy tests.
 
 All tests in ObsPy are located in the tests directory of each specific
 module.
 
-:copyright:
-    The ObsPy Development Team (devs@obspy.org)
-:license:
-    GNU Lesser General Public License, Version 3
-    (https://www.gnu.org/copyleft/lesser.html)
 .. rubric:: Examples
 
 (1) Run all local tests (ignoring tests requiring a network connection) on
@@ -25,10 +25,11 @@ module.
         $ obspy-runtests io/wav obspy/io/sac
 (5) Run a specific test case::
         $ obspy-runtests core/tests/test_stats.py::TestStats::test_init
-(6) Create a self-contained html-report of test results saved as
-   'obspy_report.html' in ObsPy's directory:
-        $ obspy-runtests --report
-(7) Run tests and print a coverage report to screen and save coverage.xml:
+(6) Create a self-contained html-report of test results with pytest-html
+    plugin::
+        $ obspy-runtests --html path/report.html
+(7) Run tests and print a coverage report to screen and save coverage.xml
+    with pytest-cov plugin:
         $ obspy-runtests --coverage
 (8) Save the image outputs of the testsuite, called 'obspy_image_tests':
         $ obspy-runtests --keep-images
@@ -39,6 +40,9 @@ import os
 import sys
 from pathlib import Path
 
+from pytest_jsonreport.plugin import JSONReport
+import pytest
+import requests
 import obspy
 
 # URL to upload json report
@@ -52,40 +56,40 @@ def main():
     If profiling is enabled we disable interactivity as it would wait for user
     input and influence the statistics. However the -r option still works.
     """
-    try:
-        import pytest
-    except ImportError:
-        msg = (
-            "In order to run the ObsPy test suite, you must install the test "
-            "requirements. This can be done with pip via pip install "
-            "obspy[test] or pip install -e .[test] if installing in editable "
-            "mode for development."
-        )
-        raise ImportError(msg)
+    if '-h' in sys.argv or '--help' in sys.argv:
+        print(__doc__)
     here = Path().cwd()
     base_obspy_path = Path(obspy.__file__).parent
+    plugin = JSONReport()
     os.chdir(base_obspy_path)
+    if all(['--json-report-file' not in arg for arg in sys.argv]):
+        sys.argv.append('--json-report-file=none')
     try:
-        pytest.main()
+        pytest.main(plugins=[plugin])
     finally:
-        if "--report" in sys.argv:
-            upload_json_report()
         os.chdir(here)
+    report = (True if '--report' in sys.argv else
+              False if '--no-report' in sys.argv else None)
+    upload_json_report(report=report, data=plugin.report)
 
 
-def upload_json_report(report_path='.report.json'):
+def upload_json_report(report=None, data=None):
     """Upload the json report to ObsPy test server."""
-    import json
-    import requests
-
-    print('\nUploading ObsPy test run report ...')
-    data = json.load(open(report_path, "rt"))
-    response = requests.post(f"https://{REPORT_URL}/post/v2/", json=data)
-    response_str = response.content.decode('utf-8', 'backslashreplace')
-    status_code = response.status_code
-    report_url = json.loads(response_str)['url']
-    # print("Status Code:", )
-    print(f"STATUSCODE: {status_code} \nREPORT URL: {report_url}\n")
+    if report is None:
+        msg = f"Do you want to report this to {REPORT_URL} ? [n]: "
+        answer = input(msg).lower()
+        report = 'y' in answer
+    if report:
+        response = requests.post(f"https://{REPORT_URL}/post/v2/", json=data)
+        # get the response
+        if response.status_code == 200:
+            report_url = response.json().get('url', REPORT_URL)
+            print('Your test results have been reported and are available at: '
+                  '{}\nThank you!'.format(report_url))
+        # handle errors
+        else:
+            print(f"Error: Could not sent a test report to {REPORT_URL}.")
+            print(response.reason)
 
 
 def run_tests(network=False,
