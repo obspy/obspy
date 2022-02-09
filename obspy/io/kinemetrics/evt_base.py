562b2eac4354890dfaf008a63644f4e79c238774
trichter <tom.eulenfeld@bgr.de>
2022-02-09 18:20:38 +0100

    use default precision in kinemetrics

--- obspy/io/kinemetrics/evt_base.py
+++ obspy/io/kinemetrics/evt_base.py
@@ -131,7 +131,7 @@ class EvtVirtual(object):
             frame_milli = 0
         frame_time += 315532800  # diff between 1970/1/1 and 1980/1/1
         time = UTCDateTime(frame_time) + frame_milli / 1000.0
-        return UTCDateTime(ns=time.ns, precision=3)
+        return UTCDateTime(ns=time.ns)
 
     def _strnull(self, strn,
                  unused_param=None, unused_val=None, unused_offset=None):
