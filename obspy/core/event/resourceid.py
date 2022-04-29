be8b67942afb1e6e4a3ff5be52aa5cb8f21e83e2
Tobias Megies <megies@geophysik.uni-muenchen.de>
2022-04-29 10:38:20 +0200

    replace all http://gnu.org links with https:// version

--- obspy/core/event/resourceid.py
+++ obspy/core/event/resourceid.py
@@ -7,7 +7,7 @@ This module defines the ResourceIdentifier class and associated code.
     The ObsPy Development Team (devs@obspy.org)
 :license:
     GNU Lesser General Public License, Version 3
-    (http://www.gnu.org/copyleft/lesser.html)
+    (https://www.gnu.org/copyleft/lesser.html)
 """
 import re
 import warnings
