907f275b9062e6a455a6115f598cdf0e711d8801
trichter <tom.eulenfeld@bgr.de>
2022-07-07 10:28:43 +0200

    add back python3.8

--- setup.py
+++ setup.py
@@ -753,6 +753,7 @@ def setupPackage():
             'Operating System :: OS Independent',
             'Programming Language :: Python',
             'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
             'Topic :: Scientific/Engineering',
