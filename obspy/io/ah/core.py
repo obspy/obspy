3ce16e432010c31f2591b4a5805a38508aede935
trichter <tom.eulenfeld@bgr.de>
2022-11-04 11:00:48 +0100

    specify exceptions

--- obspy/io/ah/core.py
+++ obspy/io/ah/core.py
@@ -17,7 +17,7 @@ a number of values followed by the time series data.
 """
 try:
     import xdrlib
-except:
+except (ModuleNotFoundError, DeprecationWarning):
     xdrlib = None
 XDRLIB_ERROR_MSG = (
     'Module xdrlib is necessary for the AH bindings, '
