29f6127e38963e857111a8f9773ad0d14fdf77af
Fred Massin <FMassin@users.noreply.github.com>
2022-11-16 11:47:12 +0100

    skip_on_gaps=True & subpplot spectrogram option

--- obspy/signal/spectral_estimation.py
+++ obspy/signal/spectral_estimation.py
@@ -310,7 +310,7 @@ class PPSD(object):
     # Add current version as a class attribute to avoid hard coding it.
     _CURRENT_VERSION = 3
 
-    def __init__(self, stats, metadata, skip_on_gaps=False,
+    def __init__(self, stats, metadata, skip_on_gaps=True,
                  db_bins=(-200, -50, 1.), ppsd_length=3600.0, overlap=0.5,
                  special_handling=None, period_smoothing_width_octaves=1.0,
                  period_step_octaves=0.125, period_limits=None,
@@ -355,7 +355,8 @@ class PPSD(object):
                 be skipped entirely. [McNamara2004]_ merge gappy
                 traces by filling with zeros. This results in a clearly
                 identifiable outlier psd line in the PPSD visualization. Select
-                `skip_on_gaps=True` for not filling gaps with zeros which might
+                `skip_on_gaps=False` to follow this convention. The default 
+                `skip_on_gaps=True` avoids filling gaps with zeros which might
                 result in some data segments shorter than `ppsd_length` not
                 used in the PPSD.
         :type db_bins: tuple(int, int, int) or tuple(float, float, float)
@@ -1579,7 +1580,8 @@ class PPSD(object):
         return self._split_lists(self.times_processed, self.psd_values)
 
     def plot_spectrogram(self, cmap=obspy_sequential, clim=None, grid=True,
-                         filename=None, show=True):
+                         filename=None, show=True, axes=None, flip=False, 
+                         datelabels=True, ylabel=True, idlabel=False):
         """
         Plot the temporal evolution of the PSD in a spectrogram-like plot.
 
@@ -1600,10 +1602,29 @@ class PPSD(object):
         :param filename: Name of output file
         :type show: bool
         :param show: Enable/disable immediately showing the plot.
+        :type axes: :class:`matplotlib.pyplot.axes`
+        :param axes: Reuse given axes for plotting.
+        :type flip: bool
+        :param flip: If True, plots time in Y-axis instead of default X.
+        :type datelabels: bool
+        :param datelabels: If False, hide date labels (default True), useful
+            for subplots sharing the same date axis.
+        :type ylabel: bool
+        :param ylabel: If False, hide ylabel (default True), useful
+            for subplots (only with `flip=False`).
+        :type idlabel: bool
+        :param idlabel: If True, adds trace.id label (default False), useful
+            for subplots.
         """
+        
         import matplotlib.pyplot as plt
+        from matplotlib.ticker import AutoMinorLocator 
 
-        fig, ax = plt.subplots()
+        if axes is None:
+            fig, ax = plt.subplots()
+        else:
+            fig = axes.figure
+            ax = axes
 
         quadmeshes = []
         yedges = self.period_xedges
@@ -1611,10 +1632,15 @@ class PPSD(object):
         for times, psds in self._get_gapless_psd():
             xedges = [t.matplotlib_date for t in times] + \
                 [(times[-1] + self.step).matplotlib_date]
-            meshgrid_x, meshgrid_y = np.meshgrid(xedges, yedges)
-            data = np.array(psds).T
-
-            quadmesh = ax.pcolormesh(meshgrid_x, meshgrid_y, data, cmap=cmap,
+            if not flip:
+                meshgrid_x, meshgrid_y = np.meshgrid(xedges, yedges)
+                data = np.array(psds).T
+                quadmesh = ax.pcolormesh(meshgrid_x, meshgrid_y, data, cmap=cmap,
+                                     zorder=-1)
+            else:
+                meshgrid_x, meshgrid_y = np.meshgrid(yedges, xedges)
+                data = np.array(psds)
+                quadmesh = ax.pcolormesh(meshgrid_x, meshgrid_y, data, cmap=cmap,
                                      zorder=-1)
             quadmeshes.append(quadmesh)
 
@@ -1625,34 +1651,55 @@ class PPSD(object):
 
         for quadmesh in quadmeshes:
             quadmesh.set_clim(*clim)
-
-        cb = plt.colorbar(quadmesh, ax=ax)
-
+            
         if grid:
             ax.grid()
 
-        if self.special_handling is None:
-            cb.ax.set_ylabel('Amplitude [$m^2/s^4/Hz$] [dB]')
-        elif self.special_handling == "infrasound":
-            ax.set_ylabel('Amplitude [$Pa^2/Hz$] [dB]')
+        cb = plt.colorbar(quadmesh, ax=ax, fraction=0.05, pad=0.01)
+        cb.ax.yaxis.set_minor_locator(AutoMinorLocator())
+        if ylabel:
+            if self.special_handling is None:
+                cb.ax.set_ylabel('Amplitude [$m^2/s^4/Hz$] [dB]')
+            elif self.special_handling == "infrasound":
+                ax.set_ylabel('Amplitude [$Pa^2/Hz$] [dB]')
+            else:
+                cb.ax.set_ylabel('Amplitude [dB]')
+
+        if not flip:
+            if ylabel:
+                ax.set_ylabel('Period [s]')
+            ax.set_xlim(self.times_processed[0].matplotlib_date,
+                        (self.times_processed[-1] + self.step).matplotlib_date)
+            if datelabels:
+                fig.autofmt_xdate()
+                _set_xaxis_obspy_dates(ax)
+            else:
+                ax.xaxis_date()
+                ax.xaxis.set_minor_locator(AutoMinorLocator())
+                ax.axes.xaxis.set_ticklabels([])
+            ax.set_yscale("log")
+            ax.set_ylim(yedges[0], yedges[-1])
         else:
-            cb.ax.set_ylabel('Amplitude [dB]')
-        ax.set_ylabel('Period [s]')
+            ax.yaxis_date()
+            ax.yaxis.set_minor_locator(AutoMinorLocator())
+            ax.set_xlabel('Period [s]')
+            ax.set_xscale('log')
+            ax.set_ylim(self.times_processed[0].matplotlib_date,
+                        (self.times_processed[-1] + self.step).matplotlib_date)
+            ax.invert_yaxis()
+            ax.set_xlim(yedges[0], yedges[-1])
 
-        fig.autofmt_xdate()
-        _set_xaxis_obspy_dates(ax)
+        if idlabel:
+            ax.legend([],[],title=self.id,labelspacing=-.2)
 
-        ax.set_yscale("log")
-        ax.set_xlim(self.times_processed[0].matplotlib_date,
-                    (self.times_processed[-1] + self.step).matplotlib_date)
-        ax.set_ylim(yedges[0], yedges[-1])
         try:
             ax.set_facecolor('0.8')
         # mpl <2 has different API for setting Axes background color
         except AttributeError:
             ax.set_axis_bgcolor('0.8')
 
-        fig.tight_layout()
+        if axes is None:
+            fig.tight_layout()
 
         if filename is not None:
             plt.savefig(filename)
