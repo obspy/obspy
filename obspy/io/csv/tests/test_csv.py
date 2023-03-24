fcb3701da9d0c64b752f43f4a0b037c7f01cd756
trichter <tom.eulenfeld@uni-jena.de>
2023-03-24 10:43:03 +0100

    csv tests: correct import

--- obspy/io/csv/tests/test_csv.py
+++ obspy/io/csv/tests/test_csv.py
@@ -6,7 +6,7 @@ import numpy as np
 import obspy
 from obspy import read_events
 from obspy.core.util import NamedTemporaryFile
-import obspycsv
+import obspy.io.csv.core as iocsv
 from packaging import version
 import pytest
 from obspy.core.util import get_example_file
@@ -30,7 +30,7 @@ def test_csv_reading_external_catalog():
     names = 'year mon day hour minu sec _ lat lon dep mag id'
     incomplete_names = 'year mon day hour minu sec _ lat lon dep'
     fname = '/path/to/external.csv'
-    assert not obspycsv._is_csv(fname)
+    assert not iocsv._is_csv(fname)
     events = read_events(fname, 'CSV', skipheader=1, names=names)
     events2 = read_events(fname, 'CSV', skipheader=1, names=incomplete_names)
     assert len(events) == 1
@@ -59,7 +59,7 @@ def test_csv_incomplete_catalog():
     assert events2[1].origins[0].time == events[1].origins[0].time
 
     fname = get_example_file('incomplete.csv')
-    assert obspycsv._is_csv(fname)
+    assert iocsv._is_csv(fname)
     events = read_events(fname)
     assert len(events) == 2
     assert events[0].origins[0].depth is None
@@ -74,15 +74,15 @@ def test_csv_custom_fmt():
     with NamedTemporaryFile(suffix='.csv') as ft:
         fname = ft.name
         events.write(fname, 'CSV', fields='{lat:.5f} {lon:.5f}')
-        assert not obspycsv._is_csv(fname)
+        assert not iocsv._is_csv(fname)
         data = np.genfromtxt(fname, names=True, delimiter=',')
         assert len(data) == 3
         assert len(data[0]) == 2
 
 
 def test_csv_empty():
-    assert not obspycsv._is_csv(b'')
-    empty_cat = obspycsv.read_csv(b'')
+    assert not iocsv._is_csv(b'')
+    empty_cat = iocsv._read_csv(b'')
     assert len(empty_cat) == 0
 
 
@@ -95,7 +95,7 @@ def test_csz(check_compression=False):
 
         def _test_write_read(events, **kw):
             events.write(fname, 'CSZ', **kw)
-            assert obspycsv._is_csz(fname)
+            assert iocsv._is_csz(fname)
             events2 = read_events(fname, check_compression=check_compression)
             assert len(events2) == len(events)
             for ev1, ev2 in zip(events, events2):
@@ -116,7 +116,7 @@ def test_csz(check_compression=False):
         events[0].picks[0].waveform_id = None
         with pytest.warns(Warning):
             events.write(fname, 'CSZ')
-        assert obspycsv._is_csz(fname)
+        assert iocsv._is_csz(fname)
         events2 = read_events(fname, check_compression=check_compression)
         assert len(events2) == 1
         assert (len(events2[0].origins[0].arrivals) ==
@@ -129,7 +129,7 @@ def test_csz_without_picks(check_compression=False):
     with NamedTemporaryFile(suffix='.csz') as ft:
         fname = ft.name
         events.write(fname, 'CSZ')
-        assert obspycsv._is_csz(fname)
+        assert iocsv._is_csz(fname)
         events2 = read_events(fname, check_compression=check_compression)
         assert events2[0]._format == 'CSZ'
         assert len(events2) == len(events)
@@ -146,13 +146,13 @@ def test_load_csv():
     events = read_events()
     with NamedTemporaryFile(suffix='.csv') as ft:
         events.write(ft.name, 'CSV')
-        t = obspycsv.load_csv(ft.name)
+        t = iocsv.load_csv(ft.name)
     assert t['mag'][0] == 4.4
     with NamedTemporaryFile(suffix='.csz') as ft:
         events.write(ft.name, 'CSZ')
-        t = obspycsv.load_csv(ft.name)
+        t = iocsv.load_csv(ft.name)
     assert t['mag'][0] == 4.4
-    t = obspycsv.events2array(events)
+    t = iocsv.events2array(events)
     assert t['mag'][0] == 4.4
 
 
@@ -166,7 +166,7 @@ def test_load_csv_incomplete_catalog():
     with NamedTemporaryFile(suffix='.csv') as ft:
         with pytest.warns(Warning):
             events.write(ft.name, 'CSV')
-        t = obspycsv.load_csv(ft.name)
+        t = iocsv.load_csv(ft.name)
     assert len(t) == 2
     assert np.isnan(t['mag'][1])
 
@@ -176,9 +176,9 @@ def test_load_csv_some_cols():
     with NamedTemporaryFile(suffix='.csv') as ft:
         fields = '{lat:.6f} {lon:.6f} {mag:.2f}'
         events.write(ft.name, 'CSV', fields=fields)
-        t = obspycsv.load_csv(ft.name)
-        t2 = obspycsv.load_csv(ft.name, only=['mag'])
-        t3 = obspycsv.load_csv(ft.name, skipheader=1, names={2: 'mag'})
+        t = iocsv.load_csv(ft.name)
+        t2 = iocsv.load_csv(ft.name, only=['mag'])
+        t3 = iocsv.load_csv(ft.name, skipheader=1, names={2: 'mag'})
     assert t['mag'][0] == 4.4
     assert t2['mag'][0] == 4.4
     assert t3['mag'][0] == 4.4
@@ -190,15 +190,15 @@ def test_load_csv_some_cols():
 
 def test_events2array():
     events = read_events()
-    t = obspycsv.events2array(events)
+    t = iocsv.events2array(events)
     assert t['mag'][0] == 4.4
 
 
 def test_eventtxt():
     fname = get_example_file('events.txt')
-    assert obspycsv._is_eventtxt(fname)
+    assert iocsv._is_eventtxt(fname)
     events = read_events(fname)
-    arr = obspycsv.load_eventtxt(fname)
+    arr = iocsv.load_eventtxt(fname)
     assert len(events) == 2
     assert str(events[0].origins[0].time) == '2012-04-11T08:38:37.000000Z'
     assert events[0].origins[0].creation_info.author == 'ISC'
