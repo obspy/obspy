a559282056c8973f59eba7395e970a6085499a51
TazawaK17 <135314763+TazawaK17@users.noreply.github.com>
2023-06-14 13:47:19 +0900

    Add test data

--- obspy/io/win/tests/test_core.py
+++ obspy/io/win/tests/test_core.py
@@ -51,3 +51,27 @@ class TestCore():
         assert len(st[0]) == 6000
         assert round(abs(st[0].stats.sampling_rate-100.0), 7) == 0
         assert st[0].stats.channel == 'a100'
+
+    def test_read_05_byte_data(self, testdata):
+        """
+        Read a file included 0.5 byte datawide
+        """
+        filename = testdata['1070533011_1701260003.win']
+
+        st = _read_win(filename)
+        st.sort(keys=['channel'])
+        assert len(st[0]) == 6000
+        assert len(st[1]) == 6000
+        assert len(st[2]) == 6000
+        assert st[0].stats.starttime == \
+            UTCDateTime('2017-01-26T00:03:00.000000Z')
+        assert st[0].stats.endtime == \
+            UTCDateTime('2017-01-26T00:03:59.990000Z')
+        assert st[1].stats.starttime == \
+            UTCDateTime('2017-01-26T00:03:00.000000Z')
+        assert st[1].stats.endtime == \
+            UTCDateTime('2017-01-26T00:03:59.990000Z')
+        assert st[2].stats.starttime == \
+            UTCDateTime('2017-01-26T00:03:00.000000Z')
+        assert st[2].stats.endtime == \
+            UTCDateTime('2017-01-26T00:03:59.990000Z')
