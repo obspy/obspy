df8b05758476c33d81e0f2c88f02316ba67b1b75
Tobias Megies <tobias.megies@lmu.de>
2024-05-13 16:32:58 +0200

    docset/CI: fix build script

--- misc/docs/make_docset.sh
+++ misc/docs/make_docset.sh
@@ -4,21 +4,21 @@
 # exit immediately on any error
 set -e
 
-DOCSETVERSION=`python -c "import obspy; print(obspy.__version__)"`
+VERSION=`python -c "import obspy; print(obspy.__version__)"`
 
 cd build/
 
 # this should equally work to get the version number but it seems the "dirty" part of the version numbering is missing there
 #DOCSETVERSION=`head -3 build/html/objects.inv | grep 'Version' | awk '{print $NF}'))
 
-doc2dash --verbose --name "ObsPy ${DOCSETVERSION}" --icon html/_static/obspy_logo_no_text_32x32.png --online-redirect-url https://docs.obspy.org/ html/
-sed 's#\(<string>[Oo]bs[Pp]y\) .*\(</string>\)#\1 ${DOCSETVERSION}\2#' --in-place "build/ObsPy ${DOCSETVERSION}.docset/Contents/Info.plist"
-cat ../docset_css_fixes.css >> "ObsPy ${DOCSETVERSION}.docset/Contents/Resources/Documents/_static/css/custom.css"
-if [[ "${DOCSETVERSION}" == *"post"* ]]
+doc2dash --name "ObsPy" --icon html/_static/obspy_logo_no_text_32x32.png --online-redirect-url https://docs.obspy.org/ html/  # --verbose
+# sed 's#\(<string>[Oo]bs[Pp]y\) .*\(</string>\)#\1 ${VERSION}\2#' --in-place ObsPy.docset/Contents/Info.plist
+cat ../docset_css_fixes.css >> ObsPy.docset/Contents/Resources/Documents/_static/css/custom.css
+if [[ "${VERSION}" == *"post"* ]]
 then
   DOCSETNAME="master"
 else
-  DOCSETNAME=$(DOCSETVERSION)
+  DOCSETNAME=${VERSION}
 fi
 
-tar --exclude='.DS_Store' -cvzf "obspy-${DOCSETNAME}.docset.tgz" "ObsPy ${DOCSETVERSION}.docset"; \
+tar --exclude='.DS_Store' -cvzf "obspy-${DOCSETNAME}.docset.tgz" ObsPy.docset
