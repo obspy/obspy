34b71ff15487fd5f3e4cb7a1aaef0155784a13d7
Tobias Megies <tobias.megies@lmu.de>
2024-05-31 12:57:55 +0200

    np2: add util wrapper for np.ptp and np.ma.ptp

--- obspy/signal/tests/test_detrend.py
+++ obspy/signal/tests/test_detrend.py
@@ -7,6 +7,7 @@ import numpy as np
 
 import obspy
 from obspy.signal.detrend import polynomial, spline
+from obspy.core.util.misc import ptp
 
 
 class TestDetrend:
@@ -23,13 +24,13 @@ class TestDetrend:
         for c in coeffs:
             # Create data.
             d = np.polyval(c, data)
-            original_ptp = np.ptp(d)
+            original_ptp = ptp(d)
             # Detrend with polynomial of same order.
             detrended = polynomial(d, order=len(c) - 1)
             # Make sure the maximum amplitude is reduced by some orders of
             # magnitude. It should almost be reduced to zero as we detrend a
             # polynomial with a polynomial...
-            assert np.ptp(detrended) * 1E10 < original_ptp
+            assert ptp(detrended) * 1E10 < original_ptp
 
     def test_spline_detrend(self):
         """
@@ -41,12 +42,12 @@ class TestDetrend:
         for c in coeffs:
             # Create data.
             d = np.polyval(c, data)
-            original_ptp = np.ptp(d)
+            original_ptp = ptp(d)
             # Detrend with a spline of the same order as the polynomial.
             # This should be very very similar.
             detrended = spline(d, order=len(c) - 1, dspline=10)
             # Not as good as for the polynomial detrending.
-            assert np.ptp(detrended) * 1E4 < original_ptp
+            assert ptp(detrended) * 1E4 < original_ptp
 
     def test_polynomial_detrend_plotting(self, image_path):
         """
