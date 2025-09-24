4e8b7c71886ba4c7096fc75f7a2318286eedb76c
Roman Racine <roman.racine@gmail.com>
2025-09-24 14:19:02 +0200

    now reads miniseed files memory-mapped and bisects the file if read is used with start and endtime
    This can cut down processing times by an order of magnitude, especially
    when accessing an SDS data structure.

--- obspy/io/mseed/core.py
+++ obspy/io/mseed/core.py
@@ -8,7 +8,6 @@ import os
 import warnings
 from pathlib import Path
 from struct import pack
-
 import numpy as np
 
 from obspy import Stream, Trace, UTCDateTime
@@ -22,12 +21,12 @@ from .headers import (DATATYPES, ENCODINGS, HPTERROR, HPTMODULUS, SAMPLETYPE,
                       SelectTime, Blkt100S, Blkt1001S, clibmseed)
 
 
-def _is_mseed(filename):
+def _is_mseed(file):
     """
     Checks whether a file is Mini-SEED/full SEED or not.
 
-    :type filename: str
-    :param filename: Mini-SEED/full SEED file to be checked.
+    :type file: str or file-like object
+    :param file: Mini-SEED/full SEED file to be checked.
     :rtype: bool
     :return: ``True`` if a Mini-SEED file.
 
@@ -41,26 +40,27 @@ def _is_mseed(filename):
     Thus it cannot be used to validate a Mini-SEED or SEED file.
     """
     # Open filehandler or use an existing file like object.
-    if not hasattr(filename, 'read'):
-        file_size = os.path.getsize(filename)
-        with io.open(filename, 'rb') as fh:
+    if not hasattr(file, 'read'):
+        file_size = os.path.getsize(file)
+        with io.open(file, 'rb') as fh:
             return __is_mseed(fh, file_size=file_size)
     else:
-        initial_pos = filename.tell()
+        initial_pos = file.tell()
         try:
-            if hasattr(filename, "getbuffer"):
-                file_size = filename.getbuffer().nbytes
-            try:
-                file_size = os.fstat(filename.fileno()).st_size
-            except Exception:
-                _p = filename.tell()
-                filename.seek(0, 2)
-                file_size = filename.tell()
-                filename.seek(_p, 0)
-            return __is_mseed(filename, file_size)
+            if hasattr(file, "getbuffer"):  # BytesIO
+                file_size = file.getbuffer().nbytes
+            else:
+                try:
+                    file_size = os.fstat(file.fileno()).st_size
+                except Exception:
+                    _p = file.tell()
+                    file.seek(0, 2)
+                    file_size = file.tell()
+                    file.seek(_p, 0)
+            return __is_mseed(file, file_size)
         finally:
             # Reset pointer.
-            filename.seek(initial_pos, 0)
+            file.seek(initial_pos, 0)
 
 
 def __is_mseed(fp, file_size):  # NOQA
@@ -137,6 +137,104 @@ def __is_mseed(fp, file_size):  # NOQA
             return True
         fp.seek(record_length - 1, 1)
     return False
+
+
+def __read_header(buffer, index):
+    """
+        Reads the first record of the given buffer and returns
+        header information.
+
+        :param buffer: numpy.array(dtype=int8) or compatible containing
+                       raw miniseed data.
+        :param index:  position in the buffer which should be read
+
+        returns obspy.core.trace.Stats object
+    """
+    header = _read_mseed(io.BytesIO(buffer[index:index+4096].tobytes()),
+                         headonly=True)
+    return header[0].stats
+
+
+def __bisect_mseed(buffer, timestamp, before=True):
+    """
+         Quickly finds a timestamp in a miniseed stream using interpolation
+         search.
+
+         The code is a helper for _read_mseed and is written to be efficient,
+         a record size of 4096 is assumed. The code stops when the area
+         searched is less than 1MB in size. This code is intended to fasten
+         up the code when trying to cut out a time window of data from a
+         large input file. Using this, the code potentially only needs to
+         actually read a very small portion of a file. Cutting off at 1MB
+         also eliminates the need of treating boundary conditions.
+
+         The algorithm used is "interplation search" as described by W.W.
+         Peterson 1957: doi:10.1147/rd.12.0130.
+
+         This code only works when the data is ordered (i.e. the time
+         stamps are ordered in ascending order). Otherwise the code
+         may fail, in this case, None is returned.
+
+         :param buffer: numpy.array(dtype=int8) or compatible containing
+                        raw miniseed data.
+         :param timestamp: obspy.core.UTCDateTime() timestamp to look for
+                           in buffer
+         :param before: If True, return the index of the record containing
+                        the timestamp, if False, return the index of the
+                        next record.
+
+         returns index of the buffer either including the timestamp or
+                 the next one or None if the time stamp cannot be found.
+    """
+    low = 0
+    high = len(buffer)
+
+    # cut off here, linear search will be fast enough, avoid treating
+    # boundary cases
+    stopat = 1024*1024
+
+    if high - low <= stopat:
+        if before:
+            return low
+        else:
+            return high
+
+    starttime = __read_header(buffer, low).starttime
+    endtime = __read_header(buffer, high-4096).endtime
+
+    # timestamp not in this file
+    if timestamp < starttime or timestamp > endtime:
+        return None
+
+    while high-low > stopat:
+
+        # Cannot find timestamp
+        if timestamp < starttime or timestamp > endtime:
+            return None
+
+        # classical interpolation search
+        informed_guess_percentage = (timestamp - starttime) / \
+                                    (endtime - starttime)
+        buffer_raw_guess = informed_guess_percentage * (high-low)
+        informed_guess = low + int(buffer_raw_guess // 4096) * 4096
+
+        # algorithm stuck due to rounding to 4096 boundaries
+        if informed_guess <= low or informed_guess >= high:
+            informed_guess = (high - low) / 2
+            informed_guess = low + int(informed_guess // 4096) * 4096
+
+        guess_time = __read_header(buffer, informed_guess).starttime
+
+        if guess_time > timestamp:
+            high = informed_guess
+            endtime = guess_time
+        else:
+            low = informed_guess
+            starttime = guess_time
+    if before:
+        return low
+    else:
+        return high
 
 
 def _read_mseed(mseed_object, starttime=None, endtime=None, headonly=False,
@@ -224,6 +322,7 @@ def _read_mseed(mseed_object, starttime=None, endtime=None, headonly=False,
     >>> print(len(st))
     101
     """
+
     if isinstance(mseed_object, Path):
         mseed_object = str(mseed_object)
     # Parse the headonly and reclen flags.
@@ -306,7 +405,10 @@ def _read_mseed(mseed_object, starttime=None, endtime=None, headonly=False,
     # If it's a file name just read it.
     if isinstance(mseed_object, str):
         # Read to NumPy array which is used as a buffer.
-        bfr_np = np.fromfile(mseed_object, dtype=np.int8)
+        # use memmap, faster than fromfile, same functionality
+        bfr_np = np.memmap(mseed_object, dtype=np.int8, mode="c")
+        # bfr_np = np.fromfile(mseed_object, dtype=np.int8)
+
     elif hasattr(mseed_object, 'read'):
         bfr_np = from_buffer(mseed_object.read(), dtype=np.int8)
 
@@ -333,7 +435,8 @@ def _read_mseed(mseed_object, starttime=None, endtime=None, headonly=False,
         break
     bfr_np = bfr_np[offset:]
     buflen = len(bfr_np)
-
+    bufptr_low = 0
+    bufptr_high = buflen
     # If no selection is given pass None to the C function.
     if starttime is None and endtime is None and sourcename is None:
         selections = None
@@ -347,6 +450,11 @@ def _read_mseed(mseed_object, starttime=None, endtime=None, headonly=False,
                 raise ValueError(msg)
             selections.timewindows.contents.starttime = \
                 util._convert_datetime_to_mstime(starttime)
+
+            # bisect to the actual starttime
+            # avoids parsing of the whole file
+            bufptr_low = __bisect_mseed(bfr_np, starttime, before=True) \
+                or 0
         else:
             # HPTERROR results in no starttime.
             selections.timewindows.contents.starttime = HPTERROR
@@ -356,6 +464,9 @@ def _read_mseed(mseed_object, starttime=None, endtime=None, headonly=False,
                 raise ValueError(msg)
             selections.timewindows.contents.endtime = \
                 util._convert_datetime_to_mstime(endtime)
+            bufptr_high = __bisect_mseed(bfr_np, endtime, before=False) \
+                or buflen
+
         else:
             # HPTERROR results in no starttime.
             selections.timewindows.contents.endtime = HPTERROR
@@ -396,9 +507,9 @@ def _read_mseed(mseed_object, starttime=None, endtime=None, headonly=False,
     clibmseed.verbose = bool(verbose)
     try:
         lil = clibmseed.readMSEEDBuffer(
-            bfr_np, buflen, selections, C.c_int8(unpack_data),
-            reclen, C.c_int8(verbose), C.c_int8(details), header_byteorder,
-            alloc_data)
+            bfr_np[bufptr_low:bufptr_high], bufptr_high - bufptr_low,
+            selections, C.c_int8(unpack_data), reclen, C.c_int8(verbose),
+            C.c_int8(details), header_byteorder, alloc_data)
     except InternalMSEEDError as e:
         msg = e.args[0]
         if offset and offset in str(e):
