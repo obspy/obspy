a9ca1fe422a4aa883011a373c208443b044db280
Roman Racine <roman.racine@gmail.com>
2025-10-29 14:36:28 +0100

    flake8 compliance

--- obspy/signal/quality_control.py
+++ obspy/signal/quality_control.py
@@ -448,9 +448,6 @@ class MSEEDMetadata(object):
         Computes metrics on samples contained in the specified time window
         """
 
-        self.meta['sample_min'] = min([tr.data.min() for tr in self.data])
-        self.meta['sample_max'] = max([tr.data.max() for tr in self.data])
-
         #
         # Vectorised implementation to reduce memory consumption
         # and optimise running time
@@ -463,14 +460,20 @@ class MSEEDMetadata(object):
         # arrays.
         self.meta['sample_mean'] = full_samples.mean()
 
-        self.meta['sample_median'] = np.median(full_samples)
-        self.meta['sample_lower_quartile'] = np.percentile(full_samples, 25)
-        self.meta['sample_upper_quartile'] = np.percentile(full_samples, 75)
+        _min, _lower_quartile, _median, _upper_quartile, _max = \
+            np.percentile(full_samples, [0, 25, 50, 75, 100])
+        self.meta['sample_min'] = _min
+        self.meta['sample_lower_quartile'] = _lower_quartile
+        self.meta['sample_median'] = _median
+        self.meta['sample_upper_quartile'] = _upper_quartile
+        self.meta['sample_max'] = _max
 
-        self.meta['sample_rms'] = np.sqrt(np.mean(full_samples ** 2))
+        squared = np.mean(full_samples ** 2)
+        self.meta['sample_rms'] = np.sqrt(squared)
 
         # Sample standard deviation
-        self.meta['sample_stdev'] = full_samples.std(ddof=0)
+        self.meta['sample_stdev'] = \
+            np.sqrt(squared - self.meta['sample_mean']**2)
 
         # Percentage based availability as a function of total gap length
         # over the full trace duration
