0030be38d2a582bb4aebef58b1333667f403dcd9
Robert Pickle <rpickle@gmail.com>
2025-10-30 13:30:37 +1100

    update iris references to https

--- obspy/clients/fdsn/tests/test_mass_downloader.py
+++ obspy/clients/fdsn/tests/test_mass_downloader.py
@@ -2602,9 +2602,9 @@ class TestDownloadHelper():
         assert patch.call_count == 2
 
         assert list(d._initialized_clients.keys()) == \
-            ['GFZ', 'http://service.iris.edu', 'ORFEUS']
+            ['GFZ', 'https://service.iris.edu', 'ORFEUS']
         # Make sure it is the same object.
-        assert d._initialized_clients["http://service.iris.edu"] is client
+        assert d._initialized_clients["https://service.iris.edu"] is client
 
     @mock.patch("obspy.clients.fdsn.client.Client._discover_services",
                 autospec=True)
