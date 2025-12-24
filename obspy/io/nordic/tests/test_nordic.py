329429641b2fb4d166af909753cda2cf416c2e5a
Felix Halpaap <felix.halpaap@uib.no>
2025-12-24 12:33:16 +0100

    Fixes for Seisan + Nordic files: parsing of int16 waveform files, edge cases in event files, compact event files (#3287)
    * take care of BAZ-picks when they precede the linked picked in the Nordic file
    
    * add functionality to read/write type-I lines
    
    * fix nordic event id writing
    
    * fix BAZ-pick merging and fix read nordic event ID
    
    * fix baz assignment when BAZ line before pick line
    
    * fix bug where pick removed itself
    
    * take care of letters behind nordic event ID
    
    * fix nordic ID parsing
    
    * Nordic: add option to read compact collect-files with only header lines
    
    * fix bug where some events were not read in when reading compact Nordic file
    
    * properly read and write IAMLHF magnitudes
    
    * fix npts in trace when seisan wav file has mismatch in header and data
    
    * avoid crash on long phase names in nordic format check; do not write high-acc lines without content
    
    * Nordic: write amplitude with highest possible precision
    
    * Nordic amplitude writing: avoid scientific notation that Fortran parses differently
    
    * Seisan waveform reading: correcly parse integer precision from data, e.g. int16 vs int32
    
    * Seisan reading of 16 bit files: add tests
    
    * add changelog entry
    
    * Nordic IO: add test to read compact catalog file
    
    * Nordic IO: add test for I/O of nordic event ID line
    
    * fix string formatting for amplitudes with higher exponent
    
    * fix amplitude string width
    
    * Seisan waveform reading: correcly parse integer precision from data, e.g. int16 vs int32
    
    * Seisan reading of 16 bit files: add tests
    
    * update changelog before merging in maintenance branch
    
    * final merging
    
    * event extra
    
    * flake8 my ❤️
    
    * py3.8 compat (why not) and extra bugfix
    
    * py3.8 compat (why not) and extra bugfix
    
    * catch the warning
    
    * changelog ref to the PR
    
    ---------
    
    Co-authored-by: ThomasLecocq <thlecocq@gmail.com>
    Co-authored-by: tlecocq <Thomas.Lecocq@seismology.be>

--- obspy/io/nordic/tests/test_nordic.py
+++ obspy/io/nordic/tests/test_nordic.py
@@ -147,6 +147,8 @@ class TestNordicMethods:
             #                  test_ev.amplitudes[1].snr)
             assert read_ev.amplitudes[1].category == \
                 test_ev.amplitudes[1].category
+            assert read_ev.extra.get('nordic_event_id')['value'] == \
+                test_ev.extra.get('nordic_event_id')['value']
 
     def test_write_read_quakeml(self, testdata):
         """
@@ -170,6 +172,17 @@ class TestNordicMethods:
             for event_1, event_2 in zip(cat, cat_back):
                 _assert_similarity(event_1=event_1, event_2=event_2,
                                    strict=False)
+
+    def test_read_compact_file(self, testdata):
+        """
+        Test reading of a compact Nordic file with only header lines.
+        """
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore', UserWarning)
+            cat = read_nordic(testdata['collect.out'])
+        assert len(cat) == 3
+        for event in cat:
+            assert len(event.picks) == 0
 
     def test_fail_writing(self):
         """
@@ -1286,6 +1299,11 @@ def full_test_event():
     test_event.origins.append(Origin(
         time=UTCDateTime("2012-03-26") + 1.2, latitude=45.0, longitude=25.0,
         depth=15000))
+    test_event.extra = {
+        'nordic_event_id': {
+            'value': '20120326000001',
+            'namespace':
+                'https://seis.geus.net/software/seisan/node239.html'}}
     test_event.event_descriptions.append(EventDescription())
     test_event.event_descriptions[0].text = 'LE'
     test_event.creation_info = CreationInfo(agency_id='TES')
