5db4159a535df8fd3b308832847e7c044d3e6900
Thomas Lecocq <thlecocq@gmail.com>
2026-03-04 13:57:54 +0100

    add a vred example to the tutorial

--- misc/docs/source/tutorial/code_snippets/waveform_plotting_tutorial_6.py
+++ misc/docs/source/tutorial/code_snippets/waveform_plotting_tutorial_6.py
@@ -50,3 +50,17 @@ for tr in st:
     ax.text(tr.stats.distance, 1.0, tr.stats.station, rotation=270,
             va="bottom", ha="center", transform=transform, zorder=10)
 plt.show()
+
+# Do the section plot..
+# This time with vred provided (Perform velocity reduction, in m/s.)
+fig = plt.figure()
+st.plot(type='section', vred=3000.0, plot_dx=20e3, recordlength=100,
+        time_down=True, linewidth=.25, grid_linewidth=.25, show=False, fig=fig)
+
+# Plot customization: Add station labels to offset axis
+ax = fig.axes[0]
+transform = blended_transform_factory(ax.transData, ax.transAxes)
+for tr in st:
+    ax.text(tr.stats.distance, 1.0, tr.stats.station, rotation=270,
+            va="bottom", ha="center", transform=transform, zorder=10)
+plt.show()
