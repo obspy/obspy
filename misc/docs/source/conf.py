0520c996e07cb3ef9e50d4113dd4f90d0493469f
Kiriaki Konstantinidou <kiriaki@itsak.gr>
2026-05-02 22:56:41 +0300

    Added keyword "URL" in nitpick_ignore

--- misc/docs/source/conf.py
+++ misc/docs/source/conf.py
@@ -99,6 +99,7 @@ nitpick_ignore = [
     ('py:class', 'file'),
     ('py:class', 'file-like object'),
     ('py:class', 'open file'),
+     ('py:class', 'URL'),
     ('py:class', 'valid matplotlib color'),
     ('py:class', 'valid matplotlib colormap'),
     ('py:class', 'same class as original object'),
