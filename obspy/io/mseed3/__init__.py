9a774b29051a8be60a86bf3433588097cfe4e62e
chad-earthscope <chad.trabant@earthscope.org>
2026-05-21 22:59:30 -0700

    Adaptation of obspy/io/mseed3/core.py to current pymseed release and patterns

--- obspy/io/mseed3/__init__.py
+++ obspy/io/mseed3/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-MiniSEED 3 (mseed3) read support via the external 'pymseed' library.
+MiniSEED v2 and v3 (mseed3) support for via the external 'pymseed' package.
 
 This module exposes the plugin entry points:
 - _is_mseed3(file): format detection
 - _read_mseed3(file, ...): read into ObsPy Stream
 
-Note: Writing MiniSEED 3 is not implemented yet.
 """
+
 from .core import _is_mseed3, _read_mseed3  # noqa: F401
