67056c272e9a5fc58edb1da439e35cd3261a5bdb
chad-earthscope <chad.trabant@earthscope.org>
2026-05-22 17:42:54 -0700

    Add EXTRAS_REQUIRES dependency for io.mseed3 to add pymseed

--- setup.py
+++ setup.py
@@ -107,6 +107,7 @@ EXTRAS_REQUIRES = {
     ],
     'geo': ['geographiclib'],
     'imaging': ['cartopy'],
+    'io.mseed3': ['pymseed~=0.9'],
     'io.shapefile': ['pyshp'],
 }
 EXTRAS_REQUIRES['all'] = [dep for depl in EXTRAS_REQUIRES.values()
